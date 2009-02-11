8e10486a27335355388a78508ee6197b01a7aa33
lokanada <lokanada@tdsi.com>
2009-02-11 11:39:53 +0000

    data insertion into party, person,user, assetType and project
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@27 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,27 @@
+import org.jsecurity.crypto.hash.Sha1Hash;
 class BootStrap {
 
      def init = { servletContext ->
+     	
+     	// Party And Person Details
+     	 def party = new Party(partyName:'Sigma').save()
+     	 def person = new Person(firstName:'Sigma',lastName:'Info',party:party).save() 
+	    //   Administrator user and role.
+     	 def adminRole = new Role(name: "Administrator").save()
+	     def adminUser = new UserLogin(username: "admin", password: new Sha1Hash("admin").toHex(),person:person).save()
+	   //  new JsecUserRoleRel(user: adminUser, role: adminRole).save()
+     
+	     // A normal user.
+
+	     	def userRole = new Role(name: "User").save()
+			def normalUser = new UserLogin(username: "lokanath", password: new Sha1Hash("lokanath").toHex(),person:person).save()
+			//new JsecUserRoleRel(user: normalUser, role: userRole).save()
+			new AssetType(assetType:"KVM").save();
+			new AssetType(assetType:"KVM Switch").save();
+			new AssetType(assetType:"Server").save();
+			new AssetType(assetType:"Switch").save();
+			new AssetType(assetType:"Array").save();
+			new Project(projectName:"TDS",trackChanges:'Y').save();
      }
      def destroy = {
      }
