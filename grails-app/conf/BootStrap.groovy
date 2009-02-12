7ac0157dfac6bcb2e6d0e13f3adfa789813fe5c5
lokanada <lokanada@tdsi.com>
2009-02-12 13:04:59 +0000

    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@49 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,16 +4,15 @@ class BootStrap {
      def init = { servletContext ->
      	
      	// Party And Person Details
-     	 def party = new Party(partyName:'Sigma').save()
-     	 def person = new Person(firstName:'Sigma',lastName:'Info',party:party).save() 
+     	 def person = new Person(firstName:'Lokanath',lastName:'Reddy',partyName:'Sigma',partyCreatedDate:new Date(),personCreatedDate:new Date(),active:'Y').save()
 	    //   Administrator user and role.
-     	 def adminRole = new Role(name: "Administrator").save()
+     	 def adminRole = new RoleType(roleTypeCode: "Administrator").save()
 	     def adminUser = new UserLogin(username: "admin", password: new Sha1Hash("admin").toHex(),person:person).save()
 	   //  new JsecUserRoleRel(user: adminUser, role: adminRole).save()
      
 	     // A normal user.
 
-	     	def userRole = new Role(name: "User").save()
+	     	def userRole = new RoleType(roleTypeCode: "User").save()
 			def normalUser = new UserLogin(username: "lokanath", password: new Sha1Hash("lokanath").toHex(),person:person).save()
 			//new JsecUserRoleRel(user: normalUser, role: userRole).save()
 			new AssetType(assetType:"KVM").save();
