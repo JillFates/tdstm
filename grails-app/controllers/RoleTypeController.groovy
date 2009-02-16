1443b2d16cc894b0ea181567f2aa622d7a64f954
lokanada <lokanada@tdsi.com>
2009-02-16 10:04:17 +0000

    change on save RoleType details
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@74 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/controllers/RoleTypeController.groovy
+++ grails-app/controllers/RoleTypeController.groovy
@@ -1,42 +1,42 @@
 class RoleTypeController {
     
-    def index = { redirect(action:list,params:params) }
+    def index = { redirect( action:list, params:params ) }
 
     // the delete, save and update actions only accept POST requests
-    def allowedMethods = [delete:'POST', save:'POST', update:'POST']
-
+    def allowedMethods = [ delete:'POST', save:'POST', update:'POST' ]
+    // return list of Roles
     def list = {
-        if(!params.max) params.max = 10
+        if ( !params.max ) params.max = 10
         [ roleTypeInstanceList: RoleType.list( params ) ]
     }
-
+    // return RoleType details to show form
     def show = {
         def roleTypeInstance = RoleType.get( params.id )
 
-        if(!roleTypeInstance) {
+        if ( !roleTypeInstance ) {
             flash.message = "RoleType not found with id ${params.id}"
-            redirect(action:list)
+            redirect( action:list )
         }
         else { return [ roleTypeInstance : roleTypeInstance ] }
     }
-
+    // delete RoleType details 
     def delete = {
         def roleTypeInstance = RoleType.get( params.id )
-        if(roleTypeInstance) {
+        if ( roleTypeInstance ) {
             roleTypeInstance.delete()
             flash.message = "RoleType ${params.id} deleted"
-            redirect(action:list)
+            redirect( action:list )
         }
         else {
             flash.message = "RoleType not found with id ${params.id}"
-            redirect(action:list)
+            redirect( action:list )
         }
     }
-
+    //return roleType details to update form
     def edit = {
         def roleTypeInstance = RoleType.get( params.id )
 
-        if(!roleTypeInstance) {
+        if ( !roleTypeInstance ) {
             flash.message = "RoleType not found with id ${params.id}"
             redirect(action:list)
         }
@@ -44,39 +44,40 @@ class RoleTypeController {
             return [ roleTypeInstance : roleTypeInstance ]
         }
     }
-
+    // update RoleType details
     def update = {
-        def roleTypeInstance = RoleType.get( params.id )
-        if(roleTypeInstance) {
+        def roleTypeInstance = RoleType.get( params.id[0] )
+        if ( roleTypeInstance ) {
             roleTypeInstance.properties = params
             if(!roleTypeInstance.hasErrors() && roleTypeInstance.save()) {
-                flash.message = "RoleType ${params.id} updated"
-                redirect(action:show,id:roleTypeInstance.id)
+                flash.message = "RoleType ${params.description} updated"
+                redirect( action:show, id:roleTypeInstance.id )
             }
             else {
-                render(view:'edit',model:[roleTypeInstance:roleTypeInstance])
+                render( view:'edit', model:[roleTypeInstance:roleTypeInstance] )
             }
         }
         else {
             flash.message = "RoleType not found with id ${params.id}"
-            redirect(action:edit,id:params.id)
+            redirect( action:edit, id:params.id )
         }
     }
-
+    // return create form
     def create = {
         def roleTypeInstance = new RoleType()
         roleTypeInstance.properties = params
         return ['roleTypeInstance':roleTypeInstance]
     }
-
+    // save RoleType details 
     def save = {
         def roleTypeInstance = new RoleType(params)
-        if(!roleTypeInstance.hasErrors() && roleTypeInstance.save()) {
+        roleTypeInstance.id = params.id
+        if(!roleTypeInstance.hasErrors() && roleTypeInstance.save(insert:true)) {
             flash.message = "RoleType ${roleTypeInstance.id} created"
-            redirect(action:show,id:roleTypeInstance.id)
+            redirect( action:show, id:roleTypeInstance.id )
         }
         else {
-            render(view:'create',model:[roleTypeInstance:roleTypeInstance])
+            render( view:'create', model:[roleTypeInstance:roleTypeInstance] )
         }
     }
 }
