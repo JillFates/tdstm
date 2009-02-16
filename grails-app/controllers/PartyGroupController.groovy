d6cc7a0e3107add879369f001ac0a54ff5d1f3d7
lokanada <lokanada@tdsi.com>
2009-02-16 08:48:29 +0000

    file to handle party group
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@70 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/controllers/PartyGroupController.groovy
@@ -0,0 +1,82 @@
+class PartyGroupController {
+    
+    def index = { redirect(action:list,params:params) }
+
+    // the delete, save and update actions only accept POST requests
+    def allowedMethods = [delete:'POST', save:'POST', update:'POST']
+
+    def list = {
+        if(!params.max) params.max = 10
+        [ partyGroupInstanceList: PartyGroup.list( params ) ]
+    }
+
+    def show = {
+        def partyGroupInstance = PartyGroup.get( params.id )
+
+        if(!partyGroupInstance) {
+            flash.message = "PartyGroup not found with id ${params.id}"
+            redirect(action:list)
+        }
+        else { return [ partyGroupInstance : partyGroupInstance ] }
+    }
+
+    def delete = {
+        def partyGroupInstance = PartyGroup.get( params.id )
+        if(partyGroupInstance) {
+            partyGroupInstance.delete()
+            flash.message = "PartyGroup ${params.id} deleted"
+            redirect(action:list)
+        }
+        else {
+            flash.message = "PartyGroup not found with id ${params.id}"
+            redirect(action:list)
+        }
+    }
+
+    def edit = {
+        def partyGroupInstance = PartyGroup.get( params.id )
+
+        if(!partyGroupInstance) {
+            flash.message = "PartyGroup not found with id ${params.id}"
+            redirect(action:list)
+        }
+        else {
+            return [ partyGroupInstance : partyGroupInstance ]
+        }
+    }
+
+    def update = {
+        def partyGroupInstance = PartyGroup.get( params.id )
+        if(partyGroupInstance) {
+            partyGroupInstance.properties = params
+            if(!partyGroupInstance.hasErrors() && partyGroupInstance.save()) {
+                flash.message = "PartyGroup ${params.id} updated"
+                redirect(action:show,id:partyGroupInstance.id)
+            }
+            else {
+                render(view:'edit',model:[partyGroupInstance:partyGroupInstance])
+            }
+        }
+        else {
+            flash.message = "PartyGroup not found with id ${params.id}"
+            redirect(action:edit,id:params.id)
+        }
+    }
+
+    def create = {
+        def partyGroupInstance = new PartyGroup()
+        partyGroupInstance.properties = params
+        return ['partyGroupInstance':partyGroupInstance]
+    }
+
+    def save = {
+        def partyGroupInstance = new PartyGroup(params)
+        if(!partyGroupInstance.hasErrors() && partyGroupInstance.save()) {
+            flash.message = "PartyGroup ${partyGroupInstance.id} created"
+            redirect(action:show,id:partyGroupInstance.id)
+        }
+        else {
+            render(view:'create',model:[partyGroupInstance:partyGroupInstance])
+        }
+    }
+}
