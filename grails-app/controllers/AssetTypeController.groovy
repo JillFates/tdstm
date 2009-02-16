0ca467b97f42a00852939a055416e5718152f11f
lokanada <lokanada@tdsi.com>
2009-02-16 09:44:32 +0000

    change on save AssetType details
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@72 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/controllers/AssetTypeController.groovy
+++ grails-app/controllers/AssetTypeController.groovy
@@ -6,55 +6,55 @@ class AssetTypeController {
     def allowedMethods = [delete:'POST', save:'POST', update:'POST']
 
     def list = {
-        if(!params.max) params.max = 10
+        if ( !params.max ) params.max = 10
         [ assetTypeInstanceList: AssetType.list( params ) ]
     }
-
+    // Return AssetType details 
     def show = {
         def assetTypeInstance = AssetType.get( params.id )
 
-        if(!assetTypeInstance) {
+        if ( !assetTypeInstance ) {
             flash.message = "AssetType not found with id ${params.id}"
             redirect(action:list)
         }
         else { return [ assetTypeInstance : assetTypeInstance ] }
     }
-
+    // Delate AssetType details 
     def delete = {
         def assetTypeInstance = AssetType.get( params.id )
-        if(assetTypeInstance) {
+        if ( assetTypeInstance ) {
             assetTypeInstance.delete()
             flash.message = "AssetType ${params.id} deleted"
-            redirect(action:list)
+            redirect( action:list )
         }
         else {
             flash.message = "AssetType not found with id ${params.id}"
-            redirect(action:list)
+            redirect( action:list )
         }
     }
-
+    // Return AssetType details to update form
     def edit = {
         def assetTypeInstance = AssetType.get( params.id )
 
         if(!assetTypeInstance) {
             flash.message = "AssetType not found with id ${params.id}"
-            redirect(action:list)
+            redirect( action:list )
         }
         else {
             return [ assetTypeInstance : assetTypeInstance ]
         }
     }
-
+    // Update AssetType details 
     def update = {
-        def assetTypeInstance = AssetType.get( params.id )
-        if(assetTypeInstance) {
+        def assetTypeInstance = AssetType.get( params.id[0] )
+        if ( assetTypeInstance ) {
             assetTypeInstance.properties = params
-            if(!assetTypeInstance.hasErrors() && assetTypeInstance.save()) {
-                flash.message = "AssetType ${params.id} updated"
-                redirect(action:show,id:assetTypeInstance.id)
+            if( !assetTypeInstance.hasErrors() && assetTypeInstance.save() ) {
+                flash.message = "AssetType ${params.description} updated"
+                redirect( action:show,id:assetTypeInstance.id )
             }
             else {
-                render(view:'edit',model:[assetTypeInstance:assetTypeInstance])
+                render( view:'edit', model:[assetTypeInstance:assetTypeInstance] )
             }
         }
         else {
@@ -62,21 +62,24 @@ class AssetTypeController {
             redirect(action:edit,id:params.id)
         }
     }
-
+    // return AssetType details  to create form
     def create = {
         def assetTypeInstance = new AssetType()
         assetTypeInstance.properties = params
         return ['assetTypeInstance':assetTypeInstance]
     }
-
+    
+    // save Asset type details
     def save = {
-        def assetTypeInstance = new AssetType(params)
-        if(!assetTypeInstance.hasErrors() && assetTypeInstance.save()) {
+    		
+        def assetTypeInstance = new AssetType( params )
+        assetTypeInstance.id = params.id
+        if( !assetTypeInstance.hasErrors() && assetTypeInstance.save( insert:true ) ) {
             flash.message = "AssetType ${assetTypeInstance.id} created"
-            redirect(action:show,id:assetTypeInstance.id)
+            redirect( action:show, id:assetTypeInstance.id )
         }
         else {
-            render(view:'create',model:[assetTypeInstance:assetTypeInstance])
+            render( view:'create', model:[assetTypeInstance:assetTypeInstance] )
         }
     }
 }
