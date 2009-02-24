2274849c9a2047b812b516d2a7798ac8a0ba7457
lokanada <lokanada@tdsi.com>
2009-02-24 08:54:00 +0000

    UserPreferenceService to manage the user preferences
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@126 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/services/UserPreferenceService.groovy
@@ -0,0 +1,81 @@
+import org.jsecurity.SecurityUtils
+import javax.servlet.http.HttpSession
+import org.springframework.web.context.request.RequestContextHolder
+class UserPreferenceService  {
+
+	static transactional = true
+	
+    /*
+     * Method to read all of the user's preferences into a MAP and 
+     * saved into the user's session
+     */
+    
+    def loadPreferences() {
+    	
+    	def principal = SecurityUtils.subject.principal
+    	def userLogin = UserLogin.findByUsername( principal )
+    	def userPreference = UserPreference.findAllByUserLogin( userLogin )
+    	
+    	if ( userPreference != null ) {
+    		// Initialize Map
+    		def currProj = new HashMap()
+    		
+    		for (int i = 0; i < userPreference.size(); i++) {
+    			currProj.put( userPreference[i].preferenceCode, userPreference[i].value )
+    		}
+    		// Set CURR_PROJ into User session
+    		getSession().setAttribute( "CURR_PROJ", currProj )
+    	}
+    }
+	
+    /*
+     * Method will access the map stored into the user's session and 
+     * return the value if found otherwise return null
+     */
+   
+    def String getPreference( String preferenceCode ) {
+
+    	def currProj = getSession().getAttribute( "CURR_PROJ" )
+    	
+    	def prefValue
+    	if ( currProj != null ) {
+    		prefValue = currProj[preferenceCode]
+    	}
+    	return prefValue
+    }
+    
+    /*
+     * method will call getPreference and if null insert the value 
+     * otherwise it will lookup the UserPreference record in the database and 
+     * perform an update
+     */
+    
+    def setPreference( String preferenceCode, String value ) {
+    	
+    	def prefValue = getPreference(preferenceCode)
+    	def principal = SecurityUtils.subject.principal
+    	def userLogin = UserLogin.findByUsername( principal )
+    	
+    	if ( prefValue == null ) {
+    		//	Statements to create UserPreference to login user
+    		def userPreference = new UserPreference( value: value )
+    		userPreference.userLogin = userLogin
+    		userPreference.preferenceCode = preferenceCode
+    		userPreference.save( insert: true)
+    	} else {
+    		//	Statements to Update UserPreference to login user
+    		def userPreference = UserPreference.get( new  UserPreference( userLogin:userLogin, preferenceCode: preferenceCode ) )
+    		userPreference.value = value;
+    		userPreference.save();
+    	}
+    	// call loadPreferences() to load CURR_PROJ MAP into session
+    	loadPreferences()
+    }
+    /*
+     * Return current session object
+     */
+    def HttpSession getSession() {
+        return RequestContextHolder.currentRequestAttributes().getSession()
+    }
+    
+}
