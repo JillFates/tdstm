5518523e4226284daeed4deee6b5e85bddf82bcc
John Martin <jmartin@tdsi.com>
2009-03-11 19:55:54 +0000

    Jira TM-36 Needed to add in new domain that will be required for the feature.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@193 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/domain/ProjectAssetMap.groovy
@@ -0,0 +1,17 @@
+/**
+ * This domain is used to manage the many-to-many relationship of assets that are owned 
+ * by a company but can be associated to one or more projects.
+ **/
+class ProjectAssetMap {
+    Project	project
+	Asset	asset
+    Date createdDate
+
+	static mapping  = {
+		version false
+	}
+	
+	String toString(){
+		"${project.name} : ${asset.assetName}" 
+	}
+}
