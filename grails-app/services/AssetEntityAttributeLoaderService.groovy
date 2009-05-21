3bcdd5222c7060d39a9387c4b136464f1ebffd3d
lokanada <lokanada@tdsi.com>
2009-05-21 08:55:51 +0000

    JIRA TM-139 : Fixed Bundle Team Assignment Runtime Exception when switch to Rerack
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@456 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/services/AssetEntityAttributeLoaderService.groovy
+++ grails-app/services/AssetEntityAttributeLoaderService.groovy
@@ -273,7 +273,6 @@ class AssetEntityAttributeLoaderService {
 
 	//	get Cart - #Asset count corresponding to Bundle
 	def getCartAssetCounts ( def bundleId, def cartList ) {
-		println("Inside the cart lsit of get cart asset counts===>"+cartList)
 		def cartAssetCounts = []
 		def bundleInstance = MoveBundle.findById(bundleId)
 		cartList.each { assetCart ->
@@ -281,9 +280,9 @@ class AssetEntityAttributeLoaderService {
 			def AssetEntityList = AssetEntity.findAllByMoveBundleAndCart(bundleInstance, assetCart.cart)
 			def usize = 0
 			for(int AssetEntityRow = 0; AssetEntityRow < AssetEntityList.size(); AssetEntityRow++ ) {
-				usize = usize + (AssetEntityList[AssetEntityRow]?.usize ? AssetEntityList[AssetEntityRow]?.usize : 0)
+				usize = usize + Integer.parseInt(AssetEntityList[AssetEntityRow].usize ? AssetEntityList[AssetEntityRow].usize : "0")
 			}
-			cartAssetCounts << [ cart:assetCart?.cart, cartAssetCount:cartAssetCount,usizeUsed:usize ]
+			cartAssetCounts << [ cart:assetCart.cart, cartAssetCount:cartAssetCount,usizeUsed:usize ]
 		}
 		return cartAssetCounts
 	}
