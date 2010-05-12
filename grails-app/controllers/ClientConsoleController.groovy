824c9289bc357d71264c08895af27bcbd05c90a9
Craig Macfarlane <craig@tdsi.com>
2010-05-12 17:51:10 +0000

    test2
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@1055 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/controllers/ClientConsoleController.groovy
+++ grails-app/controllers/ClientConsoleController.groovy
@@ -580,11 +580,11 @@ class ClientConsoleController {
 		def count = 0
 		tempTransitions.sort().each{
 			def processTransition = stateEngineService.getStateLabel(projectInstance.workflowCode,it)
-			def stateType = stateEngineService.getStateLabel(projectInstance.workflowCode,it)
+			def stateType =         stateEngineService.getStateLabel(projectInstance.workflowCode,it)
 			if(count == 0){
 				svgHeaderFile.append("${processTransition}")
 			} else {
-				svgHeaderFile.append("<tspan x='-11' dy='22' class='${stateType}'>${processTransition}</tspan>")
+				svgHeaderFile.append("<tspan x='-11' dy='22' class='"+${stateType}+"'>${processTransition}</tspan>")
 			}
 			count++
 		}
