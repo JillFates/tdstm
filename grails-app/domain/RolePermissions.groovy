ed11c58bc8db4c6b7b6d3b2a73f0404423bc06ea
lokanada <lokanada@tdsi.com>
2012-01-30 12:04:49 +0000

    TM-798 : Create role permission help functions and taglib to use them in GSP.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@1983 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/domain/RolePermissions.groovy
+++ grails-app/domain/RolePermissions.groovy
@@ -12,4 +12,56 @@ class RolePermissions {
 		permission( blank:false, nullable:false )
     }
 	
+	// Helper methods
+	static Boolean hasPermission(role, permission){
+		def returnVal = false
+		def rolePermissions = RolePermissions.findByPermissionAndRole(permission, role)
+		if(rolePermissions){
+			returnVal = true
+		}
+		return returnVal
+	}
+	
+	static Boolean hasAnyPermissionToRole(role, permissions){
+		def returnVal = false
+		def rolePermissions = RolePermissions.createCriteria().list {
+			and {
+				eq ('role', role)
+				'in'('permission', permissions )
+			}
+		}
+		if(rolePermissions.size()>0){
+			returnVal = true
+		}
+		return returnVal
+	}
+	
+	static Boolean hasPermissionToAnyRole(role, permission){
+		def returnVal = false
+		def rolePermissions = RolePermissions.createCriteria().list {
+			and {
+				'in' ('role', role)
+				eq('permission', permission )
+			}
+		}
+		if(rolePermissions.size()>0){
+			returnVal = true
+		}
+		return returnVal
+	}
+	
+	static Boolean lacksPermissionToAllRole(role, permission){
+		def returnVal = false
+		def rolePermissions = RolePermissions.createCriteria().list {
+			and {
+				'in' ('role', role)
+				eq('permission', permission )
+			}
+		}
+		if(rolePermissions.size() == 0){
+			returnVal = true
+		}
+		return returnVal
+	}
+	
 }
---
+++ grails-app/taglib/CustomSecurityTagLib.groovy
@@ -0,0 +1,70 @@
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+
+import org.jsecurity.SecurityUtils
+
+import com.tdssrc.grails.GormUtil;
+
+class CustomSecurityTagLib {
+	static namespace = 'tds'
+	/**
+	 * @param : roles list
+	 * @param : permission
+	 * @return : boolean true|false
+	 */
+	def hasPermissionToAnyRole = { attrs ->
+		def returnVal = false
+		def roles = attrs['roles'];
+		def permission = attrs['permission']
+		
+		def subject = SecurityUtils.subject
+		def hasRoles = subject.hasRoles(roles)
+		if(hasRoles.ontains(true)){
+			def hasPermission = RolePermissions.hasPermissionToAnyRole(roles, permission)
+			if(hasPermission)
+				returnVal = true
+		}
+		
+		return returnVal
+	}
+	/**
+	 * @param : role
+	 * @param : permission
+	 * @return : boolean true|false
+	 */
+	def hasPermission = { attrs ->
+		def returnVal = false
+		def role = attrs['roles'];
+		def permission = attrs['permission']
+		
+		def subject = SecurityUtils.subject
+		def hasRole = subject.hasRole(role)
+		if(hasRole){
+			def hasPermission = RolePermissions.hasPermission(role, permission)
+			if(hasPermission)
+				returnVal = true
+		}
+		
+		return returnVal
+	}
+	/**
+	 * @param : roles list
+	 * @param : permission
+	 * @return : boolean true|false
+	 */
+	def lacksPermissionToAllRole = { attrs ->
+		def returnVal = true
+		def roles = attrs['roles'];
+		def permission = attrs['permission']
+		
+		def subject = SecurityUtils.subject
+		def hasRoles = subject.hasRoles(roles)
+		if(hasRoles.ontains(true)){
+			def hasPermission = RolePermissions.lacksPermissionToAllRole(roles, permission)
+			if(!hasPermission)
+				returnVal = false
+		}
+		
+		return returnVal
+	}
+}
