5b9efd52852ee38f8e5c189e0ec5ada38207a89d
lokanada <lokanada@tdsi.com>
2012-02-16 01:31:45 +0000

    Script to update asssettype eav options
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@2011 fecba46a-3362-0410-80e5-8c76cdb905a0

--- scripts/AssetEntityAlter.groovy
+++ scripts/AssetEntityAlter.groovy
@@ -8,52 +8,7 @@ def masterDataTransferSet = DataTransferSet.findBySetCode("MASTER")
 def walkThruDataTransferSet = DataTransferSet.findBySetCode("WALKTHROUGH")
 def entityType = EavEntityType.findByEntityTypeCode('AssetEntity')
 def attributeSet = EavAttributeSet.findByAttributeSetName('Server')
-/*
- * Set Server as AssetEntity default type
- */
-AssetEntity.executeUpdate("UPDATE from AssetEntity set assetType = 'Server' where assetType is null")
-def eavAttribute = EavAttribute.findByAttributeCodeAndEntityType("assetType",entityType)
-def assetTypeAttribute = EavAttribute.findByAttributeCodeAndEntityType('assetType',entityType)
-EavAttributeOption.executeUpdate("Delete from EavAttributeOption where attribute = ?",[assetTypeAttribute])
-def assetTypes = AssetEntity.findAll("From AssetEntity group by assetType")
 
-assetTypes?.assetType?.each{ option->
-   try{
-      def integerAssetType =  Integer.parseInt(option)
-	  option = 'Server'
-	  AssetEntity.executeUpdate("Update AssetEntity set assetType = 'Server' where assetType = '${integerAssetType}'")
-	  println "Error : AssetType has wrong value::::::::::"+integerAssetType
-    }  catch(Exception ex){
-		if(!option){
-			option = 'Server'
-		}
-    }
-	def eavAttributeOption = EavAttributeOption.findByValueAndAttribute(option,eavAttribute)
-	if( !eavAttributeOption ){
-		eavAttributeOption = new EavAttributeOption(
-													value : option,
-													attribute : eavAttribute
-													)
-		if ( !eavAttributeOption.validate() || !eavAttributeOption.save(flush:true) ) {
-			def etext = "Unable to create eavAttributeOption" +
-			GormUtil.allErrorsString( eavAttributeOption )
-			println etext
-		}
-	}
-}
-println"**********************Create Rack asset entity option***************************************"
-def eavAttributeOption = EavAttributeOption.findByValueAndAttribute("Rack",eavAttribute)
-if( !eavAttributeOption ){
-	eavAttributeOption = new EavAttributeOption(
-			value : "Rack",
-			attribute : eavAttribute
-			)
-	if ( !eavAttributeOption.validate() || !eavAttributeOption.save(flush:true) ) {
-		def etext = "Unable to create eavAttributeOption" +
-				GormUtil.allErrorsString( eavAttributeOption )
-		println etext
-	 }
-}
 println"**************Delete Asset properties ***************"
 /*
  * Power
