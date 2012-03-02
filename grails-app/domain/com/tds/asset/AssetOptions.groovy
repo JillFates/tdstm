f816ad7e9e0f98325b65bba8e2502c61aa824a05
lokanada <lokanada@tdsi.com>
2012-03-02 15:45:36 +0000

    TM-811: Created Admin feature for Asset Plan Status,Priority,Dependency Type,Dependency Status preferences
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@2038 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/domain/com/tds/asset/AssetOptions.groovy
@@ -0,0 +1,17 @@
+package com.tds.asset
+
+class AssetOptions {
+	static enum AssetOptionsType{STATUS_OPTION,PRIORITY_OPTION,DEPENDENCY_TYPE,DEPENDENCY_STATUS}
+	
+	AssetOptionsType type
+	String value
+	
+	static mapping = {
+		version false
+	}
+	
+    static constraints = {
+		type(blank:false,nullable:false)
+		value(blank:false,nullable:false)
+    }
+}
