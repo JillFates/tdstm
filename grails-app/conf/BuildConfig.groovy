c422db57b60704bcb239ecbbbda1fb917cbec19f
John Martin <jmartin@tdsi.com>
2012-08-01 19:18:44 +0000

    TM-940 Added in the grails.project.dependency.resolution DSL but holding off on some of the more challenging details for the moment.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@2429 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,29 @@
 /*
  * Since 1.1, Grails no longer stores plugins inside your PROJECT_HOME/plugins  directory by default.
  */
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
 grails.project.plugins.dir="./plugins"
+
+grails.project.dependency.resolution = {
+	// inherit Grails' default dependencies
+	inherits("global") {
+	}
+	
+	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+	}
+	
+	dependencies {
+		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	}
+  
+	plugins {
+		// runtime ':database-migration:1.1'
+	}
+}
