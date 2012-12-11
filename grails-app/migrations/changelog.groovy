133e3b817a59548bd895ea767f1262f03b3607f5
lokanada <lokanada@tdsi.com>
2012-12-11 10:52:25 +0000

    TM-1136 : Person/UserLogin cleanup (Added  permission add script in change-log)
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@2862 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -2,5 +2,6 @@ databaseChangeLog = {
 
 	include file: '20121206-fix-staff-system-roles.groovy'
 	include file: '20121210-fix-models-manu-nulldates.groovy'
+	include file: '20121211-add-projectStaff-permission.groovy'
 	
 }
