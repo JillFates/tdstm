1ac7089c3b588711cb1e25714a225eb48c23b5e7
lokanada <lokanada@tdsi.com>
2013-05-24 12:21:42 +0000

    TM-1895 : ReAdded revised create default project script
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3300 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20130522-create-default-project.groovy
+++ grails-app/migrations/20130522-create-default-project.groovy
@@ -5,7 +5,6 @@ databaseChangeLog = {
 	changeSet(author: "lokanada", id: "2013052 TM-1895-10") {
 	grailsChange {
 			change {
-				def partyRelationshipService
 				def partyType = PartyType.read('COMPANY')
 				def partyGroup = PartyGroup.findByName('TM DEFAULT CLIENT')
 				if(!partyGroup){
@@ -29,44 +28,42 @@ databaseChangeLog = {
 						throw new RuntimeException('Creating project failed')				
 					}
 				}
-				def tempPersonId = project.id+1
 				[2,3].each{ value->
-					sql.execute("update party set party_id = ${tempPersonId} where party_id = ${value}")
-					sql.execute("update person set person_id = ${tempPersonId} where person_id = ${value}")
-					sql.execute("update party_relationship set party_id_from_id = ${tempPersonId} where party_id_from_id = ${value}")
-					sql.execute("update party_relationship set party_id_to_id = ${tempPersonId} where party_id_to_id = ${value}")
-					sql.execute("update party_role set party_id = ${tempPersonId} where party_id = ${value}")
+                    //Updating person Reference.
+                    sql.execute("update asset_comment set resolved_by=null where resolved_by = ${value}")
+                    sql.execute("update asset_comment set created_by=null where created_by = ${value}")
+                    sql.execute("update asset_comment set assigned_to_id=null where assigned_to_id = ${value}")
+                    
+                    sql.execute("update asset_dependency set updated_by=null where updated_by = ${value}")
+                    sql.execute("update asset_dependency set created_by=null where created_by = ${value}")
+                    
+                    sql.execute("update comment_note set created_by_id=null where created_by_id = ${value}")
+                    
+                    sql.execute("update exception_dates set person_id=null where person_id = ${value}")
+                    
+                    sql.execute("update model set updated_by=null where updated_by = ${value}")
+                    sql.execute("update model set created_by=null where created_by = ${value}")
+                    sql.execute("update model set validated_by=null where validated_by = ${value}")
+                    
+                    sql.execute("update model_sync set updated_by_id=null where updated_by_id = ${value}")
+                    sql.execute("update model_sync set created_by_id=null where created_by_id = ${value}")
+                    sql.execute("update model_sync set validated_by_id=null where validated_by_id = ${value}")
+                    
+                    sql.execute("update move_event_news set archived_by=null where archived_by = ${value}")
+                    sql.execute("update move_event_news set created_by=null where created_by = ${value}")
+                    
+                    sql.execute("delete from move_event_staff where person_id = ${value}")
+                    
+                    sql.execute("delete from user_login where person_id = ${value}")
+                    
+                    sql.execute("update workflow set updated_by=null where updated_by = ${value}")
+                
+                    sql.execute("delete from party_relationship where party_id_from_id = ${value}")
+                    sql.execute("delete from party_relationship where party_id_to_id = ${value}")
+                    sql.execute("delete from party_role where party_id = ${value}")
+                    sql.execute("delete from person where person_id = ${value}")
+					sql.execute("delete from party where party_id = ${value}")
 					
-					//Updating person Reference.
-					sql.execute("update asset_comment set resolved_by=${tempPersonId} where resolved_by = ${value}")
-					sql.execute("update asset_comment set created_by=${tempPersonId} where created_by = ${value}")
-					sql.execute("update asset_comment set assigned_to_id=${tempPersonId} where assigned_to_id = ${value}")
-					
-					sql.execute("update asset_dependency set updated_by=${tempPersonId} where updated_by = ${value}")
-					sql.execute("update asset_dependency set created_by=${tempPersonId} where created_by = ${value}")
-					
-					sql.execute("update comment_note set created_by_id=${tempPersonId} where created_by_id = ${value}")
-					
-					sql.execute("update exception_dates set person_id=${tempPersonId} where person_id = ${value}")
-					
-					sql.execute("update model set updated_by=${tempPersonId} where updated_by = ${value}")
-					sql.execute("update model set created_by=${tempPersonId} where created_by = ${value}")
-					sql.execute("update model set validated_by=${tempPersonId} where validated_by = ${value}")
-					
-					sql.execute("update model_sync set updated_by_id=${tempPersonId} where updated_by_id = ${value}")
-					sql.execute("update model_sync set created_by_id=${tempPersonId} where created_by_id = ${value}")
-					sql.execute("update model_sync set validated_by_id=${tempPersonId} where validated_by_id = ${value}")
-					
-					sql.execute("update move_event_news set archived_by=${tempPersonId} where archived_by = ${value}")
-					sql.execute("update move_event_news set created_by=${tempPersonId} where created_by = ${value}")
-					
-					sql.execute("update move_event_staff set person_id=${tempPersonId} where person_id = ${value}")
-					
-					sql.execute("update user_login set person_id=${tempPersonId} where person_id = ${value}")
-					
-					sql.execute("update workflow set updated_by=${tempPersonId} where updated_by = ${value}")
-					
-					tempPersonId++
 				}
 				sql.execute("update party_group set party_group_id = 3 where party_group_id= ${partyGroup.id}")
 				sql.execute("update party set party_id = 3 where party_id= ${partyGroup.id}")
