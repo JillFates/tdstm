01fb3e7f1642e96493048796c5e427951cd4378e
Ross Macfarlane <rmacfarlane@tdsi.com>
2013-06-28 19:38:22 +0000

    TM-1946 changed middle name database migration script to use the DSL and report failure when the column already exists
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3393 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20130627-add-person-middlename-column.groovy
+++ grails-app/migrations/20130627-add-person-middlename-column.groovy
@@ -5,6 +5,16 @@ databaseChangeLog = {
 	// This Changeset is used to add the nullable varchar(20) column middle_name with a default value of ''
 	changeSet(author: "Ross", id: "20130627 TM-1946-3") {
 		comment("Add column middle_name of datatype nullable varchar(20) with a default value of ''")
-		sql("ALTER TABLE tdstm.person ADD column middle_name varchar(20) NULL DEFAULT ''")
+		preConditions(onFail:'MARK_RAN') {
+			not {
+				columnExists(schemaName:'tdstm', tableName:'person', columnName:'middle_name' )
+			}
+		}
+		addColumn(tableName: 'person') {
+			String s = ''
+			column(name: 'middle_name', type: 'varchar(20)', defaultValue: s) {
+				constraints(nullable: 'true')
+			}
+        }
 	}
 }
