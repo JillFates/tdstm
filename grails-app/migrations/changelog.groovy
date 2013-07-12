c61aa1d7aed986b1768eaf8917277f945acb3ed0
dinesh <dinesh@tdsi.com>
2013-07-12 11:35:00 +0000

    TM-1989 : Added Virtual Host for Import/Export .
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3460 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -46,5 +46,6 @@ databaseChangeLog = {
 	include file: '20130709-add-new-fields-application-table.groovy'
 	include file: '20130710-add-new-app-field-import-export.groovy'
 	include file: '20130713-add-permission-view-permission.groovy'
+	include file: '20130712-add-virtual_host-import-export.groovy'
     
 }
