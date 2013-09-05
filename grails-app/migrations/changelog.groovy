c8ba0f7c9f4d72cc5b9afe940cfb606de7b46cfd
John Martin <jmartin@tdsi.com>
2013-09-05 19:55:23 +0000

    TM-2249 Commented out the ModelAlias due to error at runtime - need to fix and reimplement
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3663 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -53,7 +53,7 @@ databaseChangeLog = {
 	include file: '20130808-update-model-status.groovy'
 	include file: '20130826-update-comment-datatype-to-text.groovy'
 	include file: '20130903-add-task-report-permission.groovy'
-	include file: '20130903-add-index-to-ModelAlias.groovy'
+	// include file: '20130903-add-index-to-ModelAlias.groovy'
 	include file: '20130905-update-asset-orphan-records-with-no-racks-rooms.groovy'
 	include file: '20130904-update-blades-room-and-location.groovy'
     
