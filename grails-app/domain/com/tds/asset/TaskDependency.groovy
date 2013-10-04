cef6c3207765d8d5af528176b4f53605310502fd
John Martin <jmartin@tdsi.com>
2013-10-04 13:09:10 +0000

    TM-2289 Changed the TaskDependency to use the new Enums and had to change some data from Finish-Start to Finish-Ready
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3731 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/domain/com/tds/asset/TaskDependency.groovy
+++ grails-app/domain/com/tds/asset/TaskDependency.groovy
@@ -8,37 +8,35 @@ import com.tdsops.tm.enums.domain.TaskDependencyType
 
 class TaskDependency {
 	
-	AssetComment assetComment	// aka successor
+	AssetComment assetComment		// aka successor
 	AssetComment predecessor
 
 	Integer downstreamTaskCount	= 0	// The number of tasks that are downstream from the predecessor task
 	Integer pathDepth = 0			// The depth from the furthest sink task in the map
 	Integer pathDuration = 0		// The total duration forward to the furthest sink task on the path in the map
 
-	// AssetComment successor
-	String type = TaskDependencyType.FS
-	// Integer delayTime		// # of minutes to delay either lead or lag 
-	// Integer delayType		// 1=Lead, 2=Lag
-	
-// 	static belongsTo =  [ assetComment : AssetComment ]
-	
+	TaskDependencyType type = TaskDependencyType.getDefault()
+		
 	static constraints = {
-		assetComment(nullable:false)
-		predecessor(nullable:false)
+		assetComment()
+		predecessor()
 		type( inList:TaskDependencyType.getKeys() )
 	}
 	
 	static mapping = {
-		type sqltype: 'char', length:2
 		id column:'task_dependency_id'
 		assetComment fetch:'join'
 		predecessor fetch:'join'
+		type sqltype: 'char', length:2
+		downstreamTaskCount sqltype: 'INT(6)'
+		pathDepth sqltype: 'INT(6)'
+		pathDuration sqltype: 'INT(6)'
 	}
 
 	static transients = ['successor']
 	
 	// Add successor accessors that are easier to understand than the assetComment property
-	void setSuccessor(task) { this.assetComment = task }
+	void setSuccessor(AssetComment task) { this.assetComment = task }
 	AssetComment getSuccessor() { this.assetComment }
 
 	String toString() {
--- grails-app/migrations/20130912-add-columns-for-runbook-optimization.groovy
+++ grails-app/migrations/20130912-add-columns-for-runbook-optimization.groovy
@@ -1,7 +1,8 @@
 /**
- * This changelog add few columns to the TaskDependency and AssetComment tables in relationship to Runbook Optimization
+ * This changelog is used to modify the TaskDependency and AssetComment tables in relationship to Runbook Optimization enhancements
  */
 databaseChangeLog = {
+
 	changeSet(author: "John", id: "20130912 TM-2274-1") {
 		comment("Add columns downstream_task_count, path_duration and path_depth to task_dependency table")
 		preConditions(onFail:'MARK_RAN') {
@@ -25,6 +26,7 @@ databaseChangeLog = {
 			}
 		}
 	}
+
 	changeSet(author: "John", id: "20130912 TM-2274-2") {
 		comment("Add columns to asset_comment")
 		preConditions(onFail:'MARK_RAN') {
@@ -44,11 +46,17 @@ databaseChangeLog = {
 		}
 
 	}
+
 	changeSet(author: "John", id: "20130912 TM-2274-3") {
 		comment("Change AssetComment.duration and durationScale to not null with default values ")
 		sql("UPDATE asset_comment SET duration=0 WHERE duration IS NULL")
 		sql("UPDATE asset_comment SET duration_scale='m' WHERE duration_scale IS NULL")
 		sql("ALTER TABLE asset_comment MODIFY duration INT(11) NOT NULL DEFAULT 0, MODIFY duration_scale CHAR(1) NOT NULL DEFAULT 'd' AFTER duration")
+	}
+
+	changeSet(author: "John", id: "20130912 TM-2274-4") {
+		comment("Change TaskDependency.type default from 'FS' to 'FR'")
+		sql("UPDATE task_dependency SET type='FR' WHERE type IS NULL OR type='FS'")
 	}
 
 }
