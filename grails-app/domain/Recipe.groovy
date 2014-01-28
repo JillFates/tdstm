87100cd0838d063f0afc5abaa0a16b79b025d9a7
John Martin <jmartin@tdsi.com>
2014-01-28 11:18:00 +0000

    TM-2416 Added the three new domain classes that will be used for the Cookbook
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3942 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/domain/Recipe.groovy
@@ -0,0 +1,67 @@
+
+import com.tdssrc.grails.TimeUtil
+
+/**
+ * Recipe Domain Object
+ * 
+ * <p>Represents a recipe within cookbook that contains various properties of the recipe
+ *
+ * @author John Martin
+ */
+class Recipe {
+
+	/** The name or title of the recipe */
+	String name
+	/** A short description of what the recipe is used for*/
+	String description
+	/** Used to indicate the context that the recipe is to be used for */
+	String context
+	/** The project that the recipe is associated with */
+	Project project
+	/** The current version of the recipe that has been released, which is the version that people should use 
+		to generate tasks */
+	RecipeVersion releasedVersion
+	/** Whom created the recipe */
+	Person createdBy
+
+	Date dateCreated
+	Date lastUpdated
+
+	static constraints = {	
+		name(blank:false, nullable:false, size:40)
+		description(blank:true, nullable:true, size:255)
+		context(blank:false, nullable:false, inList: ['Event', 'Bundle', 'Application'] )	// TODO : Switch to ENUM RecipeContext
+		project(nullable:false)
+		createdBy(nullable:false)
+		dateCreated(nullable:true)
+		lastUpdated(nullable:true)
+	}
+
+	static mapping  = {	
+		version true
+		autoTimestamp false
+		id column: 'recipe_id'
+		columns {
+			name sqltype: 'varchar(40)'
+			description sqltype: 'varchar(255)'
+		}
+	}
+
+	static hasMany = [ 
+		versions : RecipeVersion,
+	]
+	
+	def beforeInsert = {
+		dateCreated = TimeUtil.nowGMT()
+		lastUpdated = dateCreated
+	}
+	
+	def beforeUpdate = {
+		lastUpdated = TimeUtil.nowGMT()
+	}
+
+	String toString() {
+		name
+	}	
+	
+}
