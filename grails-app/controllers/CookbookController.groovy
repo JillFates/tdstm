19ae66fd05cee05981993d66979e251be2ab57a5
eerrecalde <eerrecalde@tdsi.com>
2014-01-30 13:15:21 +0000

    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@3951 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/controllers/CookbookController.groovy
+++ grails-app/controllers/CookbookController.groovy
@@ -2,12 +2,11 @@
 
 class CookbookController {
 
-	def index = { 
-		def map = [ 
-			name: 'Emiliano',
-			gender: 'male'
-		]
-
-		return map
+    def index = {
+    	def map = [name:'emiliano', role:'Front-end dev', 
+    		books: [['name': '1111', 'author': 'Myself', 'description': 'bla bla bla'],
+				['name': '2222', 'author': 'Max', 'description': 'bla bla bla2'],
+				['name': '3333', 'author': 'Ann', 'description': 'bla bla bla3']]
+    	]
 	}
 }
