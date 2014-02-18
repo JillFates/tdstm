585e68fad93bf8a35f6180c8d32f9c7129efa79e
Craig Macfarlane <craig@tdsi.com>
2014-02-18 13:04:35 +0000

    TM-2456 Enhance Rack height options and room display sizes
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4018 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/domain/Model.groovy
+++ grails-app/domain/Model.groovy
@@ -73,7 +73,7 @@ class Model {
 		endOfLifeDate(nullable:true)
 		endOfLifeStatus( blank:true, nullable:true )
 		sourceURL( blank:true, nullable:true )
-		usize( nullable:true, inList:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48])
+		usize( nullable:true, inList:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52])
 		height( nullable:true )
 		weight( nullable:true )
 		depth( nullable:true )
