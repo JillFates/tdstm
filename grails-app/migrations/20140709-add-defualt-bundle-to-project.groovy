6bffb9daf366890221be8ee778fe7ec0139e4797
lokanada <lokanada@tdsi.com>
2014-07-15 08:24:41 +0000

    TM-2965 : Fixed migration failing error .
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4690 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20140709-add-defualt-bundle-to-project.groovy
+++ grails-app/migrations/20140709-add-defualt-bundle-to-project.groovy
@@ -31,7 +31,7 @@ databaseChangeLog = {
 	 * Update all projects to set "default_bundle_id" to their TBD bundle where they exist
 	 * 
 	 */
-	changeSet(author: "lokanada", id: "20140709 TM-2965-2") {
+	changeSet(author: "lokanada", id: "20140709 TM-2965-20") {
 		comment('Update all projects to set "default_bundle_id" to their TBD bundle where they exist')
 		preConditions(onFail:'MARK_RAN') {
 			columnExists(schemaName:'tdstm', tableName:'project', columnName:'default_bundle_id' )
@@ -41,7 +41,7 @@ databaseChangeLog = {
 				def projects = Project.findAllByDefaultBundleIsNull()
 				projects.each{ proj->
 					proj.defaultBundle =  projectService.getDefaultBundle( proj )
-					if ( ! proj.save() ) {
+					if ( ! proj.save(flush:true) ) {
 						throw new RuntimeException('updating project to default bundle failed');
 					}
 				}
@@ -53,7 +53,7 @@ databaseChangeLog = {
 	/**
 	 * 	update asset_entity move_bundle_id to default bundle if null
 	 */
-	changeSet(author: "lokanada", id: "20140709 TM-2965-3") {
+	changeSet(author: "lokanada", id: "20140709 TM-2965-30") {
 		comment('update asset_entity move_bundle_id to default bundle if null')
 		sql(""" UPDATE  asset_entity ae 
 			  	LEFT JOIN project p
@@ -69,7 +69,7 @@ databaseChangeLog = {
 	 * 
 	 * set asset_entity's move_bundle_id column to not null
 	 */
-	changeSet(author: "lokanada", id: "20140709 TM-2965-4") {
+	changeSet(author: "lokanada", id: "20140709 TM-2965-40") {
 		comment("set asset_entity's move_bundle_id column to not null")
 		sql(" ALTER TABLE asset_entity MODIFY COLUMN move_bundle_id  BIGINT(20) NOT NULL ")
 	}
