b82927a3c4bb61bf7a6b78e7b952d4e718f7c3c9
jdanahy <jdanahy@tdsi.com>
2014-07-17 20:29:20 +0000

    TM-2892 Added all of the required automatic testing material under "List Projects"
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4702 fecba46a-3362-0410-80e5-8c76cdb905a0

--- test/functional/components/clientProject/listProjects.spec.js
+++ test/functional/components/clientProject/listProjects.spec.js
@@ -8,5 +8,84 @@ describe('List Projects', function(){
     menu.goToClientProject('listProjects');
     expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
   });
-
+  it('should sort the projects by project code in both orders', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_projectCode')).click();
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_projectCode')).click();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+  });
+   it('should sort the projects by name in both orders', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_name')).click();
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_name')).click();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+  });
+   it('should sort the projects by start date in both orders', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_startDate')).click();
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_startDate')).click();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+  });
+   it('should sort the projects by completion date in both orders', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_completionDate')).click();
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_completionDate')).click();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+  });
+   it('should sort the projects by comments in both orders', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_comment')).click();
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('jqgh_projectGridIdGrid_comment')).click();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+  });
+   it('should search for "no" under Project Code, then delete it.', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_projectCode')).sendKeys('no');
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_projectCode')).clear();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+	browser.sleep(1000);
+  });
+     it('should search for "Only Project" under Name, then delete it.', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_name')).sendKeys('Only Project');
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_name')).clear();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+	browser.sleep(1000);
+  });
+     it('should search for "06/17/2014" under Start Date, then delete it.', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_startDate')).sendKeys('06/17/2014');
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_startDate')).clear();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+	browser.sleep(1000);
+  });
+     it('should search for "09/15/2014" under Completion Date, then delete it.', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_completionDate')).sendKeys('09/15/2014');
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_completionDate')).clear();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+	browser.sleep(1000);
+  });
+     it('should search for "test comment" under Project Code, then delete it.', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_comment')).sendKeys('test comment');
+	browser.sleep(1000);
+	browser.driver.findElement(by.id('gs_comment')).clear();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
+	browser.sleep(1000);
+  });
+    it('should go to the completed projects section', function(){
+	browser.sleep(1000);
+	browser.driver.findElement(by.css('a[href="/tdstm/project/list?active=completed"]')).click();
+    expect(listProjectPage.getTitle().getText()).toEqual('Project List - Completed Projects');
+  });
 }); // List Projects
