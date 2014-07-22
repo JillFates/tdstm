6e058af6aca493db072b867a3a97f9ca6d39a00e
jdanahy <jdanahy@tdsi.com>
2014-07-22 16:31:07 +0000

    TM-2801  Added Expectations for showProject testing.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4712 fecba46a-3362-0410-80e5-8c76cdb905a0

--- test/functional/components/clientProject/showProjects.spec.js
+++ test/functional/components/clientProject/showProjects.spec.js
@@ -7,6 +7,7 @@ describe('List Projects', function(){
 	 it('Goes to list projcts and opens a create project window',function(){
 		menu.goToClientProject('listProjects');
 		browser.driver.findElement(by.css('input[onclick="window.location.href=\'/tdstm/project/create\'"]')).click();
+		expect(listProjectPage.getTitle().getText()).toEqual('Create Project');
 		browser.sleep(2000);
 	 });
 	 it('Create a new project',function(){
@@ -15,6 +16,7 @@ describe('List Projects', function(){
 		browser.driver.findElement(by.id('workflowCode')).click;
 		browser.driver.findElement(by.css('option[value="STD_PROCESS"')).click();
 		browser.driver.findElement(by.css('input[class="save"')).click();
+		expect(listProjectPage.getTitle().getText()).toEqual('Project Details');
 		browser.sleep(1000);
 	 });
 	 it('Goes to test project just created',function(){
@@ -23,6 +25,7 @@ describe('List Projects', function(){
 	 it('Opens the edit menu and immediately saves',function(){
 		browser.driver.findElement(by.css('input[class="edit"')).click();
 		browser.driver.findElement(by.css('input[class="save"')).click();
+		expect(listProjectPage.getTitle().getText()).toEqual('Project Details');
 		browser.sleep(1000);
 	 });
 	 	 it('Deletes Project',function(){
@@ -31,6 +34,7 @@ describe('List Projects', function(){
 			var alert = browser.driver.switchTo().alert();
 			//update is executed
 			alert.accept();
+			expect(listProjectPage.getTitle().getText()).toEqual('Project List - Active Projects');
 			browser.sleep(1000);
 	 });
  });
