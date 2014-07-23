2fb83dda62b2a8ae073a2c6ffb28a5efed3fd406
jcalabrese <jcalabrese@tdsi.com>
2014-07-23 16:47:24 +0000

    TM-3011 Performance Test - Gatling - Adding user-files folder which includes needed files to run the scenario
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4720 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ test/performance/user-files/simulations/assetLists/GetAssetsLists.scala
@@ -0,0 +1,25 @@
+package assetLists
+
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+
+class GetAssetLists extends Simulation {
+
+  val httpConf = httpConfig
+      .baseURL("http://localhost:8080")
+      .acceptHeader("image/png,image/*;q=0.8,*/*;q=0.5")
+      .acceptEncodingHeader("gzip, deflate")
+      .acceptLanguageHeader("en-US,en;q=0.5")
+      .connection("keep-alive")
+      .userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0")
+
+  setUp(
+    GetListApp.scn.users(1).ramp(15).protocolConfig(httpConf),
+    GetListServers.scn.users(2).ramp(20).protocolConfig(httpConf),
+    GetListDbs.scn.users(1).ramp(5).protocolConfig(httpConf),
+    GetListStorages.scn.users(1).protocolConfig(httpConf) //.delay(15)
+    )
+}
---
+++ test/performance/user-files/simulations/assetLists/Headers.scala
@@ -0,0 +1,48 @@
+package assetLists
+
+object Headers {
+//login
+
+  //SingIn
+  val headers_3 = Map(
+      "Content-Type" -> """application/x-www-form-urlencoded"""
+  )
+
+  //Update last page Get List Servers
+  val headers_2 = Map(
+      "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+      "Cache-Control" -> """no-cache""",
+      "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+      "Pragma" -> """no-cache""",
+      "X-Prototype-Version" -> """1.6.0""",
+      "X-Requested-With" -> """XMLHttpRequest"""
+  )
+//  get list app 
+  val headers_4 = Map(
+    "Accept" -> """text/javascript, text/html, application/xml, text/xml, */*""",
+    "Cache-Control" -> """no-cache""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Pragma" -> """no-cache""",
+    "X-Prototype-Version" -> """1.6.0""",
+    "X-Requested-With" -> """XMLHttpRequest"""
+  )
+//asset entity list json get list of servers
+  val headers_5 = Map(
+      "Accept" -> """application/json, text/javascript, */*; q=0.01""",
+      "X-Requested-With" -> """XMLHttpRequest"""
+  )
+// get list of app
+   val headers_7 = Map(
+      "Accept" -> """application/json, text/javascript, */*; q=0.01""",
+      "X-Requested-With" -> """XMLHttpRequest"""
+  )
+   
+  val headers_9 = Map(
+    "Accept" -> """*/*""",
+    "Cache-Control" -> """no-cache""",
+    "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+    "Pragma" -> """no-cache""",
+    "X-Requested-With" -> """XMLHttpRequest"""
+  )
+ 
+}
