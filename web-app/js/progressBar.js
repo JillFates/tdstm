660be3deedbb290dc1529fb97b672a5b43a8c37f
erobles <erobles@tdsi.com>
2014-07-28 12:44:42 +0000

    TM-3001 Create generic progress bar
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4736 fecba46a-3362-0410-80e5-8c76cdb905a0

--- web-app/js/progressBar.js
+++ web-app/js/progressBar.js
@@ -1,8 +1,12 @@
-function TaskProgressBar(taskId, pingTime, onSuccess, onFailure) {
+function TaskProgressBar(taskId, pingTime, onSuccess, onFailure, progressTitle) {
 	this.taskId = taskId;
 	this.pingTime = pingTime;
 	this.onSuccess = onSuccess;
 	this.onFailure = onFailure;
+	if (progressTitle == undefined) {
+		progressTitle = "Working";
+	}
+	this.progressTitle = progressTitle;
 	var self = this;
 	
 	setTimeout(function() {
@@ -18,21 +22,21 @@ TaskProgressBar.prototype.initUI = function() {
 	var initialized = $('#progressBar').length != 0;
 	if (!initialized) {
 		var newcontent = document.createElement('div');
-	    newcontent.innerHTML = '<div id="globalProgressBar" style="display:none; width:100%; text-align:center; position: absolute; top: 80px; "><div style="width: 400px; height: 60px; margin: auto; background-color: #FFFFFF; border-color: #DDDDDD; border-style: solid; border-width: 1px; border-radius: 4px 4px 4px 4px; -webkit-box-shadow: none; box-shadow: none"><p id="progressStatus" style="color:#777777; font-size: 11px; font-family: verdana; margin-top: 4px;"></p><div class="progress" style="margin: 10px; margin: auto; width: 300px;"><div id="innerGlobalProgressBar" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%; font-size: 11px; font-family: verdana;">0%</div></div></div></div>';
-        document.body.appendChild(newcontent.firstChild);
+	    newcontent.innerHTML = '<div class="modal fade" id="globalProgressBar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">  <div class="modal-dialog">    <div class="modal-content">      <div class="modal-header">        <h4 id="progressTitle" class="modal-title" id="myModalLabel">Modal title</h4>      </div>      <div class="modal-body">	<p id="progressStatus" style="color:#777777; font-size: 11px; font-family: verdana; margin-top: 4px;"></p>        <div id="innerGlobalProgressBar" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%; font-size: 11px; font-family: verdana;">0%</div>	<p id="progressStatus" style="color:#777777; font-size: 11px; font-family: verdana; margin-top: 4px;"></p>      </div>      <div class="modal-footer">	<button id="progressClose" type="button" class="btn btn-default" data-dismiss="modal" style="display:none;">Close</button>        <button id="progressCancel" type="button" class="btn btn-primary" style="display:none;">Cancel</button>      </div>    </div>  </div></div>';
+	    document.body.appendChild(newcontent.firstChild);
 	}
 }
 
 TaskProgressBar.prototype.finishProgressBar = function() {
 	clearInterval(this.interval);
 	setTimeout(function() {
-		$('#globalProgressBar').hide();
+		$('#globalProgressBar').modal('hide');
 	}, 2000);
 
 }
 
 TaskProgressBar.prototype.showProgressBar = function() {
-	$('#globalProgressBar').show();
+	$('#globalProgressBar').modal('show');
 }
 
 TaskProgressBar.prototype.updateProgress = function() {
@@ -41,6 +45,8 @@ TaskProgressBar.prototype.updateProgress = function() {
 	var jqxhr = $.get('/tdstm/ws/progress/' + self.taskId, function(data) {
 		if (data) {
 			data = data.data;
+	        $('#progressTitle').html(self.progressTitle);
+
 			if (data.status == "In progress") {
 				var inner = $('#innerGlobalProgressBar');
 				var status = $('#progressStatus');
@@ -67,11 +73,15 @@ TaskProgressBar.prototype.updateProgress = function() {
 			} else {
 				var status = $('#progressStatus');
 				status.html(data.status);
+				
+				$('#progressClose').show();
+				$('#progressClose').click(function() {
+					if (self.onFailure != undefined) {
+						self.onFailure();
+					}
+					self.finishProgressBar();
+				});
 
-				if (self.onFailure != undefined) {
-					self.onFailure();
-				}
-				self.finishProgressBar();
 			}
 		}
 	}).fail(function() {
