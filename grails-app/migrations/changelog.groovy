e593d36bd3f3a1e8d1db607da867517a95ee1ed8
jdanahy <jdanahy@tdsi.com>
2014-08-14 20:45:33 +0000

    TM-2884 Added migration script for cleaning up orphaned commentnote records
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@4869 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -107,4 +107,5 @@ databaseChangeLog = {
 	include file: '20140717-task-batch-add-recipe-id-field.groovy'
 	include file: '20140804-fix-asset-comment-task-number.groovy'
 	include file: '20140805-change-asset-import-export-fields.groovy'
+	include file: '20140814-clean-up-orphaned-commentNotes.groovy'
 }
