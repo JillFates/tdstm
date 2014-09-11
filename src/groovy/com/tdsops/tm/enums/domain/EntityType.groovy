d61ff2d96ab0e965e9b9767406c859299c22d87c
erobles <erobles@tdsi.com>
2014-09-11 14:26:24 +0000

    TM-3254 fix help text for any type of device
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5008 fecba46a-3362-0410-80e5-8c76cdb905a0

--- src/groovy/com/tdsops/tm/enums/domain/EntityType.groovy
+++ src/groovy/com/tdsops/tm/enums/domain/EntityType.groovy
@@ -10,18 +10,19 @@ class EntityType {
 	static final String APP='Application'
 	static final String DB='Database'
 	static final String STORAGE='Files'
+	static final String DEVICE=AE
 	static final getList() {
-		return [ AE, APP, DB, STORAGE ]
+		return [ AE, APP, DB, STORAGE, DEVICE ]
 	}
 	static final getListAsMap() {
-		return [ asset:AE, app:APP, db:DB, file:STORAGE ]
+		return [ asset:AE, app:APP, db:DB, file:STORAGE, device: DEVICE ]
 	}
 	static final getListAsCategory(type){
 		def ctgType=[ (AE):'tt_asset', (APP):'tt_app', (DB):'tt_database', (STORAGE):'tt_storage' ]
 		return ctgType.("$type")
 	}
 	static final getKeyByText( text ){
-		def ctgType=[ 'Server' : AE, 'Storage':STORAGE, 'Database':DB, Application:APP]
+		def ctgType=[ 'Server' : AE, 'Storage':STORAGE, 'Database':DB, Application:APP, 'Device' : DEVICE]
 		return ctgType[text]
 	}
 }
