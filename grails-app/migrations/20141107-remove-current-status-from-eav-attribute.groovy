b4e1f97af6fa39eef838e63791ebfef48a69468d
John Martin <jmartin@tdsi.com>
2014-11-10 16:02:13 +0000

    TM-3543 Added 2nd migration step to delete eavEntityAttribute reference to currentStatus column
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5286 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20141107-remove-current-status-from-eav-attribute.groovy
+++ grails-app/migrations/20141107-remove-current-status-from-eav-attribute.groovy
@@ -7,4 +7,9 @@ databaseChangeLog = {
 		comment('Remove attribute current status from table eav_attribute.')
 		sql('DELETE FROM eav_attribute WHERE attribute_code like "currentStatus";')
 	}
+
+	changeSet(author: "dscarpa", id: "20141107 TM-3315-3") {
+		comment('Remove reference to the currentStatus attribute')
+		sql('DELETE FROM eav_entity_attribute WHERE attribute_id=51')
+	}
 }
