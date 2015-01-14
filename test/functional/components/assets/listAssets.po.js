507609dd2672a81d572ee9e471d6af24fa641fc8
jcalabrese <jcalabrese@tdsi.com>
2015-01-14 15:32:37 +0000

    TM-3646 Update automated test to cover this change
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5440 fecba46a-3362-0410-80e5-8c76cdb905a0

--- test/functional/components/assets/listAssets.po.js
+++ test/functional/components/assets/listAssets.po.js
@@ -9,12 +9,6 @@ var ListAssets = function(){
 
 ListAssets.prototype = {};
 
-// ListAssets.prototype.getLoadingStyle = function(asse) {
-//   return element(by.id(asse)).getAttribute('style').then(function(attClass){
-//     return (attClass.search('display: none;') !== -1);
-//   });      
-// };
-
 ListAssets.prototype.getColumnsHeaders = function() {
   return browser.executeScript('return $("[role=\'grid\'] [class=\'ui-jqgrid-labels ui-sortable\'] th:visible")');
 };
@@ -22,75 +16,6 @@ ListAssets.prototype.getColumnsHeaders = function() {
 ListAssets.prototype.getColumnsWithHeaderSelector = function() {
   return browser.executeScript('return $("[role=\'grid\'] [class=\'ui-jqgrid-labels ui-sortable\'] th:visible img")');
 };
-
-// ListAssets.prototype.isLoadingHidden = function (device) {
-//   /* Values for device base on asset type
-//   application
-//   assetList
-//   storage
-//   database */
-//   return browser.wait(function () {
-//     return element(by.id('load_'+device+'IdGrid')).isDisplayed().then(function (valor) {
-//       return !valor;
-//     });
-//   }).then(function () {
-//     return true;
-//   });
-// };
-
-// ListAssets.prototype.getListItems = function(expListItems,appName){
-//   return browser.wait(function () {
-//     return $$('[role="grid"] tbody tr.ui-widget-content').then(function(list){
-//       if(list.length===expListItems){
-//         if(list.length === 1){
-//           return list[0].$('[aria-describedby="applicationIdGrid_assetName"]').getText().then(function (text) {
-//             console.log('text','"'+text+'"');
-//             console.log('appName','"'+appName+'"');
-//             return text === appName;
-//           });
-//         }else {
-//           return true;
-//         }
-//         // return list.length===expListItems;
-//       }
-//     });
-//   }).then(function () {
-//     return true;
-//   });
-
-// };
-
-// ListAssets.prototype.verifySearchResults = function(count,device,appName){
-//   var that  =this;
-//   var asse = 'assetnotdefine';
-//   return browser.wait(function(){
-//     var d = {
-//       'device': function(){
-//           asse = that.devicesLoadingId;
-//       },
-//       'application':function(){
-//         asse = that.applicationLoadingId;
-//       },
-//       'database':function(){
-//         asse = that.databaseLoadingId;
-//       },
-//       'storage':function(){
-//         asse = that.storageLoadingId;
-//       },
-//       'dependency':function(){
-//         asse =that.dependencyLoadingId;
-//       }
-//     };
-//     d[device]();
-//     return that.getLoadingStyle(asse)&&that.getListItems(count,appName);
-//   }).then(function(){
-//       return $$('[role="grid"] tbody tr.ui-widget-content').then(function(list){
-//         return list;
-//     });
-//   },function(){
-//     return 'No results found';}
-//   );
-// };
 
 ListAssets.prototype.clickOnTaskIcon = function(appId){
   var addtaskIcon =  $('#icon_task_'+appId);
@@ -136,7 +61,8 @@ ListAssets.prototype.isCommentsIconDisplayed = function(appId){
   var that = this;
   return browser.wait(function(){
     return that.getCommentIcon(appId).getAttribute('src').then(function(ico){
-      return ico === process.env.BASE_URL+'/tdstm/icons/comments.png';
+      return ico === process.env.BASE_URL+'/tdstm/images/comment_exists.png';
+      // return ico === process.env.BASE_URL+'/tdstm/icons/comments.png';
     });
   }).then(function(){
     return true;
