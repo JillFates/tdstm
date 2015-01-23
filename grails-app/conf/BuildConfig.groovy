5fcb520a2042e43757861e7f8d6dab1d7b63365c
Diego Scarpa <dscarpa@tdsi.com>
2015-01-23 17:19:09 +0000

    TM-3661 Remove MySQL duplicate connector Jar from the build
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5460 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,4 +35,5 @@ grails.project.dependency.resolution = {
 grails.war.resources = { stagingDir -> 
 	delete(file: "${stagingDir}/WEB-INF/lib/poi-3.0-FINAL.jar")
 	delete(file: "${stagingDir}/WEB-INF/lib/poi-3.5-FINAL-20090928.jar")
+	delete(file: "${stagingDir}/WEB-INF/lib/mysql-connector-java-5.1.6-bin.jar")
 } 
