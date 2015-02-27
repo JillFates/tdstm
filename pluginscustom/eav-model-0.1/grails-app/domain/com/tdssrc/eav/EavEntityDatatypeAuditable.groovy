c72f89eb958bf2f2db78ada14b05860353893561
Diego Scarpa <dscarpa@tdsi.com>
2015-02-27 23:59:15 +0000

    TM-2808 Grails upgrade to 2.3.11 - custom plugin
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5559 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ pluginscustom/eav-model-0.1/grails-app/domain/com/tdssrc/eav/EavEntityDatatypeAuditable.groovy
@@ -0,0 +1,15 @@
+package com.tdssrc.eav
+
+abstract class EavEntityDatatypeAuditable extends EavEntityDatatype {
+	String auditAction
+
+	static mapping = {
+		tablePerHierarchy false
+		version false
+	}
+
+	static constraints = {
+		auditAction( blank:false, inList:['I', 'U', 'D'] )
+	}
+
+}
