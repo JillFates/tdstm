dd64f6bbe1d1c5b81ed0a0cf152875dfed6687c0
John Martin <jmartin@tdsi.com>
2015-04-24 19:25:21 +0000

    TM-3801 Change User / Project permissions logic - tweaked Diego's db migrations to change permission assignments
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5714 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20150410-remove-user-login-permissions-from-client-admin-role.groovy
+++ grails-app/migrations/20150410-remove-user-login-permissions-from-client-admin-role.groovy
@@ -1,17 +1,10 @@
-/**
- * Removes user login permissions from CLIEN_ADMIN role
- */
 databaseChangeLog = {
 	
 	changeSet(author: "dscarpa", id: "20150409 TM-3801-2") {
-		comment('Removes user login permissions from CLIEN_ADMIN role')
+		comment('Remove project and userlogin delete permissions from CLIENT_MGR role')
 		sql("""DELETE FROM role_permissions WHERE
-			permission_id = (select id from permissions where permission_group = 'USER' and permission_item= 'CreateUserLogin') AND role = 'CLIENT_ADMIN'""")
+			permission_id = (select id from permissions where permission_group = 'USER' and permission_item= 'EditUserLogin') AND role = 'CLIENT_MGR'""")
 		sql("""DELETE FROM role_permissions WHERE
-			permission_id = (select id from permissions where permission_group = 'USER' and permission_item= 'EditUserLogin') AND role = 'CLIENT_ADMIN'""")
-		sql("""DELETE FROM role_permissions WHERE
-			permission_id = (select id from permissions where permission_group = 'PROJECT' and permission_item= 'ProjectEditView') AND role = 'CLIENT_ADMIN'""")
-		sql("""DELETE FROM role_permissions WHERE
-			permission_id = (select id from permissions where permission_group = 'PROJECT' and permission_item= 'ProjectDelete') AND role = 'CLIENT_ADMIN'""")
+			permission_id = (select id from permissions where permission_group = 'PROJECT' and permission_item= 'ProjectDelete') AND role = 'CLIENT_MGR'""")
 	}
 }
