c0ccdd023ff7506e96f2eb631b157dcc06ef0c02
Augusto Recordon <augusto.recordon@bairesdev.com>
2015-05-26 14:22:59 +0000

    TM-3687 Changes the asset type column definition to add the NOT NULL constraint.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5832 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20150526-assettype-defaulted-to-server-null-values.groovy
+++ grails-app/migrations/20150526-assettype-defaulted-to-server-null-values.groovy
@@ -1,12 +1,19 @@
 /**
- * Assigns 'Server' as assetType to assets without a value for this field.
+ * Assigns 'Server' as assetType to models without a value for this field.
  */
 databaseChangeLog = {
 	
 	changeSet(author: "arecordon", id: "20150526 TM-3687") {
-		comment("Assigns 'Server' as assetType to assets without a value for this field.")
+		comment("Assigns 'Server' as assetType to models without a value for this field.")
 		sql("""
 			UPDATE model SET asset_type='Server' WHERE asset_type IS NULL
+		""")
+	}
+
+	changeSet(author: "arecordon", id: "20150526 TM-3687") {
+		comment("Makes the asset type column in models to be not null")
+		sql("""
+			ALTER TABLE model MODIFY asset_type varchar(255) NOT NULL;
 		""")
 	}
 }
