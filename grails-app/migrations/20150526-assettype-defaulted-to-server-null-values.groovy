b757f84c10fe00a16e6a3caa56612a9ed5bab02d
Augusto Recordon <augusto.recordon@bairesdev.com>
2015-06-23 12:31:33 +0000

    TM-3687 Adds constraints and changes to the migration script.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@5925 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20150526-assettype-defaulted-to-server-null-values.groovy
+++ grails-app/migrations/20150526-assettype-defaulted-to-server-null-values.groovy
@@ -16,4 +16,12 @@ databaseChangeLog = {
 			ALTER TABLE model MODIFY asset_type varchar(255) NOT NULL;
 		""")
 	}
+
+	changeSet(author: "arecordon", id: "20150526 TM-3687 3") {
+		comment("Assigns 'Server' as assetType to models with an empty value for this field.")
+		sql("""
+			UPDATE model SET asset_type='Server' WHERE LENGTH(TRIM(asset_type)) = 0
+		""")
+	}
+
 }
