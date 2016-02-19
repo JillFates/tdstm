8ed1c28453ccf19c58a8c8acdea402469cfd2622
John Martin <jmartin@tdsi.com>
2016-02-19 21:27:53 +0000

    TM-4664 Changed up the database migration to use insert or update logic plus define the columns being inserted into so that we do not encounter the SQL error that was occurring.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@6533 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20150731-add-additional-teams-to-role-type.groovy
+++ grails-app/migrations/20150731-add-additional-teams-to-role-type.groovy
@@ -1,48 +1,27 @@
 /*Adds new team codes for AIX, UX, DB2, VM and Backup*/
 databaseChangeLog = {
-	changeSet(author: "jdanahy", id: "20150731 TM-4023-1") {
-		comment('Create entry for SYS_ADMIN_AIX in role_type')
-		preConditions(onFail:'MARK_RAN') {
-			sqlCheck(expectedResult:'0', 'select count(*) from role_type where role_type_code="SYS_ADMIN_AIX"')
+	List teams = [
+		['SYS_ADMIN_AIX', 'Staff : System Admin-AIX', 'Manages IBM AIX servers'],
+		['SYS_ADMIN_UNIX','Staff : System Admin-Unix','Manages Unix servers'],
+		['DBA_DB2', 'Staff : Database Admin-DB2','Manages IBM DB2 databases'],
+		['VM_ADMIN','Staff : VM Admin','Manages VM Infrastructure'],
+		['VM_ADMIN_AWS','Staff : VM Admin (AWS)','Manages Amazon Cloud Infrastructure'],
+		['BACKUP_ADMIN','Staff : Backup Admin','Manages Backup infrastructure']
+	]
+
+	changeSet(author: "jmartin", id: "201602109 TM-4023-1") {
+		comment('Add several new teams to the RoleType table')
+		grailsChange {
+			change {
+				teams.each { t ->
+					sql.execute("""
+						INSERT INTO role_type (role_type_code, description, help, type) 
+							VALUES(?, ?, ?, ?)
+							ON DUPLICATE KEY UPDATE type='TEAM'
+						""", [t[0], t[1], t[2], 'TEAM'] )
+
+				}
+			}
 		}
-		sql("""
-		INSERT INTO role_type VALUES ('SYS_ADMIN_AIX','Staff : System Admin-AIX','Manages IBM AIX servers');
-		""")
-    }
-	changeSet(author: "jdanahy", id: "20150731 TM-4023-2") {
-		comment('Create entry for SYS_ADMIN_UNIX in role_type')
-		preConditions(onFail:'MARK_RAN') {
-			sqlCheck(expectedResult:'0', 'select count(*) from role_type where role_type_code="SYS_ADMIN_UNIX"')
-		}
-		sql("""
-		INSERT INTO role_type VALUES ('SYS_ADMIN_UNIX','Staff : System Admin-Unix','Manages Unix servers');
-		""")
 	}
-	changeSet(author: "jdanahy", id: "20150731 TM-4023-3") {
-		comment('Create entry for DBA_DB2 in role_type')
-		preConditions(onFail:'MARK_RAN') {
-			sqlCheck(expectedResult:'0', 'select count(*) from role_type where role_type_code="DBA_DB2"')
-		}
-		sql("""
-		INSERT INTO role_type VALUES ('DBA_DB2','Staff : Database Admin-DB2','Manages IBM DB2 databases');
-		""")
-    }
-	changeSet(author: "jdanahy", id: "20150731 TM-4023-4") {
-		comment('Create entry for VM_ADMIN in role_type')
-		preConditions(onFail:'MARK_RAN') {
-			sqlCheck(expectedResult:'0', 'select count(*) from role_type where role_type_code="VM_ADMIN"')
-		}
-		sql("""
-		INSERT INTO role_type VALUES ('VM_ADMIN','Staff : VM Admin','Manages VM Infrastructure');
-		""")
-    }
-	changeSet(author: "jdanahy", id: "20150731 TM-4023-5") {
-		comment('Create entry for BACKUP_ADMIN in role_type')
-		preConditions(onFail:'MARK_RAN') {
-			sqlCheck(expectedResult:'0', 'select count(*) from role_type where role_type_code="BACKUP_ADMIN"')
-		}
-		sql("""
-		INSERT INTO role_type VALUES ('BACKUP_ADMIN','Staff : Backup Admin','Manages Backup infrastructure');
-		""")
-    }
 }
