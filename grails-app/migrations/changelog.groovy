9a52f10255fb9684ca959accf2d676ef5be91c92
Octavio Luna <oluna@tdsi.com>
2016-03-18 02:37:29 +0000

    TM-4697 remove blocking migration script from the build
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@6629 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -116,5 +116,4 @@ databaseChangeLog = {
 	include file: '20151209-nullout-invalid-person-ref.groovy'
 	include file: '20151230-remove-from-role-user-dep-analyzer-view.groovy'
 	include file: '20160210-new-security-permission-for-critical-path.groovy'
-	include file: '20160315-update-orphan-records-asset_comment.groovy'
 }
