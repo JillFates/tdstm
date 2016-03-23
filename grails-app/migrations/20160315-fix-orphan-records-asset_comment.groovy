30ccb6c68beae4c854fa6bc3b404317ad1acb0f0
Octavio Luna <oluna@tdsi.com>
2016-03-23 05:38:10 +0000

    TM-4697 added migration
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@6647 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/migrations/20160315-fix-orphan-records-asset_comment.groovy
@@ -0,0 +1,62 @@
+/**
+ * @author @tavo_luna
+ * TM-4697 Fixing orphan records for cookbook tasks by cascade delete using a FK in the Database
+ */
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import groovy.time.TimeDuration
+
+databaseChangeLog = {	
+	changeSet(author: "oluna", id: "20160315 TM-4697-1") {
+		comment('Clear orphan task_dependencies')
+		
+		grailsChange {
+			change {
+				def limit = 500000				
+				
+				def totalDeps = sql.firstRow("select count(*) as total FROM task_dependency td LEFT OUTER JOIN asset_comment ac on ac.asset_comment_id = td.asset_comment_id where ac.asset_comment_id IS NULL")
+				log.info("Orphans: ${totalDeps.total} to clear")
+
+				def numBatchs = Math.ceil(totalDeps.total / limit).intValue()
+
+				(1..numBatchs).each{ 
+					log.info(String.format("Cleared %.2f %%", it*100/numBatchs))
+					sql.executeUpdate("""
+						DELETE FROM task_dependency WHERE task_dependency_id IN (
+							SELECT id FROM (
+								SELECT DISTINCT task_dependency_id as id FROM task_dependency td
+								LEFT OUTER JOIN asset_comment t1 on t1.asset_comment_id = td.asset_comment_id
+								WHERE t1.asset_comment_id IS NULL
+								LIMIT ${limit}
+							) t
+						);
+		    	""")
+				}
+				log.info("Orphans cleared!")
+
+				log.info("Creating Fk-Constraint")
+				sql.execute("""
+					ALTER TABLE task_dependency
+					  ADD CONSTRAINT fk_task_dep_asset_comment_id
+					  FOREIGN KEY (asset_comment_id) REFERENCES asset_comment (asset_comment_id)
+					  ON UPDATE CASCADE
+					  ON DELETE CASCADE
+				""")
+
+				confirm "task_dependencies: Orphans Cleared and Fk-Constraint Created"
+			}
+		}
+	}
+
+	changeSet(author: "oluna", id: "20160315 TM-4697-2") {
+		comment('Fix Orphan records for comment_note (tasks)')
+		//delete and set cascade to comment
+		sql("DELETE cn FROM comment_note cn LEFT OUTER JOIN asset_comment ac ON ac.asset_comment_id = cn.asset_comment_id WHERE ac.asset_comment_id IS NULL")
+		sql("""
+			ALTER TABLE comment_note
+			  ADD CONSTRAINT fk_comment_note_asset_comment_id
+			  FOREIGN KEY (asset_comment_id) REFERENCES asset_comment (asset_comment_id)
+			  ON UPDATE CASCADE
+			  ON DELETE CASCADE
+		""")
+	}
+}
