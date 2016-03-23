ebe147e5f26aa6c4f5d16acb518d30b2f0ad5034
Octavio Luna <oluna@tdsi.com>
2016-03-23 05:28:10 +0000

    TM-4703 Ooops missing files on last commit
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@6646 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ src/groovy/com/tdsops/common/os/Shell.groovy
@@ -0,0 +1,30 @@
+package com.tdsops.common.os
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Shell { 
+  /**
+   * Executes a command and bo
+   * @author @tavo_luna
+   */ 
+  static executeCommand(String command){  
+    def exitValue = -1
+    def message = ""
+    try{
+        def proc = command.execute()
+        proc.waitFor()
+        exitValue = proc.exitValue()
+        def os = (exitValue == 0) ? proc.in : proc.err
+        message = os.text     
+    }catch(IOException ioe){
+        message = ioe.message
+    }
+
+    return [exitValue:exitValue, message:message]
+  }
+ 
+  static systemLog(String message){
+    Shell.executeCommand("logger $message")  
+  }
+}
