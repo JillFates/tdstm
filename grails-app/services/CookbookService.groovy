647a16fbe9c8d204a11cb4f260bb1700d4203dac
Octavio Luna <oluna@tdsi.com>
2016-05-18 20:59:28 +0000

    TM-4883 back to original Eval
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@6938 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/services/CookbookService.groovy
+++ grails-app/services/CookbookService.groovy
@@ -939,8 +939,8 @@ class CookbookService {
 	 * @throws InvalidSyntaxException if the sourcecode is invalid
 	 */
 	Map parseRecipeSyntax( sourceCode ) {	
-		//return parseRecipeSyntaxUsingEval(sourceCode)
-		return parseRecipeSyntaxWithCache(sourceCode)
+		return parseRecipeSyntaxUsingEval(sourceCode)
+		//return parseRecipeSyntaxWithCache(sourceCode)
 	}
 
 	Map parseRecipeSyntaxWithCache( sourceCode ) {		
