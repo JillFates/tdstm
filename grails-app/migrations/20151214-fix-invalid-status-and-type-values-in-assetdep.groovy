066bf5f67096485b264b5351a00ed07e5c7568cc
John Martin <jmartin@tdsi.com>
2016-05-24 06:26:46 +0000

    TM-3968 Fixed the migration scripts for the AssetDependency status and typegrails-app/migrations/20151214-fix-invalid-status-and-type-values-in-assetdep.groovy
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@6958 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/migrations/20151214-fix-invalid-status-and-type-values-in-assetdep.groovy
+++ grails-app/migrations/20151214-fix-invalid-status-and-type-values-in-assetdep.groovy
@@ -1,6 +1,3 @@
-import com.tdsops.tm.enums.domain.AssetDependencyStatus
-import com.tdsops.tm.enums.domain.AssetDependencyType
-
 /**
  * Updates values Asset Dependency / type and status columns, handling
  * case sensitive differences and setting to 'Unknown' those that don't
@@ -9,60 +6,53 @@ import com.tdsops.tm.enums.domain.AssetDependencyType
 databaseChangeLog = {	
 
 	// Updates asset dep type values with the correct capitalization.
-	changeSet(author: "arecordon", id: "20151214 TM-3968-1") {
+	changeSet(author: "jmartin", id: "20151214 TM-3968-1-Fix.1") {
 		comment("Updates asset dep type values with the correct capitalization")
-		grailsChange{
-			change{
-				def typeValues = AssetDependencyType.getList()
-				typeValues.remove("Unknown")
-				(1..typeValues.size()).each{
-					def tv = typeValues[it - 1]
-					sql.execute("UPDATE asset_dependency SET type='${tv}' WHERE LOWER(type) = LOWER('${tv}') AND type != '${tv}'")
-				}	
-			}
-			
-		}
+		sql("""
+			update asset_dependency ad
+			join asset_options ao on ao.value=ad.type and ao.type='DEPENDENCY_TYPE' and binary ao.value <> binary ad.type
+			set ad.type = ao.value
+		""")
 	}
 
-
-	// Updates asset dep status values with the correct capitalization.
-	changeSet(author: "arecordon", id: "20151214 TM-3968-2") {
+	changeSet(author: "jmartin", id: "20151214 TM-3968-1-Fix.2") {
 		comment("Updates asset dep status values with the correct capitalization")
-		grailsChange{
-			change{
-				def statusValues = AssetDependencyStatus.getList()
-				statusValues.remove("Unknown")
-				(1..statusValues.size()).each{
-					def sv = statusValues[it - 1]
-					sql.execute("UPDATE asset_dependency SET status='${sv}' WHERE LOWER(type) = LOWER('${sv}') AND status != '${sv}'")
-				}	
-			}
-			
-		}
-	}
-
-
-	// Set to 'Unknown' the Asset Dep type column for those records that don't match any of the accepted values.
-	changeSet(author: "arecordon", id: "20151214 TM-3968-3") {
-
-		comment("Set to 'Unknown' the Asset Dep type column for those records that don't match any of the accepted values.")
-
 		sql("""
-				UPDATE asset_dependency SET type = 'Unknown' WHERE type NOT IN ('Backup','Batch', 'DB', 'File', 'Hosts', 'Runs On', 'Web')
-			""")
+			update asset_dependency ad
+			join asset_options ao on ao.value=ad.status and ao.type='DEPENDENCY_STATUS' and binary ao.value <> binary ad.status
+			set ad.status = ao.value
+		""")
 	}
 
-
-	// Set to 'Unknown' the Asset Dep status column for those records that don't match any of the accepted values.
-	changeSet(author: "arecordon", id: "20151214 TM-3968-4") {
-
-		comment("Set to 'Unknown' the Asset Dep status column for those records that don't match any of the accepted values.")
-
+	changeSet(author: "jmartin", id: "20151214 TM-3968-2-Fix.1") {
+		comment("Set to 'Unknown' the Asset Dep type column for records that do not match values from AssetOptions")
 		sql("""
-				UPDATE asset_dependency SET status = 'Unknown' WHERE status NOT IN ('Validated', 'Not Applicable', 'Questioned', 'Archived', 'Future', 'Testing')
-			""")
+			update asset_dependency ad
+			set ad.type='Unknown'
+			where ad.asset_dependency_id in (
+				select id from (
+					select asset_dependency_id as id
+					from asset_dependency ad
+					left outer join asset_options ao on ao.value=ad.type and ao.type='DEPENDENCY_TYPE'
+					where ad.type is not null and ao.value is null
+				) as queryForAssetDepId
+			)
+		""")
 	}
 
-
-
+	changeSet(author: "jmartin", id: "20151214 TM-3968-2-Fix.2") {
+		comment("Set to 'Unknown' the Asset Dep type column for records that do not match values from AssetOptions")
+		sql("""
+			update asset_dependency ad
+			set ad.status='Unknown'
+			where ad.asset_dependency_id in (
+				select id from (
+					select asset_dependency_id as id
+					from asset_dependency ad
+					left outer join asset_options ao on ao.value=ad.status and ao.type='DEPENDENCY_STATUS'
+					where ad.status is not null and ao.value is null
+				) as queryForAssetDepId
+			)
+		""")
+	}
 }
