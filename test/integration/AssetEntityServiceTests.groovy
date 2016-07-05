9c14ecd7d5ef51c28a50b8c9483e63eb0a89f96d
Octavio Luna <oluna@tdsi.com>
2016-07-05 22:01:41 +0000

    Refactoring imtegration Test AssetEntityServiceTest to use grails.test.spock.IntegrationSpec
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@7184 fecba46a-3362-0410-80e5-8c76cdb905a0

--- test/integration/AssetEntityServiceTests.groovy
+++ test/integration/AssetEntityServiceTests.groovy
@@ -1,101 +1,115 @@
-import grails.test.*
-
-import com.tdsops.tm.enums.domain.AssetClass
 import com.tds.asset.AssetOptions
+import grails.test.spock.IntegrationSpec
 
-class AssetEntityServiceTests extends GrailsUnitTestCase {
+class AssetEntityServiceTests extends IntegrationSpec {
 	
 	def assetEntityService
 
-	def project    
+	void "test Entity Info Method"(){
+		setup:
+			def project = Project.read(2445)
+			def keys = ['servers', 'applications', 'dbs', 'files', 'networks', 'dependencyType', 'dependencyStatus']
 
-	void testEntityInfoMethod() {
+		when:
+			def data = assetEntityService.entityInfo(project)
+			log.info "data = $data"
 
-		def keys = ['servers', 'applications', 'dbs', 'files', 'networks', 'dependencyType', 'dependencyStatus']
-		def data = assetEntityService.entityInfo(project)
+		then: "keys exists in results and contains assets"
+			keys.each {
+				data.containsKey(it) && data[it].size() > 0
+			}
 
-		keys.each {
-			assertTrue "$it exists in results", data.containsKey(it)
-			println "$it has ${data[it].size()} assets"
-			assertTrue "$it contains assets", data[it].size() > 0
-		}
-
-		assertTrue "dependencyType contains 'Runs On'", findAssetOptionInList(data['dependencyType'], AssetOptions.AssetOptionsType.DEPENDENCY_TYPE, 'Runs On') != null
-		assertTrue "dependencyStatus contains 'Validated'", findAssetOptionInList(data['dependencyStatus'], AssetOptions.AssetOptionsType.DEPENDENCY_STATUS, 'Validated') != null
+		and: "dependencyType contains 'Runs On'"
+			findAssetOptionInList(data['dependencyType'], 'Runs On')
+		and: "dependencyStatus contains 'Validated'"
+			findAssetOptionInList(data['dependencyStatus'], 'Validated')
 	}
 
-	void testGetDependencyTypes() {
-		def data = assetEntityService.getDependencyTypes()
-		println "data=$data"
-		assertTrue "dependencyType contains 'Runs On'", findAssetOptionInList(data, AssetOptions.AssetOptionsType.DEPENDENCY_TYPE, 'Runs On') != null
-		assertTrue "Has multiple values", data.size() > 1
+	void "test get dependency types"(){
+		when:
+			def data = assetEntityService.getDependencyTypes()
+
+			log.info "data = $data"
+
+		then:
+			data.size() > 1
+
+		and: "dependencyType contains 'Runs On'"
+			findAssetOptionInList(data, 'Runs On')
 	}
 
-	void testGetDependencyStatuses() {
-		def data = assetEntityService.getDependencyStatuses()
-		println "data=$data"
-		assertTrue "dependencyStatus contains 'Validated'", findAssetOptionInList(data, AssetOptions.AssetOptionsType.DEPENDENCY_STATUS, 'Validated') != null
-		assertTrue "Has multiple values", data.size() > 1
-	}
+	void "test get dependency statuses"(){
+		when:
+			def data = assetEntityService.getDependencyStatuses()
 
+		then:
+			data.size() > 1
 
-	void testParseParamsForDependencyAssetIds() {
-		Map params = [
-			name:'test',
-			'asset_support_1': 100,
-			'asset_support_2': 200,
-			'asset_support_0': 10,
-			'asset_support_-1': 20,
-			'asset_support_-3': 30,
-			'asset_dependent_3': 300,
-			'asset_dependent_0': 40,
-			'asset_dependent_-102': 50,
-			'asset_supportelse_1': 999,
-			'somethingelse': 'blah'
-			 ]
-
-		def (existingDep, newDep) = assetEntityService.parseParamsForDependencyAssetIds('support', params)
-
-		assertEquals 'Suport existing', 2, existingDep.size()
-		assertEquals 'Support 1', 100,  existingDep.get(1)
-		assertEquals 'Support 2', 200,  existingDep.get(2)
-		assertEquals 'Support new', 3, newDep.size()
-		assertEquals 'Support 0', 20, newDep.get(0)
-		assertEquals 'Support -1', 20, newDep.get(-1)
-		assertEquals 'Support -3', 20, newDep.get(-3)
-
-		(existingDep, newDep) = assetEntityService.parseParamsForDependencyAssetIds('dependent', params)
-
-		assertEquals 'Dependent new', 2, newDep.size()
-		assertEquals 'Dependent existing', 1, existingDep.size()
-		assertEquals 'Dependent 3', 300,  existingDep.get(3)
-		assertEquals 'Dependent 2', 200,  existingDep.get(2)
-		assertEquals 'Dependent new', 3, newDep.size()
-		assertEquals 'Dependent 0', 40, newDep.get(0)
-		assertEquals 'Dependent -102', 50, newDep.get(-1)
-
-		assertFalse 'Support else existing',  existingDep.values.contains(999)
-		assertFalse 'Support else new',  newDep.values.contains(999)
-
-	}
-
-	// Helper function
-	boolean findAssetOptionInList(List list, AssetOptions.AssetOptionsType type, String key) {
-		list.find { it.type == type && it.value == key }
+		and: "dependencyStatus contains 'Validated'"
+			findAssetOptionInList(data, 'Validated')
 	}
 
 
-    protected void setUp() {
-        super.setUp()
+	void "test parse params for dependecy asset ids"(){
+		setup:
+			Map params = [
+				name:'test',
+				'asset_support_1': 100,
+				'asset_support_2': 200,
+				'asset_support_0': 10,
+				'asset_support_-1': 20,
+				'asset_support_-3': 30,
+				'asset_dependent_3': 300,
+				'asset_dependent_0': 40,
+				'asset_dependent_-102': 50,
+				'asset_supportelse_1': 999,
+				'somethingelse': 'blah'
+			]
 
-		assetEntityService = new AssetEntityService()
+		when:
+			def (existingDep, newDep) = assetEntityService.parseParamsForDependencyAssetIds('support', params)
 
-		project = Project.read(2445)
-		assertNotNull 'Loading the Marketing Demo Project', project
+		then: 'Suport existing'
+			2 == existingDep.size()
+		and: 'Support 1'
+			100 == existingDep[1L]
+		and: 'Support 2'
+			200 == existingDep[2L]
+		and: 'Support new'
+			3 == newDep.size()
+		and: 'Support 0'
+			10 == newDep[0L]
+		and: 'Support -1'
+			20 == newDep[-1L]
+		and: 'Support -3'
+			30 == newDep[-3L]
+
+		when:
+			(existingDep, newDep) = assetEntityService.parseParamsForDependencyAssetIds('dependent', params)
+
+		then: 'Dependent new'
+			2 == newDep.size()
+		and: 'Dependent existing'
+			1 == existingDep.size()
+		and: 'Dependent 3'
+			300 == existingDep[3L]
+		and: 'Dependent new'
+			2 == newDep.size()
+		and: 'Dependent 0'
+			40 == newDep[0L]
+		and: 'Dependent -102'
+			50 == newDep[-102L]
+
+		and: 'Support else existing'
+			!existingDep.values().contains(999)
+		and: 'Support else new'
+			!newDep.values().contains(999)
+
 	}
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+	// Helper functions ////////////////////
+	protected boolean findAssetOptionInList(List list, String key) {
+		list.contains(key)
+	}
 
 }
