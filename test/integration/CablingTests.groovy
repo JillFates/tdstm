fed92c35a2d29b0c6ec169cb1f2dc5ed08bce454
Octavio Luna <oluna@tdsi.com>
2016-07-06 17:54:40 +0000

    TM-4786 changes to CablingTests
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@7189 fecba46a-3362-0410-80e5-8c76cdb905a0

--- test/integration/CablingTests.groovy
+++ test/integration/CablingTests.groovy
@@ -1,62 +1,76 @@
-
-import grails.test.*
-
-import com.tds.asset.AssetCableMap
-import com.tds.asset.AssetEntity
 import com.tdssrc.grails.GormUtil
+import grails.test.spock.IntegrationSpec
 
-class CablingTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
-
-    protected void tearDown() {
-        super.tearDown()
-    }
+class CablingTests extends IntegrationSpec {
     /***********************************
      * Test model create functionality
      **********************************/
-    void testModelCreate() {
-    	def manufacturer = new Manufacturer( name : "Dell" ).save()
-    	def modelInstance = new Model( modelName : "PowerEdge 1950", 
-    									manufacturer : manufacturer, 
-										assetType : "Server",
-										poweruse : 1200,
-										connectorLabel : "PE5",
-										type : "Power",
-										connectorPosX : 250,
-										connectorPosY : 90)
-    	if ( !modelInstance.validate() || !modelInstance.save() ) {
-			def etext = "Unable to create modelInstance" +
+
+	def "Test model create functionality"(){
+		when:
+			def manufacturer = new Manufacturer(name : "Dell")
+			if(!manufacturer.validate() || !manufacturer.save()){
+				def etext = "Unable to create manufacturer" +
+					GormUtil.allErrorsString( manufacturer )
+				log.error etext
+			}
+
+			def modelInstance = new Model(
+					modelName      : "PowerEdge 1950",
+					manufacturer   : manufacturer,
+					assetType      : "Server",
+					poweruse       : 1200,
+					connectorLabel : "PE5",
+					type           : "Power",
+					connectorPosX  : 250,
+					connectorPosY  : 90
+			)
+			if(!modelInstance.validate() || !modelInstance.save()){
+				def etext = "Unable to create modelInstance" +
+					GormUtil.allErrorsString( modelInstance )
+				log.error etext
+			}
+
+		then: "modelInstance isCreated"
+		(modelInstance.validate() && modelInstance.save()) == true
+/*
+      def etext = "Unable to create modelInstance" +
             GormUtil.allErrorsString( modelInstance )
-			println etext
-		} else {
-			println"Model : ${modelInstance} created"
-		}
-    	def modelFromDB = Model.findByModelName("PowerEdge 1950")
-		
-		assertEquals modelInstance.modelName, modelFromDB.modelName
+      println etext
+    } else {
+      println"Model : ${modelInstance} created"
     }
-    /***********************************
-     * Test AssetCableMap create functionality
-     **********************************/
-    void testAssetCableMapCreate() {
-    	def assetCableMapInstance = new AssetCableMap( cable : "PowerEdge wire", 
-						    			assetFrom : AssetEntity.findById(10), 
-						    			assetTo : AssetEntity.findById(11),
-						    			assetFromPort : 52,
-						    			assetToPort : 25,
-										state : 1 
-										)
-    	if ( !assetCableMapInstance.validate() || !assetCableMapInstance.save() ) {
-			def etext = "Unable to create assetCableMapInstance" +
-            GormUtil.allErrorsString( assetCableMapInstance )
-			println etext
-		} else {
-			println"AssetCableMap : ${assetCableMapInstance} created"
-		}
-    	def assetCableMapFromDB = AssetCableMap.findByCable("PowerEdge wire")
-		
-		assertEquals assetCableMapInstance.cable, assetCableMapFromDB?.cable
-    }
+*/
+		when:
+			def modelFromDB = Model.findByModelName("PowerEdge 1950")
+
+		then:
+			modelInstance.modelName == modelFromDB.modelName
+	}
+
+
+	/***********************************
+	 * Test AssetCableMap create functionality
+	 **********************************/
+	/*
+   void testAssetCableMapCreate() {
+	   def assetCableMapInstance = new AssetCableMap( cable : "PowerEdge wire",
+									   assetFrom : AssetEntity.findById(10),
+									   assetTo : AssetEntity.findById(11),
+									   assetFromPort : 52,
+									   assetToPort : 25,
+									   state : 1
+									   )
+	   if ( !assetCableMapInstance.validate() || !assetCableMapInstance.save() ) {
+		   def etext = "Unable to create assetCableMapInstance" +
+		   GormUtil.allErrorsString( assetCableMapInstance )
+		   println etext
+	   } else {
+		   println"AssetCableMap : ${assetCableMapInstance} created"
+	   }
+	   def assetCableMapFromDB = AssetCableMap.findByCable("PowerEdge wire")
+
+	   assertEquals assetCableMapInstance.cable, assetCableMapFromDB?.cable
+   }
+   */
 }
