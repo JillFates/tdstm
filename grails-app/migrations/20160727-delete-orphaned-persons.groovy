e80b8bd9187938fc3b430f3832ca83332aaf85d6
Augusto Recordon <augusto.recordon@bairesdev.com>
2016-07-28 17:30:06 +0000

    TM-4888 Removes orphaned persons.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@7305 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/migrations/20160727-delete-orphaned-persons.groovy
@@ -0,0 +1,27 @@
+
+/**
+ * Deletes orphaned persons.
+ */
+databaseChangeLog = {
+	
+	changeSet(author: "arecordon", id: "20160727 TM-4888-1") {
+		comment('Deletes orphaned persons using personService.bulkDelete.')
+		grailsChange{
+			change{
+				List orphans = sql.rows("""
+						select p.person_id 
+						from person p left outer join party_relationship pr 
+						on pr.party_id_to_id = p.person_id and pr.role_type_code_from_id = 'COMPANY' 
+						and pr.role_type_code_to_id='STAFF'
+						left outer join party on party.party_id = p.person_id
+						where pr.party_relationship_type_id is null and first_name <> 'Automated' and last_name <> 'Task'; 
+						""")
+				List ids = orphans.collect{it.person_id.toString()}
+				Person.withNewSession{
+					ctx.getBean("personService").bulkDelete(ids, false)	
+				}
+				
+			}
+		}
+	}
+}
