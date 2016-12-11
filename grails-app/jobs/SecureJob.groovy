973882bac2c2ac3b54e85855947c2afe20fb080b
John Martin <jmartin@tdsi.com>
2016-12-11 02:40:22 +0000

    TM-5616 added the grails-app/jobs/SecureJob.groovy class to load the Spring Security Context for jobs
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@7893 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/jobs/SecureJob.groovy
@@ -0,0 +1,28 @@
+import org.quartz.JobExecutionContext
+import org.springframework.util.Assert
+import net.transitionmanager.service.SecurityService
+
+/**
+ * The SecureJob class is used to initialize the security context of a job to that of the user
+ * specified in the Job Datamap property 'username'.
+ */
+public abstract class SecureJob {
+
+	SecurityService securityService
+
+	/**
+	 * Used to initialize the security context
+	 * @param context - the Job Execution Context that requires the datamap property 'username'
+	 * @return the context mergedJobDataMap
+	 */
+	Map initialize(JobExecutionContext context) {
+		Map dataMap = context.mergedJobDataMap
+		String username = dataMap.getString('username')
+		Assert.hasText(username, 'Secure jobs must have username specified in job data.')
+
+		// Load the User context to in order to execute the job
+		securityService.assumeUserIdentity(username)
+
+		return dataMap
+	}
+}
