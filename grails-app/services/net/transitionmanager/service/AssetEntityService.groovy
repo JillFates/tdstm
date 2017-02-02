3c4b7a73e8a294eb696cdce9ba6936d0e57cee03
Augusto Recordon <augusto.recordon@bairesdev.com>
2017-02-02 12:04:30 +0000

    TM-5929 Adds middle name to app owner in app listing.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@8226 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/services/net/transitionmanager/service/AssetEntityService.groovy
+++ grails-app/services/net/transitionmanager/service/AssetEntityService.groovy
@@ -1761,7 +1761,7 @@ class AssetEntityService implements ServiceMethods {
 					joinQuery += "\n LEFT OUTER JOIN move_event me ON me.move_event_id=mb.move_event_id \n"
 					break
 				case 'appOwner':
-					query += "CONCAT(CONCAT(p3.first_name, ' '), IFNULL(p3.last_name,'')) AS appOwner,"
+					query += "CONCAT_WS(' ', p3.first_name, p3.middle_name, p3.last_name) AS appOwner,"
 					joinQuery += "\n LEFT OUTER JOIN person p3 ON p3.person_id= ae.app_owner_id \n"
 					break
 				case ~/appVersion|appVendor|appTech|appAccess|appSource|license|businessUnit|appFunction|criticality|userCount|userLocations|useFrequency|drRpoDesc|drRtoDesc|shutdownFixed|moveDowntimeTolerance|testProc|startupProc|url|shutdownBy|shutdownDuration|startupBy|startupFixed|startupDuration|testingBy|testingFixed|testingDuration/:
