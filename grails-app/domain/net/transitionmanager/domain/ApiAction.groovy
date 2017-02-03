1fabe5d25854e6bf3742cc6433819bc9d7731010
John Martin <jmartin@tdsi.com>
2017-02-03 10:32:37 +0000

    TM-5958 Added new Agents concept for interfacing with 3rd party applications
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/branches/TM-5958_AmazonDemo@8239 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/domain/net/transitionmanager/domain/ApiAction.groovy
@@ -0,0 +1,54 @@
+package net.transitionmanager.domain
+
+import net.transitionmanager.agent.AgentClass
+import net.transitionmanager.agent.CallbackMode
+
+/*
+ * The ApiAction domain represents the individual mapped API methods that can be
+ * invoked by TransitionManager.
+ */
+class ApiAction {
+	String name
+
+	// Indicates the class to use for invoking an action
+	AgentClass agentClass
+
+	// The method on the agentClass to invoke
+	String agentMethod
+
+	// The credentials that should be used with the method
+	// ApiCredential agentCredential
+
+	//Map agentParams = {
+	//	groupId: [context:ParamContext.ASSET, property:'custom12']
+	//}
+
+	// Determines how async API calls notify the complition of an action invocation
+	CallbackMode callbackMode = CallbackMode.NA
+
+	// The name of the queue that should be called back for CallbackMode.MESSAGE
+	String callbackQueue = ''
+
+	Date dateCreated
+	Date lastModified
+
+	static belongsTo = [project: Project]
+
+	static constrants = {
+		name size: 1..64
+		agentMethod size: 1..64
+		callbackQueue size: 0..64
+	}
+
+	static mapping = {
+		columns {
+			name 			sqlType: 'varchar(64)'
+			agentMethod 	sqlType: 'varchar(64)'
+			callbackQueue 	sqlType: 'varchar(64)'
+		}
+	}
+
+	String toString() {
+		name
+	}
+}
