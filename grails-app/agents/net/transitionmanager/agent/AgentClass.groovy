b17a5b2aad7ba34169f882c9a88f9424837185fe
Octavio Luna <oluna@tdsi.com>
2017-02-05 16:10:52 +0000

    TM-5987 some changes to the Enum types and adding ApiAction data for the GUI
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/branches/TM-5958_AmazonDemo@8252 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/agents/net/transitionmanager/agent/AgentClass.groovy
+++ grails-app/agents/net/transitionmanager/agent/AgentClass.groovy
@@ -5,20 +5,11 @@ package net.transitionmanager.agent
  * help keep ApiCredential, ApiAction and Agent classes logically associated together.
  */
 enum AgentClass {
-	AWS ('AWS'),
-	RIVER_MEADOW ('RIVER_MEADOW'),
-	RISK_NETWORKS ('RISK_NETWORKS'),
-	RACIME ('RACIME'),
-	CUSTOM ('CUSTOM'),
-	TRANSITION_MANAGER ('TRANSITION_MANAGER'),
-	LICENSE_MANAGER ('LICENSE_MANAGER')
-
-	String id
-	AgentClass (String id) {
-		this.id = id
-	}
-
-	static AgentClass forId(String id) {
-		values().find { it.id == id }
-	}
+	AWS,
+	RIVER_MEADOW,
+	RISK_NETWORKS,
+	RACIME,
+	CUSTOM,
+	TRANSITION_MANAGER,
+	LICENSE_MANAGER
 }
