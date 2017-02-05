8ce31f923388ad6fdbd8734f8557ebcf2961f9e1
Octavio Luna <oluna@tdsi.com>
2017-02-05 16:11:29 +0000

    TM-5987 missing file
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/branches/TM-5958_AmazonDemo@8253 fecba46a-3362-0410-80e5-8c76cdb905a0

---
+++ grails-app/services/net/transitionmanager/service/ApiActionService.groovy
@@ -0,0 +1,22 @@
+package net.transitionmanager.service
+
+import grails.transaction.Transactional
+import net.transitionmanager.domain.ApiAction
+
+@Transactional
+class ApiActionService {
+    private List<Map> store = [
+            [id:1, name: 'Action one'],
+            [id:2, name: 'Action two'],
+            [id:3, name: 'Action three'],
+            [id:4, name: 'Action four']
+    ]
+
+    ApiAction findApiAction(String id) {
+        return store.find{ it.id == id }
+    }
+
+    List<Map> list(){
+        store
+    }
+}
