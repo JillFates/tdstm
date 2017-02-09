a3aa3cacf0eeb667f9c29e52e454188795ecc9d3
Jorge Morayta <jorge.morayta@bairesdev.com>
2017-02-09 18:21:35 +0000

    TM-4012 Start mapping what is going to be the Notices Manager featured module.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@8296 fecba46a-3362-0410-80e5-8c76cdb905a0

--- web-app/tds/web-app/app-js/config/tds-app.module.ts
+++ web-app/tds/web-app/app-js/config/tds-app.module.ts
@@ -5,13 +5,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TDSAppComponent }  from './tds-app.component';
-import { HighlightDirective } from '../shared/directives/highlight.directive'
-import { GamesModule } from '../modules/games/games.module'
+// Shared Services
 import { UserService } from '../shared/services/user.service'
+// Shared Directives
+import { HighlightDirective } from '../shared/directives/highlight.directive'
+// Feature modules
+import { GamesModule } from '../modules/games/games.module'
+import { NoticesManagerModule } from '../modules/noticeManager/notices-manager.module'
+
 
 // Decorator that tells to Angular is a module.
 @NgModule({
-  imports:      [ BrowserModule, GamesModule ], // Means browser runnable, only add Angular Module here
+  imports:      [ BrowserModule, GamesModule, NoticesManagerModule ], // Means browser runnable, only add Angular Module here
   declarations: [ TDSAppComponent, HighlightDirective], // components, directives and pipes ONLY and only ONCE
   bootstrap:    [ TDSAppComponent ], // Contains the root component that is being injected on the index.html
   providers: [UserService]
