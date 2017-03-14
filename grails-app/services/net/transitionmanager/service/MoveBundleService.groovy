028376182fc0f44b03fd6f2f9e280f0920d8559f
Sidar Lopez <slopez@tdsi.com>
2017-03-14 16:34:09 +0000

    TM-6074: Dependency Analyzer generations is improperly grouping assets into one group
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@8514 fecba46a-3362-0410-80e5-8c76cdb905a0

--- grails-app/services/net/transitionmanager/service/MoveBundleService.groovy
+++ grails-app/services/net/transitionmanager/service/MoveBundleService.groovy
@@ -690,10 +690,15 @@ class MoveBundleService implements ServiceMethods {
 	 */
 	private def searchForAssetDependencies(moveBundleText, connectionTypes, statusTypes) {
 		// Query to fetch dependent asset list with dependency type and status and move bundle list with use for planning .
-		String  queryForAssets = "SELECT a.asset_entity_id as assetId, ad.asset_id as assetDepFromId, ad.dependent_id as assetDepToId," +
-				" a.move_bundle_id as moveBundleId, ad.status as status, ad.type as type, a.asset_type as assetType FROM asset_entity a" +
-				" LEFT JOIN asset_dependency ad on a.asset_entity_id = ad.asset_id OR ad.dependent_id = a.asset_entity_id" +
-				" WHERE a.move_bundle_id in ( ${moveBundleText} )"
+		String  queryForAssets = "SELECT a.asset_entity_id as assetId, ad.asset_id as assetDepFromId, ad.dependent_id as assetDepToId, " +
+				" a.move_bundle_id as moveBundleId, ad.status as status, ad.type as type, a.asset_type as assetType " +
+				" FROM asset_entity a, asset_dependency ad " +
+				" WHERE a.move_bundle_id in ( ${moveBundleText} ) " +
+				" AND (a.asset_entity_id = ad.asset_id OR ad.dependent_id = a.asset_entity_id)"
+
+		queryForAssets += connectionTypes ? " AND ad.type in (${connectionTypes}) " : ""
+		queryForAssets += statusTypes ? " AND ad.status in (${statusTypes}) " : ""
+		queryForAssets += " ORDER BY a.asset_entity_id DESC  "
 
 		logger.info 'SQL used to find assets: {}', queryForAssets
 		return jdbcTemplate.queryForList(queryForAssets)
