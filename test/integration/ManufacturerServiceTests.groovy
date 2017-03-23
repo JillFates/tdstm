af2a384785319dc83f912bcfbe9f70641fe31028
John Martin <jmartin@tdsi.com>
2017-03-23 19:22:16 +0000

    TM-6218 Tweaked the ManufacturerService Test case to remove assert commands and fixed bug in the run.sh.template for handling parameters plus bumped the memory settings some.
    git-svn-id: https://opsvcs.tdsops.com/repos/tdstm/trunk@8580 fecba46a-3362-0410-80e5-8c76cdb905a0

--- test/integration/ManufacturerServiceTests.groovy
+++ test/integration/ManufacturerServiceTests.groovy
@@ -10,34 +10,47 @@ import spock.lang.Specification
 
 @TestFor(ManufacturerService)
 class ManufacturerServiceTests extends Specification {
-	
+
 	ManufacturerService manufacturerService
-	
+
 	def '1. Test the isValidAlias method in many different situations' () {
 		setup:
 			def manCat = new Manufacturer(name: 'cat').save()
 			def manDog = new Manufacturer(name: 'dog').save()
 			def a1ias1 = new ManufacturerAlias(name:'feline', manufacturer:manCat).save()
 			def a1ias2 = new ManufacturerAlias(name:'canine', manufacturer:manDog).save()
-		
-		
-		// assertions without the local duplicate flag
-		assert manufacturerService.isValidAlias('kitty', manCat) : 'VALID: manufacturer alias is completely unique'
-		assert ! manufacturerService.isValidAlias('dog', manCat) : 'INVALID: manufacturer alias matches other manufacturer\'s name'
-		assert ! manufacturerService.isValidAlias('canine', manCat) : 'INVALID: manufacturer alias matches other manufacturer\'s alias'
-		assert ! manufacturerService.isValidAlias('cat', manCat) : 'INVALID: manufacturer alias matches the parent manufacturer\'s name'
-		assert ! manufacturerService.isValidAlias('feline', manCat) : 'INVALID: manufacturer alias matches the parent manufacturer\'s alias'
-		
-		// assertions with the local duplicate flag
-		assert manufacturerService.isValidAlias('kitty', manCat, true) : 'VALID: manufacturer alias is completely unique (local duplicates allowed)'
-		assert ! manufacturerService.isValidAlias('dog', manCat, true) : 'INVALID: manufacturer alias matches other manufacturer\'s name (local duplicates allowed)'
-		assert ! manufacturerService.isValidAlias('canine', manCat, true) : 'INVALID: manufacturer alias matches other manufacturer\'s alias (local duplicates allowed)'
-		assert ! manufacturerService.isValidAlias('cat', manCat, true) : 'INVALID: manufacturer alias matches the parent manufacturer\'s name (local duplicates allowed)'
-		assert manufacturerService.isValidAlias('feline', manCat, true) : 'VALID: manufacturer alias matches the parent manufacturer\'s alias (local duplicates allowed)'
-		
-		// test the manufacturer name parameter
-		assert manufacturerService.isValidAlias('cat', manCat, false, 'rat eater') : 'VALID: manufacturer alias matches own name, using different name'
-		
-		
+
+		expect:
+			// assertions without the local duplicate flag
+
+			// 'VALID: manufacturer alias is completely unique'
+			manufacturerService.isValidAlias('kitty', manCat)
+			// 'INVALID: manufacturer alias matches other manufacturer\'s name'
+			! manufacturerService.isValidAlias('dog', manCat)
+			// 'INVALID: manufacturer alias matches other manufacturer\'s alias'
+			! manufacturerService.isValidAlias('canine', manCat)
+			// 'INVALID: manufacturer alias matches the parent manufacturer\'s name'
+			! manufacturerService.isValidAlias('cat', manCat)
+			// 'INVALID: manufacturer alias matches the parent manufacturer\'s alias'
+			! manufacturerService.isValidAlias('feline', manCat)
+
+			// assertions with the local duplicate flag
+
+			// 'VALID: manufacturer alias is completely unique (local duplicates allowed)'
+			manufacturerService.isValidAlias('kitty', manCat, true)
+			// 'INVALID: manufacturer alias matches other manufacturer\'s name (local duplicates allowed)'
+			! manufacturerService.isValidAlias('dog', manCat, true)
+			// 'INVALID: manufacturer alias matches other manufacturer\'s alias (local duplicates allowed)'
+			! manufacturerService.isValidAlias('canine', manCat, true)
+			// 'INVALID: manufacturer alias matches the parent manufacturer\'s name (local duplicates allowed)'
+			! manufacturerService.isValidAlias('cat', manCat, true)
+			// 'VALID: manufacturer alias matches the parent manufacturer\'s alias (local duplicates allowed)'
+			manufacturerService.isValidAlias('feline', manCat, true)
+
+			// test the manufacturer name parameter
+
+			// 'VALID: manufacturer alias matches own name, using different name'
+			manufacturerService.isValidAlias('cat', manCat, false, 'rat eater')
+
 	}
 }
