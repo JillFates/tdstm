2c70603f009c3350b3eec5ac1ec1dc257536a6b3
Augusto Recordon <augusto.recordon@bairesdev.com>
2017-04-20 20:50:26 -0300

    TM-5656 Fixes conflict with the changeling file.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -164,5 +164,7 @@ databaseChangeLog = {
 	include file: "20170307-change-license-installation-column-spelling.groovy"
 	include file: "20170308-nullout-orphaned-asset-comments-by-workflow-transition.groovy"
 	include file: "20170227-delete-create-or-rename-permissions-for-new-naming-scheme.groovy"
+	include file: "20170413-add-new-permission-to-clone-assets-feature.groovy"
+	include file: "20170417-remove-orphan-move-event-asset-comments-user-preferences-references.groovy"
 	include file: "20170403-remove-racks-and-rooms-for-VMs.groovy"
 }
