73fdd6272c2a4a4d99c1e6f766756e9b55cbb46c
Arthur Albuquerque <aalbuquerque@transitionaldata.com>
2017-05-29 14:03:46 -0300

    TM-6385 Edit templates on the grid

--- web-app/tds/web-app/app-js/modules/fieldSettings/components/grid/field-settings-grid.component.ts
+++ web-app/tds/web-app/app-js/modules/fieldSettings/components/grid/field-settings-grid.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FieldSettingsModel } from '../../model/field-settings.model';
+import { FieldSettingsService } from '../../service/field-settings.service';
 import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
 import { process, State } from '@progress/kendo-data-query';
 
 @Component({
 	moduleId: module.id,
 	selector: 'field-settings-grid',
-	templateUrl: '../tds/web-app/app-js/modules/fieldSettings/components/grid/field-settings-grid.component.html'
+	templateUrl: '../tds/web-app/app-js/modules/fieldSettings/components/grid/field-settings-grid.component.html',
+	styles: [`
+		.float-right { float: right;}
+	`]
 })
 export class FieldSettingsGridComponent implements OnInit {
 	@Input('data') fieldsSettings: FieldSettingsModel[];
@@ -16,7 +20,7 @@ export class FieldSettingsGridComponent implements OnInit {
 	private gridData: GridDataResult;
 	private state: State = {
 		skip: 0,
-		take: 10,
+		take: 25,
 		sort: [{
 			dir: 'asc',
 			field: 'order'
@@ -30,6 +34,10 @@ export class FieldSettingsGridComponent implements OnInit {
 			logic: 'and'
 		}
 	};
+	private isEditing = false;
+	private isFilterDisabled = false;
+
+	constructor(private fieldService: FieldSettingsService) { }
 
 	ngOnInit(): void {
 		this.gridData = process(this.fieldsSettings, this.state);
@@ -50,6 +58,16 @@ export class FieldSettingsGridComponent implements OnInit {
 	}
 
 	protected onEdit(): void {
-		console.log('editig');
+		this.isEditing = true;
+	}
+
+	protected onSaveAll(): void {
+		this.isEditing = false;
+	}
+
+	protected onCancel(): void {
+		this.isEditing = false;
+		// TODO: need a way to clone this content at init so we can revert the changes
+		this.gridData = process(this.fieldsSettings, this.state);
 	}
 }
