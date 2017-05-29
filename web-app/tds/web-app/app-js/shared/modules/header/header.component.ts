30459a16bfa32b06eebadaf851cd2d20d72e3fe4
Jorge Morayta <jorge.morayta@bairesdev.com>
2017-05-29 12:07:07 -0500

    TM-6385 Status display added to inform user any activity when using the App, DANGER, INFO, WARNING, SUCCESS

--- web-app/tds/web-app/app-js/shared/modules/header/header.component.ts
+++ web-app/tds/web-app/app-js/shared/modules/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject, AfterViewInit} from '@angular/core';
 import {StateService} from '@uirouter/angular';
+import {NotifierService} from '../../services/notifier.service';
+import {AlertType} from '../../model/alert.model';
 // import {TranslateService} from 'ng2-translate';
 
 declare var jQuery: any;
@@ -19,15 +21,21 @@ export class HeaderComponent implements AfterViewInit {
 	};
 	taskCount: Number;
 
-	constructor(@Inject('taskCount') tasks, state: StateService // translate: TranslateService
-	) {
+	constructor(@Inject('taskCount') tasks, state: StateService, notifierService: NotifierService /* translate: TranslateService*/) {
 		tasks.subscribe(
 			(result) => {
 				this.taskCount = result.count;
 				// Please refer to https://kb.transitionmanager.com/display/TMENG/FE%3A+Workaround #3
 				jQuery('#todoCountProjectId').html(this.taskCount);
 			},
-			(err) => console.log(err));
+			(err) => {
+				notifierService.broadcast({
+					name: AlertType.WARNING,
+					message: err
+				});
+
+				console.log(err);
+			});
 		this.state = state;
 		// this language will be used as a fallback when a translation isn't found in the current language
 		// translate.setDefaultLang('en');
