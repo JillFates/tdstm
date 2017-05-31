0379df9e4b8f3db39bc125c5d71374898dc659b5
Arthur Albuquerque <aalbuquerque@transitionaldata.com>
2017-05-31 14:08:36 -0300

    TM-6385 replace hot to cold observable

--- web-app/tds/web-app/app-js/shared/services/notifier.service.ts
+++ web-app/tds/web-app/app-js/shared/services/notifier.service.ts
@@ -2,34 +2,23 @@
  * Notifier is special class that allow us to bind the events calls across the TDS App
  */
 
-import {Injectable} from '@angular/core';
-import {Observable, Observer} from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Rx';
 
-import {AlertModel} from '../model/alert.model';
+import { AlertModel } from '../model/alert.model';
 
 @Injectable()
 export class NotifierService {
 
-	private alertModel: Array<AlertModel>;
-
-	observable: Observable<any>;
-	observer: Observer<any>;
-
-	constructor() {
-		this.observable = Observable.create((observer: Observer<any>) => {
-			this.observer = observer;
-		}).share();
-	}
+	private subject = new Subject();
 
 	broadcast(event) {
-		if (this.observer) {
-			this.observer.next(event);
-		}
+		this.subject.next(event);
 	}
 
 	on(eventName, callback) {
-		let subscription = this.observable.filter((event) => {
-			return event.name === eventName;
+		let subscription = this.subject.filter((value: any, index) => {
+			return value.name === eventName;
 		}).subscribe(callback);
 		return () => {
 			subscription.remove(subscription);
