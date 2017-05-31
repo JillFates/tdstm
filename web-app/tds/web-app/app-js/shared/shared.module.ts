65c61bfd99a6639d5f9f65afae9ddd38ff0a7803
Arthur Albuquerque <aalbuquerque@transitionaldata.com>
2017-05-31 14:08:36 -0300

    TM-6385 replace hot to cold observable

--- web-app/tds/web-app/app-js/shared/shared.module.ts
+++ web-app/tds/web-app/app-js/shared/shared.module.ts
@@ -1,28 +1,28 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PopupModule } from '@progress/kendo-angular-popup';
 // import { TranslateModule } from 'ng2-translate';
-import {HttpServiceProvider} from '../shared/providers/http-interceptor.provider';
+import { HttpServiceProvider } from '../shared/providers/http-interceptor.provider';
 // Shared Services
-import {AuthService} from '../shared/services/auth.service';
-import {UserService} from '../shared/services/user.service';
-import {NotifierService} from '../shared/services/notifier.service';
-import {ComponentCreatorService} from '../shared/services/component-creator.service';
-import {UIDialogService, UIActiveDialogService} from '../shared/services/ui-dialog.service';
+import { AuthService } from '../shared/services/auth.service';
+import { UserService } from '../shared/services/user.service';
+import { NotifierService } from '../shared/services/notifier.service';
+import { ComponentCreatorService } from '../shared/services/component-creator.service';
+import { UIDialogService, UIActiveDialogService } from '../shared/services/ui-dialog.service';
 // Shared Directives
-import {UILoaderDirective} from '../shared/directives/ui-loader.directive';
-import {UIToastDirective} from '../shared/directives/ui-toast.directive';
-import {UIDialogDirective} from '../shared/directives/ui-dialog.directive';
+import { UILoaderDirective } from '../shared/directives/ui-loader.directive';
+import { UIToastDirective } from '../shared/directives/ui-toast.directive';
+import { UIDialogDirective } from '../shared/directives/ui-dialog.directive';
 // Shared Pipes
-import {UIBooleanPipe} from './pipes/types/ui-boolean.pipe';
-import {TranslatePipe} from './pipes/translate.pipe';
+import { UIBooleanPipe } from './pipes/types/ui-boolean.pipe';
+import { TranslatePipe } from './pipes/translate.pipe';
 // Shared Components
-import {PopupLegendsComponent} from './modules/popup/legends/popup-legends.component';
-import {HeaderComponent} from './modules/header/header.component';
-import {FormlyInputHorizontal} from './modules/formly/formly-input-horizontal.component';
-import {RichTextEditorComponent} from './modules/rich-text-editor/rich-text-editor.component';
+import { PopupLegendsComponent } from './modules/popup/legends/popup-legends.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { FormlyInputHorizontal } from './modules/formly/formly-input-horizontal.component';
+import { RichTextEditorComponent } from './modules/rich-text-editor/rich-text-editor.component';
 // Dictionaries
-import {en_DICTIONARY} from './i18n/en.dictionary';
+import { en_DICTIONARY } from './i18n/en.dictionary';
 
 @NgModule({
 	imports: [
@@ -49,7 +49,7 @@ import {en_DICTIONARY} from './i18n/en.dictionary';
 		ComponentCreatorService,
 		UIDialogService,
 		UIActiveDialogService,
-		{provide: 'localizedDictionary', useValue: en_DICTIONARY}
+		{ provide: 'localizedDictionary', useValue: en_DICTIONARY }
 	],
 	exports: [UILoaderDirective,
 		UIToastDirective,
@@ -64,4 +64,5 @@ import {en_DICTIONARY} from './i18n/en.dictionary';
 	]
 })
 export class SharedModule {
+	constructor(private notifier: NotifierService) { }
 }
