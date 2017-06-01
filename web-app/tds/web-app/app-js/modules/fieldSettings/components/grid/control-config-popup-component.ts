d4590b18310a4876c84f5ba355ceb7a6a08e6e7f
dontiveros <dontiveros@tdsi.com>
2017-06-01 12:08:06 -0500

    TM-6385: epic field settings * Progress on Select Control Configuration component

--- web-app/tds/web-app/app-js/modules/fieldSettings/components/grid/control-config-popup-component.ts
+++ web-app/tds/web-app/app-js/modules/fieldSettings/components/grid/control-config-popup-component.ts
@@ -1,7 +1,8 @@
 /**
  * Created by David Ontiveros on 5/31/2017.
  */
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
+import { SortableComponent } from '@progress/kendo-angular-sortable';
 
 @Component({
 	moduleId: module.id,
@@ -12,9 +13,50 @@ import {Component, Input} from '@angular/core';
 export class ControlConfigPopupComponent {
 
 	@Input() controlType: string;
+	@Input() field: any;
+	@ViewChild('kendoSortableInstance') kendoSortableInstance: SortableComponent;
+
+	public items: string[] = [];
+	public newItem = '';
 	private show = false;
+
+	private load(): void {
+		if (this.field.option) {
+			this.items = this.field.option.slice();  // make a copy to work with.
+		}
+	}
+
+	public onAdd(): void {
+		if (this.searchItem(this.newItem, false) === null) {
+			this.items.push(this.newItem);
+			this.newItem = '';
+		}
+	}
+
+	public onRemove(item: string): void {
+		this.searchItem(item, true);
+	}
+
+	public onSave(): void {
+		this.field.option = this.items;
+		this.onToggle();
+	}
+
+	private searchItem(item: string, remove: boolean): string {
+		for (let i = this.items.length - 1; i >= 0; i--) {
+			if (this.items[i] === item) {
+				if (remove) {
+					this.items.splice(i, 1);
+					return null;
+				}
+				return this.items[i];
+			}
+		}
+		return null;
+	}
 
 	public onToggle(): void {
 		this.show = !this.show;
+		this.show ? this.load() : this.items = [];
 	}
 }
