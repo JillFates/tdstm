e9f4f71ebbf06ca026baba6878b1578021127eeb
John Martin <jmartin@transitionaldata.com>
2017-06-28 03:55:26 -0400

    TM-6617 Field Settings: Updated some of the logic that was referencing the Project.CUSTOM_FIELD_COUNT field that is no longer used

--- grails-app/controllers/ProjectController.groovy
+++ grails-app/controllers/ProjectController.groovy
@@ -113,7 +113,7 @@ class ProjectController implements ControllerMethods {
 		}
 		session.setAttribute('setImage', imageId)
 		boolean isDeleteable = securityService.hasPermission(Permission.ProjectDelete) && !project.isDefaultProject()
-		
+
 		[projectInstance: project, projectPartners: partyRelationshipService.getProjectPartners(project),
 		 projectManagers: projectService.getProjectManagers(project), projectLogoForProject: projectLogo,
 		 isDeleteable: isDeleteable]
@@ -593,7 +593,7 @@ class ProjectController implements ControllerMethods {
 		fieldMap.customs = projectService.getCustoms()
 		render fieldMap as JSON
 	}
-	
+
 	/**
 	 * Initialising importance for a given entity type.
 	 *@param : entityType type of entity.
@@ -680,22 +680,6 @@ class ProjectController implements ControllerMethods {
 	}
 
 	/**
-	 * Projects customFieldsShown
-	 * @param : custom count.
-	 * @render string 'success'.
-	 */
-	@HasPermission(Permission.ProjectFieldSettingsEdit)
-	def updateProjectCustomShown() {
-		Project project = securityService.userCurrentProject
-		project.customFieldsShown = NumberUtils.toInt(request.JSON.customCount,Project.CUSTOM_FIELD_COUNT)
-		if(!project.validate() || !project.save(flush:true)){
-			def etext = "Project customs unable to Update "+GormUtil.allErrorsString( project )
-			log.error( etext )
-		}
-		render "success"
-	}
-
-	/**
 	 * Used to select project time zone
 	 * @param timezone default timezone selected
 	 * @render time zone view
@@ -713,7 +697,7 @@ class ProjectController implements ControllerMethods {
 	def showImportanceFields() {
 		render( view: "showImportance", model: [])
 	}
-	
+
 	@HasPermission(Permission.ProjectFieldSettingsEdit)
 	def editImportanceFields() {
 		render( view: "editImportance", model: [])
