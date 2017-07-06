87abcb104460c6f3647a9a4ff4099a8236f736c9
dontiveros <dontiveros@tdsi.com>
2017-07-06 09:03:33 -0500

    TM-6657 Custom Fields order should not have a negative value - comments and function rename

--- web-app/tds/web-app/app-js/modules/fieldSettings/components/grid/field-settings-grid.component.ts
+++ web-app/tds/web-app/app-js/modules/fieldSettings/components/grid/field-settings-grid.component.ts
@@ -62,6 +62,8 @@ export class FieldSettingsGridComponent implements OnInit {
 	];
 	private availableyFieldType = ['All', 'Custom Fields', 'Standard Fields'];
 
+	static readonly ORDER_MIN_VALUE = 0;
+
 	constructor(private loaderService: UILoaderService) { }
 
 	ngOnInit(): void {
@@ -222,12 +224,12 @@ export class FieldSettingsGridComponent implements OnInit {
 
 	/**
 	 * Function used by html view to prevent user input negative values on the order number input.
-	 * - Validates input value is not negative (not less than 0).
+	 * - Validates input value is not negative (not less than 0) and always a number
 	 * @param element HTML element.
 	 * @param field custom field.
 	 */
-	public validateOrder(element, field): void {
-		if ( (+element.value === 0 ) || element.value < 0) {
+	public validateOrderValue(element, field): void {
+		if ( (+element.value === 0 ) || element.value < FieldSettingsGridComponent.ORDER_MIN_VALUE) {
 			element.value = field.order;
 		} else {
 			field.order = element.value;
