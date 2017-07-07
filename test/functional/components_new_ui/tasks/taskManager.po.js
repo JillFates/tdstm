e48c6d2861cebf035419c01a0455f1166aff9a9b
Daniel Ojcius <dojcius@transitionaldata.com>
2017-07-07 07:33:32 -0300

    Created Task Manager Functional Tests for check page elements

--- test/functional/components_new_ui/tasks/taskManager.po.js
+++ test/functional/components_new_ui/tasks/taskManager.po.js
@@ -1,5 +1,169 @@
 'use strict';
+
 var TaskManager = function(){
-  this.titleh =  $('h1');
+
+	//menu text to determine Event
+	this.projEventUser = $('#nav-project-name');
+	//this.eventUserTxt = projEventUser.getText().substr(projEventUser.getText().indexOf(":") + 1);
+	//Layout elements
+	this.headerTitle =  $('h1');
+	this.controlRowElements = $$('#controlRowId');
+	this.controlRow	= $('#controlRowId');
+	this.eventLb = null; // TODO Edit after FE solve the label issue
+	this.moveEventDD = $('#moveEventId');
+  this.moveEventDDOptions = $$('#moveEventId option');
+  this.moveEventDDSelected = $('#moveEventId option:checked');
+	this.breadCrumb = $('[class="breadcrumb"]');
+	this.justRemainingCB = $('#justRemainingCB');
+	this.justRemainingLb = $('[for="justRemainingCB"]');
+	this.justMyTasksCB = $('#justMyTasksCB');
+	this.justMyTasksLb = $('[for="justMyTasksCB"]');
+	this.viewUnpublishedCB = $('#viewUnpublishedCB');
+	this.viewUnpublishedLb = $('[for="viewUnpublishedCB"]');
+	this.viewTaskGraphBt = $('#viewtaskgraph_button_graph');
+	this.viewTaskGraphBtLb = $('#viewtaskgraph_text_graph');
+	this.viewTaskGraphBtImg = this.viewTaskGraphBt.$('[ng-show="icon != \'\'"]');
+	this.viewTimeLineBt = $('#viewtimeline_button_timeline');
+	this.viewTimeLineBtLb = $('#viewtimeline_text_timeline');
+	this.viewTimeLineBtImg = this.viewTimeLineBt.$('[ng-show="icon != \'\'"]');
+	this.timeRefreshBt = $('[class="col-xs-4 item-wrapper refresh-button"]');
+	this.selectTimedBarDD = $('#selectTimedBarId');
+  this.selectTimedBarDDOptions = $$('#selectTimedBarId option');
+  this.selectTimedBarDDSelected = $('#selectTimedBarId option:checked');
+	// Action Buttons
+  this.tasksLb = null; // TODO Edit after FE solve the label issue
+  this.createTaskBt = $('#createtask_button_createTask');
+	this.createTaskBtLb = $('#createtask_text_createTask');
+	this.bulkEditBt = $('#bulkedit_button_bulkEdit');
+	this.bulkEditBtLb = $('#bulkedit_text_bulkEdit');
+	this.clearFiltersBt = $('#clearfilters_button_clearFilters');
+	this.clearFiltersBtLb = $('#clearfilters_text_clearFilters');
+	this.tableToggleBt = $('[class="ui-jqgrid-titlebar-close HeaderButton"]');
+	// Table Header
+	this.actionTCol = $('#taskListIdGrid_act');
+	this.actionTColLb = $('#jqgh_act');
+	this.taskTCol = $('#taskListIdGrid_taskNumber');
+	this.taskTColLb = $('#jqgh_taskNumber');
+	this.taskTColFlt = $('#gs_taskNumber');
+	this.descriptionTCol = $('#taskListIdGrid_comment');
+	this.descriptionTColLb = $('#jqgh_comment');
+	this.descriptionTColFlt = $('#gs_comment');
+	this.updatedTCol = $('#taskListIdGrid_updated');
+	this.updatedTColLb = $('#jqgh_updated');
+	this.dueDateTCol = $('#taskListIdGrid_dueDate');
+	this.dueDateTColLb = $('#jqgh_dueDate');
+	this.dueDateTColFlt = $('#gs_dueDate');
+	this.statusTCol = $('#taskListIdGrid_status');
+	this.statusTColLb = $('#jqgh_status');
+	this.statusTColFlt = $('#gs_status');
+	this.sucTCol = $('#taskListIdGrid_suc');
+	this.sucTColLb = $('#jqgh_suc');
+	this.scoreTCol = $('#taskListIdGrid_score	');
+	this.scoreTColLb = $('#jqgh_score');
+	this.customColumnTCol = $('#taskListIdGrid_custom'); // TODO Check and obtain custom columns for validate them
+	this.customColumnTColLb  = $('#jqgh_custom');// TODO Check and obtain custom columns for validate them
+	this.customColumnTColFlt = $('#gs_custom'); // TODO Check and obtain custom columns for validate them
+//Table Footer
+	this.tableRefreshBt = $('#taskListId');
+
+	
+	TaskManager.prototype = {};
+	
+	TaskManager.prototype.existElement = function(elem) {
+	  var that = this;
+	  return browser.wait(function () {
+	    return that.elem.isPresent().then(function (valor) {
+	      return valor;
+	    });
+	  }).then(function () {
+	    return true;
+	  });
+	};
+	
+	TaskManager.prototype.isDisplayedElement = function(elem) {
+	  var that = this;
+	  return browser.wait(function () {
+	    return that.elem.isDisplayed().then(function (value) {
+	      return value;
+	    });
+	  }).then(function () {
+	    return true;
+	  });
+	};
+
+	TaskManager.prototype.clickElement = function(elem) {
+	  var that = this;
+	  return browser.wait(function () {
+	    return elem.click().then(function (value) {
+	      return value;
+	    });
+	  }).then(function () {
+	    return true;
+	  });
+	};
+	
+/* traidas de Cookbook	
+  this.createRecipeBtn = $('#generateTask[ng-click="createRecipe()"]');
+  this.createModal = $('[class="modal fade in"]');
+  this.modalTitleCss = this.createModal.$('h3');
+  this.modalTabs = this.createModal.$$('.nav-tabs li');
+  this.modalButtons = this.createModal.$$('.modal-footer button');
+  this.nameField = this.createModal.$('#inputName');
+  this.nameLabel = this.createModal.$('[for="inputName"]');
+  this.descriptionField = this.createModal.$('#textareaDescription');
+  this.descriptionLabel = this.createModal.$('[for="textareaDescription"]');
+  this.contextLabel = this.createModal.$('[for="contextSelector2"]');
+  this.contextField = this.createModal.$('#contextSelector2');
+  this.contextOptions = this.createModal.$$('#contextSelector2 option');
+  this.contextOptionSelected = this.createModal.$('#contextSelector2 option:checked');
+
+  this.pageTabs = $$('#mainTabset li');
+  //EditorTab
+  this.editEditorBtn = $('[ng-click="showEditPopup()"]');
+  this.saveWipBtn = $('[ng-click="saveWIP()"]');
+  this.undoBtn =$('[ng-click="cancelChanges()"]');
+
+  this.releaseBtn = $('[ng-click="releaseVersion()"]');
+  this.discardWipBtn = $('[ng-click="discardWIP()"]');
+  
+  this.diffBtn = $('[ng-click="diff()"]');
+
+  this.checkSyntaxBtn = $('[ng-click="validateSyntax()"]');
+  this.syntaxErrorTab = $('[class="syntaxErrors ng-scope"]');
+  this.checkSyntaxErrorTitle = this.syntaxErrorTab.$('[ng-bind="error.reason"]');
+  this.checkSyntaxErrorDetails = this.syntaxErrorTab.$('[ng-bind-html="secureHTML(error.detail)"]');
+  this.loadingIndicator = $('loading-indicator [ng-show="isLoading"]');
+  //Editor Modal
+  this.editorModal = $('[class="modal fade code-editor-modal in"]');
+  this.editorTextAreaX = this.editorModal.$('[ng-model="modal.sourceCode"]');
+  this.editorTextArea = $$('[ng-model="modal.sourceCode"] .CodeMirror-code pre');
+  this.modalCloseBtn = $('[ng-click="storeCode()"]');
+
+  //Task Generation tab
+  this.eventLabel = $('[for="eventSelect"]');
+  this.eventOptions = $$('#eventSelect option');
+  this.eventSelected = $('#eventSelect option:checked');
+
+  this.setAsDefaultLink = $('#setDefaultContext');
+  this.clearDefaultLink = $('#clearDefaultContext');
+
+  this.autoPublishTaskLabel = $('[for="autoPublishTasks"]');
+  this.autoPublishTaskCheck = $('#autoPublishTasks');
+
+  this.generateUsingWipLabel = $('[for="generateUsingWIP"]');
+  this.generateUsingWipCheckBox = $('#generateUsingWIP');
+
+  this.generateTasksBtn = $('[ng-click="tasks.generateTask(this)"]');
+  this.errorModal = $('#errorModal');
+  this.errorModalText = this.errorModal.$('#errorModalText');
+  this.errorModalCloseBtn = this.errorModal.$('[class="btn btn-default"]');
+  
+  this.taskGenerationTabs = $('#taskGenerationTabs');
+  //task Generation Tabs
+  this.sumaryList = $('[ui-view="taskBatchCompleted"] [class="summaryList ng-scope"]');
+*/
+
 };
+
+	
 module.exports = TaskManager;
--- test/functional/components_new_ui/tasks/taskManager.spec.js
+++ test/functional/components_new_ui/tasks/taskManager.spec.js
@@ -1,18 +1,528 @@
 'use strict';
 var  Menu = require('../menu/menu.po.js');
 var TaskManager = require('./taskManager.po.js');
-describe('Task Manager page', function(){
 
+describe('Task Manager', function(){
+	
+	var taskManager = new TaskManager();
+	
   it('should load My tasks page after select Tasks - Task Manager', function(){
     var menu = new Menu();
-    menu.goToTasks('taskManager');
-    expect(menu.getCurrentUrl('/tdstm/assetEntity/listTasks?initSession=true')).toEqual(process.env.BASE_URL+'/tdstm/assetEntity/listTasks?initSession=true');
+  	var tasks = browser.driver.findElement(by.className('menu-parent-tasks'));
+  	tasks.click();
+  	var items = tasks.findElement(by.className('menu-item-expand'));
+  	if (items.isDisplayed()){
+  		items.findElement(by.className('menu-parent-tasks-task-manager')).click();
+  	}
+  	expect(menu.getCurrentUrl()).toContain(process.env.BASE_URL+'/tdstm/assetEntity/listTasks?initSession=true');
+
   });
+  
   it('should have "Task Manager" as title',function(){
-    var taskManagerPage = new TaskManager();
-    expect(taskManagerPage.titleh.getText()).toEqual('Task Manager');
+    expect(taskManager.headerTitle.getText()).toEqual('Task Manager');
   });
-  describe('Bulk edit button',function(){
-  	//should only expand task with Ready and  Started
+  
+  xit('should have "Task > Task Manager" as breadcrumb',function(){
+    expect(taskManager.breadCrumb.getText()).toEqual('Task > Task Manager');
+    // TODO check the ::before element and split the expects for each part of breadcrumb
   });
+  
+  describe('Action Bar',function(){
+  	
+	  describe('Control Row',function(){
+	    
+	  	xit('should have "Event" as title', function(){
+	      expect(taskManager.controlRow.getText()).toEqual('Event');
+	    });
+	    
+	  	describe('Event Dropdown', function() {
+	      
+		  	it('should have Event dropdown', function(){
+		      expect(taskManager.moveEventDD.isPresent()).toBe(true);
+		    });	
+		  	
+	      xit('should have 10 options',function () {
+	        expect(taskManager.moveEventDDOptions.count()).toEqual(10);
+	      });	
+	      
+	      it('should have "All" as option', function() {
+	        expect(taskManager.moveEventDDOptions.get(0).getText()).toEqual(' All'); // TODO Trim the text
+	      });
+	      
+	      xit('should have'+taskManager.eventUserTxt+' displayed by default',function () {
+	        expect(taskManager.moveEventDDSelected.getText()).toEqual(taskManager.eventUserTxt);
+	      });
+	      
+	      it('should can select "All" as option',function () {
+	      	taskManager.moveEventDDOptions.get(0).click();
+	        expect(taskManager.moveEventDDSelected.getText()).toEqual(' All'); // TODO Trim the text
+	      });
+
+	    }); //Event Dropdown
+	    
+	  	it('should have Just Remaining checkbox', function(){
+	      expect(taskManager.justRemainingCB.isPresent()).toBe(true);
+	    });
+	  	
+	  	it('should have "Just Remaining" checkbox label', function(){
+	      expect(taskManager.justRemainingLb.isPresent()).toBe(true);
+	      expect(taskManager.justRemainingLb.getText()).toEqual('Just Remaining');
+	    });  	
+	  	
+	  	it('should have Just Mine checkbox ', function(){
+	      expect(taskManager.justMyTasksCB.isPresent()).toBe(true);
+	    });
+	
+	  	it('should have "Just Mine" checkbox label', function(){
+	      expect(taskManager.justMyTasksLb.isPresent()).toBe(true);
+	      expect(taskManager.justMyTasksLb.getText()).toEqual('Just Mine');
+	    });
+	  	
+	  	it('should have View Unpublished checkbox', function(){
+	      expect(taskManager.viewUnpublishedCB.isPresent()).toBe(true);
+	    });
+	
+	  	it('should have "View Unpublished" checkbox label', function(){
+	      expect(taskManager.viewUnpublishedLb.isPresent()).toBe(true);
+	      expect(taskManager.viewUnpublishedLb.getText()).toEqual('View Unpublished');
+	    });
+	 
+	  });  // end of control Row
+	  
+	  describe('Time Line Functions',function(){ 
+	  	
+	  	it('should have View Task Graph button', function(){
+	      expect(taskManager.viewTaskGraphBt.isPresent()).toBe(true);
+	    });
+	  	
+	  	it('should have View Task Graph button image', function(){
+	      expect(taskManager.viewTaskGraphBtImg.isPresent()).toBe(true);
+	    });	
+	  	
+	  	it('should have "View Task Graph" button label', function(){
+	      expect(taskManager.viewTaskGraphBtLb.isPresent()).toBe(true);
+	      expect(taskManager.viewTaskGraphBtLb.getText()).toEqual('View Task Graph');
+	    });
+
+	  	it('should have View Timeline button', function(){
+	      expect(taskManager.viewTimeLineBt.isPresent()).toBe(true);
+	    });
+	  	
+	  	it('should have View Timeline button image', function(){
+	      expect(taskManager.viewTimeLineBtImg.isPresent()).toBe(true);
+	    });	
+	  	
+	  	it('should have "View Timeline" button label', function(){
+	      expect(taskManager.viewTimeLineBtLb.isPresent()).toBe(true);
+	      expect(taskManager.viewTimeLineBtLb.getText()).toEqual('View Timeline');
+	    });
+	  	
+	    describe('Time Refresh Options', function() {
+	      
+		  	it('should have refresh button', function(){
+		      expect(taskManager.timeRefreshBt.isPresent()).toBe(true);
+		    });	
+		  	
+				xit('should refresh the list after click', function(){
+					taskManager.clearFiltersBt.click();
+				// TODO evaluate refreshed list with default filters
+				});	
+				
+		  	it('should have Time Refresh dropdown', function(){
+		      expect(taskManager.selectTimedBarDD.isPresent()).toBe(true);
+		    });	
+		  	
+	      it('should have 6 option',function () {
+	        expect(taskManager.selectTimedBarDDOptions.count()).toEqual(6);
+	      });	
+	      
+	      it('should have "Manual" as option', function() {
+	        expect(taskManager.selectTimedBarDDOptions.get(0).getText()).toEqual('Manual');
+	      });
+	      
+	      xit('should have "Manual" displayed by default',function () {
+	        expect(taskManager.selectTimedBarDDSelected.getText()).toEqual('Manual');
+	      });
+	      
+	      it('should have "1 Min" as option', function() {
+	        expect(taskManager.selectTimedBarDDOptions.get(1).getText()).toEqual('1 Min');
+	      });
+
+	      it('should have "2 Min" as option', function() {
+	        expect(taskManager.selectTimedBarDDOptions.get(2).getText()).toEqual('2 Min');
+	      });
+
+	      it('should have "3 Min" as option', function() {
+	        expect(taskManager.selectTimedBarDDOptions.get(3).getText()).toEqual('3 Min');
+	      });
+	      
+	      it('should have "4 Min" as option', function() {
+	        expect(taskManager.selectTimedBarDDOptions.get(4).getText()).toEqual('4 Min');
+	      });
+	      
+	      it('should have "5 Min" as option', function() {
+	        expect(taskManager.selectTimedBarDDOptions.get(5).getText()).toEqual('5 Min');
+	      });	      
+
+	      it('should can select "2 Min" as timer',function () {
+	      	taskManager.selectTimedBarDDOptions.get(2).click();
+	        expect(taskManager.selectTimedBarDDSelected.getText()).toEqual('2 Min');
+	      });
+
+	    }); //Time Refresh Options
+	  	
+	  });  // end of Time Line Functions
+
+  });  // end of Action Bar
+ 
+  describe('Table',function(){
+  	
+  	xit('should have "Tasks:" as label', function(){
+      expect(taskManager.tasksLb.getText()).toEqual('Tasks:');
+    });
+  	
+  	describe('Action Buttons',function(){
+  		
+  		describe('Create Task Button',function(){
+		  	
+				it('should have Create Task button', function(){
+					expect(taskManager.createTaskBt.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Create Task" button label', function(){
+		      expect(taskManager.createTaskBtLb.isPresent()).toBe(true);
+		      expect(taskManager.createTaskBtLb.getText()).toEqual('Create Task');
+				});			  	
+				
+				xit('should open Create Task modal window after click', function(){
+					taskManager.createTaskBt.click();
+					// TODO evaluate create task modal window
+				});	
+					
+		  });	// end of Create Task Button
+	
+ 
+  		describe('Bulk Edit Button',function(){
+		  	
+				it('should have Bulk Edit button', function(){
+					expect(taskManager.bulkEditBt.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Bulk Edit" button label', function(){
+		      expect(taskManager.bulkEditBtLb.isPresent()).toBe(true);
+		      expect(taskManager.bulkEditBtLb.getText()).toEqual('Bulk Edit');
+				});			  	
+				
+				xit('should show status button bars after click', function(){
+					taskManager.bulkEditBt.click();
+				// TODO evaluate status button bars
+				});	
+				xit('should hide status button bars after click again', function(){
+					taskManager.bulkEditBt.click();
+				// TODO evaluate status button bars
+				});	
+				
+		  });	// end of Bulk Edit Button
+  
+  		describe('Clear Filters Button',function(){
+		  	
+				it('should have Clear Filters button', function(){
+					expect(taskManager.clearFiltersBt.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Clear Filters" button', function(){
+		      expect(taskManager.clearFiltersBtLb.isPresent()).toBe(true);
+		      expect(taskManager.clearFiltersBtLb.getText()).toEqual('Clear Filters');
+				});			  	
+				
+				xit('should refresh list with default filters after click', function(){
+					taskManager.clearFiltersBt.click();
+				// TODO evaluate refreshed list with default filters
+				});	
+					
+		  });	// end of Clear Filters Button
+  		
+  	});	// end of Action Buttons
+  	
+		describe('Table Toggle Button',function(){
+	  	
+			it('should have Table Toggle button', function(){
+				expect(taskManager.tableToggleBt.isPresent()).toBe(true);
+			});	
+
+			xit('should hide the table after click', function(){
+				taskManager.tableToggleBt.click();
+				// TODO evaluate table hidden
+			});	
+			
+			xit('should show the table after click again', function(){
+				taskManager.tableToggleBt.click();
+				// TODO evaluate show table again
+			});
+	  
+		});	// end of Table Header Button 	
+		
+		describe('Table Columns Header',function(){
+  		
+			describe('Action Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.actionTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Action" as name', function(){
+		      expect(taskManager.actionTColLb.isPresent()).toBe(true);
+		      expect(taskManager.actionTColLb.getText()).toEqual('Action');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.actionTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.actionTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+		  });	// end of Action Column			
+
+			describe('Task Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.taskTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Task" as name', function(){
+		      expect(taskManager.taskTColLb.isPresent()).toBe(true);
+		      expect(taskManager.taskTColLb.getText()).toEqual('Task');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.taskTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.taskTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+				it('should have column filter', function(){
+					expect(taskManager.taskTColFlt.isPresent()).toBe(true);
+				});	
+				
+				xit('should filter tasks by input text', function(){
+					taskManager.taskTColFlt.setText('16600');
+					// TODO evaluate column filtering
+				});				
+		  
+			});	// end of Task Column						
+
+			describe('Description Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.descriptionTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Description" as name', function(){
+		      expect(taskManager.descriptionTColLb.isPresent()).toBe(true);
+		      expect(taskManager.descriptionTColLb.getText()).toEqual('Description');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.descriptionTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.descriptionTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+				it('should have column filter', function(){
+					expect(taskManager.descriptionTColFlt.isPresent()).toBe(true);
+				});				
+
+				xit('should filter by description input text', function(){
+					taskManager.descriptionTColFlt.setText('Test Task');
+					// TODO evaluate column filtering
+				});
+				
+			});	// end of Task Column	
+
+			describe('Updated Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.updatedTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Updated" as name', function(){
+		      expect(taskManager.updatedTColLb.isPresent()).toBe(true);
+		      expect(taskManager.updatedTColLb.getText()).toEqual('Updated');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.updatedTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.updatedTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+		  });	// end of Updated Column	
+			
+			describe('Due Date Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.dueDateTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Due Date" as name', function(){
+		      expect(taskManager.dueDateTColLb.isPresent()).toBe(true);
+		      expect(taskManager.dueDateTColLb.getText()).toEqual('Due Date');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.dueDateTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.dueDateTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+				it('should have column filter', function(){
+					expect(taskManager.dueDateTColFlt.isPresent()).toBe(true);
+				});				
+
+				xit('should filter by due date input text', function(){
+					taskManager.dueDateTColFlt.setText('07/07/2017');
+					// TODO evaluate column filtering
+				});			  				
+		  
+			});	// end of Due Date Column	
+			
+			describe('Status Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.statusTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Status" as name', function(){
+		      expect(taskManager.statusTColLb.isPresent()).toBe(true);
+		      expect(taskManager.statusTColLb.getText()).toEqual('Status');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.statusTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.statusTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+				it('should have column filter', function(){
+					expect(taskManager.statusTColFlt.isPresent()).toBe(true);
+				});				
+				
+				xit('should filter by status input text', function(){
+					taskManager.statusTColFlt.setText('Ready');
+					// TODO evaluate column filtering
+				});			  
+		  
+			});	// end of Status Column				
+			
+			describe('Suc. Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.sucTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Suc." as name', function(){
+		      expect(taskManager.sucTColLb.isPresent()).toBe(true);
+		      expect(taskManager.sucTColLb.getText()).toEqual('Suc.');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.sucTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.sucTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+		  });	// end of Suc. Column	
+			
+			describe('Score Column',function(){
+		  	
+				it('should have header', function(){
+					expect(taskManager.scoreTCol.isPresent()).toBe(true);
+				});	
+				
+				it('should have "Score" as name', function(){
+		      expect(taskManager.scoreTColLb.isPresent()).toBe(true);
+		      expect(taskManager.scoreTColLb.getText()).toEqual('Score');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.scoreTCol.click();
+					// TODO evaluate column ascending
+				});	
+
+				xit('should sort column descending after click again', function(){
+					taskManager.scoreTCol.click();
+					// TODO evaluate column descending
+				});	
+				
+		  });	// end of Score Column	
+			
+		
+			describe('Custom Column',function(){
+		  	
+				xit('should have header', function(){
+					expect(taskManager.customTCol.isPresent()).toBe(true);
+				});	
+				
+				xit('should have selected name', function(){
+		      expect(taskManager.customTColLb.isPresent()).toBe(true);
+		      expect(taskManager.customTColLb.getText()).toEqual('Score');
+				});			  	
+				
+				xit('should sort column ascending after click', function(){
+					taskManager.customTCol.click();
+					// TODO evaluate column ascending
+				});	
+	
+				xit('should sort column descending after click again', function(){
+					taskManager.customTCol.click();
+					// TODO evaluate column descending
+				});	
+			
+			});	// end of Custom Column	
+		
+		});	// end of Table Columns Header   
+
+		describe('Refresh button',function(){
+			
+		  it('should have refresh button at bottom', function(){
+		    expect(taskManager.tableRefreshBt.isPresent()).toBe(true);
+		  });	
+		  
+			xit('should refresh the list after click', function(){
+				taskManager.tableRefreshBt.click();
+			// TODO evaluate refreshed list with default filters
+			});	
+			
+		});  // end of Refresh button
+
+  });  // end of Table
+ 
 });
