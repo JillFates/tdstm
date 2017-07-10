2f3414f3680d4215dbbec260ce140b93ecc57f44
dontiveros <dontiveros@tdsi.com>
2017-07-10 02:09:21 -0500

    Added util helper method to detect IE browsers corrected comment format.

--- web-app/tds/web-app/app-js/shared/utils/validation.utils.ts
+++ web-app/tds/web-app/app-js/shared/utils/validation.utils.ts
@@ -22,4 +22,22 @@ export class ValidationUtils {
 		}
 		return true;
 	}
+
+	/**
+	 * Detects if current browser is Internet Explorer, from version 10 to newer
+	 * Code based on --> https://codepen.io/gapcode/pen/vEJNZN
+	 * @returns {boolean}
+	 */
+	public static isIEBrowser(): boolean {
+		// Detect IE >= 10
+		let msie = window.navigator.userAgent.indexOf('MSIE ');
+		// Detect IE == 11
+		let trident = window.navigator.userAgent.indexOf('Trident/');
+		// Detect IE >= 12 (Edge)
+		let edge = window.navigator.userAgent.indexOf('Edge/');
+		if (msie > 0 || trident > 0 || edge > 0) {
+			return true;
+		}
+		return false;
+	}
 }
