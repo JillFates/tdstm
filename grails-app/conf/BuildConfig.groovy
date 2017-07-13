821e18bd6b76dd72fb9f5d5cbc54d5d9d032f451
Octavio Luna <oluna@tdsi.com>
2017-07-13 13:27:51 -0500

    TM-6665 comments clarification about the use of some plugins for cached resources

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,6 +68,10 @@ grails.project.dependency.resolution = {
 		runtime ':jquery:1.11.1'
 		runtime ':mail:1.0.7'
 		runtime ':resources:1.2.14' // TODO ':asset-pipeline:2.9.1'
+		/*
+		 TODO: oluna - the next plugins help to work with the resources and the Browsers Cache
+		 they may be removed after switching to asset-pipeline, I just leave it as a reference
+		*/
 		// runtime ':cached-resources:1.0'
 		// runtime ':cache-headers:1.1.7'
 
