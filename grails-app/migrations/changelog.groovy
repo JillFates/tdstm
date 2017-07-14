95e83faaf11201df567acfcc5aff1bb0c69686d6
ecantu <ecantu@tdsi.com>
2017-07-14 15:47:23 -0300

    TM-6718 Create migration script to delete all user preference for the various asset lists columns

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -170,4 +170,5 @@ databaseChangeLog = {
 	include file: "20170504-delete-room-merge-permission.groovy"
 	include file: "20170530-create-setting-table.groovy"
 	include file: "20170623-add-planMethodology-to-Ptoject.groovy"
+	include file: "20170714-remove-all-user-preference-for-the-various-asset-list-columns.groovy"
 }
