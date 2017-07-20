b827b8a98ab2f8ba1b83d997106107dcba53ac41
Octavio Luna <oluna@tdsi.com>
2017-07-20 07:24:30 -0500

    TM-6550 adding Row iteration for speed and comments

--- src/groovy/com/tdssrc/grails/SheetWrapper.groovy
+++ src/groovy/com/tdssrc/grails/SheetWrapper.groovy
@@ -3,6 +3,7 @@ package com.tdssrc.grails
 import org.apache.poi.ss.usermodel.BuiltinFormats
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Sheet
 
 /**
@@ -15,43 +16,103 @@ import org.apache.poi.ss.usermodel.Sheet
  * This class acts as a holder for different reusable information of the spreadsheet being build
  */
 class SheetWrapper {
+	// Excel Sheet where we are writting to
 	Sheet sheet
+
+	// Dictionary of cellstyles
 	Map<Object, CellStyle> styleMap = [:]
 
 	SheetWrapper(Sheet sheet){
 		this.sheet = sheet
 	}
 
-	void addCell(int columnIdx, int rowIdx, Object value, Integer type = null) {
-		def row = sheet.getRow(rowIdx)
+	/**
+	 * Retrieves or creates a Row Object in the Spreadsheet
+	 * @param rowIdx
+	 * @return Row object of the Excel Sheet
+	 */
+	Row getOrCreateRow(int rowIdx){
+		Row row = sheet.getRow(rowIdx)
 		if (!row) {
 			row = sheet.createRow(rowIdx)
 		}
-		Cell cell = row.getCell(columnIdx)
+		return row
+	}
+
+	/**
+	 * Retrieves or creates a Cell Object in the passed Row
+	 * @param row
+	 * @param colIdx
+	 * @return
+	 */
+	Cell getOrCreateCell(Row row, int colIdx) {
+		Cell cell = row.getCell(colIdx)
 		if (!cell) {
-			cell = row.createCell(columnIdx)
+			cell = row.createCell(colIdx)
 		}
-		if (type != null) {
-			cell.setCellType(type)
-			CellStyle style = getCellStyle(type)
-			cell.setCellStyle(style)
-		}
+		return cell
+	}
+
+	/**
+	 * Creates a Cell with the default Style, passing the Row Object, col position and value
+	 * @param row
+	 * @param colIdx
+	 * @param value
+	 */
+	void addCell(Row row, int colIdx, Object value) {
+		Cell cell = getOrCreateCell(row, colIdx)
 		WorkbookUtil.setCellValue(cell, value)
 	}
 
+	/**
+	 * Creates a Cell with the default Style, passing the row & col position and value
+	 * @param colIdx
+	 * @param rowIdx
+	 * @param value
+	 */
+	void addCell(int colIdx, int rowIdx, Object value) {
+		def row = getOrCreateRow(rowIdx)
+		addCell(row, colIdx, value)
+	}
+
+	/**
+	 * Creates a Cell, passing the Row Object, col position, value and style type
+	 * @param columnIdx
+	 * @param rowIdx
+	 * @param value
+	 * @param type
+	 */
+	void addCell(Row row, int colIdx, Object value, int type) {
+		Cell cell = getOrCreateCell(row, colIdx)
+		cell.setCellType(type)
+		CellStyle style = getCellStyle(type)
+		cell.setCellStyle(style)
+		WorkbookUtil.setCellValue(cell, value)
+	}
+
+	/**
+	 * Creates a Cell, passing the row & col position, value and style type
+	 * @param columnIdx
+	 * @param rowIdx
+	 * @param value
+	 * @param type
+	 */
+	void addCell(int colIdx, int rowIdx, Object value, int type) {
+		def row = getOrCreateRow(rowIdx)
+		addCell(row, colIdx, value, type)
+	}
+
+	/**
+	 * Creates or Return a CellStyle
+	 * @param type
+	 * @return
+	 */
 	CellStyle getCellStyle(int type){
 		CellStyle style = styleMap[type]
 
 		if( ! style) {
-			style = sheet.workbook.createCellStyle() // TODO <SL> Use createCellStyle()
-			if (type == Cell.CELL_TYPE_NUMERIC) { // This resolves to a Numeric no decimal spaces Value
-				String binFormat = BuiltinFormats.getBuiltinFormat(1) //this is "0" mask format
-				def df = sheet.workbook.createDataFormat().getFormat(binFormat)
-				style.setDataFormat(df)
-			} else if (type == Cell.CELL_TYPE_STRING) {
-				style.setDataFormat((short) BuiltinFormats.getBuiltinFormat("text"))
-			}
-
+			//Reusing createCellStyle from the WorkBookUtil
+			style = WorkbookUtil.createCellStyle(sheet, type)
 			styleMap[type] = style
 		}
 
