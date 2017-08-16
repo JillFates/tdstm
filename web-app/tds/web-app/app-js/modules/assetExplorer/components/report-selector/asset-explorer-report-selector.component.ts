3f914c8a61b0b059ea5b179c68c33cedf1d40155
Jorge Morayta <jmorayta@tdsi.com>
2017-08-16 17:39:07 -0500

    TM-6520 Adding Create Report and Permissions.

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/report-selector/asset-explorer-report-selector.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/report-selector/asset-explorer-report-selector.component.ts
@@ -4,6 +4,9 @@ import { DropDownListComponent } from '@progress/kendo-angular-dropdowns';
 import { StateService } from '@uirouter/angular';
 
 import { ReportGroupModel } from '../../model/report.model';
+import {ReportType} from '../../model/report.model';
+import {PermissionService} from '../../../../shared/services/permission.service';
+
 import { AssetExplorerStates } from '../../asset-explorer-routing.states';
 @Component({
 	moduleId: module.id,
@@ -16,13 +19,12 @@ import { AssetExplorerStates } from '../../asset-explorer-routing.states';
 })
 export class AssetExplorerReportSelectorComponent implements AfterViewInit {
 	@Input() open?= false;
-	@Input('create-new') createNewLink?= true;
 	@ViewChild('kendoDropDown') dropdown: DropDownListComponent;
 	private reports: ReportGroupModel[];
 	private data: ReportGroupModel[];
 	private search = '';
 
-	constructor( @Inject('reports') reports: Observable<ReportGroupModel[]>, private stateService: StateService) {
+	constructor( @Inject('reports') reports: Observable<ReportGroupModel[]>, private stateService: StateService, private permissionService: PermissionService) {
 		reports.subscribe((result) => {
 			this.data = result;
 			this.reports = result.slice();
@@ -54,9 +56,8 @@ export class AssetExplorerReportSelectorComponent implements AfterViewInit {
 
 	protected getFolderStyle(item: ReportGroupModel) {
 		return {
-			'fa-folder': item.items.length === 0,
-			'fa-folder-o': item.items.length !== 0 && !item.open,
-			'fa-folder-open-o': item.items.length !== 0 && item.open
+			'fa-star text-yellow': item.type === ReportType.FAVORITES,
+			'fa-folder': item.type !== ReportType.FAVORITES
 		};
 	}
 
@@ -64,7 +65,36 @@ export class AssetExplorerReportSelectorComponent implements AfterViewInit {
 		item.open = !item.open;
 	}
 
-	protected onCreateNew(): void {
-		this.stateService.go(AssetExplorerStates.REPORT_CREATE.name);
+	/**
+	 * Create a new Report
+	 * @param {ReportGroupModel} item
+	 */
+	protected onCreateNew(item: ReportGroupModel): void {
+		if (this.isCreateAvailable(item)) {
+			this.stateService.go(AssetExplorerStates.REPORT_CREATE.name,
+				{ system: item.type === ReportType.SYSTEM_REPORTS });
+		}
 	}
+
+	/**
+	 * Show/Hide the Create Button if not in All / Favorites / Recent
+	 * @returns {boolean}
+	 */
+	protected isCreateVisible(item: ReportGroupModel): boolean {
+		return (item.type !== ReportType.ALL &&
+			item.type !== ReportType.FAVORITES &&
+			item.type !== ReportType.RECENT) && this.isCreateAvailable(item);
+	}
+
+	/**
+	 * Disable the Create Report if the user does not have the proper permission
+	 * @returns {boolean}
+	 */
+	protected isCreateAvailable(item: ReportGroupModel): boolean {
+		return item.type === ReportType.SYSTEM_REPORTS ?
+			this.permissionService.hasPermission('AssetExplorerSystemCreate') :
+			this.permissionService.hasPermission('AssetExplorerCreate');
+	}
+
+
 }
