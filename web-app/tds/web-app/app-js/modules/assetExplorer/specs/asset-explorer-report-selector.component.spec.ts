c39d44027949d11b77e1760404eb745cfd20d9bd
Jorge Morayta <jmorayta@tdsi.com>
2017-08-23 15:49:13 -0500

    TM-6520 TM-6502 Fixing test cases and adding export result.

--- web-app/tds/web-app/app-js/modules/assetExplorer/specs/asset-explorer-report-selector.component.spec.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/specs/asset-explorer-report-selector.component.spec.ts
@@ -12,93 +12,12 @@ import { SharedModule } from '../../../shared/shared.module';
 import { DropDownListModule } from '@progress/kendo-angular-dropdowns';
 
 import { AssetExplorerReportSelectorComponent } from '../components/report-selector/asset-explorer-report-selector.component';
-import { ReportModel, ViewType } from '../model/report.model';
+import {AssetExplorerService} from '../service/asset-explorer.service';
 
 describe('AssetExplorerReportSelectorComponent:', () => {
 	let fixture: ComponentFixture<AssetExplorerReportSelectorComponent>;
 	let comp: AssetExplorerReportSelectorComponent;
 	let de: DebugElement;
-
-	let mockData: ReportModel[] = [
-		{
-			id: 1,
-			name: 'Finance Applications',
-			isOwner: false,
-			isShared: false,
-			isSystem: true,
-			schema: {
-				domains: ['APPLICATION', 'STORAGE'],
-				columns: [],
-				filters: [],
-				sort: {
-					domain: 'APPLICATION',
-					property: 'id',
-					order: 'a'
-				}
-			}
-		},
-		{
-			id: 2,
-			name: 'HR Applicattions',
-			isOwner: false,
-			isShared: false,
-			isSystem: true,
-			schema: {
-				domains: ['APPLICATION', 'DEVICE'],
-				columns: [],
-				filters: [],
-				sort: {
-					domain: 'APPLICATION',
-					property: 'id',
-					order: 'a'
-				}
-			}
-		}, {
-			id: 3,
-			name: 'Legal Applications',
-			isOwner: false,
-			isShared: false,
-			isSystem: true,
-			schema: {
-				domains: ['APPLICATION'],
-				columns: [],
-				filters: [],
-				sort: {
-					domain: 'APPLICATION',
-					property: 'id',
-					order: 'a'
-				}
-			}
-		}, {
-			id: 4,
-			name: 'My First Report',
-			isOwner: true,
-			isShared: false,
-			isSystem: false,
-			schema: {
-				domains: ['DEVICE', 'DATABASE', 'STORAGE'],
-				columns: [],
-				filters: [],
-				sort: {
-					domain: 'DEVICE',
-					property: 'id',
-					order: 'a'
-				}
-			}
-		}, {
-			id: 5,
-			name: 'My First Shared Report',
-			isOwner: true,
-			isShared: true,
-			isSystem: false
-		}, {
-			id: 6,
-			name: 'Another user awesome report',
-			isOwner: false,
-			isShared: true,
-			isSystem: false
-		}
-	];
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -117,45 +36,7 @@ describe('AssetExplorerReportSelectorComponent:', () => {
 			],
 			declarations: [AssetExplorerReportSelectorComponent],
 			providers: [
-				{
-					provide: 'reports', useValue: Observable.from(mockData)
-						.bufferCount(mockData.length)
-						.map((items: ReportModel[]) => {
-							return [
-								{
-									name: 'All',
-									items: items,
-									open: true,
-									type: ViewType.ALL
-								}, {
-									name: 'Recent',
-									items: [],
-									open: false,
-									type: ViewType.RECENT
-								}, {
-									name: 'Favorites',
-									items: items.filter(r => r['isFavorite']),
-									open: false,
-									type: ViewType.FAVORITES
-								}, {
-									name: 'My Views',
-									items: items.filter(r => r.isOwner),
-									open: false,
-									type: ViewType.MY_VIEWS
-								}, {
-									name: 'Shared Views',
-									items: items.filter(r => r.isShared),
-									open: false,
-									type: ViewType.SHARED_VIEWS
-								}, {
-									name: 'System Views',
-									items: items.filter(r => r.isSystem),
-									open: false,
-									type: ViewType.SYSTEM_VIEWS
-								}
-							];
-						})
-				}
+				AssetExplorerService
 			]
 		}).compileComponents();
 	}));
