28da687165fe5e0b064d65dcda0a5fa4247b86a3
Augusto Recordon <augusto.recordon@bairesdev.com>
2017-08-29 20:49:19 -0300

    TM-7142 Reverts previous commit

--- grails-app/controllers/CommonController.groovy
+++ grails-app/controllers/CommonController.groovy
@@ -124,7 +124,7 @@ class CommonController implements ControllerMethods {
 	def tmLinkableUrl() {
 		String errMsg
 		try {
-			def linkableUrl = request.getQueryString() - "linkableUrl="
+			def linkableUrl = params["linkableUrl"]
 			def isLinkableUrl = HtmlUtil.isMarkupURL(linkableUrl)
 			if(!isLinkableUrl){
 				errMsg = "The format of the linkable URL is invalid."
