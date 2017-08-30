338819e7f1bce46eb5452854ea33d091bcab7ad2
Diego Correa <diegomartincorrea@gmail.com>
2017-08-30 17:32:21 -0300

    TM-5161 Update TEAM Role Types with a migration database script.
    It takes RoleType with type == ‘TEAM’ and removes Suffix “Staff : ” using RoleType domain objects

---
+++ grails-app/migrations/20170830-remove-staff-prefix-for-team-descriptions.groovy
@@ -0,0 +1,43 @@
+import net.transitionmanager.domain.RoleType
+
+/**
+ * @author: Diego Correa
+ *
+ * Ticket TM-5161
+ *
+ * Remove the "Staff : " prefix from the Team descriptions and hide other types from the Role Type CRUD pages
+ *
+ *
+ */
+databaseChangeLog = {
+
+    changeSet(author: "dcorrea", id: "20170830 TM-5161") {
+        comment('This script removes "Staff : " prefix from TEAM type descriptions.')
+
+        grailsChange {
+            change {
+
+                updateRoleTypeDescriptions()
+            }
+        }
+
+    }
+}
+
+
+void updateRoleTypeDescriptions() {
+
+    List<RoleType> roleTypes = RoleType.where { type == RoleType.TEAM }.list()
+
+    roleTypes.each { RoleType roleType ->
+
+        def newDescription = roleType.toString().trim()
+        roleType.description = newDescription
+
+        if (roleType.validate()) {
+            roleType.save(flush: true)
+        } else {
+            throw new RuntimeException("Couldn't update role type: ${roleType.toString()} id: ${roleType.id}")
+        }
+    }
+}
