8a92f9cae356cdf7bcf789f91f42d15048998c56
Arthur Albuquerque <aalbuquerque@transitionaldata.com>
2017-09-04 10:17:07 -0300

    TM-6534 resize and column change order horizontaly

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/view-grid/asset-explorer-view-grid.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/view-grid/asset-explorer-view-grid.component.ts
@@ -10,8 +10,33 @@ export class AssetExplorerViewGridComponent {
 
 	@Input() model: ViewSpec;
 
+	mouseDown = false;
+	lastEvent: MouseEvent;
+	selectColumn: ViewColumn;
+
 	protected toggleProperty(column: ViewColumn, property: 'edit' | 'locked') {
 		column[property] = !column[property];
+	}
+
+	onMouseUp(): void {
+		this.mouseDown = false;
+		this.selectColumn = null;
+	}
+
+	onMouseDown(event: MouseEvent, column: ViewColumn): void {
+		this.mouseDown = true;
+		this.selectColumn = column;
+		this.lastEvent = event;
+	}
+
+	onMouseMove(event: MouseEvent): void {
+		if (this.mouseDown) {
+			let xValueChange = event.clientX - this.lastEvent.clientX;
+			let width = this.selectColumn.width + (xValueChange);
+			console.log(width, xValueChange);
+			this.selectColumn.width = width < 50 ? 50 : width;
+			this.lastEvent = event;
+		}
 	}
 
 }
