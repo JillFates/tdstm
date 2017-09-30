b63318ec8ca2d09ed5bd19edfb6b14e48c95a7c7
Diego Correa <diegomartincorrea@gmail.com>
2017-09-30 00:44:23 -0300

    TM-7212 Working with Script base and custom binding

---
+++ src/groovy/com/tdsops/etl/ETLProcessorBaseScript.groovy
@@ -0,0 +1,10 @@
+package com.tdsops.etl
+
+abstract class ETLProcessorBaseScript extends Script {
+
+    @Delegate @Lazy ETLProcessor etlProcessor = this.binding.etlProcessor
+
+    def methodMissing(String methodName, args) {
+        etlProcessor."${methodName}"(*args)
+    }
+}
