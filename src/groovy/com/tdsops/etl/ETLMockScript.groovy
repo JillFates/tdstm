1b727bda85c813d2c90457f1164b37f7486f010a
Diego Correa <diegomartincorrea@gmail.com>
2017-09-30 00:44:23 -0300

    TM-7212 Working with Script base and custom binding

---
+++ src/groovy/com/tdsops/etl/ETLMockScript.groovy
@@ -0,0 +1,57 @@
+package com.tdsops.etl
+
+abstract class ETLMockScript extends Script {
+
+    Integer currentRowPosition = 0
+    Integer currentColumnPosition = 0
+    String currentFieldValue
+
+    ETLMockScript domain(DomainAssets domain) {
+        getMetadata().domain = domain
+        getDebugConsole().info("Selected Domain: $domain")
+        this
+    }
+
+    ETLMockScript domain(String domain) {
+        getMetadata().domain = DomainAssets.values().find { it.name() == domain } ?: DomainAssets.External
+        getDebugConsole().info("Selected Domain: $domain")
+        this
+    }
+
+    ETLProcessor read(DataPart dataPart) {
+
+        if (dataPart == DataPart.labels) {
+            getDebugConsole().info("Reading labels")
+
+            getDatasource().get(currentRowPosition++).eachWithIndex { String columnName, Integer index ->
+                getMetadata().columns.names[columnName] = [ordinal: index]
+                getMetadata().columns.ordinals[index] = [name: columnName]
+            }
+        }
+        this
+    }
+
+    private DebugConsole getDebugConsole() {
+        if (!getBinding().hasVariable(DebugConsole.class.name)) {
+            getBinding().setVariable(DebugConsole.class.name, new DebugConsole(buffer: new StringBuffer()))
+        }
+        getBinding().getVariable(DebugConsole.class.name)
+    }
+
+    private Map getMetadata() {
+        if (!getBinding().hasVariable('metadata')) {
+            getBinding().setVariable('metadata',
+                    [domain: null, columns: [names: [:], ordinals: [:]], rows: []])
+        }
+        getBinding().getVariable('metadata')
+    }
+
+    private List<List<String>> getDatasource() {
+        if (!getBinding().hasVariable('datasource')) {
+            getBinding().setVariable('datasource', [])
+        }
+        getBinding().getVariable('datasource')
+    }
+
+
+}
