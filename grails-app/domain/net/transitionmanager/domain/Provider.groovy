7635bc4dd77e8434ca9bbc6869b9c51e43aae491
Augusto Recordon <augusto.recordon@bairesdev.com>
2017-10-03 14:48:07 -0300

    TM-7221 Add comment and description columns for Provider

--- grails-app/domain/net/transitionmanager/domain/Provider.groovy
+++ grails-app/domain/net/transitionmanager/domain/Provider.groovy
@@ -2,6 +2,8 @@ package net.transitionmanager.domain
 
 class Provider {
     String name
+    String description
+    String comment
 
     Date dateCreated
     Date lastUpdated
@@ -10,11 +12,15 @@ class Provider {
 
     static constraints = {
         name size:1..255, unique: 'project'
+        description size:0..255
+        comment size:0..65254
         lastUpdated nullable: true
     }
 
     static mapping = {
         name sqlType: 'VARCHAR(255)'
+        description sqlType: 'VARCHAR(255)'
+        comment sqlType: 'TEXT'
 
         sort 'name'
     }
