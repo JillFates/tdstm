5b951c241f412a3b3f276d2fa42add3bc5127867
Diego Correa <diegomartincorrea@gmail.com>
2017-10-04 21:52:50 -0300

    TM-7212 Updating MockETLController with latest version

--- grails-app/controllers/MockETLController.groovy
+++ grails-app/controllers/MockETLController.groovy
@@ -1,13 +1,11 @@
 import com.tdsops.etl.*
+import com.tdsops.tm.enums.domain.AssetClass
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import net.transitionmanager.controller.ControllerMethods
 import net.transitionmanager.domain.Project
-import org.codehaus.groovy.control.CompilerConfiguration
 import org.codehaus.groovy.control.ErrorCollector
 import org.codehaus.groovy.control.MultipleCompilationErrorsException
-import org.codehaus.groovy.control.customizers.ImportCustomizer
-import org.codehaus.groovy.control.customizers.SecureASTCustomizer
 
 @Secured('isAuthenticated()')
 class MockETLController implements ControllerMethods {
@@ -15,25 +13,19 @@ class MockETLController implements ControllerMethods {
 
     def customDomainService
 
-    def index() {
+    def index () {
 
         Project project = securityService.userCurrentProject
 
-        ETLDomainFieldsValidator domainAssetFieldsMapper = new ETLDomainFieldsValidator()
-
-//        List<Map<String, ?>> applicationFieldSpecs = customDomainService.allFieldSpecs(project,
-//                AssetClass.APPLICATION.name())[AssetClass.APPLICATION.name()]["fields"]
-//        List<Map<String, ?>> deviceFieldSpecs = customDomainService.allFieldSpecs(project,
-//                AssetClass.DEVICE.name())[AssetClass.DEVICE.name()]["fields"]
-//        List<Map<String, ?>> databaseFieldSpecs = customDomainService.allFieldSpecs(project,
-//                AssetClass.DATABASE.name())[AssetClass.DATABASE.name()]["fields"]
-//        List<Map<String, ?>> storageFieldSpecs = customDomainService.allFieldSpecs(project,
-//                AssetClass.STORAGE.name())[AssetClass.STORAGE.name()]["fields"]
-//
-//        domainAssetFieldsMapper.setFieldsSpecFor(AssetClass.APPLICATION, applicationFieldSpecs)
-//        domainAssetFieldsMapper.setFieldsSpecFor(AssetClass.DEVICE, deviceFieldSpecs)
-//        domainAssetFieldsMapper.setFieldsSpecFor(AssetClass.DATABASE, databaseFieldSpecs)
-//        domainAssetFieldsMapper.setFieldsSpecFor(AssetClass.STORAGE, storageFieldSpecs)
+        Map<ETLDomain, List<Map<String, ?>>> domainFieldsSpec = [:]
+        domainFieldsSpec[ETLDomain.Application] = customDomainService.allFieldSpecs(project,
+                AssetClass.APPLICATION.name())[AssetClass.APPLICATION.name()]["fields"]
+        domainFieldsSpec[ETLDomain.Device] = customDomainService.allFieldSpecs(project,
+                AssetClass.DEVICE.name())[AssetClass.DEVICE.name()]["fields"]
+        domainFieldsSpec[ETLDomain.Database] = customDomainService.allFieldSpecs(project,
+                AssetClass.DATABASE.name())[AssetClass.DATABASE.name()]["fields"]
+        domainFieldsSpec[ETLDomain.Storage] = customDomainService.allFieldSpecs(project,
+                AssetClass.STORAGE.name())[AssetClass.STORAGE.name()]["fields"]
 
 
         def mockData = params.mockData ? """${params.mockData}""" : """DEVICE ID,MODEL NAME,MANUFACTURER NAME,ENVIRONMENT
@@ -45,67 +37,59 @@ class MockETLController implements ControllerMethods {
 152266,2U Cable Management,Generic,Prod
 152275,ProLiant BL465c G7,HP,Dev"""
 
-        def data = []
+        List<List<String>> data = []
 
         mockData.trim().eachLine { line, count ->
-            data.addAll([line.split(',')])
+            data.addAll([line.split(',') as List])
         }
 
         def script = params.script ?: """console on
 domain Application
 read labels
 iterate {
-    extract 'MODEL NAME' transform lowercase load 'modelName'
-    extract 2 transform uppercase  load 'appName'
+    extract 0 load id
+    extract 'MODEL NAME' transform lowercase load Name
+    extract 2 transform uppercase  load description
+    load environment with 'Production'
 }
 """
 
-        ImportCustomizer customizer = new ImportCustomizer()
-        customizer.addStaticStars ETLDomain.class.name
-        customizer.addStaticStars DataPart.class.name
-        customizer.addStaticStars ConsoleStatus.class.name
-
-        SecureASTCustomizer secureASTCustomizer = new SecureASTCustomizer()
-//        secureASTCustomizer.closuresAllowed = false             // disallow closure creation
-        secureASTCustomizer.methodDefinitionAllowed = false     // disallow method definitions
-        secureASTCustomizer.importsWhitelist = []  // Empty withe list means forbid imports
-        secureASTCustomizer.starImportsWhitelist = []
-
-        CompilerConfiguration configuration = new CompilerConfiguration()
-        configuration.addCompilationCustomizers customizer, secureASTCustomizer
-
         DebugConsole console = new DebugConsole(buffer: new StringBuffer())
-        ETLProcessor etlProcessor = new ETLProcessor(dataSource: data, debugConsole: console)
 
-        Binding binding = new Binding([
-                etlProcessor: etlProcessor,
-                domain      : etlProcessor.&domain,
-                read        : etlProcessor.&read,
-                console     : etlProcessor.&console,
-                iterate     : etlProcessor.&iterate,
-                transform   : etlProcessor.&transform,
-                translate   : etlProcessor.&translate,
-                uppercase   : new StringTransformation(closure: { String value -> value.toUpperCase() }),
-                lowercase   : new StringTransformation(closure: { String value -> value.toLowerCase() }),
-                first       : new StringTransformation(closure: { String value -> value.size() > 0 ? value[0] : "" }),
-                blanks      : new StringTransformation(closure: { String value -> value.replaceAll(" ", "") })
+        ETLProcessor etlProcessor = new ETLProcessor(data, console, domainFieldsSpec)
+
+        ETLBinding binding = new ETLBinding(etlProcessor, [
+                uppercase: new StringTransformation(closure: { String value -> value.toUpperCase() }),
+                lowercase: new StringTransformation(closure: { String value -> value.toLowerCase() }),
+                first    : new StringTransformation(closure: { String value -> value.size() > 0 ? value[0] : "" }),
+                blanks   : new StringTransformation(closure: { String value -> value.replaceAll(" ", "") })
         ])
 
         ErrorCollector errorCollector
         String missingPropertyError
         Integer lineNumber
         try {
-            GroovyShell shell = new GroovyShell(this.class.classLoader, binding, configuration)
-            shell.evaluate(script, "ETLProcessor")
+            new GroovyShell(this.class.classLoader, binding, binding.configuration).evaluate(script, ETLProcessor.class.name)
 
         } catch (MultipleCompilationErrorsException cfe) {
             errorCollector = cfe.getErrorCollector()
         } catch (MissingPropertyException mpe) {
-            lineNumber = mpe.stackTrace.find { StackTraceElement ste -> ste.fileName == "ETLProcessor" }?.lineNumber
+            lineNumber = mpe.stackTrace.find { StackTraceElement ste -> ste.fileName == ETLProcessor.class.name }?.lineNumber
             missingPropertyError = mpe.getMessage()
+        } catch (ETLProcessorException pe) {
+            missingPropertyError = pe.getMessage()
         }
 
-        [mockData: mockData, script: script.trim(), etlProcessor: etlProcessor, errorCollector: errorCollector, lineNumber: lineNumber, missingPropertyError: missingPropertyError, logContent: console.content(), jsonResult: (etlProcessor?.results as JSON)?.toString(true)]
+        [
+                mockData            : mockData,
+                script              : script.trim(),
+                etlProcessor        : etlProcessor,
+                errorCollector      : errorCollector,
+                lineNumber          : lineNumber,
+                missingPropertyError: missingPropertyError,
+                logContent          : console.content(),
+                jsonResult          : (etlProcessor?.results as JSON)?.toString(true)
+        ]
     }
 
 
