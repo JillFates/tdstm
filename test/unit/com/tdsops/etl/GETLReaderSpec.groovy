e0afc1d1f2ae3c83e23e9ed67f8b326d293996cc
Diego Correa <diegomartincorrea@gmail.com>
2017-10-10 20:38:31 -0300

    TM-7212 TM-7211 Adding test cases for GETL CSV and Excel files readers

--- test/unit/com/tdsops/etl/GETLReaderSpec.groovy
+++ test/unit/com/tdsops/etl/GETLReaderSpec.groovy
@@ -3,11 +3,17 @@ package com.tdsops.etl
 import getl.csv.CSVConnection
 import getl.csv.CSVDataset
 import getl.data.Field
+import getl.excel.ExcelConnection
+import getl.excel.ExcelDataset
 import spock.lang.Specification
 
 class GETLReaderSpec extends Specification {
 
-    void 'test can read csv file defining fields' () {
+    /**
+     *
+     * CSV tests
+     */
+    void 'test can read a csv file defining fields' () {
 
         given:
             String fileName = "applications.csv"
@@ -15,7 +21,7 @@ class GETLReaderSpec extends Specification {
 
         and:
             CSVConnection csvCon = new CSVConnection(config: "csv", path: pathName)
-            CSVDataset csvFile = new CSVDataset(connection: csvCon, fileName: fileName, header: true)
+            CSVDataset csvFile = new CSVDataset(connection: csvCon, fileName: fileName)
 
         when: 'CSVDataset has defined field'
             csvFile.field << new Field(name: 'Id', type: Field.Type.BIGINT)
@@ -25,9 +31,37 @@ class GETLReaderSpec extends Specification {
 
         then: 'It can read rows count'
             csvFile.readRowCount() == 3
+
+        and: 'First row results contains fields values'
+            csvFile.rows()[0].Id == 114054
+            csvFile.rows()[0].Name == "BlackBerry Enterprise Server"
+            csvFile.rows()[0].Description == "Email sync to Blackberry handhelds"
+            csvFile.rows()[0].Environment == "Production"
     }
 
-    void 'test can read csv file without defining fields' () {
+    void 'test can read a csv file without defining fields' () {
+
+        given:
+            String fileName = "applications.csv"
+            String pathName = "test/unit/com/tdsops/etl/resources"
+
+        and:
+            CSVConnection csvCon = new CSVConnection(config: "csv", path: pathName)
+
+        when: 'CSVDataset can read column names without fields definition'
+            CSVDataset csvFile = new CSVDataset(connection: csvCon, fileName: fileName, header: true)
+
+        then: 'It can read total amount of rows'
+            csvFile.readRowCount() == 3
+
+        and: 'First row results contains fields values'
+            csvFile.rows()[0].id == 114054
+            csvFile.rows()[0].name == "BlackBerry Enterprise Server"
+            csvFile.rows()[0].description == "Email sync to Blackberry handhelds"
+            csvFile.rows()[0].environment == "Production"
+    }
+
+    void 'test can read a csv and iterate fields' () {
 
         given:
             String fileName = "applications.csv"
@@ -40,7 +74,58 @@ class GETLReaderSpec extends Specification {
             CSVDataset csvFile = new CSVDataset(connection: csvCon, fileName: fileName, header: true)
 
         then: 'It can read rows count'
-            csvFile.readRowCount() == 3
+            csvFile.rows().collectEntries() == []
+    }
+
+    /**
+     *
+     * Excel tests
+     *
+     */
+
+    void 'test can read an excel file defining fields' () {
+
+        given:
+            String fileName = "applications.xlsx"
+            String pathName = "test/unit/com/tdsops/etl/resources"
+
+        and:
+            ExcelConnection excelConnection = new ExcelConnection(path: pathName, fileName: fileName)
+
+            ExcelDataset excelDataset = new ExcelDataset(
+                    connection: excelConnection,
+                    listName: "applications",
+                    header: true,
+                    showWarnings: true,
+                    manualSchema: true)
+
+        when: 'CSVDataset has defined field'
+            excelDataset.field << new Field(name: 'Id', type: Field.Type.BIGINT)
+            excelDataset.field << new Field(name: 'Name', type: Field.Type.STRING)
+            excelDataset.field << new Field(name: 'Description', type: Field.Type.STRING)
+            excelDataset.field << new Field(name: 'Environment', type: Field.Type.STRING)
+
+        then: 'It can read rows count'
+            excelDataset.rows().size() == 3
+    }
+
+    void 'test can read an excel file without defining fields' () {
+
+        given:
+            String fileName = "applications.xlsx"
+            String pathName = "test/unit/com/tdsops/etl/resources"
+
+        and:
+            ExcelConnection excelConnection = new ExcelConnection(path: pathName, fileName: fileName)
+
+        when:
+            ExcelDataset excelDataset = new ExcelDataset(
+                    connection: excelConnection,
+                    listName: "applications",
+                    showWarnings: true)
+
+        then: 'It can read rows count'
+            excelDataset.rows().size() == 3
     }
 
 }
