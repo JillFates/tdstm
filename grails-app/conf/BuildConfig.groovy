ef6cc20be7afe72c9bfea4fecc51311337a900b3
Diego Correa <diegomartincorrea@gmail.com>
2017-10-11 09:09:08 -0300

    TM-7212 Working with CSV connection and reading files from file system

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,6 +38,7 @@ grails.project.dependency.resolution = {
 		// CSV Parser - https://github.com/xlson/groovycsv
 		runtime 'com.xlson.groovycsv:groovycsv:1.2'
 
+		// GETL Groovy ETL - https://github.com/ascrus/getl/
 		runtime 'net.sourceforge.getl:getl:1.2.05'
 
 		// test 'com.canoo.webtest:webtest:3.0'
