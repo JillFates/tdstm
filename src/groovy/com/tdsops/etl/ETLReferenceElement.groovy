4a81fdee16b2c10525c6441535e5d2ff8ea48646
Diego Correa <diegomartincorrea@gmail.com>
2017-10-16 17:39:24 -0300

    TM-7726 Loading references using AssetEntity domain queries

--- src/groovy/com/tdsops/etl/ETLReferenceElement.groovy
+++ src/groovy/com/tdsops/etl/ETLReferenceElement.groovy
@@ -1,4 +1,29 @@
 package com.tdsops.etl
 
 class ETLReferenceElement {
+
+    ETLProcessor processor
+    List<String> fields
+
+    ETLReferenceElement (ETLProcessor processor, List<?> fields) {
+        this.processor = processor
+        this.fields = fields
+    }
+    /**
+     *
+     *
+     *
+     * @param values
+     * @return
+     */
+    ETLReferenceElement with (Object... values) {
+        List assets = AssetClassQueryHelper.where(processor.selectedDomain, fields, values as List)
+
+        if (assets.size() == 1) {
+            processor.currentRow.instance = assets.first()
+        } else if (assets.size() > 1) {
+            ETLProcessorException.methodMissing(fields)
+        }
+        this
+    }
 }
