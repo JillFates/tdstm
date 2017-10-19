cbd7eace05f8eab5fa742ecf4c206a6e8ac9684a
dontiveros <dontiveros@tdsi.com>
2017-10-19 01:19:04 -0500

    TM-7765 - Implement new Manual Asset Import view

--- web-app/tds/web-app/app-js/modules/importAssets/components/manual-import/manual-import.component.ts
+++ web-app/tds/web-app/app-js/modules/importAssets/components/manual-import/manual-import.component.ts
@@ -1,5 +1,4 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
 import {ImportAssetsService} from '../../service/import-assets.service';
 
 @Component({
@@ -12,9 +11,17 @@ export class ManualImportComponent implements OnInit {
 	private dataScriptOptions = [];
 	private selectedManualOption: any;
 	private selectedScriptOption: any;
+	private fetchResult: any;
+	private fetchInProcess = false;
+	private transformResult: any;
+	private transformInProcess = false;
+	private importResult: any;
+	private importInProcess = false;
+	private fileContent: any;
+	private viewDataType: string;
+	private HARD_CODED_DELAY = 1000;
 
-	constructor( private importAssetsService: ImportAssetsService) {
-	}
+	constructor( private importAssetsService: ImportAssetsService) { }
 
 	ngOnInit(): void {
 		this.importAssetsService.getManualOptions().subscribe( (result) => {
@@ -25,17 +32,44 @@ export class ManualImportComponent implements OnInit {
 		});
 	}
 
-	private fetchResult: any;
 	private onFetch(): void {
+		this.fetchInProcess = true;
+		this.fetchResult = null;
+		this.transformResult = null;
+		this.selectedScriptOption = null;
 		this.importAssetsService.postFetch(this.selectedManualOption).subscribe( (result) => {
-			this.fetchResult = result;
+			setTimeout(() => { this.fetchResult = result; this.fetchInProcess = false; }, this.HARD_CODED_DELAY);
 		} );
 	}
 
-	private fileContent: any;
+	private onTransform(): void {
+		this.transformInProcess = true;
+		this.transformResult = null;
+		this.importAssetsService.postTransform(this.selectedScriptOption).subscribe( (result) => {
+			setTimeout(() => { this.transformResult = result; this.transformInProcess = false; }, this.HARD_CODED_DELAY);
+		} );
+	}
+
+	private onImport(): void {
+		this.importInProcess = true;
+		this.importResult = null;
+		this.importAssetsService.postImport(this.transformResult.outputFilename).subscribe( (result) => {
+			setTimeout(() => { this.importResult = result; this.importInProcess = false; }, this.HARD_CODED_DELAY);
+		} );
+	}
+
+	private getFileContentValue(): string {
+		if (this.fileContent) {
+			return JSON.stringify(this.fileContent);
+		} else {
+			return '';
+		}
+	}
+
 	private onViewData(type: string): void {
 		this.fileContent = null;
-		if (type === 'FETCH') {
+		this.viewDataType = type;
+		if (this.viewDataType === 'FETCH') {
 			this.importAssetsService.getFileContent(this.fetchResult.filename, this.fetchResult.extension).subscribe((result) => {
 				this.fileContent = result;
 			});
@@ -46,21 +80,9 @@ export class ManualImportComponent implements OnInit {
 		}
 	}
 
-	private transformResult: any;
-	private onTransform(): void {
-		this.importAssetsService.postTransform(this.selectedScriptOption).subscribe( (result) => {
-			this.transformResult = result;
-		} );
-	}
-
-	private importResult: any;
-	private onImport(): void {
-		this.importAssetsService.postImport(this.transformResult.outputFilename).subscribe( (result) => {
-			this.importResult = result;
-		} );
-	}
-
 	private onClearTextArea(): void {
 		this.fileContent = null;
+		this.viewDataType = null;
 	}
+
 }
