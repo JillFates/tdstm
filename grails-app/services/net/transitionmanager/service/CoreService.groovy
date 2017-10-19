1734dc4403a693d3824106318f6e80cefd079b85
John Martin <jmartin@transitionaldata.com>
2017-10-19 16:52:58 -0400

    TM-7807 Created new FileSystemService class

--- grails-app/services/net/transitionmanager/service/CoreService.groovy
+++ grails-app/services/net/transitionmanager/service/CoreService.groovy
@@ -12,6 +12,11 @@ class CoreService implements ServiceMethods {
 
 	static transactional = false
 
+	// The application property setting to use to determine where temporary files will reside
+	// TODO - Refactor the tempDirProperty to not be tied to graph
+	static final String tempDirProperty = 'graph.tmpDir'
+
+
 	GrailsApplication grailsApplication
 
 	/**
@@ -87,10 +92,9 @@ class CoreService implements ServiceMethods {
 	 * should use for writing files to.
 	 */
 	String getAppTempDirectory() {
-		String propName = 'graph.tmpDir'
-		String tmpDir = getConfigSetting(propName)
+		String tmpDir = getConfigSetting(tempDirProperty)
 		if (! tmpDir) {
-			throw new GrailsConfigurationException("The application temp directory configuration setting was not found ($propName)")
+			throw new GrailsConfigurationException("The application temp directory configuration setting was not found ($tempDirProperty)")
 		}
 		if (tmpDir[-1] == '/') {
 			tmpDir = tmpDir[0..-2]
