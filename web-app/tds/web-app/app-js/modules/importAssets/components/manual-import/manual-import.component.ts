080a69452b04e69ca3c207cae1239f54558804c6
dontiveros <dontiveros@tdsi.com>
2017-10-20 13:43:20 -0500

    TM-7765 - Implement new Manual Asset Import view

--- web-app/tds/web-app/app-js/modules/importAssets/components/manual-import/manual-import.component.ts
+++ web-app/tds/web-app/app-js/modules/importAssets/components/manual-import/manual-import.component.ts
@@ -1,5 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ImportAssetsService} from '../../service/import-assets.service';
+import {NotifierService} from '../../../../shared/services/notifier.service';
+import {AlertType} from '../../../../shared/model/alert.model';
 
 @Component({
 	selector: 'manual-import',
@@ -21,7 +23,7 @@ export class ManualImportComponent implements OnInit {
 	private viewDataType: string;
 	private HARD_CODED_DELAY = 1000;
 
-	constructor( private importAssetsService: ImportAssetsService) { }
+	constructor( private importAssetsService: ImportAssetsService, private notifier: NotifierService) { }
 
 	ngOnInit(): void {
 		this.importAssetsService.getManualOptions().subscribe( (result) => {
@@ -35,23 +37,43 @@ export class ManualImportComponent implements OnInit {
 		this.fetchInProcess = true;
 		this.fetchResult = null;
 		this.transformResult = null;
-		this.selectedScriptOption = null;
+		// this.selectedScriptOption = null;
 		this.importAssetsService.postFetch(this.selectedActionOption).subscribe( (result) => {
 			setTimeout(() => {
 				console.log(result);
 				this.fetchResult = result;
+				if (result.status === 'error') {
+					this.notifier.broadcast({
+						name: AlertType.DANGER,
+						message: result.errors[0]
+					});
+				}
 				this.fetchInProcess = false;
 			}, this.HARD_CODED_DELAY );
 		} );
 	}
 
+	private onActionScriptChange(event: any): void {
+		let matchedScript = this.dataScriptOptions.find( script => script.id === event.defaultDataScriptId );
+		if (matchedScript) {
+			this.selectedScriptOption = matchedScript;
+		}
+	}
+
 	private onTransform(): void {
 		this.transformInProcess = true;
 		this.transformResult = null;
+		this.importResult = null;
 		this.importAssetsService.postTransform(this.selectedScriptOption, this.fetchResult.filename).subscribe( (result) => {
 			setTimeout(() => {
 				console.log(result);
 				this.transformResult = result;
+				if (result.status === 'error') {
+					this.notifier.broadcast({
+						name: AlertType.DANGER,
+						message: result.errors[0]
+					});
+				}
 				this.transformInProcess = false;
 				}, this.HARD_CODED_DELAY);
 		} );
