fa1d1ddb9333438168ae7fd8e0749f454c83a5b7
dontiveros <dontiveros@tdsi.com>
2017-10-25 14:55:44 -0500

    TM-6622 Create a migration script to remove all legacy fields and tables no longer used for Field Settings

--- grails-app/migrations/20151020-set-custom-fields-to-hidden.groovy
+++ grails-app/migrations/20151020-set-custom-fields-to-hidden.groovy
@@ -18,7 +18,8 @@ databaseChangeLog = {
 				def phases = ValidationType.getListAsMap().keySet()
 				def attrValues
 
-				entityTypes.each { entityType ->
+				// TM-6622 - Below code has been commented because FieldImportance table and domain class no longer exists.
+				/*entityTypes.each { entityType ->
 					def fieldImportance = FieldImportance.findByProjectAndEntityType(defaultProject, entityType)
 					def configMap = JSON.parse(fieldImportance.config)
 					(1..Project.CUSTOM_FIELD_COUNT).each { i ->
@@ -31,7 +32,7 @@ databaseChangeLog = {
 
 					fieldImportance.config = (configMap as JSON).toString()
 					fieldImportance.save(flush: true)
-				}
+				}*/
 			}
 		}
 
