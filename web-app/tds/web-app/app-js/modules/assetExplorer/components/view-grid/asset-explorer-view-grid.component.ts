a8369407bb8061d2e67ebf344da21b436a5a3042
Arthur Albuquerque <aalbuquerque@transitionaldata.com>
2017-10-26 16:46:49 -0200

    TM-7693 Grey out cell which belongs to other asset

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/view-grid/asset-explorer-view-grid.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/view-grid/asset-explorer-view-grid.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angu
 
 import { ViewSpec, ViewColumn, VIEW_COLUMN_MIN_WIDTH } from '../../model/view-spec.model';
 import { State } from '@progress/kendo-data-query';
-import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
+import { GridDataResult, DataStateChangeEvent, RowClassArgs } from '@progress/kendo-angular-grid';
 import { PreferenceService } from '../../../../shared/services/preference.service';
 
 @Component({
@@ -22,6 +22,12 @@ import { PreferenceService } from '../../../../shared/services/preference.servic
 	}
 	.grid-message label{
 		font-weight: normal;
+	}
+	.application .dev, .application .dat, .application .sto,
+	.device .app, .device .dat, .device .sto,
+	.database .dev, .database .app, .database .sto,
+	.storage .dev, .storage .dat, .storage .app {
+		background-color:#f4f4f4;
 	}
 	`],
 	encapsulation: ViewEncapsulation.None
@@ -46,6 +52,18 @@ export class AssetExplorerViewGridComponent {
 
 	constructor(private userPref: PreferenceService) {
 		this.state.take = +this.userPref.preferences['assetListSize'] || 25;
+	}
+
+	rowCallbackClass(context: RowClassArgs) {
+		let obj = {};
+		obj[context.dataItem.common_assetClass.toLowerCase()] = true;
+		return obj;
+	}
+
+	cellCallbackClass(domain: string) {
+		let obj = {};
+		obj[domain.toLowerCase().substr(0, 3)] = true;
+		return obj;
 	}
 
 	onClearFilters(): void {
