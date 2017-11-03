1cad0bda8fc42003e51e9cf7c635c37f1cc29d58
Sidar LC <sidarlc@Sidars-MacBook-Pro.local>
2017-11-03 08:53:28 -0600

    TM-7851: Investigate and Fix the Apache Camel issues (implement batch processing)

--- grails-app/routes/net/transitionmanager/routes/AwsSqsRoute.groovy
+++ grails-app/routes/net/transitionmanager/routes/AwsSqsRoute.groovy
@@ -1,15 +1,21 @@
 package net.transitionmanager.routes
 
+import com.tdsops.camel.ArrayListAggregationStrategy
 import com.tdsops.common.security.spring.CamelHostnameIdentifier
 import org.apache.camel.builder.RouteBuilder
 import net.transitionmanager.service.AwsService
 import groovy.util.logging.Slf4j
+import org.apache.camel.processor.aggregate.AggregationStrategy
+import org.springframework.context.annotation.Bean
 
 /**
  * Route to handle inbound messages coming from AWS SQS service for the TM Response Queue
  */
 @Slf4j(value='logger')
 class AwsSqsRoute extends RouteBuilder {
+	//private static final long BATCH_TIME_OUT = 5000L;
+	//private static final int MAX_RECORDS = 10;
+
 	AwsService awsService
 	CamelHostnameIdentifier camelHostnameIdentifier
 
@@ -22,11 +28,21 @@ class AwsSqsRoute extends RouteBuilder {
 			.filter {
 				it.in.body.contains(camelHostnameIdentifier.hostnameIdentifierDigest)
 			}
-			.log('AwsSqsRoute received a message and forwarded it')
-			.to('bean:routingService?method=processMessage')
+			//.convertBodyTo(JSON)
+			.aggregate(constant(true), batchAggregationStrategy())
+			.completionFromBatchConsumer()
+			//.completionSize(MAX_RECORDS)
+			//.completionTimeout(BATCH_TIME_OUT)
+			.to('bean:routingService?method=processMessages')
+			.end()
 		} else {
 			logger.info '**** AwsSqsRoute initialization was skipped'
 		}
+	}
+
+	@Bean
+	AggregationStrategy batchAggregationStrategy() {
+		return new ArrayListAggregationStrategy();
 	}
 
 }
