5470dd8e9e2303d98cc39b1a9add034beaea35cc
Daniel Ojcius <dojcius@transitionaldata.com>
2017-12-15 17:15:48 -0300

    Task Manager section modified

--- test/e2e/src/test/groovy/specs/TaskManager/TaskManagerSpec.groovy
+++ test/e2e/src/test/groovy/specs/TaskManager/TaskManagerSpec.groovy
@@ -1,118 +1,121 @@
 package specs.TaskManager
 import geb.spock.GebReportingSpec
-import pages.Cookbook.CookbookPage
-import pages.Cookbook.CreateRecipePage
-import javafx.scene.control.Alert
+import pages.TaskManager.TaskDetailsPage
+import pages.TaskManager.TaskEditionPage
 import pages.TaskManager.TaskManagerPage
-import pages.TaskManager.CreateTaskPage
+import pages.TaskManager.TaskCreationPage
 import pages.common.LoginPage
 import pages.Dashboards.UserDashboardPage
 import spock.lang.Stepwise
-import spock.util.Exceptions
 
 @Stepwise
 class TaskManagerSpec extends GebReportingSpec{
 
+    def testKey
+    static testCount
     //Define the names of the tasks you will Create and Edit
-    def  taskName = "01 Testing QA Automation Task Manager Run"
-    def  taskNameEdit = "QA Automation Edited"
+    static taskName = "ZZ Task For E2E Automation"
+    static taskNameEdit = "ZZ Task For E2E Automation Edited"
 
     def setupSpec() {
+        testCount = 0
         given:
         to LoginPage
-
         when:
         username = System.properties['tm.creds.username'] ?: "e2e_test_user"
         password = System.properties['tm.creds.password'] ?: "e2e_password"
         println "setupSpec(): Login as user: e2e_test_user"
         submitButton.click()
-
         then:
         at UserDashboardPage
-
     }
 
+    def setup() {
+        testCount++
+    }
+
+    def cleanup() {
+        String sCount = String.format("%03d", testCount)
+        println "cleanup(): ${testKey} #${sCount} ${specificationContext.currentIteration.name} "
+    }
 
     def "open Task Manager"() {
+        testKey = "TM-XXXX"
         given:
         at UserDashboardPage
-
         when:
         waitFor { taskMenu.click() }
         taskManagerMenuItem.click()
-
         then:
         at TaskManagerPage
-
     }
 
 
     def "open Create Task Pop Up"() {
-
+        testKey = "TM-XXXX"
+        given:
+        at TaskManagerPage
         when:
         createTaskBtLb.click()
-
         then:
-        at CreateTaskPage
-        ctModalTitle.text() == "Create Task"
-
-
+        at TaskCreationPage
     }
 
     def "Create Task"() {
-
+        testKey = "TM-XXXX"
+        given:
+        at TaskCreationPage
         when:
-
         ctModalNameTA = taskName
-        waitFor { ctModalSaveBt.click() }
-
-
-
+        waitFor { ctModalSaveBtn.click() }
         then:
-
         at TaskManagerPage
-
     }
 
 
     def "Open Edit Task"() {
-
+        testKey = "TM-XXXX"
+        given:
+        at TaskManagerPage
         when:
         waitFor {descriptionTColFlt.click()}
         descriptionTColFlt = taskName
         waitFor{firstElementDesc.text() == taskName}
         waitFor{firstElementTaskTbl.click()}
-
         then:
-        at CreateTaskPage
-        ctModalTitle.text() == "Edit Task"
-
+        at TaskEditionPage
     }
 
 
     def "Edit Task"() {
-
+        testKey = "TM-XXXX"
+        given:
+        at TaskEditionPage
         when:
-        ctModalNameTA = taskNameEdit
-        waitFor { ctModalSaveBt.click() }
-
+        waitFor {etModalNameTA == taskName}
+        etModalNameTA = taskNameEdit
+        waitFor { etModalSaveBtn.click() }
         then:
-        waitFor{ctModalEdited == taskNameEdit}
-        at CreateTaskPage
-
+        waitFor{etModalEdited == taskNameEdit}
+        at TaskDetailsPage
     }
 
+    def "Task Details"() {
+        testKey = "TM-XXXX"
+        when:
+        at TaskDetailsPage
+        then:
+        waitFor{tdModalTaskName.text() == taskNameEdit}
+    }
 
     def "Delete Task"() {
-
+        testKey = "TM-XXXX"
+        at TaskDetailsPage
         when:
-        withConfirm(true){waitFor { deletebtn.click() }}
-
+        withConfirm(true){waitFor {tdModalDeleteBtn.click() }}
         then:
         at TaskManagerPage
-
     }
-
 
 }
 
