0662a1f6e891c2556457e88f8ee0265ca2cbe4e0
ecantu <ecantu@tdsi.com>
2017-12-27 17:17:16 -0300

    TM-7806 Do not create TBD room or rack when importing VM or Virtual servers

--- grails-app/services/net/transitionmanager/service/DeviceService.groovy
+++ grails-app/services/net/transitionmanager/service/DeviceService.groovy
@@ -69,12 +69,19 @@ class DeviceService implements ServiceMethods {
 			return "Unable to create rack $location/$roomName/$rackName (${isSource ? 'Source' : 'Target'})"
 		}
 
-		if (isSource) {
-			asset.rackSource = rack
-		}
-		else {
-			asset.rackTarget = rack
-		}
+    /* @See TM-7806
+       If an asset is of type 'VM', 'Virtual' or 'Blade', this should not be tracked at the rack level,
+       since this are not physical devices. */
+    String assetType = asset.assetType.toUpperCase()
+    if (!(assetType == 'VM' || assetType == 'VIRTUAL' || assetType == 'BLADE')) {
+       if (isSource) {
+          asset.rackSource = rack
+       }
+       else {
+          asset.rackTarget = rack
+       }
+    }
+
 
 		logger.debug 'assignDeviceToLocationRoomRack() END {} {}/{}/{}', asset, asset.sourceLocation,
 				asset.sourceRoom, asset.sourceRack
