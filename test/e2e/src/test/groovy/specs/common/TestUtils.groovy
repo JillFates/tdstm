a1f60919a4ee9022d30e7ad60aee013e617c39f4
Daniel Ojcius <dojcius@transitionaldata.com>
2017-12-27 11:22:19 -0300

    Change the function to handle all alphanumeric characters

--- test/e2e/src/test/groovy/specs/common/TestUtils.groovy
+++ test/e2e/src/test/groovy/specs/common/TestUtils.groovy
@@ -4,7 +4,7 @@ class TestUtils {
 
     static randStr(Integer len) {
         def charset = (('A'..'Z') + ('a'..'z') + ('0'..'9')).join()
-        def randomStr = (1..len).inject("") { a, b -> a += charset[new Random().nextFloat() * 62 as int] }.capitalize()
+        def randomStr = (1..len).inject("") { a, b -> a += charset[new Random().nextFloat() * 62 as int] }
         return randomStr
     }
 
