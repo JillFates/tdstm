bc1be3e08fcfd69a37e93b25993c01ea4ce4f8bc
Sidar LC <sidarlc@Sidars-MacBook-Pro-Touch.local>
2017-12-28 13:36:37 -0600

    TM-8692: (changes made to implement code review comments)

--- test/unit/net/transitionmanager/integration/ActionRequestSpec.groovy
+++ test/unit/net/transitionmanager/integration/ActionRequestSpec.groovy
@@ -6,77 +6,80 @@ import spock.lang.Specification
 class ActionRequestSpec extends Specification {
 
     def 'test set parameter value will throw MissingPropertyException'() {
-        setup:
+        setup: 'giving an ActionRequest object with parameters being set'
             Map<String, Object> parameters = ['property1': 'value1']
             ActionRequest actionRequest = new ActionRequest(parameters)
-        when:
+        when: 'trying to set a non existing parameter'
             actionRequest.param.property2 = 'value2'
-        then:
-            thrown MissingPropertyException
+        then: 'an exception should be thrown indicating that passed parameter is missing'
+            MissingPropertyException e = thrown MissingPropertyException
+            "No such property: property2" == e.message
     }
 
     def 'test set existing parameter value when action request is not readonly' () {
-        setup:
+        setup: 'giving an ActionRequest object with parameters being set'
             Map<String, Object> parameters = ['property1': 'value1']
             ActionRequest actionRequest = new ActionRequest(parameters)
-        when:
+        when: 'trying to set an existing parameter value and readonly flag is false'
             actionRequest.param.property1 = 'value2'
-        then:
+        then: 'the parameter value should be updated to the expected value'
             actionRequest.param.property1 == 'value2'
     }
 
     def 'test set existing parameter value when action request is readonly will throw ReadOnlyPropertyException' () {
-        setup:
+        setup: 'giving an ActionRequest object with parameters being set and readonly flag is turned on'
             Map<String, Object> parameters = ['property1': 'value1']
             ActionRequest actionRequest = new ActionRequest(parameters)
             actionRequest.readonly = true
-        when:
+        when: 'trying to set an existing parameter value and readonly flag is true'
             actionRequest.param.property1 = 'value2'
-        then:
-            thrown ReadOnlyPropertyException
+        then: 'an exception should be thrown indicating that we are trying to write on a readonly property'
+            ReadOnlyPropertyException e = thrown ReadOnlyPropertyException
+            "Cannot set readonly property: property1 for class: net.transitionmanager.integration.ActionRequestParameter" == e.message
     }
 
     def 'test read parameter value when action request is readonly will return the expected value' () {
-        setup:
+        setup: 'giving an ActionRequest object with parameters being set and readonly flag is turned on'
             Map<String, Object> parameters = ['property1': 'value1']
             ActionRequest actionRequest = new ActionRequest(parameters)
             actionRequest.readonly = true
-        expect:
+        expect: 'we should be able to read values despite the readonly flag status'
             actionRequest.param.property1 == 'value1'
     }
 
     def 'test set header value when action request is not readonly' () {
-        setup:
+        setup: 'giving an ActionRequest object'
             ActionRequest actionRequest = new ActionRequest()
-        when:
+        when: 'trying to set a new header value'
             actionRequest.headers.add('header1', 'value1')
-        then:
+        then: 'the new header value should be written and red as expected'
             actionRequest.headers.get('header1') == 'value1'
     }
 
     def 'test set header value when action request is readonly will throw ReadOnlyPropertyException' () {
-        setup:
+        setup: 'giving an ActionRequest object and readonly flag is turned on'
             ActionRequest actionRequest = new ActionRequest()
             actionRequest.readonly = true
-        when:
+        when: 'trying to set a new header value'
             actionRequest.headers.add('header1', 'value1')
-        then:
-            thrown ReadOnlyPropertyException
+        then: 'an exception should be thrown indicating that we are trying to write on a readonly property'
+            ReadOnlyPropertyException e = thrown ReadOnlyPropertyException
+            "Cannot set readonly property: header1 for class: net.transitionmanager.integration.ActionRequestHeader" == e.message
     }
 
     def 'test set config value will set the expected value' () {
-        setup:
+        setup: 'giving an ActionRequest object'
             ActionRequest actionRequest = new ActionRequest()
-        when:
+        when: 'trying to set a new configuration property'
             actionRequest.config.setProperty(Exchange.CONTENT_TYPE, 'application/json')
-        then:
+        then: 'the new configuration value should be written and red as expected'
             actionRequest.config.getProperty(Exchange.CONTENT_TYPE) == 'application/json'
     }
 
     def 'test get non existing config value will return null' () {
-        setup:
+        setup: 'giving an ActionRequest object'
             ActionRequest actionRequest = new ActionRequest()
-        expect:
+        expect: 'null should be received if trying to read a non existing configuration value'
             null == actionRequest.config.getProperty(Exchange.CONTENT_TYPE)
     }
 
