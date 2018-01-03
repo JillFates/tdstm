11759dd9af1b343a1a2802e258796bf3d4987494
Diego Correa <dcorrea@tdsi.com>
2018-01-03 13:59:16 -0300

    TM-8696 Just adding docs to try to link ticket TM-8696 with TM-8690 in pull-request PR#722

--- src/groovy/net/transitionmanager/integration/ReactionScriptCode.groovy
+++ src/groovy/net/transitionmanager/integration/ReactionScriptCode.groovy
@@ -1,5 +1,8 @@
 package net.transitionmanager.integration
 
+/**
+ * The the ReactionScriptCode enum specifies all of the codes that will exist in the ApiAction.reactionJson hash.
+ */
 enum ReactionScriptCode {
 
     EVALUATE,
