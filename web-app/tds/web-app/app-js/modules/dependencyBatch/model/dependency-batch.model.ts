3b0d9e29cae781f436fdb0ce54a139c215b5c92d
dontiveros <dontiveros@tdsi.com>
2018-01-18 00:33:18 -0600

    TM-8830 Import Batch: Initial Page Load added more columns for grid.

--- web-app/tds/web-app/app-js/modules/dependencyBatch/model/dependency-batch.model.ts
+++ web-app/tds/web-app/app-js/modules/dependencyBatch/model/dependency-batch.model.ts
@@ -26,7 +26,92 @@ export class DependencyBatchColumnsModel {
 				property: 'id',
 				type: 'text',
 				width: 100,
-				locked: true
+				locked: false
+			},
+			{
+				label: 'Status',
+				property: 'status',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Imported At',
+				property: 'importedDate',
+				type: 'date',
+				format: '{0:d}',
+				width: 170,
+				locked: false
+			},
+			{
+				label: 'Imported By',
+				property: 'importedBy',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Domain',
+				property: 'domain',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Provider',
+				property: 'provider',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Datascript',
+				property: 'datascript',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'File Name',
+				property: 'filename',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Records',
+				property: 'records',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Errors',
+				property: 'errors',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Pending',
+				property: 'pending',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Processed',
+				property: 'processed',
+				type: 'text',
+				width: 100,
+				locked: false
+			},
+			{
+				label: 'Processed',
+				property: 'ignored',
+				type: 'text',
+				width: 100,
+				locked: false
 			},
 		];
 	}
