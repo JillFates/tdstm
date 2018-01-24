2ac23c07d849b808df0a88a814fd73c9c75789ba
Augusto Recordon <augusto.recordon@bairesdev.com>
2018-01-24 09:40:22 -0300

    TM-8569 Prevent API Action's scalars to take negative values.

--- grails-app/domain/net/transitionmanager/domain/ApiAction.groovy
+++ grails-app/domain/net/transitionmanager/domain/ApiAction.groovy
@@ -126,8 +126,8 @@ class ApiAction {
 		lastModified nullable: true
 		methodParams nullable: true
 		name nullable: false, size: 1..64, unique: 'project'
-		pollingLapsedAfter nullable: false
-		pollingStalledAfter nullable: false
+		pollingLapsedAfter nullable: false, min: 0
+		pollingStalledAfter nullable: false, min: 0
 		producesData nullable: false, range:0..1
 		provider nullable: false, validator: providerValidator
 		reactionScripts size: 1..65535, blank: false, validator: reactionJsonValidator
