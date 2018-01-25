edf7489211c6d70e67c056ce2493ce5367b10d0d
Sidar LC <slopez@tdsi.com>
2018-01-25 14:57:04 -0600

    TM-8809 Changes after comments in PR

--- test/unit/com/tdssrc/grails/FileSystemUtilTests.groovy
+++ test/unit/com/tdssrc/grails/FileSystemUtilTests.groovy
@@ -8,29 +8,29 @@ class FileSystemUtilTests extends Specification{
 	@Unroll
 	void "Test isValidExtension for extension #ext given the extensions #exts responds #result"() {
 		expect: "The function responds as expected"
-		FileSystemUtil.isValidExtension(ext, exts) == result
+			FileSystemUtil.isValidExtension(ext, exts) == result
 		where: "The following data table is given."
-		ext     |       exts                ||  result
-		null    |    null                   ||  false
-		null    |   []                      ||  false
-		"json"  |   []                      ||  false
-		"json"  |   ["json", "xls"]         ||  true
-		"JSON"  |   ["json", "xls"]         ||  true
-		"JSON"  |   ["txt", "xls"]          ||  false
-		".JSON" |   ["json", "xls"]         ||  true
+			ext     |       exts                ||  result
+			null    |    null                   ||  false
+			null    |   []                      ||  false
+			"json"  |   []                      ||  false
+			"json"  |   ["json", "xls"]         ||  true
+			"JSON"  |   ["json", "xls"]         ||  true
+			"JSON"  |   ["txt", "xls"]          ||  false
+			".JSON" |   ["json", "xls"]         ||  true
 	}
 
 	@Unroll
 	void "Test formatExtension for extension #ext responds #result"() {
 		expect: "The function responds as expected"
-		FileSystemUtil.formatExtension(ext) == result
+			FileSystemUtil.formatExtension(ext) == result
 		where: "The following data table is given."
-		ext     ||  result
-		null    ||  null
-		"json"  ||  "json"
-		".json" ||  "json"
-		"JSON"  ||  "json"
-		".JSON" ||  "json"
+			ext     ||  result
+			null    ||  null
+			"json"  ||  "json"
+			".json" ||  "json"
+			"JSON"  ||  "json"
+			".JSON" ||  "json"
 	}
 
 	@Unroll
