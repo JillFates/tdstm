85f2fdfa715f69515e3ef936f5739a8aed926787
dontiveros <dontiveros@tdsi.com>
2018-02-28 16:55:10 -0600

    TM-9070 Implement the Manage Dependency Batches - Record Detail Modal TM-9081 Import Batch: Record Detail Modal: Fields Pane work in progress ..

--- web-app/tds/web-app/app-js/modules/dependencyBatch/components/dependency-batch-record-detail-fields/dependency-batch-record-detail-fields.component.ts
+++ web-app/tds/web-app/app-js/modules/dependencyBatch/components/dependency-batch-record-detail-fields/dependency-batch-record-detail-fields.component.ts
@@ -1,12 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {DependencyBatchService} from '../../service/dependency-batch.service';
+import {ImportBatchRecordModel} from '../../model/import-batch-record.model';
 
 @Component({
 	selector: 'dependency-batch-record-detail-fields',
 	templateUrl: '../tds/web-app/app-js/modules/dependencyBatch/components/dependency-batch-record-detail-fields/dependency-batch-record-detail-fields.component.html'
 })
-export class DependencyBatchRecordDetailFieldsComponent {
+export class DependencyBatchRecordDetailFieldsComponent implements OnInit {
 
+	@Input('batchRecord') batchRecord: ImportBatchRecordModel;
+
+	private fields: Array<any> = [];
 	private fieldsFilter: any = {
 		options: [
 			{text: 'All', value: 1},
@@ -17,5 +21,26 @@ export class DependencyBatchRecordDetailFieldsComponent {
 
 	constructor(private dependencyBatchService: DependencyBatchService) {
 		// Silence is golden
+	}
+
+	ngOnInit(): void {
+		this.loadRecordFieldDetails();
+	}
+
+	/**
+	 * TODO: document
+	 */
+	private loadRecordFieldDetails(): void {
+		this.dependencyBatchService.getImportBatchRecordFieldDetail(this.batchRecord.id).subscribe( result => {
+			this.fields = result;
+		}, error => this.handleError(error) );
+	}
+
+	/**
+	 * TODO: document
+	 * @param e
+	 */
+	private handleError(e): void {
+		console.log(e);
 	}
 }
