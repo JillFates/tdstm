b42fc2aeafb3126d0c5b22ef9ebfb223156b9bfd
Cristian Nadj <cnadj@tdsi.com>
2018-03-01 07:37:48 -0300

    TM-9333 Spock modifications on TaskManagerSpec

--- test/e2e/src/test/groovy/specs/TaskManager/TaskManagerSpec.groovy
+++ test/e2e/src/test/groovy/specs/TaskManager/TaskManagerSpec.groovy
@@ -28,13 +28,14 @@ class TaskManagerSpec extends GebReportingSpec {
         println "cleanup(): ${testKey} #${sCount} ${specificationContext.currentIteration.name} "
     }
 
-    def "Go To Task Manager"() {
+    def "1. Going To The Task Manager Section"() {
         testKey = "TM-XXXX"
-        given:
-        at MenuPage
-        when:
-        menuModule.goToTasksManager()
-        then:
+        given: 'The User is at the Menu Page'
+            at MenuPage
+        when: 'User Goes to the Tasks > Task Manager Section'
+            menuModule.goToTasksManager()
+
+        then: 'Task Creation Pop-Up should be displayed'
         at TaskManagerPage
     }
 }
