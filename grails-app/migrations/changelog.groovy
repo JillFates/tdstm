89d5be225c58520d70b209c04079c7f2548dafd0
John Martin <jmartin@transitionaldata.com>
2018-03-05 10:34:26 -0500

    TM-9070 / TM-9675 Added the intial implementation of the service for posting the ImportBatchRecords. This commit includes a migration to help with the process progress logic.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -219,4 +219,6 @@ databaseChangeLog = {
 	include file: '20180213-change-credential-and-add-credential-permission.groovy'
 	include file: '20180216-additional-changes-to-credential.groovy'
 	include file: '20180225-changes-to-import-batch.groovy'
+	include file: '20180228-alter-table-api-action-change-last-modified-column-name-to-last-updated.groovy'
+	include file: '20180304-changes-for-batch-processing.groovy'
 }
