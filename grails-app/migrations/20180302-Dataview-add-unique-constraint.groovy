b031c9e62ac63910793440a8dcbe8102064df90f
Sidar LC <slopez@tdsi.com>
2018-03-05 13:07:28 -0600

    Fix dataview migration that updates duplicates and prevent them for happening anymore

--- grails-app/migrations/20180302-Dataview-add-unique-constraint.groovy
+++ grails-app/migrations/20180302-Dataview-add-unique-constraint.groovy
@@ -1,26 +1,22 @@
-import java.util.List
 /**
  * @author ecantu
  * TM-8310 Prevent user from creating a Dataview with the same name - Back end Work
  */
 databaseChangeLog = {
 	changeSet(author: "ecantu", id: "TM-8310-1") {
-		comment("If there are already duplicated records for the same name + project_id, rename after applying the constraint.")
+		comment("If there are already duplicated records for the same name + it.id, rename after applying the constraint.")
 		grailsChange{
-			change{
-				// Check for duplicate records for name and project_id
-				List duplicateRecords = sql.rows('''
-					select count(*), name 
-					from datavdiew d
-					group by d.name, d.project_id
-					having count(*) > 1
+			change {
+				def duplicatedDataViews = sql.rows('''
+					SELECT project_id, name 
+					FROM dataview 
+					GROUP  BY project_id, name
+					HAVING COUNT(*) > 1
 					''')
-				if (duplicateRecords.size() > 0) {
-					// This logic will rename duplicates, so we can apply the constraint after
-					int i = 1
-					for (data in duplicateRecords) {
-						data.name = data.name + i
-						i++
+
+				if (duplicatedDataViews) {
+					duplicatedDataViews.each { duplicated ->
+						sql.execute("UPDATE dataview SET name = CONCAT(name, ' ', id) WHERE name = '${duplicated.name}' AND project_id = ${duplicated.project_id}")
 					}
 				}
 			}
@@ -31,35 +27,9 @@ databaseChangeLog = {
 		comment("Add unique constraint to Dataview table on name and project_id fields.")
 
 		addUniqueConstraint(
-						constraintName: 'uq_dataview_name_project_id',
-						tableName: 'dataview',
-						columnNames: 'name, project_id'
+			constraintName: 'uq_dataview_name_project_id',
+			tableName: 'dataview',
+			columnNames: 'name, project_id'
 		)
-	}
-}
-
-
-/**
- * If there are duplicated" records for the same name + project_id,
- * rename, so that will put the data consistent to apply
- * the unique constraint on those two fields
- * @param sql
- */
-void renameDuplicateColumns(sql) {
-	
-	// Check for duplicate records for name and project_id
-	List duplicateRecords = sql.rows('''
-					select count(*), name 
-					from dataview d
-					group by d.name, d.project_id
-					having count(*) > 1
-					''')
-	if (duplicateRecords.size() > 0) {
-		// This logic will rename duplicates, so we can apply the constraint after
-		int i = 1
-		for (data in duplicateRecords) {
-			data.name = data.name + i
-			i++
-		}
 	}
 }
