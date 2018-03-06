2719caa2c915f0530e343f3e39dda1fd67ea50d5
Jorge Morayta <jmorayta@tdsi.com>
2018-03-06 17:15:50 -0600

    TM-9618 Migration to Angular 5 + WebPack 4

--- web-app/tds/web-app/app-js/modules/taskManager/task-manager.module.ts
+++ web-app/tds/web-app/app-js/modules/taskManager/task-manager.module.ts
@@ -13,13 +13,10 @@ import {TASK_MANAGER_STATES} from './task-manager-routing.states';
 import {TaskListComponent} from './components/list/task-list.component';
 import {TaskCreateComponent} from './components/create/task-create.component';
 import {SharedModule} from '../../shared/shared.module';
-import {FormlyInputHorizontal} from '../../shared/modules/formly/formly-input-horizontal.component';
 // Services
 import {TaskService} from './service/task.service';
 // Import Kendo Modules
 import {GridModule} from '@progress/kendo-angular-grid';
-// Formly
-import {FormlyModule, FormlyBootstrapModule} from 'ng-formly';
 
 @NgModule({
 	imports: [
@@ -28,19 +25,6 @@ import {FormlyModule, FormlyBootstrapModule} from 'ng-formly';
 		GridModule,
 		FormsModule,
 		ReactiveFormsModule,
-		FormlyModule.forRoot({
-			wrappers: [
-				{
-					name: 'FormlyInputHorizontal',
-					component: FormlyInputHorizontal,
-					types: ['formlyInputHorizontalWrapper', 'formlySelectHorizontalWrapper']
-				}],
-			types: [
-				{name: 'formlyInputHorizontalWrapper', extends: 'textarea'},
-				{name: 'formlySelectHorizontalWrapper', extends: 'select'}
-			]
-		}),
-		FormlyBootstrapModule,
 		UIRouterModule.forChild({states: TASK_MANAGER_STATES}), // Same as { states: [state1, state2 ] }
 	],
 	providers: [TaskService],
--- web-app/tds/web-app/app-js/shared/shared.module.ts
+++ web-app/tds/web-app/app-js/shared/shared.module.ts
@@ -26,7 +26,6 @@ import { FilterPipe } from './pipes/filter.pipe';
 // Shared Components
 import { PopupLegendsComponent } from './modules/popup/legends/popup-legends.component';
 import { HeaderComponent } from './modules/header/header.component';
-import { FormlyInputHorizontal } from './modules/formly/formly-input-horizontal.component';
 import { CodeMirrorComponent } from './modules/code-mirror/code-mirror.component';
 import { DynamicComponent } from './components/dynamic.component';
 import { CheckActionComponent } from './components/check-action/check-action.component';
@@ -58,7 +57,6 @@ import { DictionaryService } from './services/dictionary.service';
 		UIDialogDirective,
 		HeaderComponent,
 		PopupLegendsComponent,
-		FormlyInputHorizontal,
 		UIPromptDirective,
 		UISVGIconDirectiveDirective,
 		UIFloatingHeaderKGridDirective,
@@ -98,7 +96,6 @@ import { DictionaryService } from './services/dictionary.service';
 		FilterPipe,
 		HeaderComponent,
 		PopupLegendsComponent,
-		FormlyInputHorizontal,
 		DynamicComponent,
 		CodeMirrorComponent,
 		CheckActionComponent,
