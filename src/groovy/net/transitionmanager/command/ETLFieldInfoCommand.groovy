142b067a2921143f81bb8aa1cbd4ef64add7cae7
Augusto Recordon <augusto.recordon@bairesdev.com>
2018-03-07 13:02:25 -0300

    TM-9680 Small changes to some Import Batch endpoints.

---
+++ src/groovy/net/transitionmanager/command/ETLFieldInfoCommand.groovy
@@ -0,0 +1,21 @@
+package net.transitionmanager.command
+
+import grails.validation.Validateable
+
+/**
+ * General purpose command object for updating an individual field.
+ */
+@Validateable
+class ETLFieldInfoCommand{
+
+	// Field Name.
+	String fieldName
+
+	// Value for the field.
+	Object value
+
+	static constraints = {
+		fieldName nullable: false, blank: false
+		value nullable: false
+	}
+}
