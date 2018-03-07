d24ac75b65bb5f059daaf6194add9ae141e8ed5a
dontiveros <dontiveros@tdsi.com>
2018-03-07 17:13:04 -0600

    TM-9070 Implement the Manage Dependency Batches - Record Detail Modal - Adding filtering to record field details (work in progress)

--- web-app/tds/web-app/app-js/modules/dependencyBatch/components/dependency-batch-record-detail-fields/dependency-batch-record-detail-fields.component.ts
+++ web-app/tds/web-app/app-js/modules/dependencyBatch/components/dependency-batch-record-detail-fields/dependency-batch-record-detail-fields.component.ts
@@ -4,25 +4,41 @@ import {ImportBatchRecordModel} from '../../model/import-batch-record.model';
 import {ApiResponseModel} from '../../../../shared/model/ApiResponseModel';
 import {BatchStatus, ImportBatchModel} from '../../model/import-batch.model';
 import {UIActiveDialogService} from '../../../../shared/services/ui-dialog.service';
+import {CompositeFilterDescriptor, FilterDescriptor, process, State} from '@progress/kendo-data-query';
+import {GridDataResult} from '@progress/kendo-angular-grid';
 
 @Component({
 	selector: 'dependency-batch-record-detail-fields',
 	templateUrl: '../tds/web-app/app-js/modules/dependencyBatch/components/dependency-batch-record-detail-fields/dependency-batch-record-detail-fields.component.html'
 })
-export class DependencyBatchRecordDetailFieldsComponent implements OnInit, OnChanges {
+export class DependencyBatchRecordDetailFieldsComponent implements OnInit {
 
 	@Input('importBatch') importBatch: ImportBatchModel;
 	@Input('batchRecord') batchRecord: ImportBatchRecordModel;
 	@Output('onCancel') cancelEvent = new EventEmitter<any>();
 	@Output('onUpdateSuccess') updateSuccessEvent = new EventEmitter<any>();
 
-	private fieldsInfo: Array<any>;
+	private fieldsInfo: Array<{
+		name: string,
+		currentValue: string,
+		importValue: string,
+		error: boolean,
+		overridedValue: string
+	}> = [];
+	private state: State = {
+		filter: {
+			filters: [],
+			logic: 'or'
+		}
+	};
+	private gridData: GridDataResult;
 	private fieldsFilter: any = {
 		options: [
 			{text: 'All', value: 1},
 			{text: 'With Errors', value: 2}
 			],
-		selected: {text: 'All', value: 1}
+		selected: {text: 'All', value: 1},
+		text: ''
 	};
 
 	constructor(
@@ -30,6 +46,9 @@ export class DependencyBatchRecordDetailFieldsComponent implements OnInit, OnCha
 		// Silence is golden
 	}
 
+	/**
+	 * On Init Load Record Field details.
+	 */
 	ngOnInit(): void {
 		this.loadRecordFieldDetails();
 	}
@@ -37,18 +56,19 @@ export class DependencyBatchRecordDetailFieldsComponent implements OnInit, OnCha
 	/**
 	 * On Changes detected Batch Record selection reload it's Fields info.
 	 * @param {SimpleChanges} changes
-	 */
 	ngOnChanges(changes: SimpleChanges): void {
 		this.loadRecordFieldDetails();
 	}
+	 */
 
 	/**
 	 * Gets the Batch Record Field Details from API.
 	 */
 	private loadRecordFieldDetails(): void {
-		this.dependencyBatchService.getImportBatchRecordFieldDetail(this.batchRecord.importBatch.id, this.batchRecord.id).subscribe( (result: ApiResponseModel) => {
+		this.dependencyBatchService.getImportBatchRecordFieldDetail(this.batchRecord.importBatch.id, this.batchRecord.id)
+			.subscribe( (result: ApiResponseModel) => {
 			if (result.status === ApiResponseModel.API_SUCCESS) {
-				this.fieldsInfo = result.data.fieldsInfo;
+				this.buildGridData(result.data.fieldsInfo);
 			} else {
 				this.handleError(result.errors[0] ? result.errors[0] : 'error calling endpoint');
 			}
@@ -58,27 +78,40 @@ export class DependencyBatchRecordDetailFieldsComponent implements OnInit, OnCha
 		});
 	}
 
+	private buildGridData(fields): void {
+		// let data: Array<{name: string, currentValue: string, importValue: string, error: boolean}> = [];
+		for (const fieldName of this.importBatch.fieldNameList) {
+			this.fieldsInfo.push({
+				name: fieldName,
+				currentValue: fields[fieldName].originalValue,
+				importValue: fields[fieldName].value,
+				error: fields[fieldName].error,
+				overridedValue: null
+			});
+		}
+		this.gridData = process(this.fieldsInfo, this.state);
+	}
+
 	/**
 	 * On Update button click.
 	 */
 	private onUpdate(): void {
 		let newFieldsValues: Array<{fieldName: string, value: string}> = [];
-		for (let field in this.fieldsInfo) {
-			if (this.fieldsInfo.hasOwnProperty(field) && this.fieldsInfo[field].overridedValue) {
-				// console.log(this.fieldsInfo[field]);
-				const newFieldValue = {fieldName: field, value: this.fieldsInfo[field].overridedValue};
-				newFieldsValues.push(newFieldValue);
-			}
-		}
-		console.log(newFieldsValues);
-		this.dependencyBatchService.updateBatchRecordFieldsValues(this.importBatch.id, this.batchRecord.id, newFieldsValues)
-			.subscribe((result: ApiResponseModel) => {
-				if (result.status === ApiResponseModel.API_SUCCESS) {
-					this.updateSuccessEvent.emit();
-				} else {
-					this.handleError(result.errors[0] ? result.errors[0] : 'error updating field values');
-				}
-		}, error => this.handleError(error));
+		// for (let field in this.fieldsInfo) {
+		// 	if (this.fieldsInfo.hasOwnProperty(field) && this.fieldsInfo.overridedValue) {
+		// 		// console.log(this.fieldsInfo[field]);
+		// 		const newFieldValue = {fieldName: field, value: this.fieldsInfo.overridedValue};
+		// 		newFieldsValues.push(newFieldValue);
+		// 	}
+		// }
+		// this.dependencyBatchService.updateBatchRecordFieldsValues(this.importBatch.id, this.batchRecord.id, newFieldsValues)
+		// 	.subscribe((result: ApiResponseModel) => {
+		// 		if (result.status === ApiResponseModel.API_SUCCESS) {
+		// 			this.updateSuccessEvent.emit();
+		// 		} else {
+		// 			this.handleError(result.errors[0] ? result.errors[0] : 'error updating field values');
+		// 		}
+		// }, error => this.handleError(error));
 	}
 
 	/**
@@ -111,5 +144,29 @@ export class DependencyBatchRecordDetailFieldsComponent implements OnInit, OnCha
 	 */
 	private handleError(e): void {
 		console.log(e);
+	}
+
+	private onTextFilter(): void {
+		let foundedMatch = this.state.filter.filters.find((r: any) => r.field === 'name');
+		if (foundedMatch) {
+			(foundedMatch as FilterDescriptor).value = this.fieldsFilter.text;
+		} else {
+			let nameFilter: FilterDescriptor = {
+				field: 'name',
+				value: this.fieldsFilter.text,
+				operator: 'contains',
+				ignoreCase: true
+			};
+			this.state.filter.filters = [nameFilter];
+		}
+		this.gridData = process(this.fieldsInfo, this.state);
+	}
+
+	private clearTextFilter(): void {
+		this.fieldsFilter.text = '';
+		this.onTextFilter();
+		// const filterIndex = this.state.filter.filters.findIndex((r: any) => r.field === 'name');
+		// this.state.filter.filters.splice(filterIndex, 1);
+		// this.gridData = process(this.fieldsInfo, this.state);
 	}
 }
