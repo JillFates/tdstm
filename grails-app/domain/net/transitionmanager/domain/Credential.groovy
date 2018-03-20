66a31152ceb0c4c47a69d1a6945abaf8f2550dd8
ecantu <ecantu@tdsi.com>
2018-03-20 03:32:32 -0300

    TM-9035 Fixes bug when saving a new Credential with BASIC_AUTH method, because authenticationUrl was mandatory

--- grails-app/domain/net/transitionmanager/domain/Credential.groovy
+++ grails-app/domain/net/transitionmanager/domain/Credential.groovy
@@ -157,8 +157,8 @@ class Credential {
 	 * Used to validate if the authenticationUrl property is set for AuthenticationMethods that require a value
 	 *      AWS - doen't need on since URL is predefined
 	 */
-	static Closure authenticationUrlValidator = { value, target ->
-		List methodsThatRequireProp = [AuthenticationMethod.BASIC_AUTH, AuthenticationMethod.COOKIE, AuthenticationMethod.HEADER, AuthenticationMethod.JWT]
+	static Closure authenticationUrlValidator = { value, target -> // For "BASIC_AUTH", authenticationUrl is not mandatory
+		List methodsThatRequireProp = [AuthenticationMethod.COOKIE, AuthenticationMethod.HEADER, AuthenticationMethod.JWT]
 		if ( target.authenticationMethod in methodsThatRequireProp ) {
 			if (value == null || value.trim() == '') {
 				return 'default.blank.message'
