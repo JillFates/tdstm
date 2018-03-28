0567eaa9a8789d73176215d00ca97596958d7ca9
John Martin <jmartin@transitionaldata.com>
2018-03-28 11:25:49 -0400

    TM-9963 Changed ActionRequest to have comments, renamed param to params and added new options property. Good luck Sidar on the rest!

--- src/groovy/net/transitionmanager/integration/ActionRequest.groovy
+++ src/groovy/net/transitionmanager/integration/ActionRequest.groovy
@@ -5,8 +5,18 @@ package net.transitionmanager.integration
  */
 class ActionRequest {
 	private boolean readonly = false
-	ActionRequestParameter param
+
+	// Contains the parameters with the contextual values that are defined in the ApiAction
+	// These parameters will end up in the query string and/or json payload of requests
+	ActionRequestParameter params
+
+	// Contain properties need by the API logic (used internally by the implemenation code)
+	ActionRequestParameter options
+
+	// Contains any header that will be use in the request that can be populated in the Action PRE script
 	ActionRequestHeader headers
+
+	// Contains setting used by Camel that can be populated in the Action PRE script
 	ActionRequestConfig config
 
 	ActionRequest() {
