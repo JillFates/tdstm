0faa8ea96883c63b9ce30925782c89f64490f67f
ingrid <imachado@tdsi.com>
2018-04-03 16:01:01 -0300

    Added number to each def. Deleted explanatory comments and made the defs self explanatory as per good practices.

--- test/e2e/src/test/groovy/specs/AssetViewManager/AssetViewCreationSpec.groovy
+++ test/e2e/src/test/groovy/specs/AssetViewManager/AssetViewCreationSpec.groovy
@@ -29,8 +29,7 @@ class AssetViewCreationSpec extends GebReportingSpec {
         at MenuPage
         waitFor { menuModule.goToAssetViewManager() }
     }
-    //validates I can reach"my views"
-    def "Go To myViews"() {
+    def "1. validates user can reach'my views' "() {
         testKey = "TM-8500"
         given: "I am in Asset Views Page"
             at AssetViewsPage
@@ -39,8 +38,7 @@ class AssetViewCreationSpec extends GebReportingSpec {
         then: "I am taken to My Views page"
             viewsModule.displayed
     }
-    /*validates I reach create view Module*/
-    def "Go To Create View"() {
+    def "2. Validates user can reach Create View Modulew"() {
         testKey = "TM-8500"
         given: "I am in My Views"
             viewsModule.displayed
@@ -49,8 +47,8 @@ class AssetViewCreationSpec extends GebReportingSpec {
         then: "The Create View Module is displayed"
             createViewModule.displayed
     }
-    //Validates I can go back to asset class by Clicking on the tab
-    def "Go back to Asset Class tab"() {
+    //
+    def "3. Validates user can go back to asset class by Clicking on the tab"() {
         testKey = "TM-8500"
         given: "I am in the Create View Module"
             createViewModule.displayed
@@ -61,8 +59,7 @@ class AssetViewCreationSpec extends GebReportingSpec {
         then: "I am taken back back to Application options tab"
             createViewModule.applicationOption.displayed
     }
-    //Validate I can go back to Fields tab by Clicking on it
-    def "Go back to Fields tab"() {
+    def "4. Validate I can go back to Fields tab by Clicking on it"() {
         testKey = "TM-8500"
         given: "I am in Application options"
             createViewModule.applicationOption.displayed
@@ -71,8 +68,7 @@ class AssetViewCreationSpec extends GebReportingSpec {
         then: "The application takes me to the Fields tab"
             createViewModule.searchField.displayed
     }
-    //filters the selected fields and validates they are displayed.
-    def "Filter Selected Fields"() {
+    def "5. Filter Selected Fields and validate only those are displayed"() {
         testKey = "TM-8500"
         given: "I am creating a view"
             createViewModule.displayed
@@ -82,40 +78,37 @@ class AssetViewCreationSpec extends GebReportingSpec {
         then: "Only the selected checkboxes are displayed"
             assert(createViewModule.selectedCheckboxesDisplayed())
     }
-    //Validate I can filter only unselected fields
-    def "Filter Unselected Fields"() {
+    def "6. Validate user can filter only Unselected fieldss"() {
         testKey = "TM-8500"
-        given: "I am creating a view"
+        given: "User is creating a view"
             createViewModule.fieldsFilter.displayed
         when: "I have filtered the checkboxes by UNSELECTED"
             waitFor{createViewModule.filterFields("Unselected")}
         then: "Only the UNSELECTED checkboxes are visible"
             createViewModule.unselectedCheckboxesDisplayed()
     }
-    //Validate I can filter by field names
-    def "Filter Field names"() {
+    //
+    def "7. Validate user can filter by field name"() {
         testKey = "TM-8500"
         given: "The filter by name is present and all fields are diplayed"
             waitFor{createViewModule.searchField.displayed}
             waitFor{createViewModule.filterFields("All Fields")}
-        when: "I filter by a random checkbox name"
+        when: "User filters by a random checkbox name"
             filteredName =createViewModule.searchFieldName()
         then: "Only checkboxes containing the text entered in the filter are displayed"
             createViewModule.filteredFieldMatchesDisplay(filteredName)
     }
-    //Validate I can choose fields and they are added below
-    def "Chosen Filters are added below"() {
+    def "8. Validate chosen Filters are added below"() {
         testKey = "TM-8500"
-        given: "I am creating a view"
+        given: "User is creating a view"
             createViewModule.fieldsFilter.displayed
             waitFor{createViewModule.clearSearch()}
-        when: "I have selected some fields to be displayed"
+        when: "User has selected some fields to be displayed"
             waitFor{createViewModule.filterFields("Selected")}
         then: "Column headers for the selected fields are displayed below"
             createViewModule.compareColumns()
     }
-    //will click on preview and validate there are data rows displayed
-    def "Click preview and validate data is dislayed"() {
+    def "9. Click preview and validate data is displayed"() {
         testKey = "TM-8500"
         given: "Preview button is present"
             createViewModule.previewBtn.displayed
@@ -124,23 +117,21 @@ class AssetViewCreationSpec extends GebReportingSpec {
         then: "Preview data rows are displayed"
             createViewModule.previewDataIsDisplayed()
     }
-    //saves the created view and validates it´s listed
-    def "Created View is listed"() {
+    def "10. Validate created View is listed"() {
         testKey = "TM-8500"
-        given: "I save the created view"
+        given: "User saves the created view"
             waitFor{createViewModule.clickSave()}
             at SaveViewPage
             waitFor{enterName(viewName)}
             waitFor{clickSave()}
-        when: "I go to Asset View Manager and filter the views by name"
+        when: "User goes to Asset View Manager and filter the views by name"
             waitFor { menuModule.goToAssetViewManager() }
             at AssetViewsPage
             filterViewByName(randStr)
         then: "The newly created view is filtered and displayed"
             vwGrid.find("tr")[1].find("a")[1].displayed
     }
-
-    def "User can go the the newly crated view page"() {
+    def "11. User can go the the newly crated view page"() {
         testKey = "TM-8500"
         given: "I have filtered the view"
             println "validate the filtered view is displayed"
