122c9bf29643034718363cd0ca4f6d2c456dea26
Sidar LC <slopez@tdsi.com>
2018-04-04 19:27:01 -0600

    TM-10047: Update the default set of the ServiceNow methods that will be in the Agent

--- src/groovy/net/transitionmanager/agent/ServiceNowAgent.groovy
+++ src/groovy/net/transitionmanager/agent/ServiceNowAgent.groovy
@@ -14,21 +14,34 @@ import groovy.transform.CompileStatic
 @Singleton(strict=false)
 @CompileStatic
 class ServiceNowAgent extends AbstractAgent {
-
+	private static final String DOCUMENTATION_URL = 'https://developer.servicenow.com/app.do#!/rest_api_doc?v=jakarta&id=r_TableAPI-GET'
 	public ServiceNowService serviceNowService
 
-	private static final List<LinkedHashMap> COMMON_PARAMS = [
-		[
+	private static final LinkedHashMap HOSTNAME_PARAM = [
 			paramName: 'HOSTNAME',
-			desc: 'The ServiceNow Hostname of the instance to intract with',
+			desc: 'The ServiceNow Hostname of the instance to interact with',
 			type: 'String',
 			context: ContextType.USER_DEF,
 			fieldName: null,
-			value: 'false',
-			required:1,
-			readonly:0,
-			encoded:1
-		],
+			value: 'Enter your FQDN to ServiceNow',
+			required: 1,
+			readonly: 0,
+			encoded: 0
+	]
+
+	private static final LinkedHashMap TABLE_PARAM = [
+			paramName: 'TABLE',
+			desc: 'The table name from the Now Table API',
+			type: 'String',
+			context: ContextType.USER_DEF,
+			fieldName: '$fieldName',
+			value: 'Enter table name',
+			required: 1,
+			readonly: '$readOnly',
+			encoded: 1
+	]
+
+	private static final List<LinkedHashMap> COMMON_PARAMS = [
 		[
 			paramName: 'sysparm_display_value',
 			desc: 'Set to true will return all fields, false returns only the fields in sysparm_fields (option true|false, default false)',
@@ -36,9 +49,9 @@ class ServiceNowAgent extends AbstractAgent {
 			context: ContextType.USER_DEF,
 			fieldName: null,
 			value: 'false',
-			required:1,
-			readonly:0,
-			encoded:1
+			required: 1,
+			readonly: 0,
+			encoded: 1
 		],
 		[
 			paramName: 'sysparm_limit',
@@ -47,9 +60,9 @@ class ServiceNowAgent extends AbstractAgent {
 			context: ContextType.USER_DEF,
 			fieldName: null,
 			value: 10000,
-			required:1,
-			readonly:0,
-			encoded:1
+			required: 1,
+			readonly: 0,
+			encoded: 1
 		],
 		[
 			paramName: 'sysparm_offset',
@@ -58,9 +71,9 @@ class ServiceNowAgent extends AbstractAgent {
 			context: ContextType.USER_DEF,
 			fieldName: null,
 			value: 0,
-			required:1,
-			readonly:0,
-			encoded:1
+			required: 1,
+			readonly: 0,
+			encoded: 1
 		],
 		[
 			paramName: 'sysparm_exclude_reference_link',
@@ -69,18 +82,18 @@ class ServiceNowAgent extends AbstractAgent {
 			context: ContextType.USER_DEF,
 			fieldName: null,
 			value: 'true',
-			required:1,
-			readonly:1,
-			encoded:1
+			required: 1,
+			readonly: 1,
+			encoded: 1
 		],
 		[
 			paramName: 'sysparm_suppress_pagination_header',
 			desc: 'Set this value to true to remove the Link header from the response (option true|false)',
 			type: 'String',
 			context: ContextType.USER_DEF,
-			required:1,
-			readonly:1,
-			encoded:1,
+			required: 1,
+			readonly: 1,
+			encoded: 1,
 			fieldName: null,
 			value: 'true'
 		],
@@ -93,7 +106,7 @@ class ServiceNowAgent extends AbstractAgent {
 			value: 'true',
 			required: 1,
 			readonly: 1,
-			encoded:1
+			encoded: 1
 		]
 	]
 
@@ -106,80 +119,184 @@ class ServiceNowAgent extends AbstractAgent {
 		Map dictionary = [
 			ApplicationList:
 				new DictionaryItem( [
-					// https://dev33184.service-now.com/cmdb_ci_appl.do?CSV&sysparm_fields=name,short_description,used_for,sys_id,sys_updated_on,vendor,sys_class_name,department,supported_by,owned_by,warranty_expiration,fqdn&sysparm_display_value=false
 					agentMethod: 'ApplicationList',
-					name: 'Application List',
-					description: 'Retrieves a list of applications from ServiceNow',
-					endpointUrl: 'https://{HOSTNAME}.service-now.com/cmdb_ci_appl.do',
-					docUrl: 'https://developer.servicenow.com/app.do#!/rest_api_doc?v=jakarta&id=r_TableAPI-GET',
+					name: 'Application List (cmdb_ci_appl)',
+					description: 'List of all Applications',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
 					method: 'fetchAssetList',
 					producesData: 1,
 					results: invokeResults(),
 					params: [
+						HOSTNAME_PARAM,
 						[
 							paramName: 'sysparm_fields',
 							desc: 'Comma-separated field names to return in the response',
 							type: 'String',
 							context: ContextType.USER_DEF,
 							fieldName: null,
-							value: 'name,short_description,used_for,sys_id,sys_updated_on,vendor,sys_class_name,department,supported_by,owned_by,warranty_expiration,fqdn',
-							required:1,
-							readonly:0,
+							value: 'operational_status,sys_created_on,used_for,owned_by,install_status,supported_by,name,short_description,manufacturer,vendor,department,comments,location,sys_updated_on,version',
+							required: 1,
+							readonly: 0,
 							encoded: 0
-						]
+						],
+						tableParam('cmdb_ci_appl', 1)
 					] + COMMON_PARAMS
 				]),
-			ServerListWindows:
+			ServerList:
 				new DictionaryItem(	[
-					// https://dev23132.service-now.com/cmdb_ci_win_server.do?CSV=true&sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields=sys_id,host_name,short_description,asset_tag,chassis_type,location,os,vendor,manufacturer,model_id,serial_number,department,ip_address,cpu_count,ram,form_factor,classification&sysparm_limit=10
-					agentMethod: 'ServerListWindows',
-					name: 'Windows Server List',
-					description: 'Retrieves a list of Windows Servers from ServiceNow',
-					method: 'fetchAssets',
-					endpointUrl: ' https://{HOSTNAME}.service-now.com/cmdb_ci_win_server.do',
-					docUrl: 'https://developer.servicenow.com/app.do#!/rest_api_doc?v=jakarta&id=r_TableAPI-GET',
+					agentMethod: 'ServerList',
+					name: 'Server List (cmdb_ci_server)',
+					description: 'List of all server types',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
 					method: 'fetchAssetList',
 					producesData: 1,
 					results: invokeResults(),
 					params: [
+						HOSTNAME_PARAM,
 						[
 							paramName: 'sysparm_fields',
 							desc: 'Comma-separated field names to return in the response',
 							type: 'String',
 							context: ContextType.USER_DEF,
 							fieldName: null,
-							value: 'sys_id,host_name,short_description,asset_tag,chassis_type,location,os,vendor,manufacturer,model_id,serial_number,department,ip_address,cpu_count,ram,form_factor,classification',
-							required:1,
-							readonly:0,
-							encoded: 0
-						]
+							value: 'sys_id,name,asset_tag,manufacturer,model_id,location,category,device_type,fqdn,ip_address,operational_status,dns_domain,short_description,supported_by,owned_by,sys_class_name,ram,disk_space,cpu_count,os_version,form_factor,os,sys_updated_on,sys_created_on',
+							required: 1,
+							readonly: 0,
+							encoded:  0
+						],
+						tableParam('cmdb_ci_server', 1)
 					] + COMMON_PARAMS
 				] ),
-			ServerListLinux:
+			NetworkGearList:
 				new DictionaryItem( [
-					// https://dev23132.service-now.com/api/now/table/cmdb_ci_linux_server?sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_suppress_pagination_header=true&sysparm_limit=10&sysparm_fields=sys_id%2Cname%2Casset_tag%2Chost_name%2Cserial_number%2Cshort_description%2Cclassification%2Cdepartment%2Clocation%2Cmanufacturer%2Cmodel_id%2Cos%2Ckernel_release%2Ccpu_count%2Cram%2Cdisk_space%2Cvirtual%2Cchassis_type%2Cmac_address%2Cip_address
-					agentMethod: 'ServerListLinux',
-					name: 'Linux Server List',
-					description: 'Retrieves a list of Linux Servers from ServiceNow',
-					method: 'fetchAssets',
-					endpointUrl: 'https://{HOSTNAME}.service-now.com/cmdb_ci_linux_server.do',
-					docUrl: 'ServiceNow REST API for Tables|https://developer.servicenow.com/app.do#!/rest_api_doc?v=jakarta&id=r_TableAPI-GET',
+					agentMethod: 'NetworkGearList',
+					name: 'Network Gear List (cmdb_ci_netgear)',
+					description: 'List of network gear',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
 					method: 'fetchAssetList',
 					producesData: 1,
 					results: invokeResults(),
 					params: [
+						HOSTNAME_PARAM,
 						[
 							paramName: 'sysparm_fields',
 							desc: 'Comma-separated field names to return in the response',
 							type: 'String',
 							context: ContextType.USER_DEF,
 							fieldName: null,
-							value: 'sys_id,name,asset_tag,host_name,serial_number,short_description,classification,department,location,manufacturer,model_id,os,kernel_release,cpu_count,ram,disk_space,virtual,chassis_type,mac_address,ip_address',
-							required:1,
-							readonly:0,
+							value: 'sys_id,name,asset_tag,location,manufacturer,model_id,category,device_type,fqdn,ip_address,operational_status,supported_by,owned_by,dns_domain,short_description,sys_class_name,sys_updated_on,sys_created_on',
+							required: 1,
+							readonly: 0,
 							encoded: 0
-						]
-
+						],
+						tableParam('cmdb_ci_netgear', 1)
+					] + COMMON_PARAMS
+				] ),
+			DatabaseList:
+				new DictionaryItem( [
+					agentMethod: 'DatabaseList',
+					name: 'Database List (cmdb_ci_database)',
+					description: 'List of databases',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
+					method: 'fetchAssetList',
+					producesData: 1,
+					results: invokeResults(),
+					params: [
+						HOSTNAME_PARAM,
+						[
+							paramName: 'sysparm_fields',
+							desc: 'Comma-separated field names to return in the response',
+							type: 'String',
+							context: ContextType.USER_DEF,
+							fieldName: null,
+							value: 'sys_id,name,category,operational_status,short_description,raid_type,dr_backup,supported_by,type,owned_by,supported_by,db_server',
+							required: 1,
+							readonly: 0,
+							encoded: 0
+						],
+						tableParam('cmdb_ci_server', 1)
+					] + COMMON_PARAMS
+				] ),
+			MSDList:
+				new DictionaryItem( [
+					agentMethod: 'MSDList',
+					name: 'Mass Storage Device List (cmdb_ci_msd)',
+					description: 'List of mass storage devices',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
+					method: 'fetchAssetList',
+					producesData: 1,
+					results: invokeResults(),
+					params: [
+						HOSTNAME_PARAM,
+						[
+							paramName: 'sysparm_fields',
+							desc: 'Comma-separated field names to return in the response',
+							type: 'String',
+							context: ContextType.USER_DEF,
+							fieldName: null,
+							value: 'sys_id,name,asset_tag,manufacturer,model_id,category,device_type,ip_address,operational_status,short_description,sys_class_name,raid_type,dr_backup,owned_by,supported_by,capacity,location,sys_updated_on,sys_created_on',
+							required: 1,
+							readonly: 0,
+							encoded: 0
+						],
+						tableParam('cmdb_ci_msd', 1)
+					] + COMMON_PARAMS
+				] ),
+			RelationshipsList:
+				new DictionaryItem( [
+					agentMethod: 'RelationshipsList',
+					name: 'CI Relationships List (cmdb_rel_ci)',
+					description: 'List of relationships (aka dependencies)',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
+					method: 'fetchAssetList',
+					producesData: 1,
+					results: invokeResults(),
+					params: [
+						HOSTNAME_PARAM,
+						[
+							paramName: 'sysparm_fields',
+							desc: 'Comma-separated field names to return in the response',
+							type: 'String',
+							context: ContextType.USER_DEF,
+							fieldName: null,
+							value: 'sys_id,parent,child,type,sys_updated_on,sys_created_on,connection_strength,port',
+							required: 1,
+							readonly: 0,
+							encoded: 0
+						],
+						tableParam('cmdb_rel_ci', 1)
+					] + COMMON_PARAMS
+				] ),
+			CustomTableList:
+				new DictionaryItem( [
+					agentMethod: 'CustomTableList',
+					name: 'Custom Table List (User defined)',
+					description: 'List of entities for user defined table',
+					endpointUrl: 'https://{HOSTNAME}.service-now.com/{TABLE}.do',
+					docUrl: DOCUMENTATION_URL,
+					method: 'fetchAssetList',
+					producesData: 1,
+					results: invokeResults(),
+					params: [
+						HOSTNAME_PARAM,
+						[
+							paramName: 'sysparm_fields',
+							desc: 'Comma-separated field names to return in the response',
+							type: 'String',
+							context: ContextType.USER_DEF,
+							fieldName: null,
+							value: 'operational_status,sys_created_on,name,short_description,department,comments,location,sys_updated_on',
+							required: 1,
+							readonly: 0,
+							encoded: 0
+						],
+						tableParam('User defined table name', 0)
 					] + COMMON_PARAMS
 				] )
 		]
@@ -228,6 +345,19 @@ class ServiceNowAgent extends AbstractAgent {
 				https://dev23132.service-now.com/api/now/table/cmdb_ci_san_endpoint?sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_suppress_pagination_header=true&sysparm_fields=sys_id%2Cname%2Casset_tag%2Chost_name%2Cos_version%2Cserial_number%2Cshort_description%2Cowned_by%2Cclassification%2Cdepartment%2Clocation%2Cmanufacturer%2Cmodel_id%2Cos%2Ckernel_release%2Ccpu_count%2Cram%2Cdisk_space%2Cvirtual%2Cchassis_type%2Cmac_address%2Cip_address&sysparm_limit=10
 		*/
 
+	}
+
+	/**
+	 * Complete the table param with passed variables
+	 * @param fieldName - the field name variable
+	 * @param readOnly - the read only flag
+	 * @return a LinkedHashMap
+	 */
+	private LinkedHashMap tableParam(String fieldName, int readOnly) {
+		LinkedHashMap<String, ?> table = new LinkedHashMap<>(TABLE_PARAM)
+		table.put('fieldName', fieldName)
+		table.put('readOnly', readOnly)
+		return table;
 	}
 
 	/**
