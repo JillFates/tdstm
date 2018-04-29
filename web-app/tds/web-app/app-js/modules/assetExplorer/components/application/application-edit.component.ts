7345324ecb762250ff62a5359d65b53c726a82cf
Sam Zuniga <szuniga@transitionaldata.com>
2018-04-29 17:56:04 -0500

    TM-9671 Add shuffle person option
    Add initmodel 
    Add List [Yes, No}

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/application/application-edit.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/application/application-edit.component.ts
@@ -4,10 +4,17 @@
  *
  *  Use angular/views/TheAssetType as reference
  */
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ViewChild, Inject, OnInit } from '@angular/core';
 import { UIActiveDialogService } from '../../../../shared/services/ui-dialog.service';
 
 import { PreferenceService } from '../../../../shared/services/preference.service';
+import {DateUtils} from '../../../../shared/utils/date.utils';
+import * as R from 'ramda';
+
+interface SelectedItem {
+	class: string;
+	id: number;
+}
 
 export function ApplicationEditComponent(template: string, editModel: any): any {
 	@Component({
@@ -17,16 +24,62 @@ export function ApplicationEditComponent(template: string, editModel: any): any 
 			{ provide: 'model', useValue: editModel }
 		]
 	})
-	class ApplicationShowComponent {
+	class ApplicationShowComponent implements OnInit {
+		defaultItem = {fullName: 'Please Select', personId: 0};
+		yesNoList = ['Y', 'N'];
+		private dateFormat: string;
+
 		constructor( @Inject('model') private model: any, private activeDialog: UIActiveDialogService, private preference: PreferenceService) {}
 
 		ngOnInit(): void {
 			console.log('Loading application-edit.component');
+			this.dateFormat = this.preference.preferences['CURR_DT_FORMAT'];
+			this.dateFormat = this.dateFormat.toLowerCase().replace(/m/g, 'M');
+			this.initModel();
 		}
 
-		shufflePerson(control1: string, control2: string) {
-			console.log('Swapping controls');
+		private initModel(): void {
+			this.model.asset = R.clone(editModel.asset);
+			this.model.asset.retireDate = DateUtils.compose(this.model.asset.retireDate);
+			this.model.asset.maintExpDate = DateUtils.compose(this.model.asset.maintExpDate);
+
+			this.model.asset.sme = this.model.asset.sme || { id: null };
+			this.model.asset.sme2 = this.model.asset.sme2 || { id: null };
+			this.model.asset.appOwner = this.model.asset.appOwner || { id: null };
+
+			if (this.model.asset.scale === null) {
+				this.model.asset.scale = {
+					name: ''
+				};
+			}
+			this.model.asset.assetTypeSelectValue = {id: null};
+			if (this.model.asset.assetType) {
+				this.model.asset.assetTypeSelectValue.id = this.model.asset.assetType;
+				this.model.asset.assetTypeSelectValue.text = this.model.asset.assetType;
+			}
+			this.model.asset.manufacturerSelectValue = {id: null};
+			if (this.model.asset.manufacturer) {
+				this.model.asset.manufacturerSelectValue.id = this.model.asset.manufacturer.id;
+				this.model.asset.manufacturerSelectValue.text = this.model.asset.manufacturer.text;
+			}
 		}
+
+		getCurrentValue(control: string) {
+			console.log('get current value');
+		}
+
+		shufflePerson(source: string, target: string) {
+			const sourceId = this.model.asset && this.model.asset[source] && this.model.asset[source].id || null;
+			const targetId = this.model.asset && this.model.asset[target] && this.model.asset[target].id || null;
+
+			if (sourceId && targetId) {
+				const backSource = sourceId;
+
+				this.model.asset[source].id = targetId;
+				this.model.asset[target].id = backSource;
+			}
+		}
+
 	}
 
 	return ApplicationShowComponent;
