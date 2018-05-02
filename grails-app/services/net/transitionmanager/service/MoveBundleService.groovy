7ec1905d53ed5d2f7c8ce5adaa8f38ee5b8916bf
Esteban Cantu <ecantu@tdsi.com>
2018-05-02 15:08:42 -0300

    TM-10261 Change Dependency Analyzer grouping to exclude dependencies of non planning assets

--- grails-app/services/net/transitionmanager/service/MoveBundleService.groovy
+++ grails-app/services/net/transitionmanager/service/MoveBundleService.groovy
@@ -637,7 +637,7 @@ class MoveBundleService implements ServiceMethods {
 			def started = new Date()
 			progressService.update(progressKey, 10I, ProgressService.STARTED, "Search assets and dependencies")
 
-			def results = searchForAssetDependencies(moveBundleText, connectionTypes, statusTypes)
+			def results = searchForAssetDependencies(moveBundleText, moveBundleIds, connectionTypes, statusTypes)
 
 			logger.info 'Dependency groups generation - Search assets and dependencies time {}', TimeUtil.elapsed(started)
 			started = new Date()
@@ -691,17 +691,22 @@ class MoveBundleService implements ServiceMethods {
 	/**
 	 * Performs a search for all the assets and dependencies that match the parameters.
 	 * @param connectionTypes : filter for asset types on connection
-	 * @param statusTypes : filter for status tyoes
+	 * @param statusTypes : filter for status types
 	 * @param moveBundleText : bundle ids to analyze
 	 * @return List of records
 	 */
-	private def searchForAssetDependencies(moveBundleText, connectionTypes, statusTypes) {
+	private def searchForAssetDependencies(moveBundleText, List<Long> moveBundleIds, connectionTypes, statusTypes) {
+		// First we need the list of assets that belongs to planning bundles - See TM-10261
+		List<Long> assetIds = AssetEntity.where { moveBundle.id in moveBundleIds}.list()*.id
+		String assetIdsText = GormUtil.asCommaDelimitedString(assetIds)
+
 		// Query to fetch dependent asset list with dependency type and status and move bundle list with use for planning .
 		String  queryForAssets = "SELECT a.asset_entity_id as assetId, ad.asset_id as assetDepFromId, ad.dependent_id as assetDepToId, " +
 				" a.move_bundle_id as moveBundleId, ad.status as status, ad.type as type, a.asset_type as assetType " +
 				" FROM asset_entity a, asset_dependency ad " +
 				" WHERE a.move_bundle_id in ( ${moveBundleText} ) " +
-				" AND (a.asset_entity_id = ad.asset_id OR ad.dependent_id = a.asset_entity_id)"
+				" AND ((a.asset_entity_id = ad.asset_id AND ad.dependent_id IN ( ${assetIdsText} )) " +
+				" OR ad.dependent_id = a.asset_entity_id)"
 
 		queryForAssets += connectionTypes ? " AND ad.type in (${connectionTypes}) " : ""
 		queryForAssets += statusTypes ? " AND ad.status in (${statusTypes}) " : ""
