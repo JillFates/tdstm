b96d61e601110dd94185c854e3474cd1925f8a90
Jorge Morayta <jmorayta@tdsi.com>
2018-05-03 20:00:11 -0500

    TM-9671 Open View Mode after update the Asset Edit.

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/database/database-edit.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/database/database-edit.component.ts
@@ -5,11 +5,12 @@
  *  Use angular/views/TheAssetType as reference
  */
 import {Component, Inject, OnInit} from '@angular/core';
-import {UIActiveDialogService} from '../../../../shared/services/ui-dialog.service';
+import {UIActiveDialogService, UIDialogService} from '../../../../shared/services/ui-dialog.service';
 import {PreferenceService} from '../../../../shared/services/preference.service';
 import {AssetExplorerService} from '../../service/asset-explorer.service';
 import {DateUtils} from '../../../../shared/utils/date.utils';
 import * as R from 'ramda';
+import {AssetShowComponent} from "../asset/asset-show.component";
 
 declare var jQuery: any;
 
@@ -29,7 +30,8 @@ export function DatabaseEditComponent(template, editModel) {
 			@Inject('model') private model: any,
 			private activeDialog: UIActiveDialogService,
 			private preference: PreferenceService,
-			private assetExplorerService: AssetExplorerService) {
+			private assetExplorerService: AssetExplorerService,
+			private dialogService: UIDialogService) {
 
 			this.dateFormat = this.preference.preferences['CURR_DT_FORMAT'];
 			this.dateFormat = this.dateFormat.toLowerCase().replace(/m/g, 'M');
@@ -59,10 +61,31 @@ export function DatabaseEditComponent(template, editModel) {
 			this.activeDialog.close();
 		}
 
+		/**
+		 * Prepare te model and format all pending changes
+		 */
 		public onUpdate(): void {
-			this.assetExplorerService.saveAsset(this.model).subscribe((res) => {
-				console.log(res);
+			let modelRequest = R.clone(this.model);
+			// Scale Format
+			modelRequest.asset.scale = (modelRequest.asset.scale.name.value) ? modelRequest.asset.scale.name.value : modelRequest.asset.scale.name;
+			this.model.customs.forEach((custom: any) => {
+				let customValue = modelRequest.asset[custom.field.toString()];
+				if (customValue && customValue.value) {
+					modelRequest.asset[custom.field.toString()] = customValue.value;
+				}
 			});
+			this.assetExplorerService.saveAsset(modelRequest).subscribe((res) => {
+				console.log(res);
+				this.showAssetDetailView(this.model.asset.assetClass.name, this.model.assetId);
+			});
+		}
+
+
+		private showAssetDetailView(assetClass: string, id: number) {
+			this.dialogService.replace(AssetShowComponent, [
+					{ provide: 'ID', useValue: id },
+					{ provide: 'ASSET', useValue: assetClass }],
+				'lg');
 		}
 	}
 	return DatabaseShowComponent;
