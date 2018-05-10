33b3695e8194c3997b725583339193d1020fce25
Esteban Cantu <ecantu@tdsi.com>
2018-05-10 15:19:43 -0300

    TM-10115 corrected indentation.

--- test/integration/CredentialSpec.groovy
+++ test/integration/CredentialSpec.groovy
@@ -21,25 +21,25 @@ class CredentialSpec extends IntegrationSpec {
 
 		where:
 		method				| sessionName	| valExp				 | authUrl		 | renewUrl		| httpMethod 	| requestMode		| result
-		am.BASIC_AUTH		| ''			| '' 					 | ''			 | '' 			| null			| null				| true		// HAPPY PATH
-		am.BASIC_AUTH		| 'bogus'		| 'bogus'				 | ''			 | '' 			| null			| null				| true		// Still happy with bogus values that are unnecessary
-		am.JWT				| ''			| '' 					 | 'http://b.ic' | 'http://p.ic'| null			| null				| true		// HAPPY PATH
-		am.JWT				| 'bogus'		| 'bogus' 				 | 'http://b.ic' | 'http://p.ic'| null			| null				| true		// Still happy with bogus values that are unnecessary
-		am.JWT				| ''			| '' 					 | 'http://b.ic' | 'http://b.ic'| null			| null				| false		// Authentication and renewUrl can not be the same
-		am.JWT				| ''			| '' 					 | 'http://b.ic' | ''			| null			| null				| false		// Missing renewal URL
-		am.JWT				| ''			| '' 					 | ''		 	 | 'http://b.ic'| null			| null				| false		// Missing authentication URL
-		am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| true		// HAPPY PATH
-		am.COOKIE			| ''			| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing sessionName
-		am.COOKIE			| 'a@cookie:b'	| 'invalid'				 | 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Invalid validation expression
-		am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| ''			 | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing authentication URL
-		am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| 'http://b.ic' | ''			| null			| arm.BASIC_AUTH	| false		// Missing httpMethod
-		am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| null				| false		// Missing requestMode
-		am.HEADER			| 'a@header:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| true		// HAPPY PATH
-		am.HEADER			| ''			| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing sessionName
-		am.HEADER			| 'a@header:b'	| ''					 | 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Invalid validation expression
-		am.HEADER			| 'a@header:b'	| 'status code equal 200'| ''			 | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing authentication URL
-		am.HEADER			| 'a@header:b'	| 'status code equal 200'| 'http://b.ic' | ''			| null			| arm.BASIC_AUTH	| false		// Missing httpMethod
-		am.HEADER			| 'a@header:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| null				| false		// Missing requestmode
+			am.BASIC_AUTH		| ''			| '' 					 | ''			 | '' 			| null			| null				| true		// HAPPY PATH
+			am.BASIC_AUTH		| 'bogus'		| 'bogus'				 | ''			 | '' 			| null			| null				| true		// Still happy with bogus values that are unnecessary
+			am.JWT				| ''			| '' 					 | 'http://b.ic' | 'http://p.ic'| null			| null				| true		// HAPPY PATH
+			am.JWT				| 'bogus'		| 'bogus' 				 | 'http://b.ic' | 'http://p.ic'| null			| null				| true		// Still happy with bogus values that are unnecessary
+			am.JWT				| ''			| '' 					 | 'http://b.ic' | 'http://b.ic'| null			| null				| false		// Authentication and renewUrl can not be the same
+			am.JWT				| ''			| '' 					 | 'http://b.ic' | ''			| null			| null				| false		// Missing renewal URL
+			am.JWT				| ''			| '' 					 | ''		 	 | 'http://b.ic'| null			| null				| false		// Missing authentication URL
+			am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| true		// HAPPY PATH
+			am.COOKIE			| ''			| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing sessionName
+			am.COOKIE			| 'a@cookie:b'	| 'invalid'				 | 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Invalid validation expression
+			am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| ''			 | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing authentication URL
+			am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| 'http://b.ic' | ''			| null			| arm.BASIC_AUTH	| false		// Missing httpMethod
+			am.COOKIE			| 'a@cookie:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| null				| false		// Missing requestMode
+			am.HEADER			| 'a@header:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| true		// HAPPY PATH
+			am.HEADER			| ''			| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing sessionName
+			am.HEADER			| 'a@header:b'	| ''					 | 'http://b.ic' | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Invalid validation expression
+			am.HEADER			| 'a@header:b'	| 'status code equal 200'| ''			 | ''			| chm.POST		| arm.BASIC_AUTH	| false		// Missing authentication URL
+			am.HEADER			| 'a@header:b'	| 'status code equal 200'| 'http://b.ic' | ''			| null			| arm.BASIC_AUTH	| false		// Missing httpMethod
+			am.HEADER			| 'a@header:b'	| 'status code equal 200'| 'http://b.ic' | ''			| chm.POST		| null				| false		// Missing requestmode
 	}
 
 	/**
