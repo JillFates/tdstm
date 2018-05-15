235a6fb5ee07be36d9691c4d58ecf952b2073578
Sebastian <sbigatton@tdsi.com>
2018-05-15 18:13:25 -0300

    8505: try/catch wait for loader icon

--- test/e2e/src/test/groovy/modules/CommonsModule.groovy
+++ test/e2e/src/test/groovy/modules/CommonsModule.groovy
@@ -1,6 +1,7 @@
 package modules
 
 import geb.Module
+import geb.error.RequiredPageContentNotPresent
 
 class CommonsModule extends Module {
 
@@ -9,8 +10,15 @@ class CommonsModule extends Module {
     }
 
     def waitForLoader() {
-        waitFor { $('#main-loader') }
-        waitFor { !$('#main-loader') }
+        try {
+            // try to wait loader icon is displayed then gone loading page content
+            // there are big pages where lot of information is loaded
+            waitFor { $('#main-loader') }
+            waitFor { !$('#main-loader') }
+        } catch (RequiredPageContentNotPresent e) {
+            // nothing to do here, in case server manage fast the page information
+            // and the loader icon is not detected, just prevent test fails
+        }
     }
 
     def waitForGlobalProgressBarModal(){
