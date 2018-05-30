1f545c877bc08af9fcefcd8c1714798fd70f91b1
Jorge Morayta <jmorayta@tdsi.com>
2018-05-30 08:42:14 -0500

    TM-8306 Created Detail view for Asset Comments.

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/single-comment/single-comment.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/single-comment/single-comment.component.ts
@@ -1,23 +1,23 @@
-import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {SingleCommentModel} from './single-comment.model';
 import {KEYSTROKE, ModalType} from '../../../../shared/model/constants';
 import {UIExtraDialog} from '../../../../shared/services/ui-dialog.service';
+import {PreferenceService} from '../../../../shared/services/preference.service';
+import {DateUtils} from '../../../../shared/utils/date.utils';
 
 @Component({
 	selector: `single-comment`,
 	templateUrl: '../tds/web-app/app-js/modules/assetExplorer/components/single-comment/single-comment.component.html',
 	styles: []
 })
-export class SingleCommentComponent  extends UIExtraDialog  implements OnInit  {
+export class SingleCommentComponent  extends UIExtraDialog {
 
 	public modalType = ModalType;
+	public dateFormatTime: string;
 
-	constructor(public singleCommentModel: SingleCommentModel) {
+	constructor(public singleCommentModel: SingleCommentModel, public userPreferenceService: PreferenceService) {
 		super('#single-comment-component');
-	}
-
-	ngOnInit(): void {
-		console.log('init');
+		this.dateFormatTime = this.userPreferenceService.getUserTimeZone() + ' ' + DateUtils.DEFAULT_FORMAT_TIME;
 	}
 
 	/**
