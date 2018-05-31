e788931a7123e77925d5f259c4141c50e7b7dcbd
Sebastian <sbigatton@tdsi.com>
2018-05-31 17:06:52 -0300

    E2E Scripts: fix applicationCreationSpec

--- test/e2e/src/test/groovy/specs/Assets/ApplicationCreationSpec.groovy
+++ test/e2e/src/test/groovy/specs/Assets/ApplicationCreationSpec.groovy
@@ -123,9 +123,9 @@ class ApplicationCreationSpec extends GebReportingSpec {
 
         then: 'The User Searches by the AppName, SME1, SME2, AppOwner'
         // TODO some items cannot located due to missing ID's
-        waitFor{adModalAppName[1].text().trim() == appName}
+        waitFor{adModalAppName.text().trim() == appName}
         adModalSME1.text().trim() == appSME1
-        adModalSME2.text().trim() == appSME2
+        adModalSME2.text().trim().contains(appSME2) // contains(because user can part of a different company)
         adModalAppOwner.text().trim() == appOwner
     }
 }
