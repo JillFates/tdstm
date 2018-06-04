2d2f0d037d85b4e46631f425027789b20143c44f
Sam Zuniga <szuniga@transitionaldata.com>
2018-06-04 10:06:26 -0500

    TM-10677 Unset initial width/height of textarea control

--- web-app/tds/web-app/app-js/modules/dataIngestion/components/data-script-console/data-script-console.component.ts
+++ web-app/tds/web-app/app-js/modules/dataIngestion/components/data-script-console/data-script-console.component.ts
@@ -4,41 +4,23 @@ import {ScriptConsoleSettingsModel} from '../../model/script-result.models';
 
 declare var jQuery: any;
 const DIALOG_ID = '#viewConsole';
-const TEXT_AREA_LOG_ID = '#textAreaLog';
 
 @Component({
 	selector: 'data-script-console',
 	templateUrl: '../tds/web-app/app-js/modules/dataIngestion/components/data-script-console/data-script-console.component.html'
 })
 export class DataScriptConsoleComponent extends UIExtraDialog implements AfterViewInit {
-
-	private dialogInstance: any;
-	private textAreaInstance: any;
-
-	constructor(private consoleSettings: ScriptConsoleSettingsModel) {
+	constructor(public consoleSettings: ScriptConsoleSettingsModel) {
 		super(DIALOG_ID);
 	}
 
 	ngAfterViewInit(): void {
-		this.dialogInstance = jQuery(DIALOG_ID);
-		this.textAreaInstance = jQuery(TEXT_AREA_LOG_ID);
-		// fix to resize modal on textarea resize
-		this.textAreaInstance.resizable({
-			resize: function (event) {
-				let modalDialogInstance = jQuery('#console-modal-dialog');
-				modalDialogInstance.css('width', event.target.offsetWidth);
-			}
-		});
 	}
 
 	/**
 	 * On close dialog store position/size settings.
 	 */
 	protected cancelCloseDialog(): void {
-		this.consoleSettings.top =  parseInt(this.dialogInstance.css('top'), 10);
-		this.consoleSettings.left = parseInt(this.dialogInstance.css('left'), 10);
-		this.consoleSettings.height = parseInt(this.textAreaInstance.css('height'), 10);
-		this.consoleSettings.width = parseInt(this.textAreaInstance.css('width'), 10);
 		this.dismiss();
 	}
 
