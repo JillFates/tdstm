e34925f1bdc853e804c1fafec737aaed98fc6ccd
Diego Correa <dcorrea@tdsi.com>
2018-06-05 13:38:55 -0300

    TM-11007 Fix Test case in 4.4.3

--- test/unit/ScriptProcessorServiceSpec.groovy
+++ test/unit/ScriptProcessorServiceSpec.groovy
@@ -439,7 +439,7 @@ application id,vendor name,technology,location
 		and:
 			GroovyMock(AssetEntity, global: true)
 			AssetEntity.executeQuery(_, _, _) >> { String query, Map namedParams, Map metaParams ->
-				applications.findAll { it.id == namedParams.id && it.project.id == namedParams.project.id }
+				applications.findAll { it.id == namedParams.id && it.project.id == namedParams.project.id }*.getId()
 			}
 			AssetEntity.isAssignableFrom(_) >> { Class<?> clazz ->
 				return true
