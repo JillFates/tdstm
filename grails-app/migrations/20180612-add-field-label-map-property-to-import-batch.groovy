de117f66613145449b8d8c382008c6a904b5aa92
Diego Correa <dcorrea@tdsi.com>
2018-06-13 13:03:11 -0300

    TM-11076 Add initialization of previous field_label_map

--- grails-app/migrations/20180612-add-field-label-map-property-to-import-batch.groovy
+++ grails-app/migrations/20180612-add-field-label-map-property-to-import-batch.groovy
@@ -6,8 +6,16 @@ databaseChangeLog = {
 		comment('Add fieldLabelMap property to ImportBatch domain')
 		sql ("""
                 ALTER TABLE `import_batch`
-					ADD COLUMN `field_label_map` JSON NOT NULL after `field_name_list`;
+					ADD COLUMN `field_label_map` JSON after `field_name_list`;
  		""")
+
+		sql ("""
+                 UPDATE `import_batch`
+					SET `field_label_map` = '{}'
+				  WHERE `field_label_map` IS NULL;
+ 		""")
+
+		sql("ALTER TABLE `import_batch` MODIFY `field_label_map` JSON NOT NULL")
 	}
 
 }
