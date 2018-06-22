16949f4f51c26f9d57c9c8a08fea157c7d8a689f
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-06-22 10:48:40 -0400

    TM-11109 adding service to take in bulk changes and delegate them out.

---
+++ grails-app/services/net/transitionmanager/service/BulkAssetChangeService.groovy
@@ -0,0 +1,69 @@
+package net.transitionmanager.service
+
+import com.tds.asset.AssetEntity
+import grails.transaction.Transactional
+import net.transitionmanager.command.bulk.BulkChangeCommand
+import net.transitionmanager.command.bulk.EditCommand
+import net.transitionmanager.domain.Project
+
+/**
+ * This handles taking in a bulk change json and delegating the bulk change to the appropriate service.
+ */
+@Transactional
+class BulkAssetChangeService implements ServiceMethods {
+	TagAssetService tagAssetService
+	DataviewService dataviewService
+
+	/**
+	 * A list of valid field names
+	 */
+	static final List<String> fields = ['tags']
+
+	/**
+	 * A map of field names to actions, and to the methods that support them.
+	 */
+	static final Map actions = [
+		tags: [
+			add    : 'bulkAdd',
+			clear  : 'bulkRemove',
+			replace: 'bulkReplace',
+			remove : 'bulkRemove'
+		]
+	]
+
+	/**
+	 * Handles the bulk change json, and delegates the bulk change to the appropriate service.
+	 *
+	 * @param currentProject used fot checking security.
+	 * @param bulkChange the command object that holds the bulk change json.
+	 */
+	void bulkChange(Project currentProject, BulkChangeCommand bulkChange) {
+		List assetIds = []
+		Map assetQueryFilter = [:]
+		def service
+		def value
+		String action
+
+		//Maps field names to services.
+		Map filedToService = [
+			tags: tagAssetService
+		]
+
+		if (bulkChange.allAssets) {
+			assetQueryFilter = dataviewService.getAssetIdsHql(currentProject, bulkChange.dataViewId, bulkChange.userParams)
+		} else {
+			assetIds = bulkChange.assetIds
+			assetIds.each { Long id ->
+				get(AssetEntity, id, currentProject)
+			}
+		}
+
+		bulkChange.edits.each { EditCommand edit ->
+			service = filedToService[edit.fieldName]
+			action = actions[edit.fieldName][edit.action]
+			value = service.coerceBulkValue(currentProject, edit.value)
+
+			service."$action"(value, assetIds, assetQueryFilter)
+		}
+	}
+}
