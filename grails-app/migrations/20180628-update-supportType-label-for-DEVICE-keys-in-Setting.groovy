528d086a6008dbba9f07a8396372d36de659d4c1
Diego Correa <dcorrea@tdsi.com>
2018-06-28 18:11:49 -0300

    TM-11229 Refactor DEVICE.supportType field label in Setting domain class

---
+++ grails-app/migrations/20180628-update-supportType-label-for-DEVICE-keys-in-Setting.groovy
@@ -0,0 +1,33 @@
+import com.tdsops.tm.enums.domain.AssetClass
+import com.tdsops.tm.enums.domain.SettingType
+import net.transitionmanager.domain.Setting
+import net.transitionmanager.service.DatabaseMigrationService
+import org.codehaus.groovy.grails.web.json.JSONObject
+
+databaseChangeLog = {
+	changeSet(author: 'dcorrea', id: 'TM-11229') {
+		comment('Change Device.SupportType label for Device.Support label')
+		grailsChange {
+			change {
+				// Fetch all the existing settings
+				List<Setting> settingList = Setting.findAllByTypeAndKey(SettingType.CUSTOM_DOMAIN_FIELD_SPEC, AssetClass.DEVICE)
+
+				// Script that will iterate over the list of fields of a Setting and replace 'SupportType' for 'Support' in field 'supportType'
+				Closure updateSettingScript = { JSONObject settingJson ->
+					for (field in settingJson.fields) {
+						if(field.field == 'supportType'){
+							field.label = 'Support'
+						}
+					}
+					return settingJson
+				}
+
+				DatabaseMigrationService databaseMigrationService = ctx.getBean("databaseMigrationService")
+
+				databaseMigrationService.updateJsonObjects(settingList, "json", updateSettingScript)
+			}
+		}
+
+	}
+}
+
