36a4f48dacb1820cfc32539e7dc66a3cb4b3c3b2
Jorge Morayta <jmorayta@tdsi.com>
2018-06-29 08:59:16 -0500

    TM-11009 Created initial angular1 directive for the Tag System.

---
+++ web-app/js/shared/asset-tag-selector/tmAssetTagSelectorDirective.js
@@ -0,0 +1,36 @@
+tds.cookbook.directive.TmAssetTagSelectorDirective = function ($http, utils) {
+	return {
+		template: `<div class="asset-tag-selector-component">
+						<label>Tag:</label>
+						<select class="asset-tag-selector"></select>
+						
+						<script id="asset-tag-selector-item" type="text/x-kendo-template">
+							<div class="#:data.css#">#:data.Name#</div>
+						</script>
+						
+						<script id="asset-tag-selector-tag" type="text/x-kendo-template">
+							<div class="#:data.css#">#:data.Name#</div>
+						</script>
+					</div>`,
+		restrict: 'E',
+		controller: function ($scope) {
+			$(".asset-tag-selector").kendoMultiSelect({
+				itemTemplate: $("#asset-tag-selector-item").html(),
+				tagTemplate: $("#asset-tag-selector-tag").html(),
+				dataSource: {
+					transport: {
+						read: (e) => {
+							$http.get(utils.url.applyRootPath('/ws/tag')).success(function (data, status, headers, config) {
+								return e.success(data);
+							}). error(function (data, status, headers, config) {
+								return e.success([]);
+							});
+						}
+					}
+				}
+			});
+		}
+	};
+}
+
+tds.cookbook.module.directive('tmAssetTagSelector', ['$http', 'utils', tds.cookbook.directive.TmAssetTagSelectorDirective]);
