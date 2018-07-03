e942c1e0e149691a19623d8f07919f9b80d4af23
Sebastian <sbigatton@tdsi.com>
2018-07-03 16:47:00 -0300

    TM-11149: Remove duplicated selector

--- test/e2e/src/test/groovy/pages/Datascripts/DatascriptDetailsPage.groovy
+++ test/e2e/src/test/groovy/pages/Datascripts/DatascriptDetailsPage.groovy
@@ -15,7 +15,6 @@ class DatascriptDetailsPage extends Page{
         modaltitle(required:false) { $("div", class:"modal-header").find("h4" , class:"modal-title")[0]}
         datascriptDetail { $('div.modal-header h4[_ngcontent-c7]')}
         dsDetailXIcon {$("div", class:"modal-header").find("button","aria-label":"Close", class:"close")[0]}
-        dsDesignerButton {$("data-script-view-edit").find("button", text: contains("DataScript Designer"))}
         dsProvider {$('.label-detail')[0]}
         dsName {$('.label-detail')[1]}
         dsMode {$('.label-detail')[2]}
