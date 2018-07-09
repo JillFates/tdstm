b99070f2df2cfbdb8028b7cf89157502e88e74c1
Sam Zuniga <szuniga@transitionaldata.com>
2018-07-09 12:46:32 -0500

    TM-11214 Hide on init auto-center

--- web-app/tds/web-app/app-js/shared/directives/autocenter-directive.ts
+++ web-app/tds/web-app/app-js/shared/directives/autocenter-directive.ts
@@ -1,12 +1,27 @@
 /**
  * UI AutoCenter Directive, center horizontally the attached component
  */
-import {Directive, AfterViewInit, ElementRef} from '@angular/core';
+import {Directive, AfterViewInit, OnInit, ElementRef} from '@angular/core';
 
 declare var jQuery: any;
 @Directive({ selector: '[tds-autocenter]' })
-export class UIAutoCenterDirective implements AfterViewInit {
+export class UIAutoCenterDirective implements AfterViewInit, OnInit {
+	private modalDialog: any;
+	private modalContent: any;
 	constructor(private el: ElementRef) {
+	}
+
+	ngOnInit() {
+		this.modalDialog = jQuery(jQuery(this.el.nativeElement).closest('.modal-dialog'));
+		this.modalContent = jQuery(this.el.nativeElement);
+
+		// hide to avoid flashing
+		if (this.modalDialog) {
+			jQuery(this.modalDialog).css({
+				'display': 'block',
+				'visibility': 'hidden'
+			});
+		}
 	}
 
 	ngAfterViewInit() {
@@ -17,24 +32,20 @@ export class UIAutoCenterDirective implements AfterViewInit {
 	 * Center horizontally the host component calculating the left position using the widths of window and the host component
 	 */
 	private center(): void {
-		let modalDialog = jQuery(jQuery(this.el.nativeElement).closest('.modal-dialog'));
-		const modalContent = jQuery(this.el.nativeElement);
+		if (this.modalDialog) {
+			const left = (jQuery(window).width() - this.modalContent.width()) / 2;
 
-		const left = (jQuery(window).width() - modalContent.width()) / 2;
-
-		// handle container width whenever the content exceeds the container width
-		const width = modalContent.width() > modalDialog.width() ? modalContent.width()  : modalDialog.width();
-
-		jQuery(modalDialog).css({
-			'display': 'block',
-			'visibility': 'hidden'
-		}).css({
-			'top': '30px',
-			'left': `${left}px`,
-			'width': `${width}px`
-		}).css({
-			'visibility': 'visible'
-		});
+			// handle container width whenever the content exceeds the container width
+			const width = this.modalContent.width() > this.modalDialog.width() ? this.modalContent.width()  : this.modalDialog.width();
+			jQuery(this.modalDialog)
+				.css({
+					'top': '30px',
+					'left': `${left}px`,
+					'width': `${width}px`
+				}).css({
+				'visibility': 'visible'
+			});
+		}
 	}
 
 }
