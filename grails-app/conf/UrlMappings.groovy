d5281f42c8ec6f6752a2fce8f5deec0a66a01311
Octavio Luna <oluna@tdsi.com>
2018-07-11 12:52:00 -0500

    TM-9749 saving the uploaded filename in the DataScript Designer

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -806,7 +806,14 @@ class UrlMappings {
 
 		/*
 		 * Get the sample filename related to a DataScript Object
-		 */
+		 */ 
+		"/ws/dataingestion/datascript/$id/source" {
+			controller = "wsDataScript"
+			action = [
+					  GET: "getDataScriptSourceCode"
+			]
+		}
+
 		"/ws/dataingestion/datascript/$id/sampleData/$filename" {
 			controller = "wsDataScript"
 			action = [
