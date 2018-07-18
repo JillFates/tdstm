e1d0cfda49a9beab516fe6e55b57cd10cbddbf3e
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-07-18 10:42:32 -0400

    TM-11081 making context_id nullable

--- grails-app/migrations/20180713-drop-context-type.groovy
+++ grails-app/migrations/20180713-drop-context-type.groovy
@@ -8,4 +8,10 @@ databaseChangeLog = {
 
 		dropColumn(tableName: 'task_batch', columnName: 'context_type')
 	}
+
+	changeSet(author: 'tpelletier', id: 'TM-11077-2-2') {
+		comment('Make contextId in task batch nullable')
+
+		dropNotNullConstraint(tableName: 'task_batch', columnName: 'context_id', columnDataType:'BIGINT(20)')
+	}
 }
