b0e9170babf453adcba4a5476df667e165a01df7
Esteban Cantu <ecantu@tdsi.com>
2018-07-23 17:37:13 -0300

    TM-11518 Asset Export not able to export assets

---
+++ grails-app/migrations/20180627-change-tag-field-to-tagAssets-for-common-fields.groovy
@@ -0,0 +1,42 @@
+import com.tdsops.tm.enums.domain.SettingType
+import net.transitionmanager.domain.Setting
+import net.transitionmanager.service.DatabaseMigrationService
+
+
+/**
+ * Add the AssetClass field to the field settings for every project.
+ */
+
+databaseChangeLog = {
+
+    changeSet(author: "ecantu", id: "20180627 TM-11518") {
+        comment("Update the field name from 'tags' to 'tagAssets'.")
+
+        grailsChange {
+            change {
+                // List all the field settings for the different domains.
+                List<Setting> fieldSettings = Setting.where {
+                    type == SettingType.CUSTOM_DOMAIN_FIELD_SPEC
+                }.list()
+
+                // This closure replace changes the 'tags' field (if exists) to 'tagAssets'.
+                Closure changeScript = { settingsJson ->
+                    // Add the field to the list of fields for this domain
+                    if(settingsJson.fields) {
+                        def mapForTags = settingsJson.fields.find { it.field == "tags"}
+                        if (mapForTags) {
+                            mapForTags.field = "tagAssets"
+                        }
+                    }
+                    return settingsJson
+                }
+
+
+                DatabaseMigrationService databaseMigrationService = ctx.getBean("databaseMigrationService")
+                // Update the different settings with the Asset Class fields.
+                databaseMigrationService.updateJsonObjects(fieldSettings, "json", changeScript)
+
+            }
+        }
+    }
+}
--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -255,5 +255,6 @@ databaseChangeLog = {
 	include file: '20180628-update-supportType-label-for-DEVICE-keys-in-Setting.groovy'
 	include file: '20180627-add-tag-to-common-fields.groovy'
 	include file: '20180702-create-api-catalog-table.groovy'
+	include file: '20180627-change-tag-field-to-tagAssets-for-common-fields.groovy'
 
 }
