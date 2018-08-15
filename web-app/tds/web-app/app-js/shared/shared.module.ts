f645c60bf95f01241fa919fe27e55951e55af7f0
Sam Zuniga <szuniga@transitionaldata.com>
2018-08-15 13:09:07 -0500

    TM-10707 Display show device model and device manufacturer views

--- web-app/tds/web-app/app-js/shared/shared.module.ts
+++ web-app/tds/web-app/app-js/shared/shared.module.ts
@@ -43,6 +43,8 @@ import { SupportsDependsComponent } from './components/supports-depends/supports
 import { DependentCommentComponent } from './components/dependent-comment/dependent-comment.component';
 import { AddPersonComponent } from './components/add-person/add-person.component';
 import { AssetTagSelectorComponent } from './components/asset-tag-selector/asset-tag-selector.component';
+import { AkaComponent } from './components/aka/aka.component';
+import { ConnectorComponent } from './components/connector/connector.component';
 // Dictionaries
 import { en_DICTIONARY } from './i18n/en.dictionary';
 // Pages
@@ -93,7 +95,9 @@ import { DictionaryService } from './services/dictionary.service';
 		UIModalDecoratorDirective,
 		AddPersonComponent,
 		AssetTagSelectorComponent,
-		UIAutoCenterDirective
+		UIAutoCenterDirective,
+		AkaComponent,
+		ConnectorComponent
 	],
 	providers: [
 		AuthService,
@@ -135,7 +139,9 @@ import { DictionaryService } from './services/dictionary.service';
 		UIModalDecoratorDirective,
 		AddPersonComponent,
 		AssetTagSelectorComponent,
-		UIAutoCenterDirective
+		UIAutoCenterDirective,
+		AkaComponent,
+		ConnectorComponent
 	],
 	entryComponents: [
 		DynamicComponent,
