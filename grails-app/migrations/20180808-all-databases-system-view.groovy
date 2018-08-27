835c2c7729a7834e774d880623b20cc86299cd4f
Sidar LC <slopez@tdsi.com>
2018-08-27 10:06:25 -0600

    TM-9670: (Remove exta columns to match XLS columns order)

--- grails-app/migrations/20180808-all-databases-system-view.groovy
+++ grails-app/migrations/20180808-all-databases-system-view.groovy
@@ -27,16 +27,7 @@ databaseChangeLog = {
 							{"domain":"database","property":"rateOfChange","width":200,"locked":false,"edit":false,"label":"Rate Of Change","filter":""},
 							{"domain":"common","property":"supportType","width":200,"locked":false,"edit":false,"label":"Support","filter":""},
 							{"domain":"database","property":"retireDate","width":200,"locked":false,"edit":false,"label":"Retire Date","filter":""},
-							{"domain":"database","property":"maintExpDate","width":200,"locked":false,"edit":false,"label":"Maint Expiration","filter":""},
-							{"domain":"database","property":"custom1","width":200,"locked":false,"edit":false,"label":"Network Interfaces","filter":""},
-							{"domain":"database","property":"custom2","width":200,"locked":false,"edit":false,"label":"Custom2","filter":""},
-							{"domain":"database","property":"custom3","width":200,"locked":false,"edit":false,"label":"Cost Basis","filter":""},
-							{"domain":"database","property":"custom4","width":200,"locked":false,"edit":false,"label":"Custom4","filter":""},
-							{"domain":"database","property":"custom5","width":200,"locked":false,"edit":false,"label":"Custom5","filter":""},
-							{"domain":"database","property":"custom6","width":200,"locked":false,"edit":false,"label":"Custom6","filter":""},
-							{"domain":"common","property":"custom7","width":200,"locked":false,"edit":false,"label":"CNN","filter":""},
-							{"domain":"database","property":"custom8","width":200,"locked":false,"edit":false,"label":"Custom8","filter":""},
-							{"domain":"common","property":"custom9","width":200,"locked":false,"edit":false,"label":"IPs","filter":""}
+							{"domain":"database","property":"maintExpDate","width":200,"locked":false,"edit":false,"label":"Maint Expiration","filter":""}
                         ]
                     }
                     '''.stripIndent()
