ffc30010f0a8980e1fe98d598ff6339866d55001
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-09-06 08:41:44 -0400

    TM-12175 fixing another test.

--- src/groovy/test/helper/ApplicationTestHelper.groovy
+++ src/groovy/test/helper/ApplicationTestHelper.groovy
@@ -5,19 +5,23 @@ import com.tds.asset.AssetEntity
 import com.tdsops.tm.enums.domain.AssetClass
 import net.transitionmanager.domain.MoveBundle
 import net.transitionmanager.domain.Project
+import net.transitionmanager.service.CustomDomainService
 import org.apache.commons.lang3.RandomStringUtils
 
 class ApplicationTestHelper {
 
-    Application createApplication(AssetClass assetClass, Project project, MoveBundle moveBundle) {
-        Application application = new Application(
-                project: project,
-                moveBundle: moveBundle,
-                assetClass: assetClass,
-                assetName: 'Test AssetEntity-' + RandomStringUtils.randomAlphabetic(10)
-        ).save(flush: true, failOnError: true)
+	Application createApplication(AssetClass assetClass, Project project, MoveBundle moveBundle) {
+		Application application = new Application(
+			project: project,
+			moveBundle: moveBundle,
+			assetClass: assetClass,
+			assetName: 'Test AssetEntity-' + RandomStringUtils.randomAlphabetic(10)
+		)
 
-        return application
-    }
+		application.customDomainService = new CustomDomainService()
+		application.save(flush: true, failOnError: true)
+
+		return application
+	}
 
 }
