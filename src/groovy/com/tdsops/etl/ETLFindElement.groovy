1615a250fa4c8e083c6f05d2da9ccd6670c07b9d
Diego Correa <dcorrea@tdsi.com>
2018-09-06 21:46:32 -0300

    TM-10696 Remove incorrect validation

--- src/groovy/com/tdsops/etl/ETLFindElement.groovy
+++ src/groovy/com/tdsops/etl/ETLFindElement.groovy
@@ -94,9 +94,6 @@ class ETLFindElement implements ETLStackableCommand {
 	 * @return
 	 */
 	ETLFindElement into(String property) {
-		if(!this.currentFind.kv){
-			throw ETLProcessorException.incorrectFindCommandStructure()
-		}
 		validateReference(property)
 		currentFind.property = property
 		currentFind.fieldDefinition = processor.lookUpFieldDefinitionForCurrentDomain(property)
