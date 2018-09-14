d7958f30711fa2ec97c498234cb4eea2c6d03fbb
Sam Zuniga <szuniga@transitionaldata.com>
2018-09-14 07:17:46 -0500

    TM-12249 Handle esc key, warning about loosing changes

--- web-app/tds/web-app/app-js/shared/components/add-person/add-person.component.ts
+++ web-app/tds/web-app/app-js/shared/components/add-person/add-person.component.ts
@@ -3,6 +3,7 @@ import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 import { UIExtraDialog} from '../../../shared/services/ui-dialog.service';
 import { PersonModel } from './model/person.model';
 import { PersonService } from '../../services/person.service';
+import { UIPromptService} from '../../directives/ui-prompt.directive';
 
 @Component({
 	selector: 'add-person',
@@ -12,9 +13,11 @@ export class AddPersonComponent extends UIExtraDialog  implements  OnInit {
 	yesNoList = ['Y', 'N'];
 	teams: any[] = [];
 	errors: any;
+	dataSignature: string;
 	constructor(
 		public personModel: PersonModel,
-		private personService: PersonService) {
+		private personService: PersonService,
+		private promptService: UIPromptService) {
 		super('#add-person-component');
 		this.errors = {};
 	}
@@ -39,15 +42,49 @@ export class AddPersonComponent extends UIExtraDialog  implements  OnInit {
 			selectedTeams: []
 		};
 		this.personModel = Object.assign({},  defaultPerson, this.personModel)
+		this.dataSignature = JSON.stringify(this.personModel);
+	}
+	/**
+	 * Verify the Object has not changed
+	 * @returns {boolean}
+	 */
+	protected isDirty(): boolean {
+		return this.dataSignature !== JSON.stringify(this.personModel) || this.teams.length > 0;
 	}
 
-	/***
-	 * Close the Active Dialog
+	/**
+	 * Verify if user filled all required fiedls
+	 * @returns {boolean}
+	 */
+	protected canSave(): boolean {
+		const errors = this.errors || {};
+		return Boolean(this.personModel.firstName.trim() && this.personModel.active && Object.keys(errors).length === 0);
+	}
+
+	/**
+	 * Close the Dialog but first it verify is not Dirty
 	 */
 	protected cancelCloseDialog(): void {
-		this.dismiss();
+		if (this.isDirty()) {
+
+			this.promptService.open(
+				'Confirmation Required',
+				'You have changes that have not been saved. Do you want to continue and lose those changes?',
+				'Confirm', 'Cancel')
+				.then(confirm => {
+					if (confirm) {
+						this.dismiss();
+					}
+				})
+				.catch((error) => console.log(error));
+		} else {
+			this.dismiss();
+		}
 	}
 
+	/**
+	 *  Add a team
+	 */
 	addTeam(): void {
 		let defaultTeam = null;
 		if (this.personModel.teams && this.personModel.teams.length) {
@@ -58,14 +95,23 @@ export class AddPersonComponent extends UIExtraDialog  implements  OnInit {
 		this.teams.push(team);
 	}
 
+	/**
+	 *  Remove teams located in index position
+	 */
 	removeTeam(index: number): void {
 		this.teams.splice(index, 1);
 	}
 
+	/**
+	 *  Save the reference to current team selected
+	 */
 	onTeamSelected(team: any, index: number): void {
 		this.teams[index].team = team;
 	}
 
+	/**
+	 *  Save changes  after passing validation process
+	 */
 	save(): void {
 		this.validateFields()
 			.then(() => {
@@ -84,6 +130,9 @@ export class AddPersonComponent extends UIExtraDialog  implements  OnInit {
 			.catch(err => console.log('Error validating: ', err.message || err))
 	}
 
+	/**
+	 *  Validates fields meet with proper values based on data types
+	 */
 	validateFields(): Promise<boolean> {
 		const emailExp = /^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]+\.[a-zA-Z]{2,4})+$/
 		const mobileExp = /^([0-9 +-])+$/
@@ -110,11 +159,9 @@ export class AddPersonComponent extends UIExtraDialog  implements  OnInit {
 			return this.errors && Object.keys(this.errors).length ?  reject(new Error('Error validating fields')) : resolve(true); });
 	}
 
-	canSave(): boolean {
-		const errors = this.errors || {};
-		return Boolean(this.personModel.firstName.trim() && this.personModel.active && Object.keys(errors).length === 0);
-	}
-
+	/**
+	 *  Clean errors collection
+	 */
 	resetFieldError(fieldName: string): void {
 		if (this.errors &&  this.errors[fieldName]) {
 			delete this.errors[fieldName];
