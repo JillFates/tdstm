d17a0a568fd56ad31654293c844950bc6656b368
Augusto Recordon <arecordon@transitionaldata.com>
2018-09-21 15:07:40 -0300

    Revert "TM-12393 Fix issue in Asset Import caused by assetType being hardcoded to 'Server' for all classes."
    This reverts commit 0413f850564bbe0f00092e61cc4c018c1ffe7590.

--- grails-app/domain/com/tds/asset/AssetEntity.groovy
+++ grails-app/domain/com/tds/asset/AssetEntity.groovy
@@ -60,7 +60,7 @@ class AssetEntity {
 	String application = ''
 	String assetName
 	String shortName
-	String assetType = AssetType.getDefaultAssetTypeForDomain(this).name
+	String assetType = 'Server'
 	Integer priority
 	String planStatus = UNASSIGNED
 	Date   purchaseDate
