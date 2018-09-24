37c44d3e803a95513683bdee0234d1db66d99a0e
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-09-24 15:39:02 -0400

    TM-12280 updating db migration, for validation/planstatus/movebundle

---
+++ grails-app/migrations/20180919-add-bulk-move-bundle-to-file-specs.groovy
@@ -0,0 +1,45 @@
+import com.tdsops.tm.enums.domain.SettingType
+import net.transitionmanager.domain.Setting
+import net.transitionmanager.service.DatabaseMigrationService
+
+
+/**
+ * Modify the field specs to have mapping for bulk actions and service.
+ */
+databaseChangeLog = {
+
+	changeSet(author: "tpelletier", id: "20180919 TM-12280") {
+		comment("Update field specs to have bulk service and actions for move bundle.")
+
+		grailsChange {
+			change {
+				// List all the field settings for the different domains.
+				List<Setting> fieldSettings = Setting.where {
+					type == SettingType.CUSTOM_DOMAIN_FIELD_SPEC
+				}.list()
+
+				// This closure adds the service and actions to moveBundle.
+				Closure changeScript = { settingsJson ->
+					if (settingsJson.fields) {
+						//Adds the service and actions for moveBundle
+						def mapForMoveBundle = settingsJson.fields.find { it.field == "moveBundle" }
+
+						if (mapForMoveBundle) {
+							mapForMoveBundle.bulkChangeService = "bulkChangeMoveBundleService"
+							mapForMoveBundle.bulkChangeActions = [
+								replace: 'bulkReplace'
+							]
+						}
+					}
+					return settingsJson
+				}
+
+
+				DatabaseMigrationService databaseMigrationService = ctx.getBean("databaseMigrationService")
+				// Update the different settings with the Asset Class fields.
+				databaseMigrationService.updateJsonObjects(fieldSettings, "json", changeScript)
+
+			}
+		}
+	}
+}
