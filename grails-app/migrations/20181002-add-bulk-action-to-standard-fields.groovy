b8f83e4ca309b7719e8c10bc02d0ac771c5c7482
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-10-04 09:44:09 -0400

    TM-12271 adding db migration for standard fields.

---
+++ grails-app/migrations/20181002-add-bulk-action-to-standard-fields.groovy
@@ -0,0 +1,68 @@
+import com.tdsops.tm.enums.domain.SettingType
+import net.transitionmanager.domain.Setting
+import net.transitionmanager.service.DatabaseMigrationService
+
+
+/**
+ * Modify the field specs to have mapping for bulk actions and service.
+ */
+databaseChangeLog = {
+
+	changeSet(author: "tpelletier", id: "20180919 TM-12280-2") {
+		comment("Update field specs to have bulk actions for string fields.")
+
+		grailsChange {
+			change {
+				// List all the field settings for the different domains.
+				List<Setting> fieldSettings = Setting.where {
+					type == SettingType.CUSTOM_DOMAIN_FIELD_SPEC
+				}.list()
+
+				// This closure adds the service and actions to moveBundle.
+				Closure changeScript = { settingsJson ->
+					if (settingsJson.fields) {
+						List<String> lookUpFields = [
+							'criticality',
+							'latency',
+							'maintExpDate',
+							'railType',
+							'rateOfChange',
+							'retireDate',
+							'shutdownDuration',
+							'shutdownFixed',
+							'size',
+							'sourceBladePosition',
+							'sourceRackPosition',
+							'startupDuration',
+							'startupFixed',
+							'startupProc',
+							'targetBladePosition',
+							'targetRackPosition',
+							'testingDuration',
+							'testingFixed',
+							'testProc'
+						]
+						//Adds the actions for string fields.
+						def fields = settingsJson.fields.findAll { lookUpFields.contains(it.field) }
+
+						if (fields) {
+							fields.each { field ->
+								field.bulkChangeActions = [
+									'replace',
+									'clear'
+								]
+							}
+						}
+					}
+					return settingsJson
+				}
+
+
+				DatabaseMigrationService databaseMigrationService = ctx.getBean("databaseMigrationService")
+				// Update the different settings with the Asset Class fields.
+				databaseMigrationService.updateJsonObjects(fieldSettings, "json", changeScript)
+
+			}
+		}
+	}
+}
--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -276,4 +276,5 @@ databaseChangeLog = {
 	include file: '20180917-move-bulk-specification-to-field-specs.groovy'
 	include file: '20180919-add-bulk-move-bundle-to-file-specs.groovy'
 	include file: '20180919-add-bulk-validation-planstatus-to-file-specs.groovy'
+	include file: '20181002-add-bulk-action-to-standard-fields.groovy'
 }
