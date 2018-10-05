0e046aa9e5e20e07c56f02007cfe9fb189ad8b19
Diego Correa <dcorrea@tdsi.com>
2018-10-05 18:29:05 -0300

    TM-12302 Move business logic to a transcational service

--- grails-app/controllers/PermissionsController.groovy
+++ grails-app/controllers/PermissionsController.groovy
@@ -3,6 +3,7 @@ import net.transitionmanager.controller.ControllerMethods
 import net.transitionmanager.domain.Permissions
 import net.transitionmanager.domain.RolePermissions
 import net.transitionmanager.security.Permission
+import net.transitionmanager.service.PermissionsService
 import org.springframework.jdbc.core.JdbcTemplate
 
 import grails.plugin.springsecurity.annotation.Secured
@@ -11,55 +12,21 @@ class PermissionsController implements ControllerMethods {
 
 	static defaultAction = 'list'
 
-	JdbcTemplate jdbcTemplate
+	PermissionsService permissionsService
 
 	@HasPermission(Permission.RolePermissionView)
 	def show() {
-		def permissions = Permissions.withCriteria {
-			and {
-			   order('permissionItem','asc')
-			}
-		}
-		[permissions:permissions]
+		[permissions: permissionsService.findAll()]
 	}
 
 	@HasPermission(Permission.RolePermissionEdit)
 	def edit() {
-		List permissions = Permissions.withCriteria {
-			and {
-				order('permissionItem','asc')
-			}
-		}
-		[permissions:permissions]
+		[permissions: permissionsService.findAll()]
 	}
 
 	@HasPermission(Permission.RolePermissionEdit)
 	def update() {
-		def paramList = params.column
-		jdbcTemplate.update("delete from role_permissions")
-		for (Permissions permission in Permissions.list()) {
-			for (String role in Permissions.Roles.NAMES) {
-				def param = params['role_' + permission.id + '_' + role]
-				if (param == "on") {
-					def rolePermissions = new RolePermissions(role: role, permission: permission)
-					if (!rolePermissions.save(flush: true)) {
-						println "Error while updating rolePermissions : $rolePermissions"
-						rolePermissions.errors.each { println it }
-					}
-				}
-			}
-		}
-		for(String id in paramList){
-			Permissions permissions = Permissions.get(id)
-			if(permissions){
-				permissions.description = params["description_"+id]
-				if(!permissions.save(flush:true)){
-					permissions.errors.allErrors.each {
-						println it
-				    }
-			    }
-			}
-		}
+		permissionsService.update(params)
 		redirect(action:"show")
 	}
 }
