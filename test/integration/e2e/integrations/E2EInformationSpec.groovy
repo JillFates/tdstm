92c5ecfdbf150f8266000475f2d56a57a40eace7
Sebastian <sbigatton@transitionaldata.com>
2018-10-10 13:32:00 -0300

    TM-11183: requested changes from PR-1856

--- test/integration/e2e/integrations/E2EInformationSpec.groovy
+++ test/integration/e2e/integrations/E2EInformationSpec.groovy
@@ -3,19 +3,19 @@ package e2e.integrations
 import net.transitionmanager.domain.Project
 import net.transitionmanager.service.ProjectService
 import spock.lang.Specification
-import test.helper.E2eInformationHelper
+import test.helper.ProjectTestHelper
 
-class E2eInformationSpec extends Specification {
+class E2EInformationSpec extends Specification {
 	// Set transactional false to persist at database when spec finishes
 	static transactional = false
 
 	// IOC
 	ProjectService projectService
-	private E2eInformationHelper e2eHelper = new E2eInformationHelper()
+	private ProjectTestHelper projectHelper = new ProjectTestHelper()
 	private Project project
 
 	// E2E Data
-	def e2eProjectData = [
+	Map<String, String> e2eProjectData = [
 			"projectName": "E2E Project",
 			"projectCode": "E2E Project",
 			"projectDesc": "E2E Project for automation purposes",
@@ -24,7 +24,7 @@ class E2eInformationSpec extends Specification {
 	]
 
 	void setup(){
-		project = e2eHelper.createProject(e2eProjectData)
+		project = projectHelper.createProject(e2eProjectData)
 	}
 
 	void "Setup E2E Project data"() {
--- test/integration/e2e/integrations/E2eInformationHelper.groovy
+++
@@ -1,4 +0,0 @@
-package e2e.integrations
-
-class E2eInformationHelper extends test.helper.E2eInformationHelper {
-}
