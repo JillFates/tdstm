e690c4bc3515bdb07c46cf715e69548d6f24720c
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-11-06 12:37:54 -0500

    TM-12280 bundle should only have replace bulk action.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -276,4 +276,5 @@ databaseChangeLog = {
 	include file: '20180917-move-bulk-specification-to-field-specs.groovy'
 	include file: '20181012-add-bulk-actions-for-standard-custom-and-other.groovy'
 	include file: '20181105-move-bundle-should-have-list-control.groovy'
+	include file: '20181106-bundle-should-only-have-replace.groovy'
 }
