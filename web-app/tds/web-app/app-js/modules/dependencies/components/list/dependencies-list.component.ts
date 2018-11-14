e1015642ae7f73262f2725ec3a39d1baaf050ab1
Sam Zuniga <szuniga@transitionaldata.com>
2018-11-14 12:14:53 -0600

    TM-12169 Customize grid footer actions

--- web-app/tds/web-app/app-js/modules/dependencies/components/list/dependencies-list.component.ts
+++ web-app/tds/web-app/app-js/modules/dependencies/components/list/dependencies-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {CompositeFilterDescriptor, State, process} from '@progress/kendo-data-query';
-import {CellClickEvent, GridDataResult} from '@progress/kendo-angular-grid';
+import {CellClickEvent, GridDataResult, DataStateChangeEvent} from '@progress/kendo-angular-grid';
 
 import {UIDialogService} from '../../../../shared/services/ui-dialog.service';
 import {PermissionService} from '../../../../shared/services/permission.service';
@@ -17,21 +17,12 @@ import {ActionType} from '../../../dataScript/model/data-script.model';
 	templateUrl: '../tds/web-app/app-js/modules/dependencies/components/list/dependencies-list.component.html'
 })
 export class DependenciesListComponent implements OnInit {
-	protected assets: any[];
 	protected skip = 0;
 	protected pageSize = GRID_DEFAULT_PAGE_SIZE;
+	protected maxOptions = GRID_DEFAULT_PAGINATION_OPTIONS;
 	protected dependenciesColumnModel: DependenciesColumnModel;
 	public gridData: GridDataResult;
-	protected state: State = {
-		sort: [{
-			dir: 'asc',
-			field: 'name'
-		}],
-		filter: {
-			filters: [],
-			logic: 'and'
-		}
-	};
+	protected state: State;
 
 	constructor(
 		private route: ActivatedRoute,
@@ -43,6 +34,11 @@ export class DependenciesListComponent implements OnInit {
 	}
 
 	ngOnInit() {
+		this.gridData = {
+			data: [],
+			total: 0
+		};
+		this.state = this.getInitialGridState();
 		this.dependenciesColumnModel = new DependenciesColumnModel();
 		this.state.take = this.pageSize;
 		this.state.skip = this.skip;
@@ -50,8 +46,12 @@ export class DependenciesListComponent implements OnInit {
 		console.log('dependencies init');
 		this.dependenciesService.getDependencies()
 			.subscribe((results) => {
-				this.assets = results.data.assets;
-				this.gridData = process(this.assets, this.state);
+				this.gridData = {
+					data: results.data && results.data.assets,
+					total: results.data.pagination &&results.data.pagination.total
+				};
+
+				// this.gridData = process(this.assets, this.state);
 				console.log('The results are');
 				console.log(results);
 			})
@@ -74,12 +74,12 @@ export class DependenciesListComponent implements OnInit {
 
 	protected filterChange(filter: CompositeFilterDescriptor): void {
 		this.state.filter = filter;
-		this.gridData = process(this.assets, this.state);
+		// this.gridData = process(this.assets, this.state);
 	}
 
 	protected sortChange(sort): void {
 		this.state.sort = sort;
-		this.gridData = process(this.assets, this.state);
+		// this.gridData = process(this.assets, this.state);
 	}
 
 	/**
@@ -98,7 +98,41 @@ export class DependenciesListComponent implements OnInit {
 		this.state.skip = this.skip;
 		this.state.take = event.take || this.state.take;
 		this.pageSize = this.state.take;
-		this.gridData = process(this.assets, this.state);
+		// this.gridData = process(this.assets, this.state);
+	}
+
+	protected dataStateChange(state: DataStateChangeEvent): void {
+		console.log('State:');
+		console.log(state);
+
+		/*
+		if (state.sort[0]) {
+			// Invert the Order to remove the Natural/Default from the UI (no arrow)
+			if (!state.sort[0].dir) {
+				state.sort[0].dir = (this.model.sort.order === 'a' ? 'desc' : 'asc');
+			}
+
+			let field = state.sort[0].field.split('_');
+			this.model.sort.domain = field[0];
+			this.model.sort.property = field[1];
+			this.model.sort.order = state.sort[0].dir === 'asc' ? 'a' : 'd';
+		}
+		this.updateGridState(state);
+		this.modelChange.emit();
+		*/
+	}
+
+	getInitialGridState(): any {
+		return {
+			sort: [{
+				dir: 'asc',
+				field: 'name'
+			}],
+			filter: {
+				filters: [],
+				logic: 'and'
+			}
+		}
 	}
 
 }
