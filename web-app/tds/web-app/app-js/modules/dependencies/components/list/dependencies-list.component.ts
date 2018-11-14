98e56b760784795fbf7c0f00930b740f5aa4bc70
Sam Zuniga <szuniga@transitionaldata.com>
2018-11-14 12:14:53 -0600

    TM-12169 Customize grid footer actions

--- web-app/tds/web-app/app-js/modules/dependencies/components/list/dependencies-list.component.ts
+++ web-app/tds/web-app/app-js/modules/dependencies/components/list/dependencies-list.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { GridDataResult, DataStateChangeEvent} from '@progress/kendo-angular-grid';
 import {BehaviorSubject} from 'rxjs';
 import {CompositeFilterDescriptor, State, process} from '@progress/kendo-data-query';
-import {CellClickEvent, GridDataResult} from '@progress/kendo-angular-grid';
+import {CellClickEvent, GridDataResult, DataStateChangeEvent} from '@progress/kendo-angular-grid';
 
 import {UIDialogService} from '../../../../shared/services/ui-dialog.service';
 import {PermissionService} from '../../../../shared/services/permission.service';
@@ -25,18 +25,10 @@ export class DependenciesListComponent implements OnInit {
 	protected assets: any[];
 	protected skip = 0;
 	protected pageSize = GRID_DEFAULT_PAGE_SIZE;
+	protected maxOptions = GRID_DEFAULT_PAGINATION_OPTIONS;
 	protected dependenciesColumnModel: DependenciesColumnModel;
 	public gridData: GridDataResult;
-	protected state: State = {
-		sort: [{
-			dir: 'asc',
-			field: 'name'
-		}],
-		filter: {
-			filters: [],
-			logic: 'and'
-		}
-	};
+	protected state: State;
 
 	constructor(
 		private route: ActivatedRoute,
@@ -48,6 +40,7 @@ export class DependenciesListComponent implements OnInit {
 
 	ngOnInit() {
 		this.gridData = { data: [], total: 0 };
+		this.state = this.getInitialGridState();
 		this.dependenciesColumnModel = new DependenciesColumnModel();
 		this.gridStateSubject = new BehaviorSubject(this.getInitialGridState());
 
@@ -102,12 +95,12 @@ export class DependenciesListComponent implements OnInit {
 
 	protected filterChange(filter: CompositeFilterDescriptor): void {
 		this.state.filter = filter;
-		this.gridData = process(this.assets, this.state);
+		// this.gridData = process(this.assets, this.state);
 	}
 
 	protected sortChange(sort): void {
 		this.state.sort = sort;
-		this.gridData = process(this.assets, this.state);
+		// this.gridData = process(this.assets, this.state);
 	}
 
 	/**
@@ -126,7 +119,41 @@ export class DependenciesListComponent implements OnInit {
 		this.state.skip = this.skip;
 		this.state.take = event.take || this.state.take;
 		this.pageSize = this.state.take;
-		this.gridData = process(this.assets, this.state);
+		// this.gridData = process(this.assets, this.state);
+	}
+
+	protected dataStateChange(state: DataStateChangeEvent): void {
+		console.log('State:');
+		console.log(state);
+
+		/*
+		if (state.sort[0]) {
+			// Invert the Order to remove the Natural/Default from the UI (no arrow)
+			if (!state.sort[0].dir) {
+				state.sort[0].dir = (this.model.sort.order === 'a' ? 'desc' : 'asc');
+			}
+
+			let field = state.sort[0].field.split('_');
+			this.model.sort.domain = field[0];
+			this.model.sort.property = field[1];
+			this.model.sort.order = state.sort[0].dir === 'asc' ? 'a' : 'd';
+		}
+		this.updateGridState(state);
+		this.modelChange.emit();
+		*/
+	}
+
+	getInitialGridState(): any {
+		return {
+			sort: [{
+				dir: 'asc',
+				field: 'name'
+			}],
+			filter: {
+				filters: [],
+				logic: 'and'
+			}
+		}
 	}
 
 }
