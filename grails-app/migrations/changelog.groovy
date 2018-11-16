d2fd4878ebee3f71696f4c0e1f0a3c238419628c
Sidar LC <slopez@tdsi.com>
2018-11-15 22:34:01 -0600

    TM-13024: Change the column ordering of the new System Views

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -280,5 +280,5 @@ databaseChangeLog = {
 	include file: '20181012-add-bulk-actions-for-standard-custom-and-other.groovy'
 	include file: '20181105-move-bundle-should-have-list-control.groovy'
 	include file: 'version/v4_6_0/20181115-delete-bogus-user-preferences.groovy'
-
+	include file: '20181115-reorder-system-views-columns.groovy'
 }
