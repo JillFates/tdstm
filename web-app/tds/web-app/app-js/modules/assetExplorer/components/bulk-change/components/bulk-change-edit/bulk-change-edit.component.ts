b345091c480b2b6aac4eb2d86362a4baaff50672
John Martin <jmartin@transitionaldata.com>
2018-11-19 14:46:31 -0500

    TM-12962 Made changes to UI to account for new Reference control type. Fixed bug with invalid method for UserDateTime.

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/bulk-change/components/bulk-change-edit/bulk-change-edit.component.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/bulk-change/components/bulk-change-edit/bulk-change-edit.component.ts
@@ -31,6 +31,7 @@ export class BulkChangeEditComponent extends UIExtraDialog implements OnInit {
 	protected readonly TYPE_INLIST_CONTROL = 'InList';
 	protected readonly TYPE_CUSTOM_FIELD_LIST_CONTROL = 'List';
 	protected readonly TYPE_CUSTOM_FIELD_YES_NO = 'YesNo';
+	protected readonly TYPE_REFERENCE_CONTROL = 'Reference';
 
 	isLoaded: boolean;
 	private defaultDomain: IdTextItem = {id: 'COMMON', text: 'Common Fields'};
@@ -61,7 +62,7 @@ export class BulkChangeEditComponent extends UIExtraDialog implements OnInit {
 			this.domains = [];
 			this.listOptions = {};
 			this.editRows = {listOptions: [], fields: [], selectedValues: [] };
-			this.userDateFormat = this.preferenceService.getUserDateFormatForMomentJS();
+			this.userDateFormat = this.preferenceService.getUserDateFormat();
 			this.userTimeZone = this.preferenceService.getUserTimeZone();
 	}
 
@@ -189,14 +190,16 @@ export class BulkChangeEditComponent extends UIExtraDialog implements OnInit {
 	}
 
 	/**
-	 * Check if field control is any system Options or InList.
-	 * @param fieldControl
+	 * Check if field control is any system Options or InList where the select options will be
+	 * from the this.listOptions.
+	 * @param fieldControl -
 	 * @returns {boolean}
 	 */
-	private isFieldControlOptionsList(fieldControl: any): boolean {
-		return fieldControl === this.TYPE_OPTIONS_CONTROL
-			|| fieldControl === this.TYPE_INLIST_CONTROL
-			|| fieldControl.startsWith(this.TYPE_OPTIONS_CONTROL);
+	private isFieldControlOptionsList(fieldControlName: String): boolean {
+		return fieldControlName === this.TYPE_OPTIONS_CONTROL
+			|| fieldControlName === this.TYPE_INLIST_CONTROL
+			|| fieldControlName.startsWith(this.TYPE_OPTIONS_CONTROL)
+			|| fieldControlName === this.TYPE_REFERENCE_CONTROL;
 	}
 
 	onActionValueChange(action: IdTextItem, index: number): void {
