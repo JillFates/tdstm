5afd088d6de4da4cf2acf30756451bcc557da893
Jorge Morayta <jmorayta@tdsi.com>
2018-11-21 07:16:08 -0600

    TM-13181 Double check the validation

--- web-app/tds/web-app/app-js/modules/assetExplorer/components/asset/asset-common-edit.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/asset/asset-common-edit.ts
@@ -42,7 +42,7 @@ export class AssetCommonEdit implements OnInit {
 			this.assetTagsModel = {tags: metadata.assetTags};
 			this.tagList = metadata.tagList;
 			this.dateFormat = this.preference.preferences['CURR_DT_FORMAT'];
-			if (this.dateFormat != null) {
+			if (this.dateFormat && this.dateFormat !== null) {
 				this.dateFormat = this.dateFormat.toLowerCase().replace(/m/g, 'M');
 			}
 	}
