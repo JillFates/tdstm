0005a094aee5b7fcab40b0825d46b5032e3ca58e
John Martin <jmartin@transitionaldata.com>
2018-11-21 16:25:33 -0500

    TM-12854 Fixed a bit more logic and forgot to save a few changes

--- src/groovy/net/transitionmanager/dataview/FieldSpecCache.groovy
+++ src/groovy/net/transitionmanager/dataview/FieldSpecCache.groovy
@@ -2,6 +2,7 @@ package net.transitionmanager.dataview
 
 import com.tdsops.tm.enums.domain.AssetClass
 import net.transitionmanager.service.CustomDomainService
+import net.transitionmanager.service.InvalidParamException
 import groovy.transform.CompileStatic
 import groovy.transform.TypeCheckingMode
 
@@ -23,6 +24,7 @@ import groovy.transform.TypeCheckingMode
  * 	]
  * </pre>
  */
+@CompileStatic
 class FieldSpecCache {
 
 	/*
@@ -36,34 +38,41 @@ class FieldSpecCache {
 	 * {@code CustomDomainService # fieldSpecsWithCommon} results.
 	 * It defines common fields and manages field specs type during a {@code DataviewService # query}
 	 */
-	FieldSpecCache(Map<String, ?> fieldsSpec) {
-		addFieldSpecs(CustomDomainService.COMMON, fieldsSpec)
-		addFieldSpecs(AssetClass.APPLICATION.name(), fieldsSpec)
-		addFieldSpecs(AssetClass.DEVICE.name(), fieldsSpec)
-		addFieldSpecs(AssetClass.STORAGE.name(), fieldsSpec)
-		addFieldSpecs(AssetClass.DATABASE.name(), fieldsSpec)
-	}
-
-	private void addFieldSpecs(String key, Map<String, ?> fieldsSpec) {
-		List<Map<String, ?>> fieldsSpecList = fieldsSpec[key].fields
-
-		fieldsSpecMap[key] = fieldsSpecList.collectEntries {
-			[(it.field): new FieldSpec(it)]
+	FieldSpecCache(Map<String, Map> fieldsSpec) {
+		Set keys = fieldsSpec.keySet()
+		for (String key in keys) {
+			fieldsSpecMap[key] = [:]
+			addFieldSpecs(key, fieldsSpec[key])
 		}
 	}
 
-	String getType(String assetClass, String fieldName) {
-		FieldSpec fieldSpec = fieldsSpecMap[assetClass.toUpperCase()][fieldName]
-		return fieldSpec.type
+	/**
+	 * Loads a single Domain's Field Specifications Map into the cache.
+	 * Each field is converted to a FieldSpec object keyed by the fieldname
+	 */
+	@CompileStatic(TypeCheckingMode.SKIP)
+	private void addFieldSpecs(String key, Map<String, ?> domainFieldsSpec) {
+		List<Map<String, ?>> fieldsSpecList = domainFieldsSpec.fields
+
+		for (Map fieldInfo in fieldsSpecList) {
+			fieldsSpecMap[key].put(fieldInfo.field, new FieldSpec(fieldInfo))
+		}
 	}
 
 	/**
 	 * Returns an instance of {@code FieldSpec} based on an assetclass definition and a field name
-	 * @param assetClass
+	 * @param assetClassName
 	 * @param fieldName
 	 * @return
 	 */
-	FieldSpec getFieldSpec(String assetClass, String fieldName) {
-		return fieldsSpecMap[assetClass.toUpperCase()][fieldName]
+	FieldSpec getFieldSpec(String assetClassName, String fieldName) {
+		String key = assetClassName.toUpperCase()
+		if (! fieldsSpecMap.containsKey(key)) {
+			throw new InvalidParamException("Domain $assetClassName not found in cache")
+		}
+		if (! fieldsSpecMap[key].containsKey(fieldName)) {
+			throw new InvalidParamException("Field $fieldName of Domain $assetClassName not found in cache")
+		}
+		return fieldsSpecMap[key][fieldName]
 	}
 }
