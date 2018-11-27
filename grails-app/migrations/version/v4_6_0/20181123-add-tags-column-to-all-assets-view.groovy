0cd6cea22b175645af36dac41fb3ea6368c89e2b
John Martin <jmartin@transitionaldata.com>
2018-11-27 07:06:59 -0500

    TM-13152 Changed so that the column order matches other views (bundle, tags, validation)

--- grails-app/migrations/version/v4_6_0/20181123-add-tags-column-to-all-assets-view.groovy
+++ grails-app/migrations/version/v4_6_0/20181123-add-tags-column-to-all-assets-view.groovy
@@ -4,8 +4,8 @@ import net.transitionmanager.domain.Dataview
 import net.transitionmanager.service.DatabaseMigrationService
 
 databaseChangeLog = {
-	changeSet(author: 'slopez', id: '20181123-TM-13152-1') {
-		comment('Add the Tags column to the All Assets List View.')
+	changeSet(author: 'slopez', id: '20181123-TM-13152-2') {
+		comment('Add the Tags column to the All Assets List View')
 		grailsChange {
 			change {
 				// List system dataviews that requires updates
@@ -19,18 +19,27 @@ databaseChangeLog = {
 					// Iterate columns to see if "tags" column was already added, then do nothing
 					def hasTagsColumns = settingsJson.columns.find { col -> col.property == 'tagAssets' }
 					if (hasTagsColumns) {
-						println 'All Assets view already has "Tags" column added.'
+						println 'All Assets DataView already has "Tags" column added'
 						return settingsJson
 					}
 
 					// If "Tags" columns is not already present in the view definition, then
 					// iterate columns to find where "validation" column is and add new "tags" column after
 					def columns = []
+					def validationCol
 					settingsJson.columns.each { col ->
-						columns << col
 						if (col.property == 'validation') {
+							// Hold on to the Validation column so that it can be reordered (bundle/tag/validation)
+							validationCol = col
+						} else {
+							columns << col
+						}
+						if (col.property == 'moveBundle') {
 							// add tags column
 							columns << [domain: 'common', 'edit': false, filter: '', label: 'Tags', locked: false, property: 'tagAssets', width: 200]
+
+							// Add validation column
+							columns << validationCol
 						}
 					}
 					settingsJson.columns = columns
