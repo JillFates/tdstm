a8bca542327626e2fc626d5904a30b071457b077
dontiveros <dontiveros@tdsi.com>
2018-11-28 16:48:43 -0600

    TM-13224 last changes, ready to go.

--- web-app/tds/web-app/app-js/shared/components/custom-control/number/number-control.component.ts
+++ web-app/tds/web-app/app-js/shared/components/custom-control/number/number-control.component.ts
@@ -8,6 +8,7 @@ import {NumberControlHelper} from './number-control.helper';
 		<div>
             <kendo-numerictextbox [format]="format"
                                   [(ngModel)]="numberValue"
+                                  [min]="realMinRange" [max]="maxRange"
                                   [autoCorrect]="true"
                                   (ngModelChange)="onValueChange($event)"
                                   class="form-control">
@@ -36,7 +37,9 @@ export class NumberControlComponent implements OnInit {
 	 * On Init build the number format.
 	 */
 	ngOnInit(): void {
-		this.numberValue = this.value ? +this.value : null;
+		this.numberValue = this.value ? +this.value : 0;
+		// double check
+		this.numberValue = Number.isNaN(this.numberValue) ? 0 : this.numberValue;
 		this.realMinRange = this.minRange;
 	}
 
