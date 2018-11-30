5080525e719ee0b254c4de1f613832a22d7b4ab4
Tucker Pelletier <tpelletier@transitionaldata.com>
2018-11-30 14:50:54 -0500

    TM-12271/TM-13368 validation default to Unknown and Fixing Bulk update issues one with the size having the wrong control type and the custom validation needs to have the project passed on the instance, so it looks up the correct field settings.

---
+++ grails-app/migrations/version/v4_6_0/20181130-validated-should-only-have-replace.groovy
@@ -0,0 +1,62 @@
+package version.v4_6_0
+
+import com.tdsops.tm.enums.domain.SettingType
+import net.transitionmanager.domain.Setting
+import net.transitionmanager.service.DatabaseMigrationService
+
+/**
+ * Replacing validation actions with just remove, it shouldn't have replace.
+ * Updating Size to have a number control
+ * Setting validation to Unknown for blank or null.
+ */
+databaseChangeLog = {
+
+	changeSet(author: "tpelletier", id: "20181130 TM-13368-1") {
+		comment("Replacing actions for validated, it should only have replace and updating size to have a number control")
+
+		grailsChange {
+			change {
+				// List all the field settings for the different domains.
+				List<Setting> fieldSettings = Setting.where {
+					type == SettingType.CUSTOM_DOMAIN_FIELD_SPEC
+				}.list()
+
+				// This closure adds an empty list of actions to each field, and the actions for tags.
+				Closure changeScript = { settingsJson ->
+
+					// Add the default actions of [:] and the default service of null.
+					if (settingsJson.fields) {
+
+						//update actions for validated
+						def fieldsToUpdate = settingsJson.fields.find { it.field == "validated" }
+
+						if (fieldsToUpdate) {
+							fieldsToUpdate.bulkChangeActions = [
+								'replace'
+							]
+						}
+
+						//Updating size field to have a number control.
+						fieldsToUpdate = settingsJson.fields.findAll { it.field == 'size' }
+
+						fieldsToUpdate.each { Map map ->
+							map.control = "Number"
+						}
+					}
+					return settingsJson
+				}
+
+
+				DatabaseMigrationService databaseMigrationService = ctx.getBean("databaseMigrationService")
+				// Update the different settings with the Asset Class fields.
+				databaseMigrationService.updateJsonObjects(fieldSettings, "json", changeScript)
+
+			}
+		}
+	}
+
+	changeSet(author: "tpelletier", id: "20181130 TM-13368-2") {
+		comment("Set the validation='Unknown' black or null")
+		sql("UPDATE asset_entity SET validation='Unknown' WHERE (validation = '' OR validation IS NULL)")
+	}
+}
