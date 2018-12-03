8c7acccaa7057d88710d73954ebd9847625c3ef1
Esteban Cantu <ecantu@tdsi.com>
2018-12-03 13:09:21 -0300

    TM-13381 Fix Custom Validator for Integer allowing decimals and unit tests

--- src/groovy/com/tdsops/validators/CustomValidators.groovy
+++ src/groovy/com/tdsops/validators/CustomValidators.groovy
@@ -281,14 +281,19 @@ class CustomValidators {
 					// if 'allowNegative' field is not present and the number is negative, it should error
 					addError ('field.invalid.negativeNotAllowed', [value, getLabel()] )
 				}
-				// if 'precision' field is present and the value has a fractional part, we should check
+				// if the value has a fractional part, we should check
 				// that the count of fractional digits does not exceed the 'precision' value
-				if (precision > 0 && value.contains('.')){
-					String fractionalPart = value.substring(value.indexOf('.') + 1, value.size())
-					println "fractionalPart=[$fractionalPart] size=${fractionalPart?.size()}, precision=$precision"
-					if (fractionalPart.size() > precision) {
-						addError ('field.invalid.precisionExceeded', [value, getLabel(), precision])
+				if ( value.contains('.')){
+					if (precision > 0) {
+						String fractionalPart = value.substring(value.indexOf('.') + 1, value.size())
+						println "fractionalPart=[$fractionalPart] size=${fractionalPart?.size()}, precision=$precision"
+						if (fractionalPart.size() > precision) {
+							addError ('field.invalid.precisionExceeded', [value, getLabel(), precision])
+						}
+					} else { // no decimal places are allowed, so it should error
+						addError ('field.invalid.precisionExceeded', [value, getLabel(), 0])
 					}
+
 				}
 			}
 		}
