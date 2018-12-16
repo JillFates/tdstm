3b9d9b84a78062ff89d0e20768f3ca67fe2c29f9
Sam Zuniga <szuniga@transitionaldata.com>
2018-12-15 21:33:36 -0600

    TM-13475 Implement field validation errors control
    Component used to display the angular form errors reported by some custom field

---
+++ web-app/tds/web-app/app-js/shared/components/custom-control/field-validation-errors/field-validation-errors.component.ts
@@ -0,0 +1,40 @@
+/**
+ * Component used to display the angular form errors reported by some custom field
+ */
+import {Component, Input} from '@angular/core';
+import {pathOr} from 'ramda';
+
+interface ErrorConstraints {
+	required?: boolean;
+	range?: {
+		given: number;
+		min: number;
+		max: number;
+	}
+}
+// TODO internationalize texts
+@Component({
+	selector: 'tds-field-validation-errors',
+	template: `
+		<div class="error" *ngIf="hasErrors">
+			<div *ngIf="errors.required">{{label}} is required</div>
+			<div *ngIf="errors.range; let range">
+				<span>Invalid range number</span>
+				<div>Min: {{range.min}} Max: {{range.max}}</div>
+			</div>
+			<div *ngIf="errors.notNegative">Not could be negative</div>
+		</div>
+	`
+})
+export class TDSFieldValidationErrorsComponent {
+	@Input() label = '';
+	@Input() submitted = false;
+	@Input() valid = false;
+	@Input() dirty = false;
+	@Input('errors') errors: ErrorConstraints = {};
+
+	// Determine if the field has errors
+	get hasErrors(): boolean {
+		return  (this.submitted &&  !this.valid) || (this.dirty && !this.valid);
+	}
+}
