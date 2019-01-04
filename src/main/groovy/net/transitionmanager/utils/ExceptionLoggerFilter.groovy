be6599124bc0e46f2ab6fbc2e46d7c974da1a012
Tucker Pelletier <tpelletier@transitionaldata.com>
2019-01-04 18:21:41 -0500

    TM-13321 this doesn't override anything could be an issue...

--- src/main/groovy/net/transitionmanager/utils/ExceptionLoggerFilter.groovy
+++ src/main/groovy/net/transitionmanager/utils/ExceptionLoggerFilter.groovy
@@ -1,38 +1,35 @@
-
-import org.apache.log4j.spi.Filter; 
-import org.apache.log4j.spi.LoggingEvent; 
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
 
 /**
  *
  * Recipe Domain Object
  * Mutes log logging events for a given loggerClass.
- * 
+ *
  * @author estebancantu
  */
 class ExceptionLoggerFilter extends Filter {
-    
-    String loggerClass
-    
-	
-        @Override
-    public int decide(LoggingEvent event) {
 
-        if (event.getLoggerName().equals(loggerClass)) {
+	String loggerClass
 
-            if (event.getThrowableInformation() != null) {
-                return Filter.DENY;
-            }
-        }
-        return Filter.NEUTRAL;
-    }
+	int decide(LoggingEvent event) {
 
-    public String getLoggerClass() {
-        return loggerClass;
-    }
+		if (event.getLoggerName().equals(loggerClass)) {
 
-    public void setLoggerClass(String loggerClass) {
-        this.loggerClass = loggerClass;
-    }
-	
+			if (event.getThrowableInformation() != null) {
+				return Filter.DENY;
+			}
+		}
+		return Filter.NEUTRAL;
+	}
+
+	String getLoggerClass() {
+		return loggerClass;
+	}
+
+	void setLoggerClass(String loggerClass) {
+		this.loggerClass = loggerClass;
+	}
+
 }
 
