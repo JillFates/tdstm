71b4fa43645018112d58366e8d58b3c8f7f9459d
Jamie Danahy <jdanahy@transitionaldata.com>
2019-01-09 12:48:45 -0500

    TM-13540 - Created new date/time component with time controls

--- web-app/tds/web-app/app-js/shared/components/custom-control/date-time/datetime-control.component.ts
+++ web-app/tds/web-app/app-js/shared/components/custom-control/date-time/datetime-control.component.ts
@@ -19,14 +19,26 @@ import {ValidationRulesFactoryService} from '../../../services/validation-rules-
 @Component({
 	selector: 'tds-datetime-control',
 	template: `
-		<kendo-datepicker
-			[value]="dateValue"
-			(blur)="onTouched()"
-			[format]="displayFormat"
-			[tabindex]="tabindex"
-			(valueChange)="onValueChange($event)"
-			class="form-control">
-		</kendo-datepicker>
+		<table>
+			<tr>
+				<td>
+                    <kendo-datepicker
+                            [(ngModel)]="dateValue"
+                            (blur)="onTouched()"
+                            [format]="displayFormat"
+                            [tabindex]="tabindex"
+                            (valueChange)="onDateChange($event)"
+                            class="form-control datepicker">
+                    </kendo-datepicker>
+				</td>
+				<td>
+					<kendo-timepicker
+						[value]="dateValue"
+						(valueChange)="onTimeChange($event)">
+					</kendo-timepicker>
+				</td>
+			</tr>
+		</table>
 	`,
 	providers: [
 		{
@@ -49,6 +61,8 @@ export class TDSDateTimeControlComponent extends TDSCustomControl implements OnI
 	protected outputFormat: string;
 	protected displayFormat: string;
 	protected dateValue: Date;
+	protected selectedDate;
+	protected selectedTime;
 	private readonly KENDO_DATETIME_DISPLAY_FORMAT = 'yyyy-MM-dd HH:mm:ss';
 
 	constructor(
@@ -65,19 +79,60 @@ export class TDSDateTimeControlComponent extends TDSCustomControl implements OnI
 	ngOnInit(): void {
 		let localDateFormatted = DateUtils.convertFromGMT(this.value, this.userPreferenceService.getUserTimeZone());
 		this.dateValue = this.value ? DateUtils.toDateUsingFormat(localDateFormatted, DateUtils.SERVER_FORMAT_DATETIME) : null;
+		this.selectedDate = this.dateValue ? DateUtils.convertToGMT(this.dateValue, this.userPreferenceService.getUserTimeZone()).substr(0, 10) : null;
+		this.selectedTime = this.dateValue ? this.dateValue.toString().substr(16, 8) : null;
 	}
 
 	/**
-	 * On value Change on the component, emits the value to the listeners.
+	 * On value Change on the datepicker component, emits the value to the listeners.
 	 * @param {value} Date
 	 */
-	onValueChange(value: Date): void {
+	onDateChange(value): void {
 		if (value && value !== null) {
-			this.value = DateUtils.convertToGMT(value, this.userPreferenceService.getUserTimeZone());
-		} else {
-			this.value = null;
+			this.selectedDate = DateUtils.convertToGMT(value, this.userPreferenceService.getUserTimeZone()).substr(0, 10); // Get the date from the given value
+			if (!this.selectedTime) {
+				this.dateValue = value;
+				this.value = DateUtils.convertToGMT(value, this.userPreferenceService.getUserTimeZone());
+			} else {
+				this.dateValue.setFullYear(value.getFullYear());
+				this.dateValue.setMonth(value.getMonth());
+				this.dateValue.setDate(value.getDate());
+				this.setValueDateTime();
+			}
 		}
 		this.onTouched();
+	}
+
+	/**
+	 * On value Change on the timepicker component, emits the value to the listeners.
+	 * @param {value} Time
+	 */
+	onTimeChange(value): void {
+		if (value && value !== null) {
+			this.selectedTime =  value.toString().substr(16, 8); // Get the time from the given value
+			if (!this.selectedDate) {
+				this.dateValue = value;
+				this.value = DateUtils.convertToGMT(value, this.userPreferenceService.getUserTimeZone());
+			} else {
+				let tempDate = new Date(); // Need to create a new date so the datepicker gets updated.
+				tempDate.setTime(this.dateValue.getTime());
+				tempDate.setHours(value.getHours());
+				tempDate.setMinutes(value.getMinutes());
+				tempDate.setSeconds(value.getSeconds());
+				this.dateValue = tempDate;
+				this.setValueDateTime();
+			}
+		}
+		this.onTouched();
+	}
+
+	/**
+	 * Combines date and time from Kendo pickers into format to be saved.
+	 */
+	setValueDateTime(): void {
+		if (this.selectedDate && this.selectedTime) {
+			this.value = this.selectedDate + 'T' + this.selectedTime + 'Z';
+		}
 	}
 
 	ngOnChanges(inputs: SimpleChanges) {
