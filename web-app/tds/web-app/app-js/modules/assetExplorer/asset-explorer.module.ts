66509b8a81f1a39d600bb98a165739d543f29b87
Sam Zuniga <szuniga@transitionaldata.com>
2019-01-12 12:31:04 -0600

    TM-13754 Add dependency show component

--- web-app/tds/web-app/app-js/modules/assetExplorer/asset-explorer.module.ts
+++ web-app/tds/web-app/app-js/modules/assetExplorer/asset-explorer.module.ts
@@ -40,6 +40,7 @@ import {AssetShowComponent} from './components/asset/asset-show.component';
 import {AssetEditComponent} from './components/asset/asset-edit.component';
 import {TaskCommentComponent} from './components/task-comment/task-comment.component';
 import {AssetDependencyComponent} from './components/asset-dependency/asset-dependency.component';
+import {AssetDependencyShowComponent} from './components/asset-dependency/show/asset-dependency-show.component';
 import {SingleCommentComponent} from './components/single-comment/single-comment.component';
 import {TaskDetailComponent} from '../taskManager/components/detail/task-detail.component';
 import {TaskEditComponent} from '../taskManager/components/edit/task-edit.component';
@@ -81,6 +82,7 @@ import {TaskActionsComponent} from '../taskManager/components/task-actions/task-
 		AssetCreateComponent,
 		TaskCommentComponent,
 		AssetDependencyComponent,
+		AssetDependencyShowComponent,
 		SingleCommentComponent,
 		ModelDeviceShowComponent,
 		ManufacturerShowComponent,
@@ -114,6 +116,7 @@ import {TaskActionsComponent} from '../taskManager/components/task-actions/task-
 		AssetEditComponent,
 		AssetCreateComponent,
 		AssetDependencyComponent,
+		AssetDependencyShowComponent,
 		AssetExplorerIndexComponent,
 		TaskCommentComponent],
 	entryComponents: [
---
+++ web-app/tds/web-app/app-js/modules/assetExplorer/components/asset-dependency/show/asset-dependency-show.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'asset-dependency-show',
+	template: `
+		<div class="dependency-row">
+			<div class="dependency-row-legend">Frequency</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.dataFlowFreq}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.dataFlowFreq}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">Type</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.type}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.type}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">Status</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.status}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.status}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">Direction</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.dataFlowDirection}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.dataFlowDirection}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">C1</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.c1}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.c1}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">C2</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.c2}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.c2}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">C3</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.c3}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.c3}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">C4</div>
+			<div class="dependency-a"><label class="dependency-label">{{dependencyA?.c4}}</label></div>
+			<div class="dependency-b"><label class="dependency-label">{{dependencyB?.c4}}</label></div>
+		</div>
+
+		<div class="dependency-row">
+			<div class="dependency-row-legend">Comment</div>
+			<div class="dependency-a"><label class="dependency-label dependency-comment">{{dependencyA?.comment}}</label></div>
+			<div class="dependency-b"><label class="dependency-label dependency-comment">{{dependencyB?.comment}}</label></div>
+		</div>
+			`
+})
+export class AssetDependencyShowComponent {
+	@Input() dependencyA: any;
+	@Input() dependencyB: any;
+}
