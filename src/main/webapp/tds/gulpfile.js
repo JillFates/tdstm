97fb4c2895955e06e9c278f70348218afe118280
Jorge Morayta <jmorayta@tdsi.com>
2019-01-24 15:24:10 -0600

    TM-13581 - Found some issues on the compilation of the SASS plus adding new location to the gitignore

--- src/main/webapp/tds/gulpfile.js
+++ src/main/webapp/tds/gulpfile.js
@@ -6,32 +6,31 @@ let _ = require('lodash');
 let autoPreFixer = require('gulp-autoprefixer');
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-let sourceMaps = require('gulp-sourcemaps');
 let shell = require('gulp-shell');
 
 gulp.task('sass-main-style', function () {
-	return gulp.src('./src/main/webapp/css/tds-style.sass')
+	return gulp.src('../../../../grails-app/assets/stylesheets/css/tds-style.sass')
 		.pipe(sass({errLogToConsole: true}))
 		.pipe(autoPreFixer({browsers: ['last 2 version'], cascade: false}))
-		.pipe(gulp.dest('./src/main/webapp/css'));
+		.pipe(gulp.dest('../../../../grails-app/assets/stylesheets/css'));
 });
 
 gulp.task('sass-manager-compiler', function () {
-	return gulp.src('../../webapp/css/managerStyle.scss')
+	return gulp.src('../../../../grails-app/assets/stylesheets/css/managerStyle.sass')
 		.pipe(sass({errLogToConsole: true}))
 		.pipe(autoPreFixer({browsers: ['last 2 version'], cascade: false}))
-		.pipe(gulp.dest('./src/main/webapp/css'));
+		.pipe(gulp.dest('../../../../grails-app/assets/stylesheets/css'));
 });
 
 gulp.task('sass-angular-compiler', function () {
-	return gulp.src('./web-app/css/style.sass')
+	return gulp.src('./web-app/css/tds-style.sass')
 		.pipe(sass({errLogToConsole: true}))
 		.pipe(autoPreFixer({browsers: ['last 2 version'], cascade: false}))
 		.pipe(gulp.dest('./web-app/css'));
 });
 
 gulp.task('sass-angular-manager-compiler', function () {
-	return gulp.src('./src/main/webapp/css/managerStyle.scss')
+	return gulp.src('./web-app/css/managerStyle.scss')
 		.pipe(sass({errLogToConsole: true}))
 		.pipe(autoPreFixer({browsers: ['last 2 version'], cascade: false}))
 		.pipe(gulp.dest('./web-app/css'));
