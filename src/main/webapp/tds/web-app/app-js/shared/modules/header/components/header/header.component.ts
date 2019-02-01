0c481bec8231413a5751599407790234fc54994b
Jorge Morayta <jmorayta@tdsi.com>
2019-02-01 11:20:28 -0600

    TM-13284 - Mapped the rest of the elements to the menu so it can jump to the Angular Section. - Also fixed the missing height on load the page

--- src/main/webapp/tds/web-app/app-js/shared/modules/header/components/header/header.component.ts
+++ src/main/webapp/tds/web-app/app-js/shared/modules/header/components/header/header.component.ts
@@ -1,20 +1,33 @@
 // Angular
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 // Service
 import {UserContextService} from '../../../../../modules/security/services/user-context.service';
 import {UserContextModel} from '../../../../../modules/security/model/user-context.model';
+
+declare var jQuery: any;
 
 @Component({
 	selector: 'tds-header',
 	templateUrl: '../tds/web-app/app-js/shared/modules/header/components/header/header.component.html',
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
 	protected userContext: UserContextModel;
 
-	constructor(private userContextService: UserContextService) {
+	constructor(
+		private userContextService: UserContextService) {
 		this.getUserContext();
+	}
+
+	ngOnInit(): void {
+		/**
+		 * AdminLTE is main js that handles the layout, this could me removed later when implementing the footer
+		 */
+		if (jQuery.AdminLTE) {
+			jQuery.AdminLTE.layout.fix();
+		}
+		jQuery('.main-footer').show();
 	}
 
 	protected getUserContext(): void {
