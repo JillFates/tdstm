e1ecccc8cc2e673aa64ba0b8cbd727859390b351
anavarro <anvarro@tdsi.com>
2019-02-04 18:00:49 -0300

    Commit for TM-14068

--- test/e2e/src/test/groovy/pages/Projects/Providers/ProvidersPage.groovy
+++ test/e2e/src/test/groovy/pages/Projects/Providers/ProvidersPage.groovy
@@ -29,6 +29,8 @@ class ProvidersPage extends Page{
         noRecordsRow {$('.k-grid-norecords')}
         noRecordsMessage {noRecordsRow.find("td")}
         projectsModule { module ProjectsModule}
+        arrayOfProviders {$(class:"k-virtual-content").find(class:"k-grid-table-wrap", role:"presentation").find("tbody", role:"presentation").find("tr")}
+
     }
 
     def filterByName(provName){
@@ -54,6 +56,35 @@ class ProvidersPage extends Page{
 
     def clickOnFirstProviderDeleteActionButton(){
         waitFor{firstProviderDeleteButton.click()}
+    }
+
+    /**
+     * The objective of this method is to cleanup the records on the Providers List.
+     * We need at least 3 providers to play with, which is why you'll see that no providers are deleted if 3 or less
+     * are the only ones displayed.
+     * Also, we will delete a max of 8 records per run.
+     * @author Alvaro Navarro
+     */
+    def cleanUpProviders() {
+        def amountOfProviders = arrayOfProviders.size()
+        def providersLeft = amountOfProviders - 3
+        if(providersLeft > 0)
+        {
+           def providersToDelete
+            if((providersLeft-8) > 0)
+                providersToDelete=8
+            else
+                providersToDelete=providersLeft
+
+            while(providersToDelete!=0)
+            {
+                clickOnFirstProviderDeleteActionButton()
+                commonsModule.waitForPromptModalDisplayed()
+                commonsModule.clickOnDeleteYesPromptModal()
+                commonsModule.waitForPromptModalHidden()
+                providersToDelete--
+            }
+        }
     }
 
 }
