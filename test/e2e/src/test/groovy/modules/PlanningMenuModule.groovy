d786639b16c43f72131bc8bb135fae9920a20e90
Sam Zuniga <szuniga@transitionaldata.com>
2019-03-05 12:50:42 -0600

    TM-14391 Fix issue opening pre-event checklist report menu

--- test/e2e/src/test/groovy/modules/PlanningMenuModule.groovy
+++ test/e2e/src/test/groovy/modules/PlanningMenuModule.groovy
@@ -14,7 +14,7 @@ class PlanningMenuModule extends Module {
         planningListEvents { planningMenu.find("li.menu-parent-planning-event-list")}
         planningEventDetails { planningMenu.find("li.menu-parent-planning-event-detail-list")}
         planningListEventNews { planningMenu.find("li.menu-parent-planning-event-news")}
-        planningPreEventCheckList { planningMenu.find("li.menu-child-item").find("a", "href": "/tdstm/reports/preMoveCheckList")}
+        planningPreEventCheckList { planningMenu.find("li.menu-child-item").find("a", "href": "/tdstm/module/reports/preEventCheckList")}
         planningExportRunbook { planningMenu.find("li.menu-parent-planning-export-runbook")}
         planningBundlesTitle { planningMenu.find("li.menu-parent-item")[1]}
         planningListBundles { planningMenu.find("li.menu-parent-planning-list-bundles")}
