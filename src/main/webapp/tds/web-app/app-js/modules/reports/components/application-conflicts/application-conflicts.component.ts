a098981d8e2c3b66895c28fd5578426439069301
Sam Zuniga <szuniga@transitionaldata.com>
2019-04-04 17:13:12 -0600

    TM-13133 Move template to external file

--- src/main/webapp/tds/web-app/app-js/modules/reports/components/application-conflicts/application-conflicts.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/reports/components/application-conflicts/application-conflicts.component.ts
@@ -23,122 +23,13 @@ import {ActivatedRoute} from '@angular/router';
 import {TranslatePipe} from '../../../../shared/pipes/translate.pipe';
 import {ReportsService} from '../../service/reports.service';
 import {NotifierService} from '../../../../shared/services/notifier.service';
+import { ApplicationConflict } from '../../model/application-conflicts.model';
 
 declare var jQuery: any;
 
 @Component({
 	selector: 'tds-application-conflicts',
-	template: `
-		<!--
-		<div class="actions">
-						<div>
-						<div>Output:</div>
-						<div>
-						<input type="radio" id="output" name="output" checked>
-						<label for="output">Web</label>
-						</div>
-						</div>
-						<div>
-						<tds-button-custom
-						class="btn-primary"
-						(click)="onGenerateReport(model.defaultEvent.id)"
-						title="Generate"
-						tooltip="Generate report"
-						icon="check-square">
-						</tds-button-custom>
-						</div>
-						</div>
-		-->
-		<div class="application-conflicts">
-			<div class="application-section">
-				<div class="section-title">Bundle</div>
-				<div class="section-control">
-					<kendo-dropdownlist
-							name="bundleList"
-							class="form-control"
-							[data]="model.moveBundleList"
-							[textField]="'name'"
-							[valueField]="'id'"
-							[(ngModel)]="model.defaultBundle">
-					</kendo-dropdownlist>
-				</div>
-			</div>
-
-			<div class="application-section">
-				<div class="section-title">App Owner</div>
-				<div class="section-control">
-					<kendo-dropdownlist
-							name="appOwner"
-							class="form-control"
-							[data]="model.appOwnerList"
-							[textField]="'name'"
-							[valueField]="'id'"
-							[(ngModel)]="model.defaultAppowner">
-					</kendo-dropdownlist>
-				</div>
-			</div>
-
-			<div class="application-section">
-				<div class="section-title">
-					<input [checked]="model.bundleConflict" (change)="model.bundleConflict = !model.bundleConflict"
-					id="bundleConflict" name="bundleConflict" type="checkbox">
-					<label for="bundleConflict">Bundle Conflict</label>
-				</div>
-				<div class="section-control">
-					<label>Having dependency references to assets assigned to unrelated bundles</label>
-				</div>
-			</div>
-
-			<div class="application-section">
-				<div class="section-title">
-					<input [checked]="model.unresolvedDependencies" (change)="model.unresolvedDependencies = !model.unresolvedDependencies"
-					id="unresolvedDependencies" name="unresolvedDependencies" type="checkbox">
-					<label for="unresolvedDependencies">Unresolved Dependencies</label>
-				</div>
-				<div class="section-control">
-					<label>Having dependencies with status Unknown or Questioned</label>
-				</div>
-			</div>
-
-			<div class="application-section">
-				<div class="section-title">
-					<input [checked]="model.missingDependencies" (change)="model.missingDependencies = !model.missingDependencies"
-					id="missingDependencies" name="missingDependencies" type="checkbox">
-					<label for="missingDependencies">Missing Dependencies</label>
-				</div>
-				<div class="section-control">
-					<label>Having no defined Supports or Requires dependencies</label>
-				</div>
-			</div>
-
-			<div class="application-section">
-				<div class="section-title">
-					<div>Maximum Applications to report</div>
-				</div>
-				<div class="section-control">
-					<kendo-dropdownlist name="appOwner"
-						class="form-control"
-						[data]="model.maxApplicationsList"
-						[textField]="'value'"
-						[valueField]="'value'"
-						[(ngModel)]="model.maxApplications">
-					</kendo-dropdownlist>
-				</div>
-			</div>
-			<div class="application-section">
-				<div class="section-title">
-					<tds-button-custom
-						class="btn-primary"
-						title="Generate"
-						tooltip="Generate report"
-						icon="check-square">
-					</tds-button-custom>
-				</div>
-				<div class="section-control">
-				</div>
-			</div>
-		</div>
-`
+	templateUrl: 'application-conflicts.component.html'
 })
 export class ApplicationConflictsComponent implements OnInit {
 	public model = {
@@ -152,6 +43,7 @@ export class ApplicationConflictsComponent implements OnInit {
 		maxApplications: {value: 100},
 		maxApplicationsList: [{value: 100}, {value: 250}, {value: 500}]
 	};
+	applicationConflicts: Array<ApplicationConflict> = [];
 
 	constructor(
 		private sanitizer: DomSanitizer,
@@ -171,15 +63,16 @@ export class ApplicationConflictsComponent implements OnInit {
 				const [events, defaultsApplication] = results;
 				this.model.moveBundleList = defaultsApplication.moveBundleList;
 				this.model.appOwnerList = defaultsApplication.appOwnerList;
+			});
+	}
 
-				/*
-				this.model.events = events.map((item) => ({id: item.id.toString(), text: item.name}));
-				this.model.defaultEvent.id = pathOr(null, ['preferences', 'TASK_CREATE_EVENT'], defaults);
-				if (this.model.defaultEvent.id === 'null' || this.model.defaultEvent.id === null) {
-					this.model.defaultEvent.id = this.model.events[0].id;
-				}
-				*/
-			})
+	onGenerateReport(): void {
+		this.reportsService.getApplicatioConflicts()
+			.subscribe((results: Array<ApplicationConflict>) => {
+				console.log('The application conficts are:');
+				console.log(results);
+				this.applicationConflicts = results;
+			});
 	}
 
 }
