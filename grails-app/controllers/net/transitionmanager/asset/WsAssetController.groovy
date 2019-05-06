eb61f71eb7f5a7ef27906a162e6ef7427b8c4284
Augusto Recordon <arecordon@tdsi.com>
2019-05-06 14:54:26 -0300

    TM-14566 Add endpoint for the Asset Summary Table content.

--- grails-app/controllers/net/transitionmanager/asset/WsAssetController.groovy
+++ grails-app/controllers/net/transitionmanager/asset/WsAssetController.groovy
@@ -5,9 +5,11 @@ import com.tdsops.common.security.spring.HasPermission
 import com.tdsops.tm.enums.FilenameFormat
 import com.tdsops.tm.enums.domain.AssetClass
 import com.tdsops.tm.enums.domain.AssetCommentCategory
+import com.tdsops.tm.enums.domain.UserPreferenceEnum
 import com.tdssrc.grails.FilenameUtil
 import com.tdssrc.grails.GormUtil
 import com.tdssrc.grails.NumberUtil
+import com.tdssrc.grails.StringUtil
 import grails.gsp.PageRenderer
 import grails.plugin.springsecurity.annotation.Secured
 import groovy.util.logging.Slf4j
@@ -22,6 +24,7 @@ import net.transitionmanager.security.Permission
 import net.transitionmanager.common.ControllerService
 import net.transitionmanager.person.UserPreferenceService
 import net.transitionmanager.task.AssetComment
+import org.apache.commons.lang3.BooleanUtils
 
 /**
  * Created by @oluna on 4/5/17.
@@ -557,5 +560,17 @@ class WsAssetController implements ControllerMethods {
 		boolean viewUnpublished = securityService.viewUnpublished()
 		List<AssetComment> assetComments = commentService.listAssetComments(project, viewUnpublished)
 		renderAsJson (assetComments*.toMap() )
+	}
+
+
+	/**
+	 * Return the content for the Asset Summary Table
+	 * @return the content for the asset table.
+	 */
+	def getSummaryTable() {
+		Project project = getProjectForWs()
+		String justPlanningPref = userPreferenceService.getPreference(null, UserPreferenceEnum.ASSET_JUST_PLANNING, "false")
+		Boolean justPlanning = StringUtil.toBoolean(justPlanningPref)
+		renderSuccessJson(assetEntityService.getAssetSummary(project, justPlanning))
 	}
 }
