f102d8bf357c4b2ee29dba213d9eeadd1f826299
Tucker Pelletier <tpelletier@transitionaldata.com>
2019-05-09 15:48:50 -0400

    TM-14672 there was nothing wrong with the core service tests.

--- src/integration-test/groovy/CoreServiceTests.groovy
+++ src/integration-test/groovy/CoreServiceTests.groovy
@@ -4,10 +4,7 @@ import grails.util.Metadata
 import net.transitionmanager.common.CoreService
 import org.grails.config.NavigableMap
 import org.grails.core.exceptions.GrailsConfigurationException
-import spock.lang.Ignore
 import spock.lang.Specification
-
-@Ignore
 @Integration
 @Rollback
 class CoreServiceTests extends Specification {
