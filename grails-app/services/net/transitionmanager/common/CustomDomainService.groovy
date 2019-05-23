eaee4c9b1dc0d99cecf366222f5f5af49b61b325
Tucker Pelletier <tpelletier@transitionaldata.com>
2019-05-23 08:27:49 -0400

    TM-15041 while it works either way, moving the commands for consistency.

--- grails-app/services/net/transitionmanager/common/CustomDomainService.groovy
+++ grails-app/services/net/transitionmanager/common/CustomDomainService.groovy
@@ -273,6 +273,8 @@ class CustomDomainService implements ServiceMethods {
 
         return AssetEntity.where {
             project == project
+            isNotNull(fieldName)
+            ne(fieldName, '')
 
             if (!shared) {
                 assetClass == assetClass
@@ -280,12 +282,9 @@ class CustomDomainService implements ServiceMethods {
 
             projections {
                 distinct(fieldName)
-                isNotNull(fieldName)
             }
 
-        }.list([sort: fieldName, order: 'asc']){
-            ne(fieldName, '')
-        }
+        }.list([sort: fieldName, order: 'asc'])
     }
 
     /**
