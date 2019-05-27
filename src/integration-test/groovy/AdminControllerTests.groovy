1e0eaefafe2c5dd098b7e3dab5e95b96fb32573c
Augusto Recordon <augusto.recordon@bairesdev.com>
2019-05-27 15:25:02 -0300

    TM-15064 Remove the Ignore annotation in the AdminController Integration Test and delete unnecessary test.

--- src/integration-test/groovy/AdminControllerTests.groovy
+++ src/integration-test/groovy/AdminControllerTests.groovy
@@ -22,7 +22,6 @@ import test.helper.PersonTestHelper
  */
 @Integration
 @Rollback
-@Ignore
 // TODO - SL - 01/31: Ignoring this test now, but it needs to be moved to a functional test using GebSpec
 class AdminControllerTests extends Specification {
 
@@ -77,36 +76,5 @@ class AdminControllerTests extends Specification {
         //controller.importAccountsTemplate()
         then:
         x == 1
-    }
-
-    /**
-     * The property <code>serviceRestartCommand</code> is defined in the tdstm-config.groovy file.
-     * Basically is a OS command for creating an empty file that it will be checked later by a cron process,
-     * and if the file exists then it will restart the app service.
-     * The command is 'touch' and is an OS dependent command, as it is intended to be run on UNIX systems.
-     * If you want to run this test on Windows, there is a workaround that consists of installing a touch command
-     * for Windows. This can be done with http://www.binarez.com/touch_dot_exe/
-     *
-     * TODO: (oluna@tdsi.com) I think that this should not be tested since we are getting the command from a configuration file
-     */
-    @See('TM-7670')
-    def 'Test the correct creation of the restart file in restartAppServiceAction()'() {
-        setup: 'Check that the command that will be used internally to create the file is ok'
-            String command = coreService.getAppConfigSetting(AdminController.APP_RESTART_CMD_PROPERTY)
-            assert command
-            def touchCmd = command.split()[0]   /* get the command name */
-            assert touchCmd == 'touch'
-        and: 'make sure the file does not exist before running the test.'
-            def filePath = command.split()[1]   /*  get the file path */
-            def file = new File(filePath)       /*  if the file exists, delete it */
-            if (file.exists()) {
-                file.delete()
-            }
-            !file.exists()
-        when:'the restartAppServiceAction() method is called'
-            def returned = controller.restartAppServiceAction()
-        then:'the method executes successfully and the file gets created.'
-            controller.response.status == 200
-            file.exists()
     }
 }
