a21dacaaa228a5c28b4d1768190cb24cdb2208fd
Sam Zuniga <szuniga@transitionaldata.com>
2019-05-31 09:23:28 -0500

    TM-15196 Port changes from 4.6 to 4.7

--- src/main/webapp/tds/web-app/app-js/modules/noticeManager/notice-routing.states.ts
+++ src/main/webapp/tds/web-app/app-js/modules/noticeManager/notice-routing.states.ts
@@ -44,7 +44,7 @@ export const NoticeManagerRoute: Routes = [
 		path: NoticeManagerStates.NOTICE_POST.url,
 		data: {
 			page: {
-				pageTitle: 'Notice',
+				title: 'NOTICE.NOTICE',
 				instruction: '',
 				menu: ['GLOBAL.ADMIN', 'NOTICE.NOTICE', 'NOTICE.POST_NOTICES'],
 			},
