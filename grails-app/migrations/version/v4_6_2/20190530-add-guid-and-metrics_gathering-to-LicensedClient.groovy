aa2b02c4ba07deb981551dc78305bdc4e348d36e
Octavio Luna <oluna@tdsi.com>
2019-05-31 10:51:06 -0500

    TM-14902 refactoring metrics gathering field

--- grails-app/migrations/version/v4_6_2/20190530-add-guid-and-metrics_gathering-to-LicensedClient.groovy
+++ grails-app/migrations/version/v4_6_2/20190530-add-guid-and-metrics_gathering-to-LicensedClient.groovy
@@ -3,7 +3,7 @@ package version.v4_6_2
 databaseChangeLog = {
 
 	changeSet(author: "oluna", id: "20190530 TM-14902-1") {
- 		comment('Adding guid column')
+ 		comment('Adding *guid* column')
 		preConditions(onFail: 'MARK_RAN') {
 			not {
 				columnExists(tableName: 'licensed_client', columnName: 'guid')
@@ -18,15 +18,15 @@ databaseChangeLog = {
 	}
 
 	changeSet(author: "oluna", id: "20190530 TM-14902-2") {
-		comment('Adding metrics_gathering columns')
+		comment('Adding *collect_metrics* columns')
 		preConditions(onFail: 'MARK_RAN') {
 			not {
-				columnExists(tableName: 'licensed_client', columnName: 'metrics_gathering')
+				columnExists(tableName: 'licensed_client', columnName: 'collect_metrics')
 			}
 		}
 
 		addColumn(tableName: "licensed_client") {
-			column(name: "metrics_gathering", type: 'TINYINT(1)', defaultValueNumeric: 0) {
+			column(name: "collect_metrics", type: 'TINYINT(1)', defaultValueNumeric: 0) {
 				constraints(nullable: false)
 			}
 		}
