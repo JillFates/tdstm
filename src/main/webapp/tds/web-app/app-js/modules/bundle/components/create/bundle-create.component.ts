6f9fcf8630558491c12a80ea176533c058d573e6
Jorge Morayta <jmorayta@tdsi.com>
2019-06-11 10:11:38 -0500

    TM-13170 - Fixed a lot of lint issues

--- src/main/webapp/tds/web-app/app-js/modules/bundle/components/create/bundle-create.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/bundle/components/create/bundle-create.component.ts
@@ -10,8 +10,9 @@ export class BundleCreateComponent implements OnInit {
 	public managers;
 	public workflowCodes;
 	public rooms;
-	public orderNums = Array (25 ).fill(0).map((x, i) => i + 1);
+	public orderNums = Array(25).fill(0).map((x, i) => i + 1);
 	public bundleModel = null;
+
 	constructor(private bundleService: BundleService) {
 	}
 
@@ -21,19 +22,20 @@ export class BundleCreateComponent implements OnInit {
 	}
 
 	private getModel() {
-		this.bundleService.getModelForBundleCreate()
-			.subscribe((result) => {
-				console.log(result);
-				let data = result.data;
-				this.managers = data.managers;
-				this.managers = data.managers.filter((item, index) => index === 0 || item.name !== data.managers[index - 1].name); // Filter duplicate names
-				this.workflowCodes = data.workflowCodes;
-				this.rooms = data.rooms;
-			});
+		this.bundleService.getModelForBundleCreate().subscribe((result: any) => {
+			console.log(result);
+			let data = result.data;
+			this.managers = data.managers;
+			this.managers = data.managers.filter((item, index) => index === 0 || item.name !== data.managers[index - 1].name); // Filter duplicate names
+			this.workflowCodes = data.workflowCodes;
+			this.rooms = data.rooms;
+		});
 	}
 
 	public saveForm() {
 		console.log(this.bundleModel);
-		this.bundleService.saveBundle(this.bundleModel);
+		this.bundleService.saveBundle(this.bundleModel).subscribe((result: any) => {
+			console.log(result);
+		});
 	}
 }
