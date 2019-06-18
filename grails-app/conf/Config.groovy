9bd4006ab710f2a8ecee394364fcf7e0e6d5f7bb
Sidar LC <slopez@tdsi.com>
2019-06-17 19:10:59 -0600

    TM-15354: TMD - Change the TTL of a JWT Token to 4 hours

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -420,6 +420,11 @@ grails {
 					validation {
 						enableAnonymousAccess = true
 					}
+					storage {
+						jwt {
+							expiration = 14400 // default expiration to 4 hours
+						}
+					}
 				}
 			}
 
