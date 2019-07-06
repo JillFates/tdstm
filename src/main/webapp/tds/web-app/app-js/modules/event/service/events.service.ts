17bc45843d95b5f04b91895f1d2dc842dc0d1a01
Sam Zuniga <szuniga@transitionaldata.com>
2019-07-05 22:33:00 -0500

    TM-13171 Remove mock data

--- src/main/webapp/tds/web-app/app-js/modules/event/service/events.service.ts
+++ src/main/webapp/tds/web-app/app-js/modules/event/service/events.service.ts
@@ -191,87 +191,87 @@ export class EventsService {
 
 		steps.push(headerRow);
 		steps.push([
-			{ text: 'p 1', classes: '' },
-			{ text: 'p 2', classes: '' },
-			{ text: 'p 3', classes: '' },
-			{ text: 'p 4', classes: '' },
-			{ text: 'p 5', classes: '' },
-			{ text: 'p 6', classes: '' },
-			{ text: 'p 7', classes: '' },
-			{ text: 'p 8', classes: '' },
-			{ text: 'p 9', classes: '' },
-			{ text: 'p 10', classes: '' },
-			{ text: 'p 11', classes: '' }
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' }
 		]);
 
 		steps.push([
-			{ text: 't a', classes: '' },
-			{ text: 't b', classes: '' },
-			{ text: 't c', classes: '' },
-			{ text: 't d', classes: '' },
-			{ text: 't e', classes: '' },
-			{ text: 't f', classes: '' },
-			{ text: 't g', classes: '' },
-			{ text: 't h', classes: '' },
-			{ text: 't i', classes: '' },
-			{ text: 't j', classes: '' },
-			{ text: 't k', classes: '' }
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' }
 		]);
 
 		steps.push([
-			{ text: 'ps a', classes: '' },
-			{ text: 'ps b', classes: '' },
-			{ text: 'ps c', classes: '' },
-			{ text: 'ps d', classes: '' },
-			{ text: 'ps e', classes: '' },
-			{ text: 'ps f', classes: '' },
-			{ text: 'ps g', classes: '' },
-			{ text: 'ps h', classes: '' },
-			{ text: 'ps i', classes: '' },
-			{ text: 'ps j', classes: '' },
-			{ text: 'ps k', classes: '' }
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' }
 		]);
 
 		steps.push([
-			{ text: 'pc a', classes: '' },
-			{ text: 'pc b', classes: '' },
-			{ text: 'pc c', classes: '' },
-			{ text: 'pc d', classes: '' },
-			{ text: 'pc e', classes: '' },
-			{ text: 'pc f', classes: '' },
-			{ text: 'pc g', classes: '' },
-			{ text: 'pc h', classes: '' },
-			{ text: 'pc i', classes: '' },
-			{ text: 'pc j', classes: '' },
-			{ text: 'pc k', classes: '' }
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' }
 		]);
 
 		steps.push([
-			{ text: 'as a', classes: '' },
-			{ text: 'as b', classes: '' },
-			{ text: 'as c', classes: '' },
-			{ text: 'as d', classes: '' },
-			{ text: 'as e', classes: '' },
-			{ text: 'as f', classes: '' },
-			{ text: 'as g', classes: '' },
-			{ text: 'as h', classes: '' },
-			{ text: 'as i', classes: '' },
-			{ text: 'as j', classes: '' },
-			{ text: 'as k', classes: '' }
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' }
 		]);
 
 		steps.push([
-			{ text: 'ac a', classes: '' },
-			{ text: 'ac b', classes: '' },
-			{ text: 'ac c', classes: '' },
-			{ text: 'ac d', classes: '' },
-			{ text: 'ac e', classes: '' },
-			{ text: 'ac f', classes: '' },
-			{ text: 'ac g', classes: '' },
-			{ text: 'ac h', classes: '' },
-			{ text: 'ac i', classes: '' },
-			{ text: 'ac j', classes: '' },
-			{ text: 'ac k', classes: '' }
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' },
+			{ text: '', classes: '' }
 		]);
 
 		return {
