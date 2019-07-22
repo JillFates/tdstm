99e8d47c169cb97aca9fb954603f35554d235b59
Tucker Pelletier <tpelletier@transitionaldata.com>
2019-07-22 15:13:51 -0400

    TM-15568 fix default role.

--- grails-app/services/net/transitionmanager/person/UserService.groovy
+++ grails-app/services/net/transitionmanager/person/UserService.groovy
@@ -95,7 +95,7 @@ class UserService implements ServiceMethods {
 
 		boolean autoProvision = domain.autoProvision
 		Long defaultProject = domain.defaultProject
-		String defaultRole = RoleType.ROLE_PREFIX + domain.defaultRole ?: ''
+		String defaultRole = domain.defaultRole ? RoleType.ROLE_PREFIX + domain.defaultRole : ''
 		String defaultTimezone = domain.defaultTimezone ?: ''
 
 		PartyGroup company = PartyGroup.get(userInfo.companyId)
