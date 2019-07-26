e780b8574ea8a6232725164bb14cee6ed2246620
Admin <junk@junk.com>
2019-07-26 15:48:43 -0400

    TM-15612 update action column icon's font size to 1em TM-15612 add class k-custom-command-button to make action columns fit content TM-15612 add constant array variable to hold the labels of common columns that could be shrunk TM-15612 add constant variable with width for common columns that could be shrunk TM-15612 add with for kendo grid columns based on label to set limited width to common columns TM-15612 add OnDestroy hook to effectively unsubscribe from all subscriptions when component is destroyed, thus avoiding memory leaks

---
+++ src/main/webapp/tds/web-app/app-js/shared/constants/common-shrunk-columns.ts
@@ -0,0 +1,25 @@
+
+// Common columns that should be shrunk to allow more space for primary info
+export const COMMON_SHRUNK_COLUMNS = [
+    'Asset Class',
+    'Environment',
+    'Archived',
+    'External Ref Id',
+    'Validation',
+    'Plan Status',
+    'Created',
+    'Asset Type',
+    'Created By',
+    'Updated',
+    'Category',
+    'Auth Method',
+    'Status',
+    'Last Modified',
+    'Date Create',
+    'Mode',
+    'Last Updated',
+    'Method'
+];
+
+// standard width for mentioned columns
+export const COMMON_SHRUNK_COLUMNS_WIDTH = 103;
