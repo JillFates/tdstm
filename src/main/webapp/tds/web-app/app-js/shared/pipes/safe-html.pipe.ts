ab4040272dd558bc050c34601f0c6f4095e29169
Wilfredo Mejia <wmejia@tdsi.com>
2019-07-30 18:49:54 -0600

    TM-15608 Pre-Login Notices and Notice Detail View display an additional character Updated files with new lint and string util for changing double quotes with double quotes instead of single quotes

--- src/main/webapp/tds/web-app/app-js/shared/pipes/safe-html.pipe.ts
+++ src/main/webapp/tds/web-app/app-js/shared/pipes/safe-html.pipe.ts
@@ -1,14 +1,15 @@
+/* tslint:disable:indent */
 import {Pipe, PipeTransform} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 
 @Pipe({
-    name: 'safeHtml',
+  name: 'safeHtml',
 })
 export class SafeHtmlPipe implements PipeTransform {
 
-    constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer) {}
 
-    transform(html) {
-        return this.sanitizer.bypassSecurityTrustHtml(html);
-    }
+  transform(html) {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
 }
