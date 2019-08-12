62ba244ab5f36cd8e0a665bd8b869defdcbccd4e
Sidar LC <slopez@tdsi.com>
2019-08-12 12:31:54 -0600

    TM-15700: TMD: Null Values when the task is sent to Hold by invoking the actionError (fix failing unit test)

--- src/test/groovy/net/transitionmanager/service/TaskActionServiceSpec.groovy
+++ src/test/groovy/net/transitionmanager/service/TaskActionServiceSpec.groovy
@@ -307,10 +307,10 @@ class TaskActionServiceSpec extends Specification implements ServiceUnitTest<Tas
 			when: 'updating the action status'
 				service.actionError(action, assetComment.id, whom)
 			then: 'Task notes are added'
-				notes.size() == 3
+				notes.size() == 4
 				notes.contains('some message')
 				notes[1] == 'ERROR script failure: api invocation exception'
-				notes[2] == "Placed task on HOLD, previous state was 'Ready'"
+				notes[3] == "Placed task on HOLD, previous state was 'Ready'"
 		}
 
 	void 'Test updateRemoteActionStatus success with exception thrown in invokeReactionScript'() {
@@ -344,10 +344,10 @@ class TaskActionServiceSpec extends Specification implements ServiceUnitTest<Tas
 			when: 'updating the action status'
 				service.actionDone(action, assetComment.id, whom)
 			then: 'Task notes are added'
-				notes.size() == 3
+				notes.size() == 4
 				notes.contains('some message')
 				notes[1] == 'SUCCESS script failure: api invocation exception'
-				notes[2] == "Placed task on HOLD, previous state was 'Ready'"
+				notes[3] == "Placed task on HOLD, previous state was 'Ready'"
 		}
 
 	void 'Test updateRemoteActionStatus success with exception thrown in invokeReactionScript for success but not error reaction scripts'() {
