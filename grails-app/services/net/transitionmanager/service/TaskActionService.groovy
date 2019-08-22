cda2ea8964d80d25f5efb9288ffb5028d05e2907
Sidar LC <slopez@tdsi.com>
2019-08-22 11:56:35 -0600

    TM-15464: TMD - Assets can not be modified in Reaction Scripts from TMD

--- grails-app/services/net/transitionmanager/service/TaskActionService.groovy
+++ grails-app/services/net/transitionmanager/service/TaskActionService.groovy
@@ -1,5 +1,6 @@
 package net.transitionmanager.service
 
+import com.tds.asset.AssetEntity
 import com.tds.asset.AssetComment
 import com.tdssrc.grails.ThreadLocalUtil
 import com.tdssrc.grails.TimeUtil
@@ -147,7 +148,9 @@ class TaskActionService implements ServiceMethods {
 		TaskFacade taskFacade = grailsApplication.mainContext.getBean(TaskFacade.class, task, whom)
 		JSONObject reactionScripts = (JSONObject) ThreadLocalUtil.getThreadVariable(ActionThreadLocalVariable.REACTION_SCRIPTS)
 		String script = reactionScripts[code.name()]
-		AssetFacade assetFacade = assetService.getAssetFacade(task.assetEntity, false)
+
+		AssetEntity asset = task.assetEntity
+		AssetFacade assetFacade = assetService.getAssetFacade(asset, false)
 
 		List<String> filenames = datafile.collect { MultipartFile file ->
 			fileSystemService.writeFile(file, fileSystemService.TMD_PREFIX)
@@ -164,19 +167,31 @@ class TaskActionService implements ServiceMethods {
 
 		try {
 			apiActionService.invokeReactionScript(code, script, actionRequest, apiActionResponse, taskFacade, assetFacade, new ApiActionJob())
+			// if asset facade is not null and task as an asset entity
+			// let's perform asset validation errors to inform the user about
+			// potential hidden errors during reaction scripts invokation
+			if (assetFacade && asset) {
+				if (!asset.validate()) {
+					asset.errors.allErrors.each {
+						log.info('Task {}-{} has asset entity field validation errors. {}', task.taskNumber, task.comment, it)
+						// add task note with asset entity validation error
+						taskService.addNote(task, whom, i18nMessage(it))
+					}
+					throw new InvalidParamException("Asset: ${asset.assetType}-${asset.assetName} have validation errors.")
+				}
+			}
 		} catch (Exception e) {
-
+			log.info('Reaction script invoke error. ', e)
+			taskService.addNote(task, whom, e.message)
 			if (code == ReactionScriptCode.ERROR) {
 				taskFacade.error("$code script failure: ${e.message}")
 			} else {
-
 				try {
 					script = reactionScripts[ReactionScriptCode.ERROR.name()]
 					apiActionService.invokeReactionScript(ReactionScriptCode.ERROR, script, actionRequest, apiActionResponse, taskFacade, assetFacade, new ApiActionJob())
 				} catch (Exception ex) {
 					taskFacade.error("$code script failure: ${ex.message}")
 				}
-
 			}
 		} finally {
 			// When the API call has finished the ThreadLocal variables need to be cleared out to prevent a memory leak
