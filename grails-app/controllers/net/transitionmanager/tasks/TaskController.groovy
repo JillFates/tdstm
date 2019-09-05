54b0683fd19f654908b8f2e28fe72c964b099840
John Martin <jmartin@tdsi.com>
2019-09-05 16:39:27 -0400

    Changed test for automatic tasks to use the isAutomatic() method on the class.

--- grails-app/controllers/net/transitionmanager/tasks/TaskController.groovy
+++ grails-app/controllers/net/transitionmanager/tasks/TaskController.groovy
@@ -44,7 +44,6 @@ import org.springframework.jdbc.core.JdbcTemplate
 
 import java.text.DateFormat
 
-import static net.transitionmanager.security.SecurityService.AUTOMATIC_ROLE
 import static com.tdsops.tm.enums.domain.AssetCommentStatus.COMPLETED
 import static com.tdsops.tm.enums.domain.AssetCommentStatus.HOLD
 import static com.tdsops.tm.enums.domain.AssetCommentStatus.PENDING
@@ -502,7 +501,7 @@ digraph runbook {
 					//def url = createLink(controller:'task', action:'neighborhoodGraph', id:task.id, absolute:false)
 
 					// TODO - JPM - outputTaskNode() the following boolean statement doesn't work any other way which is really screwy
-					if (task.role == AUTOMATIC_ROLE) {
+					if (task.isAutomatic()) {
 						fontcolor = taskStatusColorMap['AUTO_TASK'][0]
 						color = taskStatusColorMap['AUTO_TASK'][1]
 						fontsize = '8'
@@ -729,9 +728,7 @@ digraph runbook {
 
 				fillcolor = taskStatusColorMap[colorKey][1]
 
-				// log.info "task $it.task: role $it.role, $AssetComment.AUTOMATIC_ROLE, (${it.role == AssetComment.AUTOMATIC_ROLE ? 'yes' : 'no'})"
-				// if ("$it.roll" == "$AssetComment.AUTOMATIC_ROLE") {
-				if ("${it.role == AUTOMATIC_ROLE ? 'yes' : 'no'}" == 'yes') {
+				if (it.isAutomatic()) {
 					fontcolor = taskStatusColorMap['AUTO_TASK'][0]
 					color = taskStatusColorMap['AUTO_TASK'][1]
 					fontsize = '8'
