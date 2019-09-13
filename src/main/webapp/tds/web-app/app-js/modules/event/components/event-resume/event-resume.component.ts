ec75014d2cf6582b5b25a1f541494496d4cba48f
Sam Zuniga <szuniga@transitionaldata.com>
2019-09-13 09:09:40 -0500

    TM-15896 Use right user date format in event resume component

--- src/main/webapp/tds/web-app/app-js/modules/event/components/event-resume/event-resume.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/event/components/event-resume/event-resume.component.ts
@@ -1,12 +1,24 @@
 // Angular
 import {Component, Input, } from '@angular/core';
 import {EventPlanStatus} from '../../model/event.model';
+import {PreferenceService} from '../../../../shared/services/preference.service';
+import {DateUtils} from '../../../../shared/utils/date.utils';
 
 @Component({
 	selector: 'tds-event-resume',
 	templateUrl: 'event-resume.component.html'
 })
 export class EventResumeComponent {
+	public userTimeZone = '';
+	public dateTimeFormat = '';
 	@Input() event: EventPlanStatus = null;
+
+	constructor(private preferenceService: PreferenceService) {
+		this.preferenceService.getUserDatePreferenceAsKendoFormat()
+		.subscribe(() => {
+			this.userTimeZone = this.preferenceService.getUserTimeZone();
+			this.dateTimeFormat = `${this.preferenceService.getUserCurrentDateFormatOrDefault()} ${DateUtils.DEFAULT_FORMAT_TIME}`;
+		});
+	}
 
 }
