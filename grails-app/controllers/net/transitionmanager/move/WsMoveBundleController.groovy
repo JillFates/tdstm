bc9d9650d58a5f56c7faba20dbc62fdb4a3fe6c9
Jamie Danahy <jdanahy@transitionaldata.com>
2019-10-03 17:58:47 -0400

    TM-16043 - Changed error message for "not unique" in english, german, spanish, french, and italian. Also removed unnecessary duplicate validation

--- grails-app/controllers/net/transitionmanager/move/WsMoveBundleController.groovy
+++ grails-app/controllers/net/transitionmanager/move/WsMoveBundleController.groovy
@@ -97,10 +97,6 @@ class WsMoveBundleController implements ControllerMethods {
 	def save(Long moveBundleId) {
 		Project project = getProjectForWs()
 		MoveBundleCommand moveBundleCommand = populateCommandObject(MoveBundleCommand)
-		if(!moveBundleId && MoveBundle.findByName(moveBundleCommand.name)) {
-			renderErrorJson('Error: Bundle with the name \'' + moveBundleCommand.name + '\' already exists.')
-			return
-		}
 		MoveBundle moveBundle = moveBundleService.saveOrUpdate(moveBundleCommand, project, moveBundleId)
 		renderSuccessJson(moveBundle.toMap())
 	}
