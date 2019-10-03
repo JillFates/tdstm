f2ea1f57bc454b113d43f72b44724ee21eed4853
Esteban Cantu <ecantu@tdsi.com>
2019-10-03 19:01:39 -0300

    TM-15929 Modify the confirmation language used when changing the validation range on an existing Custom Field

--- src/main/webapp/tds/web-app/app-js/shared/i18n/en.dictionary.ts
+++ src/main/webapp/tds/web-app/app-js/shared/i18n/en.dictionary.ts
@@ -261,10 +261,7 @@ export const en_DICTIONARY: Object = {
 	'PROJECTS': 'Projects',
 	'FIELD_SETTINGS': {
 		'ON_SHARED': 'This will overwrite field {param1} in all asset classes and delete all existing data in this field. Do you want to continue?',
-		'WARNING_VALIDATION_CHANGE': `Changing the validation on a custom field can have adverse effects if
-		the existing data falls outside the new validation ranges.
-		Please proceed with caution, press Confirm to proceed and change the validation settings,
-		or press Cancel to make no changes.`,
+		'WARNING_VALIDATION_CHANGE': `Changing the validation range will have adverse effects if existing data falls outside the new range. Press confirm to proceed and change the validation range`,
 		'ASSET_FIELD_SETTING': 'Asset Field Settings',
 		'FIELD_NO_LONGER_EXISTS_ON_DOMAIN': 'Field no longer exists on domain',
 		'ENTER_FIELD_NAME_FILTER': 'Filter by field or label',
