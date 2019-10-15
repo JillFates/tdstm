67b2f1160d19af3a8fddf5c93069c3d09e47e07d
Esteban Cantu <ecantu@tdsi.com>
2019-10-15 19:52:46 -0300

    TM-16173 Export Runbook The process doesnt work

--- grails-app/services/net/transitionmanager/reporting/ReportsService.groovy
+++ grails-app/services/net/transitionmanager/reporting/ReportsService.groovy
@@ -1202,10 +1202,6 @@ class ReportsService implements ServiceMethods {
             exceptionString += "No Tasks"
         }
 
-        TaskTimeLineGraph graph = cpaResults.graph
-        TimelineSummary summary = cpaResults.summary
-        List<Task> tasks = cpaResults.tasks
-
         Closure<String> htmlConverter = { TaskVertex taskVertex, Task task ->
             String content = "<li>${taskVertex.taskId} ${taskVertex.taskComment?.encodeAsHTML()}"
             if (task.taskSpec) {
@@ -1219,6 +1215,10 @@ class ReportsService implements ServiceMethods {
         }
 
         if (cpaResults) {
+
+            TaskTimeLineGraph graph = cpaResults.graph
+            TimelineSummary summary = cpaResults.summary
+            List<Task> tasks = cpaResults.tasks
 
             if (!summary.hasCycles()) {
                 cyclicalsError = greenSpan('Cyclical References: OK')
