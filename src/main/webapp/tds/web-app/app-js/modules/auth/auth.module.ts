d454c483a71c60eab574b0458f47520b6145225c
Jorge Morayta <jmorayta@tdsi.com>
2019-10-17 08:48:38 -0500

    TM-15961 - Adding the loading to the login

--- src/main/webapp/tds/web-app/app-js/modules/auth/auth.module.ts
+++ src/main/webapp/tds/web-app/app-js/modules/auth/auth.module.ts
@@ -1,29 +1,26 @@
 // Angular
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-// Shared
-import { SharedModule } from '../../shared/shared.module';
-// Kendo
-// import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { ClarityModule } from '@clr/angular';
-
-// Route Module
-import { AuthRouteModule } from './auth-route.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+// Modules
+import {SharedModule} from '../../shared/shared.module';
+import {AuthRouteModule} from './auth-route.module';
+import {ClarityModule} from '@clr/angular';
+import {TdsComponentLibraryModule} from 'tds-component-library';
 // Services
-import { CookieService } from 'ngx-cookie-service';
-import { AuthService } from './service/auth.service';
-import { UserContextService } from './service/user-context.service';
-import { UserService } from './service/user.service';
-import { AuthGuardService } from './service/auth.guard.service';
-import { LoginService } from './service/login.service';
-import { PermissionService } from '../../shared/services/permission.service';
+import {CookieService} from 'ngx-cookie-service';
+import {AuthService} from './service/auth.service';
+import {UserContextService} from './service/user-context.service';
+import {UserService} from './service/user.service';
+import {AuthGuardService} from './service/auth.guard.service';
+import {LoginService} from './service/login.service';
+import {PermissionService} from '../../shared/services/permission.service';
 // Components
-import { LoginComponent } from './components/login/login.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import { StandardNoticesComponent } from '../noticeManager/components/standard-notices/standard-notices.component';
-import { MandatoryNoticesComponent } from '../noticeManager/components/mandatory-notices/mandatory-notices.component';
+import {LoginComponent} from './components/login/login.component';
+import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
+import {ChangePasswordComponent} from './components/change-password/change-password.component';
+import {StandardNoticesComponent} from '../noticeManager/components/standard-notices/standard-notices.component';
+import {MandatoryNoticesComponent} from '../noticeManager/components/mandatory-notices/mandatory-notices.component';
 
 @NgModule({
 	imports: [
@@ -32,9 +29,9 @@ import { MandatoryNoticesComponent } from '../noticeManager/components/mandatory
 		SharedModule,
 		FormsModule,
 		// Kendo
-		// DropDownsModule,
 		// Claritys
 		ClarityModule,
+		TdsComponentLibraryModule,
 		// Route
 		AuthRouteModule,
 	],
@@ -56,4 +53,5 @@ import { MandatoryNoticesComponent } from '../noticeManager/components/mandatory
 	],
 	entryComponents: [StandardNoticesComponent, MandatoryNoticesComponent],
 })
-export class AuthModule {}
+export class AuthModule {
+}
