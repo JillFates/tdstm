69904c2a05c1a05a1e5b615d9f783dc5f4342ef0
John <jmartin@transitionaldata.com>
2019-10-28 10:00:00 -0400

    TM-16268 Added check for Ajax request as Tucker had mentioned

--- grails-app/controllers/net/transitionmanager/admin/WsUserController.groovy
+++ grails-app/controllers/net/transitionmanager/admin/WsUserController.groovy
@@ -375,7 +375,7 @@ class WsUserController implements ControllerMethods {
 			// Record the page request (path) to the session if the user isn't logged in so that we can redirect
 			// them later after login.
 			String path = params.get('path')
-			if (path) {
+			if (path && ! isAjaxRequest()) {
 				SessionContext.setLastPageRequested(session, path)
 			}
 			renderAsJson(successful:false)
