5b1cf72b7310ef6ad0b77afcf80ca1b34de38a71
Tucker Pelletier <tpelletier@transitionaldata.com>
2019-11-07 17:06:37 -0500

    TM-16068 adding new endpoint JSON architecture graph and cleaning up code.

---
+++ src/main/groovy/net/transitionmanager/command/architecturegraph/ArchitectureGraphCommand.groovy
@@ -0,0 +1,19 @@
+package net.transitionmanager.command.architecturegraph
+
+import net.transitionmanager.command.CommandObject
+
+/**
+ * A command object used for taking in architecture graph parameters
+ */
+
+class ArchitectureGraphCommand implements CommandObject {
+	Long assetId
+	Integer levelsUp   = 0
+	Integer levelsDown = 3
+	String  mode
+
+	static constraints = {
+		assetId nullable: true
+		mode nullable: true
+	}
+}
