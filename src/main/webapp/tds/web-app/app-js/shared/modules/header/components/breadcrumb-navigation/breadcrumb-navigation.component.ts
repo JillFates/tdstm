aaeca32f1db3a5fb713fdd9f4182aaa3f481b4b3
David Mills <david.mills@solutiondesign.com>
2019-11-22 13:09:24 -0600

    TM-16161 - Fix breadcrumb navigation and asset ETL.

--- src/main/webapp/tds/web-app/app-js/shared/modules/header/components/breadcrumb-navigation/breadcrumb-navigation.component.ts
+++ src/main/webapp/tds/web-app/app-js/shared/modules/header/components/breadcrumb-navigation/breadcrumb-navigation.component.ts
@@ -37,7 +37,7 @@ declare var jQuery: any;
 					<li *ngFor="let menu of pageMetaData.menu; let last = last" [ngClass]="{ active: last }">
 						<a *ngIf="!last && menu.navigateTo"
 							[routerLink]="menu.navigateTo">{{ menu.text || menu | translate }}</a>
-						<a *ngIf="!last && !menu.navigateTo">{{menu.text || menu | translate}}</a>
+						<span *ngIf="!last && !menu.navigateTo">{{menu.text || menu | translate}}</span>
 						<ng-container *ngIf="last">
 							{{ menu.text || menu | translate }}
 						</ng-container>
