529a9bfa9d113be6118d136efd6182675fcae5df
dontiveros <dontiveros@tdsi.com>
2019-12-06 12:54:33 -0600

    TM-16559 - fixed formatting issues on due date at view and edition

--- src/main/webapp/tds/web-app/app-js/modules/taskManager/components/edit/task-edit.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/taskManager/components/edit/task-edit.component.ts
@@ -1,13 +1,12 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-
-import {TaskEditCreateCommonComponent} from '../common/task-edit-create-common.component';
-import {TranslatePipe} from '../../../../shared/pipes/translate.pipe';
-import {PermissionService} from '../../../../shared/services/permission.service';
-import {TaskDetailModel} from '../../model/task-detail.model';
-import {PreferenceService} from '../../../../shared/services/preference.service';
-import {UIDialogService} from '../../../../shared/services/ui-dialog.service';
-import {UIPromptService} from '../../../../shared/directives/ui-prompt.directive';
-import {TaskService} from '../../service/task.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TaskEditCreateCommonComponent } from '../common/task-edit-create-common.component';
+import { TranslatePipe } from '../../../../shared/pipes/translate.pipe';
+import { PermissionService } from '../../../../shared/services/permission.service';
+import { TaskDetailModel } from '../../model/task-detail.model';
+import { PreferenceService } from '../../../../shared/services/preference.service';
+import { UIDialogService } from '../../../../shared/services/ui-dialog.service';
+import { UIPromptService } from '../../../../shared/directives/ui-prompt.directive';
+import { TaskService } from '../../service/task.service';
 
 declare var jQuery: any;
 
