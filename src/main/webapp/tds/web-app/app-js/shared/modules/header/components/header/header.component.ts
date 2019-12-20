97bfb82afa46da803aa4286355b0caa7c0e7f4b9
dontiveros <dontiveros@tdsi.com>
2019-12-20 13:05:48 -0600

    User Preference Modal "Clarified"

--- src/main/webapp/tds/web-app/app-js/shared/modules/header/components/header/header.component.ts
+++ src/main/webapp/tds/web-app/app-js/shared/modules/header/components/header/header.component.ts
@@ -87,8 +87,12 @@ export class HeaderComponent {
 	public getUserIconText(fullName: string): string {
 		return fullName.split(' ').map(x => x.charAt(0)).join('').substr(0, 2).toUpperCase();
 	}
+
+	/**
+	 * Opens the user preferences modal.
+	 */
 	public openPrefModal(): void {
-		this.dialogService.open(UserPreferencesComponent, []).catch(result => {
+		this.dialogService.extra(UserPreferencesComponent, [], true, true).catch(result => {
 			//
 		});
 	}
