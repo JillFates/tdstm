97b5da700386ade5f6b32a645d4feec5c5ac24df
Juan Fernandez <jfernandez@tdsi.com>
2020-01-20 15:32:27 -0400

    TM-16598 - use xxl modal size instead of lg

--- src/main/webapp/tds/web-app/app-js/modules/dependencies/service/open-asset-dependencies.service.ts
+++ src/main/webapp/tds/web-app/app-js/modules/dependencies/service/open-asset-dependencies.service.ts
@@ -65,7 +65,7 @@ export class OpenAssetDependenciesService {
 					{ provide: 'ID', useValue: assetDependency.dependentId },
 					{ provide: 'ASSET', useValue: assetDependency.dependentClass }
 				]),
-				extraParams: [DIALOG_SIZE.LG],
+				extraParams: [DIALOG_SIZE.XXL],
 				open
 			},
 			[type]: {
