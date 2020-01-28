a5002747000f00766a8acf0eb7f4fe66c6dca6ff
David Ontiveros <dontiveros@tdsi.com>
2020-01-28 16:48:52 -0600

    TM-16730: Verify the name field is smaller though still enough to cover most/all of the names we've been using Verify the Created By field is larger and accommodates all our usual names without causing wrapping Verify the alignment of the columns is correct, neat and pretty Verify the filtering is applied to all column correctly and consistently Verify the number of filters is correct against the badge and in the right place (top right) Verify the Clear All Filters button is there Verify the Refresh button is there Verify the hover tool tip for the Create button is correct

--- src/main/webapp/tds/web-app/app-js/modules/assetManager/components/asset-view-manager/asset-view-manager.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/assetManager/components/asset-view-manager/asset-view-manager.component.ts
@@ -1,47 +1,42 @@
-import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
-import {ViewGroupModel, ViewModel, ViewType} from '../../../assetExplorer/model/view.model';
-import {Observable, ReplaySubject} from 'rxjs';
-import {ActivatedRoute, Router} from '@angular/router';
-import {takeUntil} from 'rxjs/operators';
-
-import {AssetExplorerService} from '../../service/asset-explorer.service';
-import {PermissionService} from '../../../../shared/services/permission.service';
-import {UIPromptService} from '../../../../shared/directives/ui-prompt.directive';
-import {Permission} from '../../../../shared/model/permission.model';
-import {NotifierService} from '../../../../shared/services/notifier.service';
-import {AlertType} from '../../../../shared/model/alert.model';
-import {DictionaryService} from '../../../../shared/services/dictionary.service';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { ViewGroupModel, ViewModel, ViewType } from '../../../assetExplorer/model/view.model';
+import { Observable, ReplaySubject } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { AssetExplorerService } from '../../service/asset-explorer.service';
+import { PermissionService } from '../../../../shared/services/permission.service';
+import { UIPromptService } from '../../../../shared/directives/ui-prompt.directive';
+import { Permission } from '../../../../shared/model/permission.model';
+import { NotifierService } from '../../../../shared/services/notifier.service';
+import { AlertType } from '../../../../shared/model/alert.model';
+import { DictionaryService } from '../../../../shared/services/dictionary.service';
 import { GRID_DEFAULT_PAGE_SIZE, LAST_SELECTED_FOLDER } from '../../../../shared/model/constants';
-import {PreferenceService, PREFERENCES_LIST} from '../../../../shared/services/preference.service';
-import {SortUtils} from '../../../../shared/utils/sort.utils';
-import {GridColumnModel} from '../../../../shared/model/data-list-grid.model';
-import {AssetViewManagerColumnsHelper} from './asset-view-manager-columns.helper';
+import { PreferenceService, PREFERENCES_LIST } from '../../../../shared/services/preference.service';
+import { GridColumnModel } from '../../../../shared/model/data-list-grid.model';
+import { AssetViewManagerColumnsHelper } from './asset-view-manager-columns.helper';
 import { HeaderActionButtonData } from 'tds-component-library';
 import { TranslatePipe } from '../../../../shared/pipes/translate.pipe';
 import { DataGridOperationsHelper } from '../../../../shared/utils/data-grid-operations.helper';
+import { SortDescriptor } from '@progress/kendo-data-query';
 
 @Component({
 	selector: 'tds-asset-view-manager',
 	templateUrl: 'asset-view-manager.component.html',
 })
 export class AssetViewManagerComponent implements OnInit, OnDestroy {
-	public disableClearFilters: Function;
 	public headerActionButtons: HeaderActionButtonData[];
 	public reportGroupModels = Array<ViewGroupModel>();
 	public searchText: string;
-	private viewType = ViewType;
 	public selectedFolder: ViewGroupModel;
 	public gridColumns: GridColumnModel[];
-	private report;
 	unsubscribeOnDestroy$: ReplaySubject<void> = new ReplaySubject(1);
-	showAssetsFilter: boolean;
 	gridHelper: DataGridOperationsHelper;
-	private initialSort: any = [{
-		dir: 'desc',
-		field: 'name'
-	}];
 	showFilters: boolean;
+	disableClearFilters: Function;
 	userDateFormat: string;
+	sortBy: string;
+	private viewType = ViewType;
+	private report;
 
 	constructor(
 		private route: ActivatedRoute,
@@ -53,16 +48,8 @@ export class AssetViewManagerComponent implements OnInit, OnDestroy {
 		private dictionary: DictionaryService,
 		private translateService: TranslatePipe,
 		private preferenceService: PreferenceService) {
+		this.sortBy = 'name';
 		this.report = this.route.snapshot.data['reports'] as Observable<ViewGroupModel[]>;
-	}
-	ngOnInit() {
-		// remove the min-height that TDSTMLayout.min.js randomly calculates wrong.
-		const contentWrapper = document.getElementsByClassName('content-wrapper')[0];
-		if (contentWrapper) {
-			// TODO: we can test this calculation among several pages and if it works correctly we can apply to the general app layout.
-			// 45px is the header height, 31px is the footer height
-			(contentWrapper as any).style.minHeight = 'calc(100vh - (45px + 31px))';
-		}
 		this.disableClearFilters = this.onDisableClearFilter.bind(this);
 		this.headerActionButtons = [
 			{
@@ -74,106 +61,23 @@ export class AssetViewManagerComponent implements OnInit, OnDestroy {
 				onClick: this.onCreateReport.bind(this),
 			},
 		];
-
 		this.gridColumns = AssetViewManagerColumnsHelper.createColumns();
-		const preferencesCodes = `${PREFERENCES_LIST.CURRENT_DATE_FORMAT},${PREFERENCES_LIST.VIEW_MANAGER_DEFAULT_SORT}`;
+		const preferencesCodes = `${ PREFERENCES_LIST.CURRENT_DATE_FORMAT },${ PREFERENCES_LIST.VIEW_MANAGER_DEFAULT_SORT }`;
 		this.preferenceService.getPreferences(preferencesCodes)
 			.pipe(takeUntil(this.unsubscribeOnDestroy$))
 			.subscribe((preferences) => {
-			this.setupDefaultSettings(preferences);
+				this.setupDefaultSettings(preferences);
 			});
 	}
 
-	private setupDefaultSettings(preferences: any[]) {
-		this.userDateFormat = preferences[PREFERENCES_LIST.CURRENT_DATE_FORMAT];
-		this.gridColumns =  AssetViewManagerColumnsHelper.setFormatToDateColumns(this.userDateFormat);
-		this.reportGroupModels = this.report;
-		const lastFolder = this.dictionary.get(LAST_SELECTED_FOLDER);
-		this.selectFolder(lastFolder || this.reportGroupModels.find((r) => r.open));
-		this.gridColumns =  AssetViewManagerColumnsHelper.setColumnAsSorted(preferences[PREFERENCES_LIST.VIEW_MANAGER_DEFAULT_SORT]);
-		// this.selectedFolder.views = SortUtils.sort(this.selectedFolder.views, AssetViewManagerColumnsHelper.getCurrentSortedColumnOrDefault() );
-		this.gridHelper = new DataGridOperationsHelper(
-			this.selectedFolder.views,
-			this.initialSort,
-			null,
-			null,
-			GRID_DEFAULT_PAGE_SIZE);
-	}
-
-	protected selectFolder(folderOpen: ViewGroupModel, $event: MouseEvent = null): void {
-		if ($event) {
-			$event.preventDefault();
+	ngOnInit() {
+		// remove the min-height that TDSTMLayout.min.js randomly calculates wrong.
+		const contentWrapper = document.getElementsByClassName('content-wrapper')[0];
+		if (contentWrapper) {
+			// TODO: we can test this calculation among several pages and if it works correctly we can apply to the general app layout.
+			// 45px is the header height, 31px is the footer height
+			(contentWrapper as any).style.minHeight = 'calc(100vh - (45px + 31px))';
 		}
-		this.dictionary.set(LAST_SELECTED_FOLDER, folderOpen);
-		this.reportGroupModels.forEach((folder) => folder.open = false);
-		this.selectedFolder = this.reportGroupModels.filter((folder) => folder.name === folderOpen.name)[0];
-		if (this.selectedFolder) {
-			this.selectedFolder.open = true;
-		}
-		this.selectedFolder.views = SortUtils.sort(this.selectedFolder.views, AssetViewManagerColumnsHelper.getCurrentSortedColumnOrDefault());
-	}
-
-	public onClearTextFilter(): void {
-		this.searchText = '';
-	}
-
-	protected onCreateReport(): void {
-		if (this.isCreateAvailable()) {
-			this.router.navigate(['asset', 'views', 'create']);
-		}
-	}
-
-	protected onEditReport(report: ViewModel): void {
-		if (this.isEditAvailable(report)) {
-			this.router.navigate(['asset', 'views', report.id, 'edit']);
-		}
-	}
-
-	protected onShowReport(report: ViewModel): void {
-		this.router.navigate(['asset', 'views', report.id, 'show']);
-	}
-
-	protected onDeleteReport(report: ViewModel): void {
-		if (this.isDeleteAvailable(report)) {
-			this.prompt.open('Confirmation Required', 'Are you sure you want to delete this view?', 'Yes', 'No')
-				.then((res) => {
-					if (res) {
-						this.assetExpService.deleteReport(report.id)
-						.pipe(takeUntil(this.unsubscribeOnDestroy$))
-						.subscribe(
-						result => {
-							this.notifier.broadcast({
-								name: AlertType.SUCCESS,
-								message: result
-							});
-							this.loadData();
-						},
-						error => console.log(error));
-					}
-				});
-		}
-	}
-
-	protected loadData() {
-		this.assetExpService.getReports()
-			.pipe(takeUntil(this.unsubscribeOnDestroy$))
-			.subscribe(result => {
-				this.reportGroupModels = result as ViewGroupModel[];
-				this.selectedFolder = this.reportGroupModels.find((r) => r.open);
-
-				this.selectedFolder.views =  SortUtils.sort(this.selectedFolder.views, AssetViewManagerColumnsHelper.getCurrentSortedColumnOrDefault());
-			});
-	}
-
-	protected changeSort(propertyName: string) {
-		this.gridColumns = AssetViewManagerColumnsHelper.setColumnAsSorted(propertyName);
-
-		this.preferenceService.setPreference(PREFERENCES_LIST.VIEW_MANAGER_DEFAULT_SORT, propertyName)
-			.pipe(takeUntil(this.unsubscribeOnDestroy$))
-			.subscribe(() => console.log('Saving sort preference'), (err) => console.log(err.message || err));
-
-		this.selectedFolder.views =  SortUtils.sort(this.selectedFolder.views, AssetViewManagerColumnsHelper.getCurrentSortedColumnOrDefault());
-		return ;
 	}
 
 	/**
@@ -184,43 +88,11 @@ export class AssetViewManagerComponent implements OnInit, OnDestroy {
 		return this.permissionService.hasPermission(Permission.AssetExplorerCreate);
 	}
 
-	protected isEditAvailable(report: ViewModel): boolean {
-		return report.isSystem ?
-			this.permissionService.hasPermission(Permission.AssetExplorerSystemEdit) ||
-			this.permissionService.hasPermission(Permission.AssetExplorerSystemSaveAs) :
-			this.permissionService.hasPermission(Permission.AssetExplorerEdit) ||
-			this.permissionService.hasPermission(Permission.AssetExplorerSaveAs);
-	}
-
-	protected isDeleteAvailable(report: ViewModel): boolean {
-		return report.isSystem ?
-			this.permissionService.hasPermission(Permission.AssetExplorerSystemDelete) :
-			report.isOwner && this.permissionService.hasPermission(Permission.AssetExplorerDelete);
-	}
-
-	protected toggleFavorite(report: ViewModel): void {
-		if (report.isFavorite) {
-			this.assetExpService.deleteFavorite(report.id)
-			.pipe(takeUntil(this.unsubscribeOnDestroy$))
-			.subscribe(d => {
-				report.isFavorite = false;
-				this.loadData();
-			});
-		} else {
-			if (this.assetExpService.hasMaximumFavorites(this.reportGroupModels.filter(x => x.name === 'Favorites')[0].views.length + 1)) {
-				this.notifier.broadcast({
-					name: AlertType.DANGER,
-					message: 'Maximum number of favorite data views reached.'
-				});
-			} else {
-				this.assetExpService.saveFavorite(report.id)
-				.pipe(takeUntil(this.unsubscribeOnDestroy$))
-				.subscribe(d => {
-					report.isFavorite = true;
-					this.loadData();
-				});
-			}
-		}
+	/**
+	 * Toggle filter show/hide flag.
+	 */
+	public toggleFilter(): void {
+		this.showFilters = !this.showFilters;
 	}
 
 	/**
@@ -235,40 +107,177 @@ export class AssetViewManagerComponent implements OnInit, OnDestroy {
 	}
 
 	/**
-	 * Toggle filter show/hide flag.
+	 * On Folder option change.
+	 * @param folderOpen
+	 * @param $event
 	 */
-	public toggleFilter(): void {
-		this.showAssetsFilter = !this.showAssetsFilter;
-	}
-
-	/**
-	 * Update the view based on the filter changes
-	 * @param value new filter change
-	 * @param column  Column throwing the event
-	 */
-	protected onFilter(value: string , column: GridColumnModel): void {
-		this.searchText = value || '';
-		column.filter = this.searchText;
-		if (!value) {
-			this.showAssetsFilter = false;
+	protected selectFolder(folderOpen: ViewGroupModel, $event: MouseEvent = null): void {
+		if ($event) {
+			$event.preventDefault();
 		}
-
-		this.loadData();
+		this.dictionary.set(LAST_SELECTED_FOLDER, folderOpen);
+		this.reportGroupModels.forEach((folder) => folder.open = false);
+		this.selectedFolder = this.reportGroupModels.filter((folder) => folder.name === folderOpen.name)[0];
+		if (this.selectedFolder) {
+			this.selectedFolder.open = true;
+		}
+		this.gridHelper.reloadData(this.selectedFolder.views);
 	}
 
 	/**
-	 * Clear the content of all filterable columns
+	 * Redirects to Create view page.
 	 */
-	protected onClearAllFilters(): void {
-		this.gridColumns.forEach((column: GridColumnModel) => {
-			this.onFilter(null, column);
-		})
+	protected onCreateReport(): void {
+		if (this.isCreateAvailable()) {
+			this.router.navigate(['asset', 'views', 'create']);
+		}
 	}
 
 	/**
-	 * Determine if we need to disable the clear all filters button
+	 * Opens a view in edit mode.
+	 * @param report
+	 */
+	protected onEditReport(report: ViewModel): void {
+		if (this.isEditAvailable(report)) {
+			this.router.navigate(['asset', 'views', report.id, 'edit']);
+		}
+	}
+
+	/**
+	 * Opens a view.
+	 * @param report
+	 */
+	protected onShowReport(report: ViewModel): void {
+		this.router.navigate(['asset', 'views', report.id, 'show']);
+	}
+
+	/**
+	 * On Delete View button.
+	 * @param report
+	 */
+	protected onDeleteReport(report: ViewModel): void {
+		if (this.isDeleteAvailable(report)) {
+			this.prompt.open('Confirmation Required', 'Are you sure you want to delete this view?', 'Yes', 'No')
+				.then((res) => {
+					if (res) {
+						this.assetExpService.deleteReport(report.id)
+							.pipe(takeUntil(this.unsubscribeOnDestroy$))
+							.subscribe(
+								result => {
+									this.notifier.broadcast({
+										name: AlertType.SUCCESS,
+										message: result
+									});
+									this.reloadData();
+								},
+								error => console.log(error));
+					}
+				});
+		}
+	}
+
+	/**
+	 * Reloads Grid data.
+	 */
+	protected reloadData() {
+		this.assetExpService.getReports()
+			.pipe(takeUntil(this.unsubscribeOnDestroy$))
+			.subscribe(result => {
+				this.reportGroupModels = result as ViewGroupModel[];
+				this.selectedFolder = this.reportGroupModels.find((r) => r.open);
+				this.gridHelper.reloadData(this.selectedFolder.views);
+			});
+	}
+
+	/**
+	 * On sort change, save to preferences.
+	 * @param sort
+	 */
+	protected changeSort(sort: Array<SortDescriptor>): void {
+		this.gridHelper.sortChange(sort);
+		this.sortBy = sort[0].field;
+		this.preferenceService.setPreference(PREFERENCES_LIST.VIEW_MANAGER_DEFAULT_SORT, this.sortBy)
+			.pipe(takeUntil(this.unsubscribeOnDestroy$))
+			.subscribe(() => console.log('Saving sort preference'), (err) => console.log(err.message || err));
+	}
+
+	/**
+	 * Check if Edit operation is available for the view.
+	 * @param report
+	 */
+	protected isEditAvailable(report: ViewModel): boolean {
+		return report.isSystem ?
+			this.permissionService.hasPermission(Permission.AssetExplorerSystemEdit) ||
+			this.permissionService.hasPermission(Permission.AssetExplorerSystemSaveAs) :
+			this.permissionService.hasPermission(Permission.AssetExplorerEdit) ||
+			this.permissionService.hasPermission(Permission.AssetExplorerSaveAs);
+	}
+
+	/**
+	 * Check if delete is available for the view.
+	 * @param report
+	 */
+	protected isDeleteAvailable(report: ViewModel): boolean {
+		return report.isSystem ?
+			this.permissionService.hasPermission(Permission.AssetExplorerSystemDelete) :
+			report.isOwner && this.permissionService.hasPermission(Permission.AssetExplorerDelete);
+	}
+
+	/**
+	 * Toggle Favorite button
+	 * @param report
+	 */
+	protected toggleFavorite(report: ViewModel): void {
+		if (report.isFavorite) {
+			this.assetExpService.deleteFavorite(report.id)
+				.pipe(takeUntil(this.unsubscribeOnDestroy$))
+				.subscribe(d => {
+					report.isFavorite = false;
+					this.reloadData();
+				});
+		} else {
+			if (this.assetExpService.hasMaximumFavorites(this.reportGroupModels.filter(x => x.name === 'Favorites')[0].views.length + 1)) {
+				this.notifier.broadcast({
+					name: AlertType.DANGER,
+					message: 'Maximum number of favorite data views reached.'
+				});
+			} else {
+				this.assetExpService.saveFavorite(report.id)
+					.pipe(takeUntil(this.unsubscribeOnDestroy$))
+					.subscribe(d => {
+						report.isFavorite = true;
+						this.reloadData();
+					});
+			}
+		}
+	}
+
+	/**
+	 * Load & Setup grid settings based on user preferences.
+	 * @param preferences
+	 */
+	private setupDefaultSettings(preferences: any[]) {
+		this.userDateFormat = preferences[PREFERENCES_LIST.CURRENT_DATE_FORMAT];
+		this.gridColumns = AssetViewManagerColumnsHelper.setFormatToDateColumns(this.userDateFormat);
+		this.reportGroupModels = this.report;
+		const lastFolder = this.dictionary.get(LAST_SELECTED_FOLDER);
+		this.sortBy = preferences[PREFERENCES_LIST.VIEW_MANAGER_DEFAULT_SORT];
+		this.gridHelper = new DataGridOperationsHelper(
+			[],
+			[{
+				dir: 'asc',
+				field: this.sortBy
+			}],
+			null,
+			null,
+			GRID_DEFAULT_PAGE_SIZE);
+		this.selectFolder(lastFolder || this.reportGroupModels.find((r) => r.open));
+	}
+
+	/**
+	 * Disable clear filters
 	 */
 	private onDisableClearFilter(): boolean {
-		return !this.searchText;
+		return this.gridHelper.getFilterCounter() === 0;
 	}
 }
