5fc02eb4cf4d41ffd2f58aa1f2394f392ebbc140
Tucker Pelletier <tpelletier@transitionaldata.com>
2020-01-30 11:20:50 -0500

    TM-16947 making sure new reference used to lookup preference gets discarded

--- grails-app/services/net/transitionmanager/person/UserPreferenceService.groovy
+++ grails-app/services/net/transitionmanager/person/UserPreferenceService.groovy
@@ -684,7 +684,10 @@ class UserPreferenceService {
 	 * @return the UserPreference if found otherwise null
 	 */
 	private UserPreference getUserPreference(UserLogin userLogin, String preferenceCode) {
-		UserPreference.get(new UserPreference(userLogin: userLogin, preferenceCode: preferenceCode))
+		UserPreference userPreference = new UserPreference(userLogin: userLogin, preferenceCode: preferenceCode)
+		UserPreference lookedUpUserPreference = UserPreference.get(userPreference)
+		userPreference.discard()
+		return lookedUpUserPreference
 	}
 
 	/**
