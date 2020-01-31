ca26b996b482a440f5a1ced0bdc45188630d660b
Sam Zuniga <szuniga@tdsi.com>
2020-01-31 16:30:16 -0600

    TM-16880 Notify changes just when the model changes

--- src/main/webapp/tds/web-app/app-js/modules/assetExplorer/components/asset-dependency/edit/asset-dependency-edit.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/assetExplorer/components/asset-dependency/edit/asset-dependency-edit.component.ts
@@ -47,6 +47,7 @@ export class AssetDependencyEditComponent implements OnInit {
 
 	/**
 	 * Notify to the host component changes on some dependency
+	 * @param {event} event Type of event which starts the change
 	 * @param {DependencyType} type Type of dependency thas was changed
 	 * @return {void)
 	 */
