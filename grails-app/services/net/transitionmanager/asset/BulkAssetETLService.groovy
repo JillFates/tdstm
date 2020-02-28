0a2a515da88d50ade70d5eeccbd7df9b4ba3e5dd
Tucker Pelletier <tpelletier@transitionaldata.com>
2020-02-28 17:25:32 -0500

    TM-16923 Adding conversion for specific domains, to match wash is seen
    in the asset views.

--- grails-app/services/net/transitionmanager/asset/BulkAssetETLService.groovy
+++ grails-app/services/net/transitionmanager/asset/BulkAssetETLService.groovy
@@ -1,6 +1,6 @@
 package net.transitionmanager.asset
 
-
+import com.tdsops.tm.enums.ControlType
 import com.tdssrc.grails.GormUtil
 import grails.converters.JSON
 import grails.gorm.transactions.Transactional
@@ -10,8 +10,12 @@ import net.transitionmanager.exception.DomainUpdateException
 import net.transitionmanager.exception.InvalidParamException
 import net.transitionmanager.imports.DataImportService
 import net.transitionmanager.imports.DataviewService
+import net.transitionmanager.person.Person
+import net.transitionmanager.project.MoveBundle
 import net.transitionmanager.project.Project
 import net.transitionmanager.service.ServiceMethods
+import net.transitionmanager.tag.TagAsset
+import net.transitionmanager.tag.TagAssetService
 /**
  * This handles taking in a bulk change ETL and executing it.
  */
@@ -20,6 +24,7 @@ class BulkAssetETLService implements ServiceMethods {
 	FileSystemService fileSystemService
 	DataviewService   dataviewService
 	DataImportService dataImportService
+	TagAssetService   tagAssetService
 
 
 	/**
@@ -49,8 +54,8 @@ class BulkAssetETLService implements ServiceMethods {
 
 		try {
 			List<AssetEntity> assets = getAssets(ids, queryFilter, currentProject)
-			List<Map> assetsUsingLabels = replaceLabels(assets, fieldMapping)
-			List<Object> tempFileObject = fileSystemService.createTemporaryFile('','json')
+			List<Map> assetsUsingLabels = replaceLabels(assets, fieldMapping, currentProject)
+			List<Object> tempFileObject = fileSystemService.createTemporaryFile('', 'json')
 
 			OutputStream outputStream = tempFileObject[1]
 			outputStream.write((assetsUsingLabels as JSON).toString().getBytes())
@@ -80,7 +85,7 @@ class BulkAssetETLService implements ServiceMethods {
 	 *
 	 * @param ids The asset Ids to look up other wise use the queryFilter.
 	 * @param queryFilter The query filter to use if ther are no assetIds.
-	 * @param currentProject, the project to get assets for.
+	 * @param currentProject , the project to get assets for.
 	 *
 	 * @return A list Of assets to use in the bulk ETL.
 	 */
@@ -107,14 +112,25 @@ class BulkAssetETLService implements ServiceMethods {
 	}
 
 	/**
-	 * This goes through a list of assets and creates a list of maps replacing property names with there labels from the FieldMapping
+	 * This goes through a list of assets and creates a list of maps replacing property names with there labels from the FieldMapping and
+	 * converting the values to be useful to the ETL:
+	 * Asset                -> value.assetName
+	 * Person               -> value.toString()
+	 * MoveBundle           -> value.name
+	 * Room                 -> value.roomHame
+	 * Rack                 -> value.tag
+	 * Tag                  -> A list of the tag names
+	 * Enum                 -> value.name()
+	 * String(not a number) -> value
+	 * String(number)       -> new BigInteger(value)
+	 * String(decimal)      -> new BigDecimal(value)
 	 *
 	 * @param assets The assets to convert to a List of maps.
 	 * @param fieldMapping The field mapping to use to look up the property names to replace.
 	 *
 	 * @return A list of Maps containing the asset properties with the labels from the field mappings as keys.
 	 */
-	List<Map> replaceLabels(List<AssetEntity> assets, Map<String, Map> fieldMapping) {
+	List<Map> replaceLabels(List<AssetEntity> assets, Map<String, Map> fieldMapping, Project currentProject) {
 		Map updatedRow
 
 		return assets.collect { AssetEntity asset ->
@@ -123,26 +139,94 @@ class BulkAssetETLService implements ServiceMethods {
 			fieldMapping[asset.assetClass.name()].each { String key, Map mapValue ->
 				String label = mapValue?.label ?: key
 
+				if(key == 'locationSource'){
+					updatedRow[label] = asset?."roomSource"?.location
+				}
+
+				if (key == 'locationTarget') {
+					updatedRow[label] = asset?."roomTarget"?.location
+				}
+
 				if (asset.hasProperty(key)) {
-					def value = asset."$key"
-
-					switch (value) {
-						case String || Number || Date || Boolean:
-							break
-						case Enum:
-							value = value.name()
-							break
-						case { GormUtil.isDomainClass(it) }:
-							value = value.id
-							break
-					}
-
-					updatedRow[label] = value
-
+					updatedRow[label] = convertValues(asset."$key", mapValue, currentProject, asset.id)
 				}
 			}
 
 			return updatedRow
 		}
+	}
+
+	/**
+	 * Converts the values to be useful to the ETL:
+	 * Asset                -> value.assetName
+	 * Person               -> value.toString()
+	 * MoveBundle           -> value.name
+	 * Room                 -> value.roomHame
+	 * Rack                 -> value.tag
+	 * Tag                  -> A list of the tag names
+	 * Enum                 -> value.name()
+	 * String(not a number) -> value
+	 * String(number)       -> new BigInteger(value)
+	 * String(decimal)      -> new BigDecimal(value)
+	 * null                 -> value
+	 * Domain (not above)   -> if has property name then value.name else value.id
+	 *
+	 * @param value The value to convert.
+	 * @param mapValue The field mapping used for converting custom fields from strings to numbers.
+	 * @param currentProject The current project used in the tag lookup.
+	 * @param assetId The asset Id also used in the tag lookup/
+	 *
+	 * @return The converted value for the ETL.
+	 */
+	def convertValues(value, Map mapValue, Project currentProject, Long assetId) {
+		switch (value) {
+			case String:
+
+				if (mapValue.control == ControlType.NUMBER.value()) {
+
+					if (mapValue.constraints.precision == 0) {
+						value = new BigInteger(value)
+					} else {
+						value = new BigDecimal(value)
+					}
+				}
+
+				break
+			case Number || Date || Boolean:
+				break
+			case AssetEntity:
+				value = value.assetName
+				break
+			case Person:
+				value = value.toString()
+				break
+			case MoveBundle:
+				value = value.name
+				break
+			case Room:
+				value = value.roomName
+				break
+			case Rack:
+				value = value.tag
+				break
+			case Collection:
+				if(value && value[0] instanceof TagAsset) {
+					value = tagAssetService.getTags(currentProject, assetId)*.name
+				}
+				break
+			case Enum:
+				value = value.name()
+				break
+			case { GormUtil.isDomainClass(it) }:
+				if (value.hasProperty('name')) {
+					value = value.name
+					break
+				}
+
+				value = value.id
+				break
+		}
+
+		return value
 	}
 }
