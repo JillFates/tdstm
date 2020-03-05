e846d33d5aa378306a4075253bd46b9abb4b08a6
Sam Zuniga <szuniga@tdsi.com>
2020-03-05 14:17:25 -0600

    TM-17055 Fix issue accessing property of null object

--- src/main/webapp/tds/web-app/app-js/modules/assetExplorer/components/asset/asset-common-show.ts
+++ src/main/webapp/tds/web-app/app-js/modules/assetExplorer/components/asset/asset-common-show.ts
@@ -77,7 +77,9 @@ export class AssetCommonShow implements OnInit, AfterContentInit {
 			let tableRow = <HTMLElement>document.querySelector('.one-column');
 			let tableRowSiblingWidth = <HTMLElement>(<HTMLElement>document.querySelector('.fit-tags-to-view')).previousSibling;
 			tagsDiv.style.width = (tableRow.offsetWidth - tableRowSiblingWidth.offsetWidth) + 'px';
-			this.assetTagUIWrapperService.updateTagsWidthForAssetShowView('.tags-container', 'span.dots-for-tags', '.one-column');
+			if (this.assetTagUIWrapperService) {
+				this.assetTagUIWrapperService.updateTagsWidthForAssetShowView('.tags-container', 'span.dots-for-tags', '.one-column');
+			}
 		}, 500);
 	}
 
