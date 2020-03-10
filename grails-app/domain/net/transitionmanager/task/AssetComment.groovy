01ed0ecd19fa17ebfb5a0ca0617a0c954170761d
Augusto Recordon <arecordon@tdsi.com>
2020-03-10 18:41:05 -0400

    TM-17247 Make the toMap in AssetComment account for the possibility of the asset being null.

--- grails-app/domain/net/transitionmanager/task/AssetComment.groovy
+++ grails-app/domain/net/transitionmanager/task/AssetComment.groovy
@@ -584,9 +584,9 @@ class AssetComment {
 	Map toMap() {
 		Map dataMap = [
 			id           : id,
-			assetEntityId: assetEntity.id,
-			assetName    : assetEntity.assetName,
-			assetType    : assetEntity.assetType,
+			assetEntityId: assetEntity?.id,
+			assetName    : assetEntity?.assetName,
+			assetType    : assetEntity?.assetType,
 			createdBy    : [id: createdBy.id, name: createdBy.toString()],
 			comment      : comment,
 			category     : category,
@@ -595,7 +595,7 @@ class AssetComment {
 			dateResolved : dateResolved,
 			assignedTo   : assignedTo?.toString() ?: '',
 			isResolved   : dateResolved ? 'Yes' : 'No',
-			assetClass: assetEntity.assetClass
+			assetClass: assetEntity?.assetClass
 		]
 		return dataMap
 	}
