d7c80759aeb0c02870642fd60e1fc60f97d85251
Sam Zuniga <szuniga@tdsi.com>
2020-03-09 20:49:23 -0600

    TM-17275 Allow set as input directive parameters the css classes to be ignored

--- src/main/webapp/tds/web-app/app-js/shared/directives/handle-double_click-directive.ts
+++ src/main/webapp/tds/web-app/app-js/shared/directives/handle-double_click-directive.ts
@@ -1,16 +1,40 @@
-import {Directive, HostListener, Output, EventEmitter} from '@angular/core';
+import {Directive, HostListener, Output, EventEmitter, Input} from '@angular/core';
+import {DecoratorOptions} from '../model/ui-modal-decorator.model';
+
+declare var jQuery: any;
 
 @Directive({
 	selector: '[tds-handle-double-click]'
 })
 export class UIHandleDoubleClickDirective {
+	private bannedClasses = [];
 	@Output() doubleClick: EventEmitter<any> = new EventEmitter();
+
+	@Input()
+	set ignoreClasses(ignoreClasses: string[]) {
+		this.bannedClasses = ignoreClasses;
+	}
+	get ignoreClasses(): string[] { return this.bannedClasses; }
 
 	/**
 	 * Listen for double click events, detecting one notify to the host component
+	 * if target event belongs to some banned element this doesn't emit the event
 	 * @param event
 	 */
-	@HostListener('dblclick', ['$event']) handleKeyboardEventUp() {
+	@HostListener('dblclick', ['$event']) handleKeyboardEventUp(event: MouseEvent) {
+		// banned by class
+		const isChildOfBannedClass = this.bannedClasses.find((className: string) => {
+			const name = event.target && event.target['className'] || '';
+
+			const counter =  jQuery(event.target).parents(`.${className}`).length;
+			const isBannedClass = this.bannedClasses.find((item: string) => name.indexOf(item) !== -1);
+
+			return counter > 0 || Boolean(isBannedClass);
+		});
+		if (isChildOfBannedClass) {
+			return;
+		}
+
 		this.doubleClick.emit();
 	}
 }
