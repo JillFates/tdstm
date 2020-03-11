454091d2550934c8b0e3400a9fb37ed18c556ff6
David Ontiveros <dontiveros@tdsi.com>
2020-03-11 09:56:53 -0600

    Fixed reopened TM-17246

--- src/main/webapp/tds/web-app/app-js/shared/components/supports-depends/supports-depends.component.ts
+++ src/main/webapp/tds/web-app/app-js/shared/components/supports-depends/supports-depends.component.ts
@@ -24,7 +24,7 @@ declare var jQuery: any;
                 class="dependents-grid"
                 [data]="dataGridSupportsOnHelper.gridData"
                 [sort]="dataGridSupportsOnHelper.state.sort"
-                [sortable]="{mode:'single'}"
+								[sortable]="false"
                 [resizable]="true"
                 (sortChange)="dataGridSupportsOnHelper.sortChange($event)">
 
