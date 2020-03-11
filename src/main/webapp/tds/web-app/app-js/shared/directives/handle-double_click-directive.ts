61645b95f0db142d908aee88fff1fe77840e7c50
Augusto <arecordon@transitionaldata.com>
2020-03-11 13:28:25 -0300

    Revert "TM-17275 Update classes to be ignored by assets"
    This reverts commit cfb894be2ff57efaa5658a872bf8f5cd62b9ba19.

--- src/main/webapp/tds/web-app/app-js/shared/directives/handle-double_click-directive.ts
+++ src/main/webapp/tds/web-app/app-js/shared/directives/handle-double_click-directive.ts
@@ -30,11 +30,9 @@ export class UIHandleDoubleClickDirective {
 			return counter > 0 || Boolean(isBannedClass);
 		});
 		if (isChildOfBannedClass) {
-			console.log('Ignored');
 			return;
 		}
 
-		console.log('Attending');
 		this.doubleClick.emit();
 	}
 }
