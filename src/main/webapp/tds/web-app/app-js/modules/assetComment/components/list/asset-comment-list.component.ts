15fda9c4e8f69585024f496cff85111590c79ecf
Augusto <arecordon@transitionaldata.com>
2020-03-11 13:27:29 -0300

    Revert "TM-17288 Make look the Comment cell name as clickable just"
    This reverts commit a5cbda8062ed0166f88054373c568baf6eb438ac.

--- src/main/webapp/tds/web-app/app-js/modules/assetComment/components/list/asset-comment-list.component.ts
+++ src/main/webapp/tds/web-app/app-js/modules/assetComment/components/list/asset-comment-list.component.ts
@@ -42,7 +42,6 @@ export class AssetCommentListComponent implements OnInit, OnDestroy {
 	public gridData: GridDataResult;
 	public resultSet: AssetCommentModel[];
 	public dateFormat = '';
-	public hasCommentViewPermission: boolean;
 	commonShrunkColumns = COMMON_SHRUNK_COLUMNS;
 	commonShrunkColumnWidth = COMMON_SHRUNK_COLUMNS_WIDTH;
 	unsubscribeOnDestroy$: ReplaySubject<void> = new ReplaySubject(1);
@@ -69,7 +68,6 @@ export class AssetCommentListComponent implements OnInit, OnDestroy {
 				this.dateFormat = dateFormat;
 				this.assetCommentColumnModel = new AssetCommentColumnModel(`{0:${ dateFormat }}`);
 			});
-		this.hasCommentViewPermission = this.permissionService.hasPermission(Permission.CommentView);
 	}
 
 	protected filterChange(filter: CompositeFilterDescriptor): void {
@@ -120,8 +118,10 @@ export class AssetCommentListComponent implements OnInit, OnDestroy {
 	 * @param {SelectionEvent} event
 	 */
 	protected cellClick(event: CellClickEvent): void {
-		if (event.columnIndex === 1 && this.hasCommentViewPermission) {
-			this.openAssetCommentDialogViewEdit(event['dataItem'], ModalType.VIEW);
+		if (event.columnIndex === 1) {
+			if ( this.permissionService.hasPermission(Permission.CommentView) ) {
+				this.openAssetCommentDialogViewEdit(event['dataItem'], ModalType.VIEW);
+			}
 		}
 	}
 
