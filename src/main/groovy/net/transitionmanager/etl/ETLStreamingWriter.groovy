1b2182062dcbc39dde8359684b1acf6c538a349c
Diego Correa <dcorrea@tdsi.com>
2020-03-24 23:28:52 -0300

    TM-17382 Improve writeObjectField

--- src/main/groovy/net/transitionmanager/etl/ETLStreamingWriter.groovy
+++ src/main/groovy/net/transitionmanager/etl/ETLStreamingWriter.groovy
@@ -14,7 +14,7 @@ import com.tdsops.etl.RowResult
 import com.tdsops.etl.TagResults
 import com.tdssrc.grails.TimeUtil
 import groovy.transform.CompileStatic
-import org.codehaus.groovy.runtime.GStringImpl
+import org.grails.web.json.JSONObject
 
 import java.text.SimpleDateFormat
 
@@ -215,24 +215,22 @@ class ETLStreamingWriter {
      */
     private void writeObjectField(String fieldName, Object pojo) {
 
-        switch (pojo?.class) {
-            case String:
-                generator.writeStringField(fieldName, pojo.toString())
-                break
-            case GStringImpl:
-                generator.writeStringField(fieldName, pojo.toString())
-                break
-            case Date:
-                generator.writeStringField(fieldName, TimeUtil.formatToISO8601DateTime((Date) pojo))
-                break
-            case Map:
-            case JsonObject:
-            case List:
-            case Set:
-                generator.writeStringField(fieldName, pojo.toString())
-                break
-            default:
-                generator.writeObjectField(fieldName, pojo)
+        if (pojo == null) {
+            generator.writeNullField(fieldName)
+        } else if (CharSequence.isAssignableFrom(pojo.class)) {
+            generator.writeStringField(fieldName, pojo.toString())
+        } else if (Number.isAssignableFrom(pojo.class)) {
+            generator.writeObjectField(fieldName, pojo)
+        } else if (Date.isAssignableFrom(pojo.class)) {
+            generator.writeStringField(fieldName, TimeUtil.formatToISO8601DateTime((Date) pojo))
+        } else if (Map.isAssignableFrom(pojo.class)
+                || Collection.isAssignableFrom(pojo.class)
+                || JsonObject.isAssignableFrom(pojo.class)
+                || JSONObject.isAssignableFrom(pojo.class)
+                || Number.isAssignableFrom(pojo.class)) {
+            generator.writeStringField(fieldName, pojo.toString())
+        } else {
+            generator.writeObjectField(fieldName, pojo)
         }
     }
 
