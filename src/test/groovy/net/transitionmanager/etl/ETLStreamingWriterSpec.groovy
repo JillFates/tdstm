25d4bd7f2d83c76fe69140ae5d498a17715f6fa6
Diego Correa <dcorrea@tdsi.com>
2020-03-24 23:07:19 -0300

    TM-17382 Fix test doing it more understandable

--- src/test/groovy/net/transitionmanager/etl/ETLStreamingWriterSpec.groovy
+++ src/test/groovy/net/transitionmanager/etl/ETLStreamingWriterSpec.groovy
@@ -82,6 +82,6 @@ class ETLStreamingWriterSpec extends ETLBaseSpec {
             [key: 'value']                                        || '[key:value]'
             ['key', 'value']                                      || '[key, value]'
             ['key', 'value'] as Set                               || '[key, value]'
-            "${'Hello World'}"                                    || 'Hello World'
+            "${'test a GString'}"                                 || 'test a GString'
     }
 }
