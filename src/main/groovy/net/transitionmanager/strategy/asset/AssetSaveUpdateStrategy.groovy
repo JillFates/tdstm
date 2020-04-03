f2cf26dff4e45b587bac10b448c7414b9086d582
Augusto <arecordon@transitionaldata.com>
2020-04-03 17:26:23 -0300

    Revert "TM-17437 Split the call to delete dependencies into two, one per side."
    This reverts commit a1ebe379bb3be509eefa7d3853accd6f47bee08c.

--- src/main/groovy/net/transitionmanager/strategy/asset/AssetSaveUpdateStrategy.groovy
+++ src/main/groovy/net/transitionmanager/strategy/asset/AssetSaveUpdateStrategy.groovy
@@ -269,10 +269,12 @@ abstract class AssetSaveUpdateStrategy {
 	 * @param assetEntity
 	 */
 	private void deleteDependencies(AssetEntity assetEntity) {
-		deleteDependencies(command.dependencyMap.supportsToDelete, assetEntity, true)
-		deleteDependencies(command.dependencyMap.dependentsToDelete, assetEntity, false)
+		List<Long> dependenciesToDelete = NumberUtil.toPositiveLongList(command.dependencyMap.deleteDependencies)
+		if (dependenciesToDelete) {
+			// Delete all the requested dependencies.
+			deleteDependencies(dependenciesToDelete, assetEntity)
+		}
 	}
-
 
 	/**
 	 * Validate that all assets associated in the dependencies section (dependent or supporting)
@@ -318,17 +320,13 @@ abstract class AssetSaveUpdateStrategy {
 	 *
 	 * @param ids - dependency ids.
 	 * @param assetEntity
-	 * @param isDependent a boolean flag that signals whether the asset if the dependent or supporting side of the dependency.
 	 */
-	private void deleteDependencies(List ids, AssetEntity assetEntity, boolean isDependent) {
-		List<Long> depsToDelete = ids? NumberUtil.toPositiveLongList(ids) : null
-
+	private void deleteDependencies(List<Long> ids, AssetEntity assetEntity) {
 		/* Delete dependencies. As an extra precaution, the query doesn't simply delete dependencies
-		 given their id, but also takes the asset being edited into account.*/
-		String side = isDependent ? 'dependent' : 'asset'
-		if (depsToDelete) {
-			String hql = "DELETE FROM AssetDependency ad WHERE $side=:asset AND id IN (:ids)"
-			Map params = [asset: assetEntity, ids: depsToDelete]
+		* given their id, but also takes the asset being edited into account.*/
+		if (ids) {
+			String hql = "DELETE FROM AssetDependency ad WHERE (asset = :asset OR dependent = :dependent) AND id IN (:ids)"
+			Map params = [asset: assetEntity, dependent: assetEntity, ids: ids]
 			AssetDependency.executeUpdate(hql, params)
 		}
 	}
