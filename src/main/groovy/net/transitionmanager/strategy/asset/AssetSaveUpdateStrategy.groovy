7f5ca67512dc8c2a42898b11a00e4a599b1cf0c5
Augusto <arecordon@transitionaldata.com>
2020-04-03 17:26:12 -0300

    Revert "TM-17437 Simplify the logic for fetching the asset being updated."
    This reverts commit ab23bdfb1af0f0a7bd7db0067d03d8f1dc9ad5ad.

--- src/main/groovy/net/transitionmanager/strategy/asset/AssetSaveUpdateStrategy.groovy
+++ src/main/groovy/net/transitionmanager/strategy/asset/AssetSaveUpdateStrategy.groovy
@@ -59,7 +59,7 @@ abstract class AssetSaveUpdateStrategy {
 		if (isNew()) {
 			assetEntity = createAndInitializeAssetEntityInstance()
 		} else {
-			assetEntity = GormUtil.findInProject(project, AssetEntity, NumberUtil.toLong(command.asset.id), true)
+			assetEntity = fetchAsset()
 		}
 
 		// Populate the asset.
@@ -108,6 +108,24 @@ abstract class AssetSaveUpdateStrategy {
 		command.asset.scale = SizeScale.asEnum(command.asset.scale)
 	}
 
+	/**
+	 * Based on the command object given when instancing this object,
+	 * retrieve the corresponding asset (if any).
+	 * @return
+	 */
+	private AssetEntity fetchAsset() {
+		Long assetId = NumberUtil.toLong(command.asset.id)
+		AssetEntity assetEntity
+		if (assetId) {
+			assetEntity = GormUtil.findInProject(project, AssetEntity, assetId, true)
+		} else {
+			throw new InvalidParamException('The requested Asset ID is invalid')
+		}
+		if (!assetEntity) {
+			throw new InvalidParamException('No asset found with the given ID.')
+		}
+		return assetEntity
+	}
 
 	/**
 	 * Format all the possible date fields.
