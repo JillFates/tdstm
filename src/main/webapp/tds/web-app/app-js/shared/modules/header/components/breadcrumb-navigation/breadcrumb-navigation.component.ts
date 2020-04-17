2110df76b04943d73b2e24419dd2466412b00bf4
Sam Zuniga <szuniga@tdsi.com>
2020-04-17 16:46:32 -0500

    TM-17343 Update header layout
    Increase the space is the licence banner is empty

--- src/main/webapp/tds/web-app/app-js/shared/modules/header/components/breadcrumb-navigation/breadcrumb-navigation.component.ts
+++ src/main/webapp/tds/web-app/app-js/shared/modules/header/components/breadcrumb-navigation/breadcrumb-navigation.component.ts
@@ -19,44 +19,47 @@ declare var jQuery: any;
 @Component({
 	selector: 'tds-breadcrumb-navigation',
 	template: `
-		<!-- Content Header (Page header) -->
-		<section class="content-header" *ngIf="!pageMetaData.hideTopNav">
-			<ng-container *ngIf="pageMetaData">
-				<div class="clr-row">
-            <div class="clr-col-4 clr-align-content-center">
-              <div class="content-middle">
-                  <h2 [style.color]="pageMetaData?.titleColor" [style.fontWeight]="pageMetaData?.titleFontWeight">
-                      {{ pageMetaData.title | translate }}
-                      <small *ngIf="pageMetaData.instruction">{{pageMetaData.instruction | translate}}</small>
-                  </h2>
-              </div>
-            </div>
-						<div class="clr-col-4 clr-align-self-center">
-                <div
-                    class="licensing-banner-message"
-                    *ngIf="userContext.license && userContext.license.banner">
-                    <div class="callout">
-                        {{ userContext.license?.banner }}
+<!-- Content Header (Page header) -->
+<!-- userContext.license && userContext.license.banner-->
+		<div *ngIf="{hasBanner: false}; let settings;">
+            <section class="content-header" *ngIf="!pageMetaData.hideTopNav">
+                <ng-container *ngIf="pageMetaData">
+                    <div class="clr-row">
+                        <div class="clr-align-content-center"
+                             [ngClass]="{'clr-col-4': settings.hasBanner, 'clr-col-8': !settings.hasBanner}">
+                            <div class="content-middle">
+                                <h2 [style.color]="pageMetaData?.titleColor" [style.fontWeight]="pageMetaData?.titleFontWeight">
+                                    {{ pageMetaData.title | translate }}
+                                    <small *ngIf="pageMetaData.instruction">{{pageMetaData.instruction | translate}}</small>
+                                </h2>
+                            </div>
+                        </div>
+						<div class="clr-col-4 clr-align-self-center" *ngIf="settings.hasBanner">
+                            <div
+                                    class="licensing-banner-message">
+                                <div class="callout">
+                                    {{ userContext.license?.banner }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clr-col-4">
+                            <div class="content-middle element-height-100-per clr-float-md-right">
+                                <ol class="breadcrumb-container">
+                                    <li *ngFor="let menu of pageMetaData.menu; let last = last" [ngClass]="{ active: last }">
+                                        <a *ngIf="!last && menu.navigateTo"
+                                           [routerLink]="menu.navigateTo">{{ menu.text || menu | translate }}</a>
+                                        <span *ngIf="!last && !menu.navigateTo">{{menu.text || menu | translate}}</span>
+                                        <ng-container *ngIf="last">
+                                            {{ menu.text || menu | translate }}
+                                        </ng-container>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
                     </div>
-                </div>
-						</div>
-						<div class="clr-col-4">
-              <div class="content-middle element-height-100-per clr-float-md-right">
-                  <ol class="breadcrumb-container">
-                      <li *ngFor="let menu of pageMetaData.menu; let last = last" [ngClass]="{ active: last }">
-                          <a *ngIf="!last && menu.navigateTo"
-                             [routerLink]="menu.navigateTo">{{ menu.text || menu | translate }}</a>
-                          <span *ngIf="!last && !menu.navigateTo">{{menu.text || menu | translate}}</span>
-                          <ng-container *ngIf="last">
-                              {{ menu.text || menu | translate }}
-                          </ng-container>
-                      </li>
-                  </ol>
-              </div>
-						</div>
-				</div>
-			</ng-container>
-		</section>
+                </ng-container>
+            </section>
+        </div>
 		<tds-ui-dialog></tds-ui-dialog>
 		<tds-ui-prompt></tds-ui-prompt>
 	`,
