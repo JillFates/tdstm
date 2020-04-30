a8bd3dfd9fecd63b2c584c3599df0591b044e71d
Jorge Morayta <jmorayta@tdsi.com>
2020-04-30 12:26:39 -0500

    TM-17456 - Final change

--- src/main/webapp/tds/web-app/app-js/shared/components/dependent-comment/dependent-comment.component.ts
+++ src/main/webapp/tds/web-app/app-js/shared/components/dependent-comment/dependent-comment.component.ts
@@ -1,38 +1,45 @@
-import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
-
-import { UIExtraDialog} from '../../../shared/services/ui-dialog.service';
-import {AssetComment} from './model/asset-coment.model';
-import {UIPromptService} from '../../directives/ui-prompt.directive';
+// Angular
+import {Component, ViewChild, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
+// Model
+import {AssetComment} from './model/asset-coment.model';
 import {TranslatePipe} from '../../pipes/translate.pipe';
-import {Dialog, DialogButtonModel, DialogButtonType} from 'tds-component-library';
+import {Dialog, DialogButtonType, DialogConfirmAction, DialogService} from 'tds-component-library';
 
 @Component({
 	selector: 'dependent-comment',
 	template: `
-        <div id="dependent-comment-component">
-			<form name="dependentForm" role="form" data-toggle="validator" #dependentForm='ngForm'>
-				<div class="box-body">
-					<div class="row">
-						<div class="col-md-12">
-							<div class="form-group">
-								<textarea rows="15" id="dependentComment" name="dependentComment" class="form-control" [(ngModel)]="assetCommentModel.comment"></textarea>
-							</div>
-						</div>
-					</div>
-				</div>
-			</form>
+        <div class="dependent-comment-component">
+            <form name="dependentForm" role="form" data-toggle="validator" #dependentForm='ngForm'>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+								<textarea
+                                        clrTextarea
+                                        required
+                                        rows="15"
+                                        id="dependentComment"
+                                        name="dependentComment"
+                                        class="form-control"
+                                        [(ngModel)]="assetCommentModel.comment"
+                                ></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
 	`,
 })
 export class DependentCommentComponent extends Dialog implements OnInit {
 	@ViewChild('dependentForm', {static: false}) dependentForm: NgForm;
 
-	public assetCommentModel: AssetComment = new AssetComment();
+	public assetCommentModel: AssetComment = null;
 
 	constructor(
 		private translatePipe: TranslatePipe,
-		private promptService: UIPromptService,
+		private dialogService: DialogService,
 	) {
 		super();
 	}
@@ -44,7 +51,7 @@ export class DependentCommentComponent extends Dialog implements OnInit {
 			name: 'save',
 			icon: 'floppy',
 			show: () => true,
-			disabled: () => !this.dependentForm && !this.dependentForm.dirty,
+			disabled: () => !this.dependentForm.dirty || !this.dependentForm.valid,
 			type: DialogButtonType.ACTION,
 			action: this.onUpdateComment.bind(this)
 		});
@@ -67,18 +74,15 @@ export class DependentCommentComponent extends Dialog implements OnInit {
 
 	public cancelCloseDialog(): void {
 		if (this.dependentForm.dirty) {
-			this.promptService.open(
+			this.dialogService.confirm(
 				this.translatePipe.transform('GLOBAL.CONFIRMATION_PROMPT.CONFIRMATION_REQUIRED'),
-				this.translatePipe.transform('GLOBAL.CONFIRMATION_PROMPT.UNSAVED_CHANGES_MESSAGE'),
-				this.translatePipe.transform('GLOBAL.CONFIRM'),
-				this.translatePipe.transform('GLOBAL.CANCEL'),
-			)
-				.then(confirm => {
-					if (confirm) {
+				this.translatePipe.transform('GLOBAL.CONFIRMATION_PROMPT.UNSAVED_CHANGES_MESSAGE')
+			).subscribe(
+				(data: any) => {
+					if (data.confirm === DialogConfirmAction.CONFIRM) {
 						super.onCancelClose();
 					}
-				})
-				.catch((error) => console.log(error));
+				});
 		} else {
 			super.onCancelClose();
 		}
