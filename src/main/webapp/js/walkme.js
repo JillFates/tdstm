f55c7ca0cb21aef648f823e488e6f13aabb51c14
Sam Zuniga <szuniga@tdsi.com>
2020-05-13 13:12:03 -0500

    TM-17706 Add walkme script

---
+++ src/main/webapp/js/walkme.js
@@ -0,0 +1,11 @@
+(function () {
+    var walkme = document.createElement('script');
+    walkme.type = 'text/javascript';
+    walkme.async = true;
+    walkme.src = 'https://cdn2.walkmedev.com/users/0221049480294ff68c73683513b6c0e2/test/walkme_0221049480294ff68c73683513b6c0e2_https.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(walkme, s);
+    window._walkemConfig = {
+        smartLoad: true
+    };
+})();
